b846513c0970c9e68d983b1a7d5e669e6ddf8de5
porres <porres@gmail.com>
2016-04-27 16:21:11 -0300

    imp~ update

--- Makefile
+++ Makefile
@@ -15,21 +15,22 @@ lib.name = Porres-ELS
 # sources
 
 # control:
+
 cents2ratio.class.sources := classes/cents2ratio.c
 ratio2cents.class.sources := classes/ratio2cents.c
 hz2rad.class.sources := classes/hz2rad.c
+rad2hz.class.sources := classes/rad2hz.c
 
 # rescale.class.sources := classes/rescale.c
 
 # signal:
+
 cents2ratio~.class.sources := classes/cents2ratio~.c
 hz2rad~.class.sources := classes/hz2rad~.c
+imp~.class.sources := classes/imp~.c
 rad2hz~.class.sources := classes/rad2hz~.c
 ratio2cents~.class.sources := classes/ratio2cents~.c
 sh~.class.sources := classes/sh~.c
-
-imp~.class.sources := classes/imp~.c
-
 
 # median~.class.sources := classes/median_tilde.c
 
--- classes/imp~.c
+++ classes/imp~.c
@@ -8,9 +8,8 @@ typedef struct _imp
 {
     t_object x_obj;
     t_float  x_phase;
-    t_float  x_lastin;
     t_inlet  *x_inlet;
-    t_float  x_lastout;
+    t_float x_sr;
 } t_imp;
 
 static t_int *imp_perform(t_int *w)
@@ -21,40 +20,24 @@ static t_int *imp_perform(t_int *w)
     t_float *in2 = (t_float *)(w[4]);
     t_float *out = (t_float *)(w[5]);
     t_float phase = x->x_phase;
-    t_float lastin = x->x_lastin;
-    t_float lastout = x->x_lastout;
-while (nblock--)
-{
-    float next_phase;
-    float input = *in1++;
-    float samples = *in2++;
-    float phase_step = 1. / samples; // phase_step
+    t_float sr = x->x_sr;
+    while (nblock--)
     {
-        next_phase = fmod(phase, 1.) + phase_step;
-        if (phase >= 1.)
-        {
-            *out++ = 1.;
-        }
-        else *out++ = 0.;
+        float hz = *in2++;
+        float phase_step = hz / sr; // phase_step
+        *out++ = phase >= 1.;
+        phase = fmod(phase, 1.); // wrapped phase
+        phase = phase + phase_step; // next phase
     }
-    phase = next_phase;
+    x->x_phase = phase;
+    return (w + 6);
 }
-x->x_phase = phase;
-return (w + 6);
-}
-
 
 static void imp_dsp(t_imp *x, t_signal **sp)
 {
     dsp_add(imp_perform, 5, x, sp[0]->s_n,
             sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec);
 }
-
-static void imp_float(t_imp *x, t_float f)
-{
-    x->x_phase = f;
-}
-
 
 static void *imp_free(t_imp *x)
 {
@@ -65,28 +48,19 @@ static void *imp_free(t_imp *x)
 static void *imp_new(t_floatarg f)
 {
     t_imp *x = (t_imp *)pd_new(imp_class);
-    x->x_phase = f;
+    x->x_phase = 0;
+    x->x_sr = sys_getsr(); // sample rate
     x->x_inlet = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
     pd_float((t_pd *)x->x_inlet, f);
     outlet_new((t_object *)x, &s_signal);
-    x->x_lastin = 0;
-    x->x_lastout = 0;
     return (x);
 }
-
-
 
 void imp_tilde_setup(void)
 {
     imp_class = class_new(gensym("imp~"),
-        (t_newmethod)imp_new,
-        (t_method)imp_free,
-        sizeof(t_imp),
-        CLASS_DEFAULT,
-        A_DEFFLOAT,
-        0);
-        class_addmethod(imp_class, nullfn, gensym("signal"), 0);
-        class_addmethod(imp_class, (t_method)imp_dsp, gensym("dsp"), A_CANT, 0);
-        class_addfloat(imp_class, (t_method)imp_float);
+        (t_newmethod)imp_new, (t_method)imp_free,
+        sizeof(t_imp), CLASS_DEFAULT, A_DEFFLOAT, 0);
+    class_addmethod(imp_class, nullfn, gensym("signal"), 0);
+    class_addmethod(imp_class, (t_method)imp_dsp, gensym("dsp"), A_CANT, 0);
 }
-
