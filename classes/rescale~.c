f3f74c6f95dc05f5cb50e95c7bee3dc1fe33f335
porres <porres@gmail.com>
2017-01-12 16:42:27 -0200

    new rescale~ object

--- classes/rescale~.c
+++ classes/rescale~.c
@@ -1,11 +1,11 @@
-// Porres 2016
+// Porres 2017
 
 #include "m_pd.h"
 #include <math.h>
 #include <string.h>
 
-#define RESCALE_MININ  0.
-#define RESCALE_MAXIN  127.
+#define RESCALE_MININ  -1.
+#define RESCALE_MAXIN  1.
 #define RESCALE_MINOUT  0.
 #define RESCALE_MAXOUT  1.
 #define RESCALE_EXPO  1.
@@ -15,10 +15,15 @@ typedef struct _rescale
     t_object x_obj;
     t_inlet  *x_inlet_1;
     t_inlet  *x_inlet_2;
-    t_int    x_classic;
+    t_float expo;
 } t_rescale;
 
 static t_class *rescale_class;
+
+void rescale_factor(t_rescale *x, t_floatarg f)
+{
+    x->expo = f < 0. ? 0. : f;
+}
 
 static t_int *rescale_perform(t_int *w)
 {
@@ -28,34 +33,19 @@ static t_int *rescale_perform(t_int *w)
     t_float *in2 = (t_float *)(w[4]);
     t_float *in3 = (t_float *)(w[5]);
     t_float *out = (t_float *)(w[6]);
-    t_int classic_flag = x->x_classic;
     while (nblock--)
     {
     float in = *in1++;
-    float il = -1; // Input LOW
-    float ih = 1; // Input HIGH
     float ol = *in2++; // Output LOW
     float oh = *in3++; // Output HIGH
-    float p = 1; // power (exponential) factor
+    float p = x->expo; // power (exponential) factor
     float output;
-    if (classic_flag != 0)
-        {p = p <= 1 ? 1 : p;
-        if (p == 1) output = ((in - il) / (ih - il) == 0) ? ol
-            : (((in - il) / (ih - il)) > 0) ? (ol + (oh - ol) * pow((in - il) / (ih - il), p))
-            : (ol + (oh - ol) * -(pow(((-in + il) / (ih - il)), p)));
-        else
-            {output = ol + (oh - ol) * ((oh - ol) * exp((il - ih) * log(p)) * exp(in * log(p)));
-            if (oh - ol <= 0) output = output * -1;
-            
-            }
-        }
-        else {
-        p = p <= 0 ? 0 : p;
-        output = ((in - il) / (ih - il) == 0) ? ol :
-        (((in - il) / (ih - il)) > 0) ?
-        (ol + (oh - ol) * pow((in - il) / (ih - il), p)) :
-        (ol + (oh - ol) * -(pow(((-in + il) / (ih - il)), p)));
-        }
+    p = p <= 0 ? 0 : p;
+    output = ((in + 1) / 2 == 0) ? ol :
+    (in + 1) / 2 > 0 ?
+    ol + (oh - ol) * pow((in + 1) / 2, p) :
+    ol + (oh - ol) * -(pow(((-in - 1) / 2), p));
+
     *out++ = output;
     }
     return (w + 7);
@@ -77,14 +67,12 @@ static void *rescale_free(t_rescale *x)
 static void *rescale_new(t_symbol *s, int argc, t_atom *argv)
 {
     t_rescale *x = (t_rescale *)pd_new(rescale_class);
-    t_float min_in, max_in, min_out, max_out, exponential;
+    t_float min_in, max_in, min_out, max_out, exp;
     min_in = RESCALE_MININ;
     max_in = RESCALE_MAXIN;
     min_out = RESCALE_MINOUT;
     max_out = RESCALE_MAXOUT;
-    exponential = RESCALE_EXPO;
-    t_int classic_exp;
-    classic_exp = 1;
+    exp = RESCALE_EXPO;
     
 	int argnum = 0;
 	while(argc > 0){
@@ -93,19 +81,13 @@ static void *rescale_new(t_symbol *s, int argc, t_atom *argv)
 			t_float argval = atom_getfloatarg(0,argc,argv);
 				switch(argnum){
 					case 0:
-						min_in = argval;
-						break;
-					case 1:
-						max_in = argval;
-						break;
-                    case 2:
                         min_out = argval;
                         break;
-                    case 3:
+                    case 1:
                         max_out = argval;
                         break;
-                    case 4:
-                        exponential = argval;
+                    case 2:
+                        exp = argval;
                         break;
 					default:
 						break;
@@ -114,43 +96,16 @@ static void *rescale_new(t_symbol *s, int argc, t_atom *argv)
 				argv++;
 				argnum++;
         }
-// attribute
-        else if(argv -> a_type == A_SYMBOL){
-            t_symbol *curarg = atom_getsymbolarg(0, argc, argv);
-            if(strcmp(curarg->s_name, "@classic")==0){
-                if(argc >= 2){
-                    t_float argval = atom_getfloatarg(1, argc, argv);
-                    classic_exp = (int)argval;
-                    argc-=2;
-                    argv+=2;
-                }
-                else{
-                    goto errstate;
-                };
-            }
-            else{
-                goto errstate;
-            };
-            
-        }
-        else{
-            goto errstate;
-        };
     };
-    
-	x->x_inlet_1 = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
-	pd_float((t_pd *)x->x_inlet_1, min_out);
+    x->expo = exp;
+    x->x_inlet_1 = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
+    pd_float((t_pd *)x->x_inlet_1, min_out);
     x->x_inlet_2 = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
     pd_float((t_pd *)x->x_inlet_2, max_out);
 
     outlet_new((t_object *)x, &s_signal);
     
-    x->x_classic = classic_exp;
-    
     return (x);
-	errstate:
-		pd_error(x, "rescale~: improper args");
-		return NULL;
 }
 
 void rescale_tilde_setup(void)
@@ -161,4 +116,5 @@ void rescale_tilde_setup(void)
 				sizeof(t_rescale), 0, A_GIMME, 0);
     class_addmethod(rescale_class, nullfn, gensym("signal"), 0);
     class_addmethod(rescale_class, (t_method)rescale_dsp, gensym("dsp"), A_CANT, 0);
+    class_addmethod(rescale_class, (t_method)rescale_factor, gensym("factor"), A_FLOAT, 0);
 }
