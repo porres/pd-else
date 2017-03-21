a233d521d9bceaf0617df759d34bf41147381ba4
porres <porres@gmail.com>
2017-03-21 02:09:54 -0300

    xfade~ help

--- classes/xfade~.c
+++ classes/xfade~.c
@@ -1,17 +1,21 @@
+
+#include <math.h>
 #include "m_pd.h"
+
+#define HALF_PI (M_PI * 0.5)
 
 static t_class *xfade_tilde_class;
 
 typedef struct _xfade_tilde {
   t_object  x_obj;
-  //t_float mix;   //The mix value (0: left input, 1: right input)
+  t_inlet  *x_inlet_mix;
   int n_in;
   int channels;
   t_sample **in;
   int n_out;
   t_sample **out;
   t_sample f;
-  t_sample* buffer;   //Input frame buffer: one sample of every input
+  t_sample* buffer;
 } t_xfade_tilde;
 
 
@@ -27,49 +31,65 @@ static t_int *xfade_tilde_perform(t_int *w)
   t_sample inv_mix_f;
   for (j =0; j < n; j++)
   {
-	  mix_f = *mix++; // RIGHT
+	  mix_f = *mix++; // Mix value [-1: left (A) input, 1: right (B) input]
 	  if (mix_f > 1) mix_f = 1;
-	  if (mix_f < 0) mix_f = 0;
-	  inv_mix_f = (1 - mix_f); // LEFT
-	  // Copy one sample of all the inputs
-	  for ( i=0; i < x->n_in;i++ )
+	  if (mix_f < -1) mix_f = -1;
+      mix_f = (mix_f + 1) * 0.5; // Mix from 0 to 1
+	  for (i=0; i < x->n_in; i++)
       {
        x->buffer[i] = (t_sample) x->in[i][j];
 	  }
-    for ( i=0; i < x->channels;i++ )
+    for (i=0; i < x->channels; i++)
       {
 	  out = (t_sample *)(x->out[i]);
-      out[j] = (x->buffer[i] * inv_mix_f) + (x->buffer[i+x->channels] * mix_f) ; 
+//      out[j] = (x->buffer[i] * (1 - mix_f)) + (x->buffer[i+x->channels] * mix_f) ; // linear
+// EQUAL POWER:
+     out[j] = (x->buffer[i] * (mix_f == 1 ? 0 : cos(mix_f * HALF_PI)))
+          + (x->buffer[i+x->channels] * sin(mix_f * HALF_PI));
       }
   }
   return (w+4);
 }
 
-// Annouce signal inputs and outputs to the DSP chain
+
 static void xfade_tilde_dsp(t_xfade_tilde *x, t_signal **sp)
 {
   int n;
-  t_sample **dummy=x->in;
-  for(n=0;n<x->n_in;n++)*dummy++=sp[n]->s_vec;
-  //Add +1 because of the mix inlet
-  dummy=x->out;
-  for(n=0;n<x->n_out;n++)*dummy++=sp[n+x->n_in+1]->s_vec;
+  t_sample **dummy = x->in;
+  for(n=0; n < x->n_in; n++) *dummy++ = sp[n]->s_vec;
+  dummy = x->out;
+  for(n=0; n < x->n_out; n++) *dummy++ = sp[n + x->n_in + 1]->s_vec;
   dsp_add(xfade_tilde_perform, 3, x, sp[0]->s_n, sp[x->n_in]->s_vec);
 }
 
 static void xfade_tilde_free( t_xfade_tilde *x)
 {
+     inlet_free(x->x_inlet_mix);
 	 freebytes(x->in, x->n_in * sizeof(t_sample *));
 	 freebytes(x->out, x->n_out * sizeof(t_sample *));
 	 freebytes(x->buffer,x->n_in * sizeof( * x->buffer));
 }
 
-static void *xfade_tilde_new(t_floatarg f)
+static void *xfade_tilde_new(t_symbol *s, int ac, t_atom *av)
 {
   t_xfade_tilde *x = (t_xfade_tilde *)pd_new(xfade_tilde_class);
 
-  x->channels = (int) f;
-  if ( x->channels  < 1 ) x->channels = 2;
+    t_float f1 = 1, f2 = 0;
+    if (ac && av->a_type == A_FLOAT)
+        {
+        f1 = av->a_w.w_float;
+        ac--; av++;
+        if (ac && av->a_type == A_FLOAT)
+            f2 = av->a_w.w_float;
+        }
+    
+  x->channels = (int) f1;
+    if (x->channels < 1) x->channels = 1;
+    if (x->channels > 64) x->channels = 64;
+
+  t_float init_mix = f2;
+  init_mix < -1 ? -1 : init_mix >= 1 ? 1 : init_mix; // clipping mix input
+
   x->n_in =  x->channels * 2;
   x->in = (t_sample **)getbytes(x->n_in * sizeof(t_sample *));
   int i=x->n_in;
@@ -83,7 +103,10 @@ static void *xfade_tilde_new(t_floatarg f)
     {
 	inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
     }
-  inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
+    
+    x->x_inlet_mix = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
+    pd_float((t_pd *)x->x_inlet_mix, init_mix);
+    
   for (i=0; i < x->n_out; i++)
     {
         outlet_new(&x->x_obj, &s_signal);
@@ -96,7 +119,7 @@ static void *xfade_tilde_new(t_floatarg f)
   xfade_tilde_class = class_new(gensym("xfade~"),
         (t_newmethod)xfade_tilde_new,
         (t_method)xfade_tilde_free, sizeof(t_xfade_tilde),
-        0, A_DEFFLOAT, 0);
+        0, A_GIMME, 0);
   CLASS_MAINSIGNALIN(xfade_tilde_class, t_xfade_tilde, f);
   class_addmethod(xfade_tilde_class,
         (t_method)xfade_tilde_dsp, gensym("dsp"), A_CANT, 0);
