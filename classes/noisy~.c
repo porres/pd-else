8c0cfb42d7a01f5cf987f5e3954962ba387aecd9
porres <porres@gmail.com>
2017-03-22 21:14:08 -0300

    noisy 1.2

--- classes/noisy~.c
+++ classes/noisy~.c
@@ -8,6 +8,10 @@ typedef struct _noisy
 {
     t_object  x_obj;
     int x_val;
+    t_float  x_lastout;
+    t_float  x_sr;
+    t_float  x_phase;
+    t_float  x_freq;
     t_outlet *x_outlet;
 } t_noisy;
 
@@ -20,14 +24,22 @@ static t_int *noisy_perform(t_int *w)
     t_float *in = (t_float *)(w[4]);
     t_sample *out = (t_sample *)(w[5]);
     int val = *vp;
+    t_float lastout = x->x_lastout;
+    double phase = x->x_phase;
+    double sr = x->x_sr;
     while (nblock--)
     {
         t_float hz = *in++;
+        double phase_step = hz / sr; // phase_step
+        phase_step = phase_step > 1 ? 1. : phase_step < 0 ? 0 : phase_step; // clipped phase_step
+        
         t_float noise = ((float)((val & 0x7fffffff) - 0x40000000)) * (float)(1.0 / 0x40000000);
         *out++ = noise;
         val = val * 435898247 + 382842987;
     }
      *vp = val;
+    x->x_phase = phase;
+    x->x_lastout = lastout;
     return (w + 6);
 }
 
@@ -42,9 +54,11 @@ static void *noisy_free(t_noisy *x)
     return (void *)x;
 }
 
-static void *noisy_new(void)
+static void *noisy_new(t_floatarg f)
 {
     t_noisy *x = (t_noisy *)pd_new(noisy_class);
+    x->x_freq = f;
+    x->x_sr = sys_getsr(); // sample rate
     static int init = 307;
     x->x_val = (init *= 1319);
     x->x_outlet = outlet_new(&x->x_obj, &s_signal);
