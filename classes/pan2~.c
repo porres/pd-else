c4f5326549dd434f2c2418f9a6cf06cccb5e83af
porres <porres@gmail.com>
2017-03-29 02:36:03 -0300

    pan2~

--- classes/pan2~.c
+++ classes/pan2~.c
@@ -1,118 +1,69 @@
 // Porres 2016
 
 #include "m_pd.h"
-#include "math.h"
 
+static t_class *pan2_class;
 
-static t_class *pimp_class;
-
-typedef struct _pimp
+typedef struct _pan2
 {
     t_object x_obj;
-    double  x_phase;
-    double  x_last_phase_offset;
-    t_float  x_freq;
-    t_inlet  *x_inlet_phase;
-    t_inlet  *x_inlet_sync;
-    t_outlet *x_outlet_dsp_0;
-    t_outlet *x_outlet_dsp_1;
-    t_float x_sr;
-} t_pimp;
+    t_inlet  *x_panlet;
+} t_pan2;
 
-static t_int *pimp_perform(t_int *w)
+static t_int *pan2_perform(t_int *w)
 {
-    t_pimp *x = (t_pimp *)(w[1]);
+    t_pan2 *x = (t_pan2 *)(w[1]);
     int nblock = (t_int)(w[2]);
-    t_float *in1 = (t_float *)(w[3]); // freq
-    t_float *in2 = (t_float *)(w[4]); // phase
-    t_float *in3 = (t_float *)(w[5]); // sync
-    t_float *out1 = (t_float *)(w[6]);
-    t_float *out2 = (t_float *)(w[7]);
-    double phase = x->x_phase;
-    double last_phase_offset = x->x_last_phase_offset;
-    double sr = x->x_sr;
+    t_float *in1 = (t_float *)(w[3]); // in
+    t_float *in2 = (t_float *)(w[4]); // pan
+    t_float *out1 = (t_float *)(w[5]); // L
+    t_float *out2 = (t_float *)(w[6]); // R
     while (nblock--)
     {
-        double hz = *in1++;
-        double phase_offset = *in2++;
-        double trig = *in3++;
-        double phase_step = hz / sr; // phase_step
-        phase_step = phase_step > 1 ? 1. : phase_step < -1 ? -1 : phase_step; // clipped phase_step
-        double phase_dev = phase_offset - last_phase_offset;
-        if (phase_dev >= 1 || phase_dev <= -1)
-            phase_dev = fmod(phase_dev, 1); // fmod(phase_dev)
-        if (hz >= 0)
-            {
-                if (trig > 0 && trig <= 1) phase = trig;
-                else
-                    {
-                        phase = phase + phase_dev;
-                        if (phase <= 0) phase = phase + 1.; // wrap deviated phase
-                    }
-                *out2++ = phase >= 1.;
-                if (phase >= 1.) phase = phase - 1; // wrapped phase
-            }
-        else
-            {
-                if (trig > 0 && trig < 1) phase = trig;
-                else if (trig == 1) phase = 0;
-                else
-                {
-                    phase = phase + phase_dev;
-                    if (phase >= 1) phase = phase - 1.; // wrap deviated phase
-                }
-                *out2++ = phase <= 0.;
-                if (phase <= 0.) phase = phase + 1.; // wrapped phase
-            }
-        *out1++ = phase; // wrapped phase
-        phase = phase + phase_step; // next phase
-        last_phase_offset = phase_offset; // last phase offset
+        float in = *in1++;
+        float pan = *in2++;
+        if (pan < -1) pan = -1;
+        if (pan > 1) pan = 1;
+        *out1++ = in;
+        *out2++ = pan;
     }
-    x->x_phase = phase;
-    x->x_last_phase_offset = last_phase_offset;
-    return (w + 8);
+    return (w + 7);
 }
 
-static void pimp_dsp(t_pimp *x, t_signal **sp)
+static void pan2_dsp(t_pan2 *x, t_signal **sp)
 {
-    dsp_add(pimp_perform, 7, x, sp[0]->s_n,
-            sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec, sp[4]->s_vec);
+    dsp_add(pan2_perform, 6, x, sp[0]->s_n,
+            sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec);
 }
 
-static void *pimp_free(t_pimp *x)
+static void *pan2_free(t_pan2 *x)
 {
-    inlet_free(x->x_inlet_phase);
-    inlet_free(x->x_inlet_sync);
-    outlet_free(x->x_outlet_dsp_0);
-    outlet_free(x->x_outlet_dsp_1);
+    inlet_free(x->x_panlet);
     return (void *)x;
 }
 
-static void *pimp_new(t_floatarg f1, t_floatarg f2)
+static void *pan2_new(t_floatarg f)
 {
-    t_pimp *x = (t_pimp *)pd_new(pimp_class);
-    t_float init_freq = f1;
-    t_float init_phase = f2;
-    init_phase < 0 ? 0 : init_phase >= 1 ? 0 : init_phase; // clipping phase input
-    if (init_phase == 0 && init_freq > 0)
-        x->x_phase = 1.;
-    x->x_last_phase_offset = 0;
-    x->x_freq = init_freq;
-    x->x_sr = sys_getsr(); // sample rate
-    x->x_inlet_phase = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
-    pd_float((t_pd *)x->x_inlet_phase, init_phase);
-    x->x_inlet_sync = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
-    pd_float((t_pd *)x->x_inlet_sync, 0);
-    x->x_outlet_dsp_0 = outlet_new(&x->x_obj, &s_signal);
-    x->x_outlet_dsp_1 = outlet_new(&x->x_obj, &s_signal);
+    t_pan2 *x = (t_pan2 *)pd_new(pan2_class);
+    if(f < -1) f = -1;
+    if(f > 1) f = 1;
+    x->x_panlet = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
+    pd_float((t_pd *)x->x_panlet, f);
+    outlet_new((t_object *)x, &s_signal);
+    outlet_new((t_object *)x, &s_signal);
     return (x);
 }
 
-void pimp_tilde_setup(void)
+void pan2_tilde_setup(void)
 {
-    pimp_class = class_new(gensym("pimp~"),
-        (t_newmethod)pimp_new, (t_method)pimp_free,
-        sizeof(t_pimp), CLASS_DEFAULT, A_DEFFLOAT, A_DEFFLOAT, 0);
-    CLASS_MAINSIGNALIN(pimp_class, t_pimp, x_freq);
-    class_addmethod(pimp_class, (t_method)pimp_dsp, gensym("dsp"), A_CANT, 0);
+    pan2_class = class_new(gensym("pan2~"),
+        (t_newmethod)pan2_new,
+        (t_method)pan2_free,
+        sizeof(t_pan2),
+        CLASS_DEFAULT,
+        A_DEFFLOAT,
+        0);
+        class_addmethod(pan2_class, nullfn, gensym("signal"), 0);
+        class_addmethod(pan2_class, (t_method)pan2_dsp, gensym("dsp"), A_CANT, 0);
 }
+
