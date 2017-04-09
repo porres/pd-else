41e912494aeb2eb52468597fba9736ac62fea7dd
porres <porres@gmail.com>
2017-04-09 12:30:59 -0300

    bandpass -bw flag and help file

--- classes/bandpass~.c
+++ classes/bandpass~.c
@@ -119,21 +119,75 @@ static void bandpass_q(t_bandpass *x)
     x->x_bw = 0;
 }
 
-static void *bandpass_new(t_floatarg f1, t_floatarg f2)
+static void *bandpass_tilde_new(t_symbol *s, int argc, t_atom *argv)
 {
     t_bandpass *x = (t_bandpass *)pd_new(bandpass_class);
-    x->x_inlet_freq = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
-    pd_float((t_pd *)x->x_inlet_freq, f1);
-    x->x_inlet_q = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
-    pd_float((t_pd *)x->x_inlet_q, f2);
-    x->x_out = outlet_new((t_object *)x, &s_signal);
     return (x);
+}
+
+
+static void *bandpass_new(t_symbol *s, int argc, t_atom *argv)
+{
+    t_bandpass *x = (t_bandpass *)pd_new(bandpass_class);
+    float freq = 1;
+    float reson = 1;
+    int bw = 0;
+/////////////////////////////////////////////////////////////////////////////////////
+    int argnum = 0;
+    while(argc > 0)
+    {
+        if(argv -> a_type == A_FLOAT)
+        { //if current argument is a float
+            t_float argval = atom_getfloatarg(0, argc, argv);
+            switch(argnum)
+            {
+                case 0:
+                    freq = argval;
+                    break;
+                case 1:
+                    reson = argval;
+                    break;
+                default:
+                    break;
+            };
+            argnum++;
+            argc--;
+            argv++;
+        }
+        else if (argv -> a_type == A_SYMBOL)
+        {
+            t_symbol *curarg = atom_getsymbolarg(0, argc, argv);
+            if(strcmp(curarg->s_name, "-bw")==0)
+            {
+                bw = 1;
+                argc -= 1;
+                argv += 1;
+            }
+            else
+            {
+                goto errstate;
+            };
+        }
+    };
+/////////////////////////////////////////////////////////////////////////////////////
+    x->x_bw = bw;
+    
+    x->x_inlet_freq = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
+    pd_float((t_pd *)x->x_inlet_freq, freq);
+    x->x_inlet_q = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
+    pd_float((t_pd *)x->x_inlet_q, reson);
+    x->x_out = outlet_new((t_object *)x, &s_signal);
+
+    return (x);
+    errstate:
+        pd_error(x, "bandpass~: improper args");
+        return NULL;
 }
 
 void bandpass_tilde_setup(void)
 {
     bandpass_class = class_new(gensym("bandpass~"), (t_newmethod)bandpass_new, 0,
-        sizeof(t_bandpass), CLASS_DEFAULT, A_DEFFLOAT, A_DEFFLOAT, 0);
+        sizeof(t_bandpass), CLASS_DEFAULT, A_GIMME, 0);
     class_addmethod(bandpass_class, (t_method)bandpass_dsp, gensym("dsp"), A_CANT, 0);
     class_addmethod(bandpass_class, nullfn, gensym("signal"), 0);
     class_addmethod(bandpass_class, (t_method)bandpass_clear, gensym("clear"), 0);
