b31c35ea9e8533f8a6a69fc92a4632853e187b91
porres <porres@gmail.com>
2017-07-05 21:23:43 -0300

    one step at a time

--- classes/fbsine~.c
+++ classes/fbsine~.c
@@ -11,15 +11,21 @@ typedef struct _fbsine
 {
     t_object x_obj;
     double  x_phase;
-    float  x_last_out;
+    float  x_yn_m1;
     float  x_last_phase_offset;
     t_float  x_freq;
+    t_int    x_filter;
     t_inlet  *x_inlet_fb;
     t_inlet  *x_inlet_phase;
     t_inlet  *x_inlet_sync;
     t_outlet *x_outlet;
     t_float x_sr;
 } t_fbsine;
+
+static void fbsine_filter(t_fbsine *x, t_floatarg f)
+{
+    x->x_filter = f != 0;
+}
 
 static t_int *fbsine_perform(t_int *w)
 {
@@ -30,7 +36,7 @@ static t_int *fbsine_perform(t_int *w)
     t_float *in3 = (t_float *)(w[5]); // phase
     t_float *in4 = (t_float *)(w[6]); // sync
     t_float *out = (t_float *)(w[7]);
-    float last_out = x->x_last_out;
+    float yn_m1 = x->x_yn_m1;
     double phase = x->x_phase;
     float last_phase_offset = x->x_last_phase_offset;
     float sr = x->x_sr;
@@ -43,12 +49,16 @@ static t_int *fbsine_perform(t_int *w)
         phase += (double)phase_dev;
         if (trig > 0 && trig <= 1)
             phase = trig;
-        float radians = (phase + last_out * fback) * TWOPI;
-        *out++ = last_out = sin(radians);
+        if(x->x_filter)
+            fback = 0;
+        else
+            fback *= yn_m1; // no filter
+        float radians = (phase + fback) * TWOPI;
+        *out++ = yn_m1 = sin(radians);
         phase += (double)(hz / sr); // next phase
         last_phase_offset = phase_offset; // last phase offset
     }
-    x->x_last_out = last_out; // last out
+    x->x_yn_m1 = yn_m1; // last out
     x->x_phase = fmod(phase, 1); // next wrapped phase
     x->x_last_phase_offset = last_phase_offset;
     return (w + 8);
@@ -73,7 +83,7 @@ static void *fbsine_free(t_fbsine *x)
 static void *fbsine_new(t_symbol *s, int ac, t_atom *av)
 {
     t_fbsine *x = (t_fbsine *)pd_new(fbsine_class);
-    t_float f1 = 0, f2 = 0, f3 = 0;
+    t_float f1 = 0, f2 = 0, f3 = 0, f4 = 0;
     if (ac && av->a_type == A_FLOAT){
         f1 = av->a_w.w_float;
         ac--; av++;
@@ -83,12 +93,17 @@ static void *fbsine_new(t_symbol *s, int ac, t_atom *av)
             if (ac && av->a_type == A_FLOAT){
                 f3 = av->a_w.w_float;
                 ac--; av++;
+                if (ac && av->a_type == A_FLOAT){
+                    f4 = av->a_w.w_float;
+                    ac--; av++;
+                }
             }
         }
     }
     t_float init_freq = f1;
     t_float init_fb = f2;
     t_float init_phase = f3;
+    x->x_filter = f4 != 0;
     init_phase < 0 ? 0 : init_phase >= 1 ? 0 : init_phase; // clipping phase input
     x->x_last_phase_offset = 0;
     x->x_freq = init_freq;
@@ -109,4 +124,5 @@ void fbsine_tilde_setup(void)
         sizeof(t_fbsine), CLASS_DEFAULT, A_GIMME, 0);
     CLASS_MAINSIGNALIN(fbsine_class, t_fbsine, x_freq);
     class_addmethod(fbsine_class, (t_method)fbsine_dsp, gensym("dsp"), A_CANT, 0);
+    class_addmethod(fbsine_class, (t_method)fbsine_filter, gensym("filter"), A_FLOAT, 0);
 }
