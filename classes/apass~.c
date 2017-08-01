5fcc538f1e7d2fd6da975d8f7d4aa0a8d553d410
porres <porres@gmail.com>
2017-08-01 20:50:11 -0300

    delay updates

--- classes/apass~.c
+++ classes/apass~.c
@@ -1,38 +1,31 @@
 // Porres 2017
-// Based on Max/cyclone's [allpass~] and SuperCollider's AllPassL
+// Adapted from cyclone's allpass
 
 #include <math.h>
 #include <stdlib.h>
 #include "m_pd.h"
 
-#define apass_STACK 48000 //stack buf size, 1 sec at 48k for good measure
-#define apass_DELAY  100.0 //maximum delay
-#define apass_MIND 1 // minumum delay in ms
-#define apass_MAXD 4294967294 //max delay = 2**32 - 2
-
-#define apass_MINMS 0. //min delay in ms
-
-#define apass_DEFGAIN 0. //default gain
+#define APASS_STACK 48000 // stack buf size, 1 sec at 48k
+#define APASS_MAXD 4294967294 // max delay = 2**32 - 2
 
 static t_class *apass_class;
 
 typedef struct _apass
 {
-    t_object  x_obj;
-    t_inlet  *x_dellet;
-    t_inlet  *x_alet;
-    t_outlet  *x_outlet;
-    int     x_sr;
-    //pointers to the delay bufs
-    double  * x_ybuf;
-    double x_ffstack[apass_STACK];
-    double * x_xbuf;
-    double x_fbstack[apass_STACK];
-    int     x_alloc; //if we are using allocated bufs
-    unsigned int     x_sz; //actual size of each delay buffer
-    
-    t_float     x_maxdel;  //maximum delay in ms
-    unsigned int       x_wh;     //writehead
+    t_object        x_obj;
+    t_inlet         *x_dellet;
+    t_inlet         *x_alet;
+    t_outlet        *x_outlet;
+    int             x_sr;
+// pointers to the delay bufs
+    double          * x_ybuf;
+    double          x_ffstack[APASS_STACK];
+    double          * x_xbuf;
+    double          x_fbstack[APASS_STACK];
+    int             x_alloc; // if we are using allocated bufs
+    unsigned int    x_sz; // actual size of each delay buffer
+    t_float         x_maxdel;  // maximum delay in ms
+    unsigned int    x_wh;     // writehead
 } t_apass;
 
 static void apass_clear(t_apass *x){
@@ -45,24 +38,22 @@ static void apass_clear(t_apass *x){
 }
 
 static void apass_sz(t_apass *x){
-    //helper function to deal with allocation issues if needed
-    //ie if wanted size x->x_maxdel is bigger than stack, allocate
+    // helper function to deal with allocation issues if needed
+    // ie if wanted size x->x_maxdel is bigger than stack, allocate
     
-    //convert ms to samps
+    // convert ms to samps
     unsigned int newsz = (unsigned int)ceil((double)x->x_maxdel*0.001*(double)x->x_sr);
-    newsz++; //add a sample for good measure since say bufsize is 2048 and
-    //you want a delay of 2048 samples,.. problem!
+    newsz++; // add a sample for good measure since say bufsize is 2048 and
+    // you want a delay of 2048 samples,.. problem!
     
     int alloc = x->x_alloc;
     unsigned int cursz = x->x_sz; //current size
     
-    if(newsz < 0){
+    if(newsz < 0)
         newsz = 0;
-    }
-    else if(newsz > apass_MAXD){
-        newsz = apass_MAXD;
-    };
-    if(!alloc && newsz > apass_STACK){
+    else if(newsz > APASS_MAXD)
+        newsz = APASS_MAXD;
+    if(!alloc && newsz > APASS_STACK){
         x->x_xbuf = (double *)malloc(sizeof(double)*newsz);
         x->x_ybuf = (double *)malloc(sizeof(double)*newsz);
         x->x_alloc = 1;
@@ -73,10 +64,10 @@ static void apass_sz(t_apass *x){
         x->x_ybuf = (double *)realloc(x->x_ybuf, sizeof(double)*newsz);
         x->x_sz = newsz;
     }
-    else if(alloc && newsz < apass_STACK){
+    else if(alloc && newsz < APASS_STACK){
         free(x->x_xbuf);
         free(x->x_ybuf);
-        x->x_sz = apass_STACK;
+        x->x_sz = APASS_STACK;
         x->x_xbuf = x->x_ffstack;
         x->x_ybuf = x->x_fbstack;
         x->x_alloc = 0;
@@ -84,17 +75,14 @@ static void apass_sz(t_apass *x){
     apass_clear(x);
 }
 
-
-
-
 static double apass_getlin(double tab[], unsigned int sz, double idx){
-    //copying from my own library, linear interpolated reader - DK
+    // linear interpolated reader, copied from Derek Kwan's library
     double output;
     unsigned int tabphase1 = (unsigned int)idx;
     unsigned int tabphase2 = tabphase1 + 1;
     double frac = idx - (double)tabphase1;
     if(tabphase1 >= sz - 1){
-        tabphase1 = sz - 1; //checking to see if index falls within bounds
+        tabphase1 = sz - 1; // checking to see if index falls within bounds
         output = tab[tabphase1];
     }
     else if(tabphase1 < 0){
@@ -102,10 +90,9 @@ static double apass_getlin(double tab[], unsigned int sz, double idx){
         output = tab[tabphase1];
     }
     else{
-        double yb = tab[tabphase2]; //linear interp
+        double yb = tab[tabphase2]; // linear interp
         double ya = tab[tabphase1];
         output = ya+((yb-ya)*frac);
-        
     };
     return output;
 }
@@ -116,9 +103,8 @@ static double apass_readmsdelay(t_apass *x, double arr[], t_float ms){
     //eventual reading head
     double rh = (double)ms*((double)x->x_sr*0.001); //converting ms to samples
     //bounds checking for minimum delay in samples
-    if(rh < apass_MIND){
-        rh = apass_MIND;
-    };
+    if(rh < 0)
+        rh = 0;
     rh = (double)x->x_wh+((double)x->x_sz-rh); //essentially subracting from writehead to find proper position in buffer
     //wrapping into length of delay buffer
     while(rh >= x->x_sz){
@@ -127,54 +113,45 @@ static double apass_readmsdelay(t_apass *x, double arr[], t_float ms){
     //now to read from the buffer!
     double output = apass_getlin(arr, x->x_sz, rh);
     return output;
-    
 }
 
-
-
-static t_int *apass_perform(t_int *w)
-{
+static t_int *apass_perform(t_int *w){
     t_apass *x = (t_apass *)(w[1]);
     int n = (int)(w[2]);
     t_float *xin = (t_float *)(w[3]);
     t_float *din = (t_float *)(w[4]);
     t_float *ain = (t_float *)(w[5]);
     t_float *out = (t_float *)(w[6]);
-    
     int i;
     for(i=0; i<n;i++){
         int wh = x->x_wh;
         double input = (double)xin[i];
-        //first off, write input to delay buf
+        double output;
+// first off, write input to delay buf
         x->x_xbuf[wh] = input;
-        //get delayed values of x and y
+// get delayed values of x and y
         t_float delms = din[i];
-        //first bounds checking
-        if(delms < 0){
+// bounds checking
+        if(delms < 0)
             delms = 0;
-        }
-        else if(delms > x->x_maxdel){
+        else if(delms > x->x_maxdel)
             delms = x->x_maxdel;
-        };
-        //now get those delayed vals
+// now get those delayed vals
         double delx = apass_readmsdelay(x, x->x_xbuf, delms);
         double dely = apass_readmsdelay(x, x->x_ybuf, delms);
-        //figure out your current y term: y[n] = -a*x[n] + x[n-d] + a*y[n-d]
-        
         if (ain[i] == 0)
             ain[i] = 0;
         else
             ain[i] = copysign(exp(log(0.001) * delms/fabs(ain[i])), ain[i]);
-        
-        double output = (double)ain[i]*-1.*input + delx + (double)ain[i]*dely;
-        //stick this guy in the ybuffer and output
+// y[n] = -a * x[n] + x[n-d] + a * y[n-d]
+        if (delms == 0)
+            output = input;
+        else
+            output = (double)ain[i]*-1.*input + delx + (double)ain[i]*dely;
         x->x_ybuf[wh] = output;
         out[i] = output;
-        
-        //increment writehead
-        x->x_wh = (wh + 1) % x->x_sz;
+        x->x_wh = (wh + 1) % x->x_sz; // increment writehead
     };
-    
     return (w + 7);
 }
 
@@ -182,119 +159,46 @@ static void apass_dsp(t_apass *x, t_signal **sp)
 {
     int sr = sp[0]->s_sr;
     if(sr != x->x_sr){
-        //if new sample rate isn't old sample rate, need to realloc
+// if new sample rate isn't old sample rate, need to realloc
         x->x_sr = sr;
         apass_sz(x);
     };
-    dsp_add(apass_perform, 6, x, sp[0]->s_n,
-            sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec,
-            sp[3]->s_vec);
+    dsp_add(apass_perform, 6, x, sp[0]->s_n, sp[0]->s_vec,
+        sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec);
 }
 
-static void apass_list(t_apass *x, t_symbol *s, int argc, t_atom * argv){
-    
-    
-    int argnum = 0; //current argument
-    while(argc){
-        if(argv -> a_type == A_FLOAT){
-            t_float curf = atom_getfloatarg(0, argc, argv);
-            switch(argnum){
-                case 0:
-                    //maxdel
-                    x->x_maxdel = curf > 0 ? curf : apass_DELAY;
-                    apass_sz(x);
-                    break;
-                case 1:
-                    //initdel
-                    if(curf < apass_MINMS){
-                        curf = apass_MINMS;
-                    }
-                    else if(curf > x->x_maxdel){
-                        curf = x->x_maxdel;
-                    };
-                    pd_float((t_pd *)x->x_dellet, curf);
-                    break;
-                case 2:
-                    //gain
-                    pd_float((t_pd *)x->x_alet, curf);
-                    break;
-                default:
-                    break;
-            };
-            argnum++;
-        };
-        argc--;
-        argv++;
-    };
-    
-    
-    
+static void apass_size(t_apass *x, t_floatarg f1){
+    if(f1 < 0)
+        f1 = 0;
+    x->x_maxdel = f1;
+    apass_sz(x);
 }
 
-
-static void *apass_new(t_symbol *s, int argc, t_atom * argv){
+static void *apass_new(t_floatarg f1, t_floatarg f2){
     t_apass *x = (t_apass *)pd_new(apass_class);
-    
-    //defaults
-    t_float maxdel = apass_DELAY;
-    t_float initdel = apass_MINMS;
-    t_float gain = apass_DEFGAIN;
     x->x_sr = sys_getsr();
-    
     x->x_alloc = 0;
-    x->x_sz = apass_STACK;
-    //clear out stack bufs, set pointer to stack
+    x->x_sz = APASS_STACK;
+// clear out stack bufs, set pointer to stack
     x->x_ybuf = x->x_fbstack;
     x->x_xbuf = x->x_ffstack;
     apass_clear(x);
-    
-    int argnum = 0; //current argument
-    while(argc){
-        if(argv -> a_type == A_FLOAT){
-            t_float curf = atom_getfloatarg(0, argc, argv);
-            switch(argnum){
-                case 0:
-                    maxdel = curf;
-                    break;
-                case 1:
-                    initdel = curf;
-                    break;
-                case 2:
-                    gain = curf;
-                    break;
-                default:
-                    break;
-            };
-            argnum++;
-        };
-        argc--;
-        argv++;
-    };
-    
-    
-    x->x_maxdel = maxdel > 0 ? maxdel : apass_DELAY;
-    //ship off to the helper method to deal with allocation if necessary
+    if (f1 < 0)
+        f1 = 0;
+    x->x_maxdel = f1;
+// ship off to the helper method to deal with allocation if necessary
     apass_sz(x);
-    //boundschecking
-    //this is 1/44.1 (1/(sr*0.001) rounded up, good enough?
+// boundschecking
+// this is 1/44.1 (1/(sr*0.001) rounded up, good enough?
     
-    if(initdel < apass_MINMS){
-        initdel = apass_MINMS;
-    }
-    else if(initdel > x->x_maxdel){
-        initdel = x->x_maxdel;
-    };
-    
-    //inlets outlets
+// inlets / outlet
     x->x_dellet = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
-    pd_float((t_pd *)x->x_dellet, initdel);
+    pd_float((t_pd *)x->x_dellet, f1);
     x->x_alet = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
-    pd_float((t_pd *)x->x_alet, gain);
+    pd_float((t_pd *)x->x_alet, f2);
     x->x_outlet = outlet_new((t_object *)x, &s_signal);
     return (x);
 }
-
-
 
 static void * apass_free(t_apass *x){
     if(x->x_alloc){
@@ -309,13 +213,10 @@ static void * apass_free(t_apass *x){
 
 void apass_tilde_setup(void)
 {
-    apass_class = class_new(gensym("apass~"),
-                              (t_newmethod)apass_new,
-                              (t_method)apass_free,
-                              sizeof(t_apass), 0,
-                              A_GIMME, 0);
+    apass_class = class_new(gensym("apass~"), (t_newmethod)apass_new,
+        (t_method)apass_free, sizeof(t_apass), 0, A_DEFFLOAT, A_DEFFLOAT, 0);
     class_addmethod(apass_class, nullfn, gensym("signal"), 0);
     class_addmethod(apass_class, (t_method)apass_dsp, gensym("dsp"), A_CANT, 0);
     class_addmethod(apass_class, (t_method)apass_clear, gensym("clear"), 0);
-    class_addlist(apass_class, (t_method)apass_list);
+    class_addmethod(apass_class, (t_method)apass_size, gensym("size"), A_DEFFLOAT, 0);
 }
--- classes/fbdelay~.c
+++ classes/fbdelay~.c
@@ -1,99 +1,81 @@
 // Porres 2017
+// Adapted from fbcomb~
 
 #include <math.h>
 #include <stdlib.h>
 #include "m_pd.h"
 
-#define fbdelay_STACK 48000 //stack buf size, 1 sec at 48k for good measure
-#define fbdelay_DELAY  100.0 //maximum delay
-#define fbdelay_MIND 1 // minumum delay in ms
-#define fbdelay_MAXD 4294967294 //max delay = 2**32 - 2
-
-#define fbdelay_MINMS 0. //min delay in ms
-
-#define fbdelay_DEFGAIN 0. //default gain
+#define FBDELAY_STACK 48000 // stack buf size, 1 sec at 48k
+#define FBDELAY_MAXD 4294967294 // max delay = 2**32 - 2
 
 static t_class *fbdelay_class;
 
 typedef struct _fbdelay
 {
-    t_object  x_obj;
-    t_inlet  *x_dellet;
-    t_inlet  *x_alet;
-    t_outlet  *x_outlet;
-    int     x_sr;
-    //pointers to the delay bufs
-    double  * x_ybuf;
-    double x_ffstack[fbdelay_STACK];
-    double * x_xbuf;
-    double x_fbstack[fbdelay_STACK];
-    int     x_alloc; //if we are using allocated bufs
-    unsigned int     x_sz; //actual size of each delay buffer
-    
-    t_float     x_maxdel;  //maximum delay in ms
-    unsigned int       x_wh;     //writehead
+    t_object        x_obj;
+    t_inlet         *x_dellet;
+    t_inlet         *x_alet;
+    t_outlet        *x_outlet;
+    int             x_sr;
+    // pointers to the delay buf
+    double          * x_ybuf;
+    double          x_fbstack[FBDELAY_STACK];
+    int             x_alloc; // if we are using allocated buf
+    unsigned int    x_sz; // actual size of each delay buffer
+    t_float         x_maxdel;  // maximum delay in ms
+    unsigned int    x_wh;     // writehead
 } t_fbdelay;
 
 static void fbdelay_clear(t_fbdelay *x){
     unsigned int i;
     for(i=0; i<x->x_sz; i++){
-        x->x_xbuf[i] = 0.;
         x->x_ybuf[i] = 0.;
     };
     x->x_wh = 0;
 }
 
 static void fbdelay_sz(t_fbdelay *x){
-    //helper function to deal with allocation issues if needed
-    //ie if wanted size x->x_maxdel is bigger than stack, allocate
+    // helper function to deal with allocation issues if needed
+    // ie if wanted size x->x_maxdel is bigger than stack, allocate
     
-    //convert ms to samps
+    // convert ms to samps
     unsigned int newsz = (unsigned int)ceil((double)x->x_maxdel*0.001*(double)x->x_sr);
-    newsz++; //add a sample for good measure since say bufsize is 2048 and
-    //you want a delay of 2048 samples,.. problem!
+    newsz++; // add a sample for good measure since say bufsize is 2048 and
+    // you want a delay of 2048 samples,.. problem!
     
     int alloc = x->x_alloc;
     unsigned int cursz = x->x_sz; //current size
     
-    if(newsz < 0){
+    if(newsz < 0)
         newsz = 0;
-    }
-    else if(newsz > fbdelay_MAXD){
-        newsz = fbdelay_MAXD;
-    };
-    if(!alloc && newsz > fbdelay_STACK){
-        x->x_xbuf = (double *)malloc(sizeof(double)*newsz);
+    else if(newsz > FBDELAY_MAXD)
+        newsz = FBDELAY_MAXD;
+    if(!alloc && newsz > FBDELAY_STACK){
         x->x_ybuf = (double *)malloc(sizeof(double)*newsz);
         x->x_alloc = 1;
         x->x_sz = newsz;
     }
     else if(alloc && newsz > cursz){
-        x->x_xbuf = (double *)realloc(x->x_xbuf, sizeof(double)*newsz);
         x->x_ybuf = (double *)realloc(x->x_ybuf, sizeof(double)*newsz);
         x->x_sz = newsz;
     }
-    else if(alloc && newsz < fbdelay_STACK){
-        free(x->x_xbuf);
+    else if(alloc && newsz < FBDELAY_STACK){
         free(x->x_ybuf);
-        x->x_sz = fbdelay_STACK;
-        x->x_xbuf = x->x_ffstack;
+        x->x_sz = FBDELAY_STACK;
         x->x_ybuf = x->x_fbstack;
         x->x_alloc = 0;
     };
     fbdelay_clear(x);
 }
 
-
-
-
 static double fbdelay_getlin(double tab[], unsigned int sz, double idx){
-    //copying from my own library, linear interpolated reader - DK
+    // linear interpolated reader, copied from Derek Kwan's library
     double output;
     unsigned int tabphase1 = (unsigned int)idx;
     unsigned int tabphase2 = tabphase1 + 1;
     double frac = idx - (double)tabphase1;
     if(tabphase1 >= sz - 1){
-        tabphase1 = sz - 1; //checking to see if index falls within bounds
+        tabphase1 = sz - 1; // checking to see if index falls within bounds
         output = tab[tabphase1];
     }
     else if(tabphase1 < 0){
@@ -101,10 +83,9 @@ static double fbdelay_getlin(double tab[], unsigned int sz, double idx){
         output = tab[tabphase1];
     }
     else{
-        double yb = tab[tabphase2]; //linear interp
+        double yb = tab[tabphase2]; // linear interp
         double ya = tab[tabphase1];
         output = ya+((yb-ya)*frac);
-        
     };
     return output;
 }
@@ -115,9 +96,8 @@ static double fbdelay_readmsdelay(t_fbdelay *x, double arr[], t_float ms){
     //eventual reading head
     double rh = (double)ms*((double)x->x_sr*0.001); //converting ms to samples
     //bounds checking for minimum delay in samples
-    if(rh < fbdelay_MIND){
-        rh = fbdelay_MIND;
-    };
+    if(rh < 1)
+        rh = 1;
     rh = (double)x->x_wh+((double)x->x_sz-rh); //essentially subracting from writehead to find proper position in buffer
     //wrapping into length of delay buffer
     while(rh >= x->x_sz){
@@ -126,45 +106,29 @@ static double fbdelay_readmsdelay(t_fbdelay *x, double arr[], t_float ms){
     //now to read from the buffer!
     double output = fbdelay_getlin(arr, x->x_sz, rh);
     return output;
-    
 }
 
-
-
-static t_int *fbdelay_perform(t_int *w)
-{
+static t_int *fbdelay_perform(t_int *w){
     t_fbdelay *x = (t_fbdelay *)(w[1]);
     int n = (int)(w[2]);
     t_float *xin = (t_float *)(w[3]);
     t_float *din = (t_float *)(w[4]);
     t_float *ain = (t_float *)(w[5]);
     t_float *out = (t_float *)(w[6]);
-    
     int i;
     for(i=0; i<n;i++){
         int wh = x->x_wh;
         double input = (double)xin[i];
-        //first off, write input to delay buf
-        x->x_xbuf[wh] = input;
-        //get delayed values of x and y
+        double output;
         t_float delms = din[i];
-        //first bounds checking
-        if(delms < 0){
-            delms = 0;
-        }
-        else if(delms > x->x_maxdel){
+        if(delms > x->x_maxdel)
             delms = x->x_maxdel;
-        };
-        double dely = fbdelay_readmsdelay(x, x->x_ybuf, delms);
-        double output = input + (double)ain[i] * dely;
-        //stick this guy in the ybuffer and output
+        double y_n = fbdelay_readmsdelay(x, x->x_ybuf, delms); // get delayed vals
+        output = input + (double)ain[i] * y_n;
         x->x_ybuf[wh] = output;
         out[i] = output;
-        
-        //increment writehead
-        x->x_wh = (wh + 1) % x->x_sz;
+        x->x_wh = (wh + 1) % x->x_sz; // increment writehead
     };
-    
     return (w + 7);
 }
 
@@ -172,125 +136,49 @@ static void fbdelay_dsp(t_fbdelay *x, t_signal **sp)
 {
     int sr = sp[0]->s_sr;
     if(sr != x->x_sr){
-        //if new sample rate isn't old sample rate, need to realloc
+        // if new sample rate isn't old sample rate, need to realloc
         x->x_sr = sr;
         fbdelay_sz(x);
     };
-    dsp_add(fbdelay_perform, 6, x, sp[0]->s_n,
-            sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec,
-            sp[3]->s_vec);
+    dsp_add(fbdelay_perform, 6, x, sp[0]->s_n, sp[0]->s_vec,
+            sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec);
 }
 
-static void fbdelay_list(t_fbdelay *x, t_symbol *s, int argc, t_atom * argv){
-    
-    
-    int argnum = 0; //current argument
-    while(argc){
-        if(argv -> a_type == A_FLOAT){
-            t_float curf = atom_getfloatarg(0, argc, argv);
-            switch(argnum){
-                case 0:
-                    //maxdel
-                    x->x_maxdel = curf > 0 ? curf : fbdelay_DELAY;
-                    fbdelay_sz(x);
-                    break;
-                case 1:
-                    //initdel
-                    if(curf < fbdelay_MINMS){
-                        curf = fbdelay_MINMS;
-                    }
-                    else if(curf > x->x_maxdel){
-                        curf = x->x_maxdel;
-                    };
-                    pd_float((t_pd *)x->x_dellet, curf);
-                    break;
-                case 2:
-                    //gain
-                    pd_float((t_pd *)x->x_alet, curf);
-                    break;
-                default:
-                    break;
-            };
-            argnum++;
-        };
-        argc--;
-        argv++;
-    };
-    
-    
-    
-}
-
-
-static void *fbdelay_new(t_symbol *s, int argc, t_atom * argv){
-    t_fbdelay *x = (t_fbdelay *)pd_new(fbdelay_class);
-    
-    //defaults
-    t_float maxdel = fbdelay_DELAY;
-    t_float initdel = fbdelay_MINMS;
-    t_float gain = fbdelay_DEFGAIN;
-    x->x_sr = sys_getsr();
-    
-    x->x_alloc = 0;
-    x->x_sz = fbdelay_STACK;
-    //clear out stack bufs, set pointer to stack
-    x->x_ybuf = x->x_fbstack;
-    x->x_xbuf = x->x_ffstack;
-    fbdelay_clear(x);
-    
-    int argnum = 0; //current argument
-    while(argc){
-        if(argv -> a_type == A_FLOAT){
-            t_float curf = atom_getfloatarg(0, argc, argv);
-            switch(argnum){
-                case 0:
-                    maxdel = curf;
-                    break;
-                case 1:
-                    initdel = curf;
-                    break;
-                case 2:
-                    gain = curf;
-                    break;
-                default:
-                    break;
-            };
-            argnum++;
-        };
-        argc--;
-        argv++;
-    };
-    
-    
-    x->x_maxdel = maxdel > 0 ? maxdel : fbdelay_DELAY;
-    //ship off to the helper method to deal with allocation if necessary
+static void fbdelay_size(t_fbdelay *x, t_floatarg f1){
+    if(f1 < 0)
+        f1 = 0;
+    x->x_maxdel = f1;
     fbdelay_sz(x);
-    //boundschecking
-    //this is 1/44.1 (1/(sr*0.001) rounded up, good enough?
+}
+
+static void *fbdelay_new(t_floatarg f1, t_floatarg f2){
+    t_fbdelay *x = (t_fbdelay *)pd_new(fbdelay_class);
+    x->x_sr = sys_getsr();
+    x->x_alloc = 0;
+    x->x_sz = FBDELAY_STACK;
+    // clear out stack buf, set pointer to stack
+    x->x_ybuf = x->x_fbstack;
+    fbdelay_clear(x);
+    if (f1 < 0)
+        f1 = 0;
+    x->x_maxdel = f1;
+    // ship off to the helper method to deal with allocation if necessary
+    fbdelay_sz(x);
+    // boundschecking
+    // this is 1/44.1 (1/(sr*0.001) rounded up, good enough?
     
-    if(initdel < fbdelay_MINMS){
-        initdel = fbdelay_MINMS;
-    }
-    else if(initdel > x->x_maxdel){
-        initdel = x->x_maxdel;
-    };
-    
-    //inlets outlets
+    // inlets / outlet
     x->x_dellet = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
-    pd_float((t_pd *)x->x_dellet, initdel);
+    pd_float((t_pd *)x->x_dellet, f1);
     x->x_alet = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
-    pd_float((t_pd *)x->x_alet, gain);
+    pd_float((t_pd *)x->x_alet, f2);
     x->x_outlet = outlet_new((t_object *)x, &s_signal);
     return (x);
 }
 
-
-
 static void * fbdelay_free(t_fbdelay *x){
-    if(x->x_alloc){
-        free(x->x_xbuf);
+    if(x->x_alloc)
         free(x->x_ybuf);
-    };
     inlet_free(x->x_dellet);
     inlet_free(x->x_alet);
     outlet_free(x->x_outlet);
@@ -299,13 +187,10 @@ static void * fbdelay_free(t_fbdelay *x){
 
 void fbdelay_tilde_setup(void)
 {
-    fbdelay_class = class_new(gensym("fbdelay~"),
-                              (t_newmethod)fbdelay_new,
-                              (t_method)fbdelay_free,
-                              sizeof(t_fbdelay), 0,
-                              A_GIMME, 0);
+    fbdelay_class = class_new(gensym("fbdelay~"), (t_newmethod)fbdelay_new,
+                             (t_method)fbdelay_free, sizeof(t_fbdelay), 0, A_DEFFLOAT, A_DEFFLOAT, 0);
     class_addmethod(fbdelay_class, nullfn, gensym("signal"), 0);
     class_addmethod(fbdelay_class, (t_method)fbdelay_dsp, gensym("dsp"), A_CANT, 0);
     class_addmethod(fbdelay_class, (t_method)fbdelay_clear, gensym("clear"), 0);
-    class_addlist(fbdelay_class, (t_method)fbdelay_list);
+    class_addmethod(fbdelay_class, (t_method)fbdelay_size, gensym("size"), A_DEFFLOAT, 0);
 }
