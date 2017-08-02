ee05f6750eb66367f42734aeeb6662dafbb22703
porres <porres@gmail.com>
2017-08-02 17:30:31 -0300

    xgate2~

--- classes/xgate2~.c
+++ classes/xgate2~.c
@@ -7,12 +7,12 @@
 
 typedef struct _xgate2
 {
-    t_object   x_obj;
-    t_float *x_ch_select; // main signal (channel selector)
-    int 	  x_n_outlets; // outlets excluding main signal
-    int 	  x_indexed; // outlets excluding main signal
-    t_float  **x_ovecs; // copying from matrix
-    t_float  *x_ivec; // single pointer since we're dealing with an array rather than an array of arrays
+    t_object    x_obj;
+    t_float     *x_ch_select; // main signal (channel selector)
+    int         x_n_outlets; // outlets excluding main signal
+    int         x_indexed; // outlets excluding main signal
+    t_float     **x_ovecs; // copying from matrix
+    t_float     *x_ivec; // single pointer since (an array rather than an array of arrays)
 } t_xgate2;
 
 static t_class *xgate2_class;
@@ -22,35 +22,48 @@ static void xgate2_index(t_xgate2 *x, t_floatarg f)
     x->x_indexed = f != 0;
 }
 
-static t_int *xgate2_perform(t_int *w)
-{
+static t_int *xgate2_perform(t_int *w){
     t_xgate2 *x = (t_xgate2 *)(w[1]);
     int nblock = (int)(w[2]);
     t_float *channel = x->x_ch_select;
     t_float **ovecs = x->x_ovecs;
     t_float *ivec = x->x_ivec;
-    t_float output;
+    int n_outlets = x->x_n_outlets;
     int max_sel = x->x_n_outlets - 1;
     int indexed = x->x_indexed;
-    int i;
+    int i, j;
     for(i = 0; i < nblock; i++){
- /*       float sel = channel[i];
-        if(!indexed)
+        t_float input = ivec[i];
+        t_float sel = channel[i];
+        if(!indexed) // default is indexed
             sel = channel[i] * max_sel;
         if(sel <= 0)
-            output = ovecs[0][i];
+            {
+            ovecs[0][i] = input;
+            for(j = 1; j < n_outlets; j++)
+                ovecs[j][i] = 0.0;
+            }
         else if(sel >= max_sel)
-            output = ovecs[max_sel][i];
-        else{
-            int ch = (int)sel;
-            float fade = (sel - ch) * HALF_PI;
-            float fadeL = fabs(sin(fade - HALF_PI)); // cos
-            float fadeR = sin(fade); // sin
-            float left = ovecs[ch][i] * fadeL; // cos
-            float right = ovecs[ch + 1][i] * fadeR; // sin
-            output = left + right;
-        }
-        ivec[i] = output; */
+            {
+            ovecs[max_sel][i] = input;
+            for(j = 0; j < max_sel; j++)
+                ovecs[j][i] = 0.0;
+            }
+
+        else
+            {
+            int ch = (int)sel;                          // selected output
+            float fade = (sel - ch) * HALF_PI;          // fade point
+            float fadeL = fabs(sin(fade - HALF_PI));    // cos fade value
+            float fadeR = sin(fade);                    // sin fade value
+            ovecs[ch][i] = input * fadeL; // cos
+            ovecs[ch + 1][i] = input * fadeR; // sin
+            for(j = 0; j < n_outlets; j++)
+                {
+                    if(j != ch && j != ch + 1)
+                        ovecs[j][i] = 0.0;
+                }
+            }
     };
     return (w + 3);
 }
