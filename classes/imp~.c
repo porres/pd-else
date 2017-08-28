b29209741d524c92aeae5de4c76cef1ab13faf0c
porres <porres@gmail.com>
2017-08-28 20:39:41 -0300

    magic 1

--- classes/imp~.c
+++ classes/imp~.c
@@ -2,6 +2,7 @@
 
 #include "m_pd.h"
 #include "math.h"
+#include "magic.h"
 
 static t_class *imp_class;
 
@@ -14,7 +15,65 @@ typedef struct _imp{
     t_inlet  *x_inlet_phase;
     t_outlet *x_outlet_dsp_0;
     t_float x_sr;
+// MAGIC:
+    t_glist *x_glist; // object list
+    t_float *x_signalscalar; // right inlet's float field
+    int x_hasfeeders; // right inlet connection flag
+    t_float  x_phase_sync_float; // float from magic
 } t_imp;
+
+
+static t_int *imp_perform_magic(t_int *w){
+    t_imp *x = (t_imp *)(w[1]);
+    int nblock = (t_int)(w[2]);
+    t_float *in1 = (t_float *)(w[3]); // freq
+    t_float *in2 = (t_float *)(w[4]); // sync
+    t_float *in3 = (t_float *)(w[5]); // phase
+    t_float *out1 = (t_float *)(w[6]);
+    double phase = x->x_phase;
+    double last_phase_offset = x->x_last_phase_offset;
+    double sr = x->x_sr;
+    while (nblock--){
+        double hz = *in1++;
+        double trig = *in2++;
+        double phase_offset = *in3++;
+        double phase_step = hz / sr; // phase_step
+        phase_step = phase_step > 1 ? 1. : phase_step < -1 ? -1 : phase_step; // clipped phase_step
+        double phase_dev = phase_offset - last_phase_offset;
+        if (phase_dev >= 1 || phase_dev <= -1)
+            phase_dev = fmod(phase_dev, 1); // fmod(phase_dev)
+        if (hz >= 0){
+            if (trig > 0 && trig <= 1) phase = trig;
+            else{
+                phase = phase + phase_dev;
+                if (phase_dev != 0 && phase <= 0)
+                    phase = phase + 1.; // wrap deviated phase
+            }
+            *out1++ = phase >= 1.;
+            if (phase >= 1.)
+                phase = phase - 1; // wrapped phase
+        }
+        else{
+            if (trig > 0 && trig < 1)
+                phase = trig;
+            else if (trig == 1)
+                phase = 0;
+            else{
+                phase = phase + phase_dev;
+                if (phase >= 1) phase = phase - 1.; // wrap deviated phase
+            }
+            *out1++ = phase <= 0.;
+            if (phase <= 0.)
+                phase = phase + 1.; // wrapped phase
+        }
+        phase = phase + phase_step; // next phase
+        last_phase_offset = phase_offset; // last phase offset
+    }
+    x->x_phase = phase;
+    x->x_last_phase_offset = last_phase_offset;
+    return (w + 7);
+}
+
 
 static t_int *imp_perform(t_int *w){
     t_imp *x = (t_imp *)(w[1]);
@@ -68,14 +127,21 @@ static t_int *imp_perform(t_int *w){
 }
 
 static void imp_dsp(t_imp *x, t_signal **sp){
+    x->x_hasfeeders = magic_inlet_connection((t_object *)x, x->x_glist, 1, &s_signal); // magic feeder flag
     x->x_sr = sp[0]->s_sr;
-    dsp_add(imp_perform, 6, x, sp[0]->s_n,
+    if (x->x_hasfeeders){
+        dsp_add(imp_perform, 6, x, sp[0]->s_n,
             sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec);
+    }
+    else{
+        dsp_add(imp_perform, 6, x, sp[0]->s_n,
+                sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec);
+    }
 }
 
 static void *imp_free(t_imp *x){
-    inlet_free(x->x_inlet_phase);
     inlet_free(x->x_inlet_sync);
+    inlet_free(x->x_inlet_phase);
     outlet_free(x->x_outlet_dsp_0);
     return (void *)x;
 }
@@ -95,6 +161,9 @@ static void *imp_new(t_floatarg f1, t_floatarg f2){
     x->x_inlet_phase = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
         pd_float((t_pd *)x->x_inlet_phase, init_phase);
     x->x_outlet_dsp_0 = outlet_new(&x->x_obj, &s_signal);
+// Magic
+    x->x_glist = canvas_getcurrent();
+    x->x_signalscalar = obj_findsignalscalar((t_object *)x, 1);
     return (x);
 }
 
