5080feb0c521c8a54d11cde5ed31cb8ad2b6b4be
porres <porres@gmail.com>
2017-08-28 21:51:37 -0300

    triangular~

--- classes/triangular2~.c
+++ classes/triangular2~.c
@@ -3,9 +3,9 @@
 #include "m_pd.h"
 #include "math.h"
 
-static t_class *triwave_class;
+static t_class *triangular_class;
 
-typedef struct _triwave
+typedef struct _triangular
 {
     t_object x_obj;
     double  x_phase;
@@ -16,11 +16,11 @@ typedef struct _triwave
     t_inlet  *x_inlet_sync;
     t_outlet *x_outlet;
     t_float x_sr;
-} t_triwave;
+} t_triangular;
 
-static t_int *triwave_perform(t_int *w)
+static t_int *triangular_perform(t_int *w)
 {
-    t_triwave *x = (t_triwave *)(w[1]);
+    t_triangular *x = (t_triangular *)(w[1]);
     int nblock = (t_int)(w[2]);
     t_float *in1 = (t_float *)(w[3]); // freq
     t_float *in2 = (t_float *)(w[4]); // phase
@@ -61,23 +61,20 @@ static t_int *triwave_perform(t_int *w)
     return (w + 7);
 }
 
-static void triwave_dsp(t_triwave *x, t_signal **sp)
-{
-    dsp_add(triwave_perform, 6, x, sp[0]->s_n,
+static void triangular_dsp(t_triangular *x, t_signal **sp){
+    dsp_add(triangular_perform, 6, x, sp[0]->s_n,
             sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec);
 }
 
-static void *triwave_free(t_triwave *x)
-{
+static void *triangular_free(t_triangular *x){
     inlet_free(x->x_inlet_phase);
     inlet_free(x->x_inlet_sync);
     outlet_free(x->x_outlet);
     return (void *)x;
 }
 
-static void *triwave_new(t_floatarg f1, t_floatarg f2)
-{
-    t_triwave *x = (t_triwave *)pd_new(triwave_class);
+static void *triangular_new(t_floatarg f1, t_floatarg f2){
+    t_triangular *x = (t_triangular *)pd_new(triangular_class);
     
     t_float init_freq = f1;
     
@@ -100,11 +97,10 @@ static void *triwave_new(t_floatarg f1, t_floatarg f2)
     return (x);
 }
 
-void triwave_tilde_setup(void)
-{
-    triwave_class = class_new(gensym("triwave~"),
-        (t_newmethod)triwave_new, (t_method)triwave_free,
-        sizeof(t_triwave), CLASS_DEFAULT, A_DEFFLOAT, A_DEFFLOAT, 0);
-    CLASS_MAINSIGNALIN(triwave_class, t_triwave, x_freq);
-    class_addmethod(triwave_class, (t_method)triwave_dsp, gensym("dsp"), A_CANT, 0);
+void triangular_tilde_setup(void){
+    triangular_class = class_new(gensym("triangular~"),
+        (t_newmethod)triangular_new, (t_method)triangular_free,
+        sizeof(t_triangular), CLASS_DEFAULT, A_DEFFLOAT, A_DEFFLOAT, 0);
+    CLASS_MAINSIGNALIN(triangular_class, t_triangular, x_freq);
+    class_addmethod(triangular_class, (t_method)triangular_dsp, gensym("dsp"), A_CANT, 0);
 }
