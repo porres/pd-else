af3856163ecce6e69c64c2b18b41b6a7338090e3
porres <porres@gmail.com>
2017-10-16 18:32:40 -0200

    fbsine~ magic

--- classes/fbsine~.c
+++ classes/fbsine~.c
@@ -21,6 +21,11 @@ typedef struct _fbsine
     t_inlet  *x_inlet_sync;
     t_outlet *x_outlet;
     t_float x_sr;
+// MAGIC:
+    t_glist *x_glist; // object list
+    t_float *x_signalscalar; // right inlet's float field
+    int x_hasfeeders; // right inlet connection flag
+    t_float  x_phase_sync_float; // float from magic
 } t_fbsine;
 
 static void fbsine_filter(t_fbsine *x, t_floatarg f)
@@ -34,8 +39,59 @@ static t_int *fbsine_perform(t_int *w)
     int nblock = (t_int)(w[2]);
     t_float *in1 = (t_float *)(w[3]); // freq
     t_float *in2 = (t_float *)(w[4]); // fb
-    t_float *in3 = (t_float *)(w[5]); // phase
-    t_float *in4 = (t_float *)(w[6]); // sync
+    t_float *in3 = (t_float *)(w[5]); // sync
+    t_float *in4 = (t_float *)(w[6]); // phase
+    t_float *out = (t_float *)(w[7]);
+// Magic Start
+    t_float *scalar = x->x_signalscalar;
+    if (!magic_isnan(*x->x_signalscalar)){
+        t_float input_phase = fmod(*scalar, 1);
+        if (input_phase < 0)
+            input_phase += 1;
+        x->x_phase = input_phase;
+        magic_setnan(x->x_signalscalar);
+    }
+// Magic End
+    float yn_m1 = x->x_yn_m1;
+    float yn_m2 = x->x_yn_m2;
+    double phase = x->x_phase;
+    float last_phase_offset = x->x_last_phase_offset;
+    float sr = x->x_sr;
+    while (nblock--){
+        float hz = *in1++;
+        float index = *in2++;
+        float phase_offset = *in4++;
+        float fback;
+        float output;
+        float phase_dev = phase_offset - last_phase_offset;
+        phase += (double)phase_dev;
+        if(x->x_filter)
+            fback = ((yn_m1 + yn_m2) * 0.5) * index; // filter
+        else
+            fback = yn_m1 * index; // no filter
+        float radians = (phase + fback) * TWOPI;
+        output = sin(radians);
+        *out++ = output;
+        phase += (double)(hz / sr); // next phase
+        last_phase_offset = phase_offset; // last phase offset
+        yn_m2 = yn_m1;
+        yn_m1 = output;
+    }
+    x->x_yn_m1 = yn_m1; // last out
+    x->x_yn_m2 = yn_m2; // last out
+    x->x_phase = fmod(phase, 1); // next wrapped phase
+    x->x_last_phase_offset = last_phase_offset;
+    return (w + 8);
+}
+
+static t_int *fbsine_perform_sig(t_int *w)
+{
+    t_fbsine *x = (t_fbsine *)(w[1]);
+    int nblock = (t_int)(w[2]);
+    t_float *in1 = (t_float *)(w[3]); // freq
+    t_float *in2 = (t_float *)(w[4]); // fb
+    t_float *in3 = (t_float *)(w[5]); // sync
+    t_float *in4 = (t_float *)(w[6]); // phase
     t_float *out = (t_float *)(w[7]);
     float yn_m1 = x->x_yn_m1;
     float yn_m2 = x->x_yn_m2;
@@ -45,8 +101,8 @@ static t_int *fbsine_perform(t_int *w)
     while (nblock--){
         float hz = *in1++;
         float index = *in2++;
-        float phase_offset = *in3++;
-        float trig = *in4++;
+        float trig = *in3++;
+        float phase_offset = *in4++;
         float fback;
         float output;
         float phase_dev = phase_offset - last_phase_offset;
@@ -74,10 +130,18 @@ static t_int *fbsine_perform(t_int *w)
 
 static void fbsine_dsp(t_fbsine *x, t_signal **sp)
 {
+    x->x_hasfeeders = magic_inlet_connection((t_object *)x, x->x_glist, 2, &s_signal); // magic feeder flag
     x->x_sr = sp[0]->s_sr;
-    dsp_add(fbsine_perform, 7, x, sp[0]->s_n, sp[0]->s_vec,
+    if (x->x_hasfeeders){
+        dsp_add(fbsine_perform_sig, 7, x, sp[0]->s_n, sp[0]->s_vec,
             sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec, sp[4]->s_vec);
+    }
+    else{
+        dsp_add(fbsine_perform, 7, x, sp[0]->s_n, sp[0]->s_vec,
+                sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec, sp[4]->s_vec);
+    }
 }
+
 
 static void *fbsine_free(t_fbsine *x)
 {
@@ -123,6 +187,9 @@ static void *fbsine_new(t_symbol *s, int ac, t_atom *av)
         pd_float((t_pd *)x->x_inlet_sync, 0);
     x->x_outlet = outlet_new(&x->x_obj, &s_signal);
     return (x);
+// Magic
+    x->x_glist = canvas_getcurrent();
+    x->x_signalscalar = obj_findsignalscalar((t_object *)x, 2);
 }
 
 void fbsine_tilde_setup(void)
