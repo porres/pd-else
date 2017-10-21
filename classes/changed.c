9d23421c6176c37f2253ff772ee7255df745febd
porres <porres@gmail.com>
2017-10-21 19:48:27 -0200

    still going through changes

--- classes/changed.c
+++ classes/changed.c
@@ -17,11 +17,6 @@ static void changed_list(t_changed *x, t_symbol *s, int argc, t_atom *argv){
     int c = argc;
     if(c == x->x_c)	// same number of elements
         for (i = 0; i < c; i++){
-            //	if(x->x_a[i].a_type != argv[i].a_type)
-            //	{
-            //		change = 1;
-            //		break;
-            //	}
             if (x->x_a[i].a_type == A_FLOAT){
                 if (argv[i].a_type != A_FLOAT || x->x_a[i].a_w.w_float != argv[i].a_w.w_float){
                     change = 1;
@@ -36,7 +31,7 @@ static void changed_list(t_changed *x, t_symbol *s, int argc, t_atom *argv){
             }
         }
     else
-        change = 1;	// different number of elems.
+        change = 1;	// different number of elements
     if (change){
         x->x_c = c;
         for (i = 0; i < c; i++){	// same new list
@@ -50,15 +45,12 @@ static void changed_bang(t_changed *x){
      outlet_list(x->x_obj.ob_outlet, NULL, x->x_c, x->x_a);
  }
 
-/*
- static void changed_float(t_changed *x, t_float f){
- if (f != x->x_a->a_w.w_float){
- SETFLOAT(x->x_a, f);
- outlet_float(x->x_obj.ob_outlet, x->x_a->a_w.w_float);
- }
+static void changed_float(t_changed *x, t_float f){
+    SETFLOAT(x->x_a, f);
+    outlet_float(x->x_obj.ob_outlet, x->x_a->a_w.w_float);
  }
 
- 
+/*
 static void changed_symbol(t_changed *x, t_symbol *s)
 {
 	if (x->x_type == A_SYMBOL)
@@ -94,7 +86,7 @@ static void *changed_new(t_symbol *s, int argc, t_atom *argv){
 void changed_setup(void){
     changed_class = class_new(gensym("changed"), (t_newmethod)changed_new, 0,
     	sizeof(t_changed), 0, A_GIMME, 0);
-//    class_addbang(changed_class, changed_bang);
+    class_addbang(changed_class, changed_bang);
 //    class_addfloat(changed_class, changed_float);
 //    class_addsymbol(changed_class, changed_symbol);
 //    class_addlist(changed_class, changed_list);
