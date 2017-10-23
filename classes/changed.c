7668fd3847e6c1b976caa7124e37842856e86fd1
porres <porres@gmail.com>
2017-10-22 22:59:40 -0200

    changed for better

--- classes/changed.c
+++ classes/changed.c
@@ -7,8 +7,10 @@ static t_class *changed_class;
 typedef struct _changed{
     t_object x_obj;
     t_atom   x_a[4096];
-	int      x_c;
+    int      x_c;
+    int      x_change;
     t_symbol *x_sym;
+    t_outlet  *x_bang_out;
 } t_changed;
 
 static void changed_bang(t_changed *x){
@@ -16,39 +18,55 @@ static void changed_bang(t_changed *x){
 }
 
 static void changed_anything(t_changed *x, t_symbol *s, int argc, t_atom *argv){
-    int i, change = 0;
+    int i;
     int c = argc;
     x->x_sym = s;
     if(c == x->x_c)	// same number of elements
         for (i = 0; i < c; i++){
             if (x->x_a[i].a_type == A_FLOAT){
-                if (argv[i].a_type != A_FLOAT || x->x_a[i].a_w.w_float != argv[i].a_w.w_float){
-                    change = 1;
-                    break;
-                }
+                if (argv[i].a_type != A_FLOAT || x->x_a[i].a_w.w_float != argv[i].a_w.w_float)
+                    x->x_change = 1;
+                break;
             }
             else if (x->x_a[i].a_type == A_SYMBOL){
-                if (argv[i].a_type != A_SYMBOL || x->x_a[i].a_w.w_symbol != argv[i].a_w.w_symbol){
-                    change = 1;
-                    break;
-                }
+                if (argv[i].a_type != A_SYMBOL || x->x_a[i].a_w.w_symbol != argv[i].a_w.w_symbol)
+                    x->x_change = 1;
+                break;
             }
         }
     else
-        change = 1;	// different number of elements
-    if (change){
+        x->x_change = 1;	// different number of elements
+    if (x->x_change){
         x->x_c = c;
         for (i = 0; i < c; i++) // same new list
             x->x_a[i] = argv[i];
         outlet_anything(x->x_obj.ob_outlet, s, argc, argv);
+        x->x_change = 0;
     }
-    if(!change)
-        post("didn't change");
+    else
+        outlet_bang(x->x_bang_out);
 }
 
 static void changed_set(t_changed *x, t_symbol *s, int argc, t_atom *argv){
     int i;
-//    x->x_sym = s; /////////////////////////////////////////// BUG????
+    if(argc == 0) // NO ARGUMENTS
+        x->x_sym = &s_bang; ///////////////////////////// change to empty symbol?
+    else if(argc == 1){ // 1 ARGUMENT
+        if ((argv)->a_type == A_SYMBOL){
+            x->x_sym = atom_getsymbol(argv);
+            argc--;
+        }
+        else if ((argv)->a_type == A_FLOAT)
+            x->x_sym = &s_float;
+    }
+    else{ // ARGUMENTS >= 2
+        if((argv)->a_type == A_SYMBOL){
+            x->x_sym = atom_getsymbol(argv++);
+            argc--;
+        }
+        else
+            x->x_sym = &s_list;
+    }
     x->x_c = argc;
     for (i = 0; i < argc; i++)
         x->x_a[i] = argv[i];
@@ -60,8 +78,10 @@ static void *changed_new(t_symbol *s, int argc, t_atom *argv){
     if(argc == 0) // NO ARGUMENTS
         x->x_sym = &s_bang; ///////////////////////////// change to empty symbol?
     else if(argc == 1){ // 1 ARGUMENT
-        if ((argv)->a_type == A_SYMBOL)
-            x->x_sym = atom_getsymbol(argv); /////////////////////////// BUG?????
+        if ((argv)->a_type == A_SYMBOL){
+            x->x_sym = atom_getsymbol(argv);
+            argc--; 
+        }
         else if ((argv)->a_type == A_FLOAT)
                 x->x_sym = &s_float;
     }
@@ -76,7 +96,9 @@ static void *changed_new(t_symbol *s, int argc, t_atom *argv){
     x->x_c = argc;
     for (i = 0; i < argc; i++)
         x->x_a[i] = argv[i];
+    x->x_change = 0;
     outlet_new(&x->x_obj, &s_anything);
+    x->x_bang_out = outlet_new(&x->x_obj, &s_bang);
     return (x);
 }
 
