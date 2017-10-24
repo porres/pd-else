86c64c41421bf7b8b2aecdb5cb851aa246322a40
porres <porres@gmail.com>
2017-10-24 18:03:34 -0200

    breaking my mind

--- classes/break.c
+++ classes/break.c
@@ -7,7 +7,6 @@ typedef struct _break{
   t_object     	x_obj;
   int 			x_break;
   char			x_separator;
-  t_symbol      *x_sym;
 } t_break;
 
 static int next_separator(char tag, int ac, t_atom *av, int* ac_a, t_atom ** av_a, int* iter){
@@ -32,14 +31,14 @@ static void break_anything(t_break *x, t_symbol *s, int argc, t_atom *argv){
         int ac_a, iter = 0;
         t_atom* av_a;
         while (next_separator(x->x_separator, argc, argv, &ac_a, &av_a, &iter)){
-            if (!ac_a)
+            if (ac_a == 0)
                 outlet_bang(x->x_obj.ob_outlet);
             else if ((av_a)->a_type == A_SYMBOL)
                 outlet_anything(x->x_obj.ob_outlet, atom_getsymbol(av_a), ac_a - 1, av_a + 1);
             else if (((av_a)->a_type == A_FLOAT) && ac_a == 1)
                 outlet_float(x->x_obj.ob_outlet, atom_getfloat(av_a));
             else
-                outlet_anything(x->x_obj.ob_outlet, &s_list, ac_a, av_a);
+                outlet_anything(x->x_obj.ob_outlet, s, ac_a, av_a);
         }
     }
     else
