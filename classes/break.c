e7cc348ee29234f5b57a5b269188c2a6902325f3
porres <porres@gmail.com>
2017-10-24 19:07:43 -0200

    no need for this either

--- classes/break.c
+++ classes/break.c
@@ -11,7 +11,7 @@ typedef struct _break{
 
 static int next_separator(char tag, int ac, t_atom *av, int* ac_a, t_atom ** av_a, int* iter){
 	int j;
-    if (*iter >= ac){
+    if (*iter >= ac){ // End While!
 		*ac_a = 0;
 		*av_a = NULL;
 		return 0;
@@ -31,9 +31,7 @@ static void break_anything(t_break *x, t_symbol *s, int argc, t_atom *argv){
         int ac_a, iter = 0;
         t_atom* av_a;
         while (next_separator(x->x_separator, argc, argv, &ac_a, &av_a, &iter)){
-            if (ac_a == 0)
-                outlet_bang(x->x_obj.ob_outlet);
-            else if ((av_a)->a_type == A_SYMBOL)
+            if ((av_a)->a_type == A_SYMBOL)
                 outlet_anything(x->x_obj.ob_outlet, atom_getsymbol(av_a), ac_a - 1, av_a + 1);
             else if (((av_a)->a_type == A_FLOAT) && ac_a == 1)
                 outlet_float(x->x_obj.ob_outlet, atom_getfloat(av_a));
