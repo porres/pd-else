fd193c17884d59c64603bc66249ddc77fd370d3f
porres <porres@gmail.com>
2017-10-26 01:39:08 -0200

    cosmetics break

--- classes/break.c
+++ classes/break.c
@@ -1,3 +1,4 @@
+// porres 2017
 
 #include "m_pd.h"
 
@@ -7,13 +8,11 @@ typedef struct _break{
   t_object     	x_obj;
   int 			x_break;
   char			x_separator;
-} t_break;
+}t_break;
 
 static void break_anything(t_break *x, t_symbol *s, int argc, t_atom *argv){
     if (x->x_break){
-        int i = 0;
-        int first = 1;
-        int ac_break;
+        int i = 0, first = 1, ac_break;
         while(i < argc){
             int j;
             for (j = i + 1; j < argc; j++)
@@ -33,18 +32,17 @@ static void break_anything(t_break *x, t_symbol *s, int argc, t_atom *argv){
         outlet_anything(x->x_obj.ob_outlet, s, argc, argv);
 }
 
-static void *break_new(t_symbol *selector, int argc, t_atom* argv) {
+static void *break_new(t_symbol *selector, int argc, t_atom* argv){
   t_break *x = (t_break *)pd_new(break_class);
   if(argc && ((argv)->a_type == A_SYMBOL)){
         x->x_separator = atom_getsymbol(argv)->s_name[0];
         x->x_break = 1;
         }
-   outlet_new(&x->x_obj, &s_anything);
+  outlet_new(&x->x_obj, &s_anything);
   return (x);
 }
 
-void break_setup(void) {
-  break_class = class_new(gensym("break"), (t_newmethod)break_new, 0,
-                          sizeof(t_break), 0, A_GIMME, 0);
+void break_setup(void){
+  break_class = class_new(gensym("break"), (t_newmethod)break_new, 0, sizeof(t_break), 0, A_GIMME, 0);
   class_addanything(break_class, break_anything);
 }
