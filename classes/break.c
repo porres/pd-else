c541f1f74766d3294144a18b7010f3792a8ad17f
porres <porres@gmail.com>
2017-10-26 20:21:10 -0200

    about to try again

--- classes/break.c
+++ classes/break.c
@@ -10,32 +10,32 @@ typedef struct _break{
   char			x_separator;
 }t_break;
 
-static void break_anything(t_break *x, t_symbol *s, int argc, t_atom *argv){
+static void break_anything(t_break *x, t_symbol *s, int ac, t_atom *av){
     if (x->x_break){
         int i = 0, first = 1, ac_break;
-        while(i < argc){
-            int j = i + 1; 
-            for (j; j < argc; j++)
-                if ((argv+j)->a_type == A_SYMBOL && x->x_separator == (atom_getsymbol(argv+j))->s_name[0])
+        while(i < ac){
+            int j = i + 1; // j = i
+            for (j; j < ac; j++) // i++; i < ac; i++
+                if ((av+j)->a_type == A_SYMBOL && x->x_separator == (atom_getsymbol(av+j))->s_name[0])
                     break;
-            ac_break = j - i;
+            ac_break = j - i; // i - j
             if(first){
-                outlet_anything(x->x_obj.ob_outlet, s, ac_break, argv + i);
+                outlet_anything(x->x_obj.ob_outlet, s, ac_break, av + i);
                 first = 0;
             }
             else
-                outlet_anything(x->x_obj.ob_outlet, atom_getsymbol(argv + i), ac_break - 1, argv + i + 1);
+                outlet_anything(x->x_obj.ob_outlet, atom_getsymbol(av + i), ac_break - 1, av + i + 1);
             i = j;
         }
     }
     else
-        outlet_anything(x->x_obj.ob_outlet, s, argc, argv);
+        outlet_anything(x->x_obj.ob_outlet, s, ac, av);
 }
 
-static void *break_new(t_symbol *selector, int argc, t_atom* argv){
+static void *break_new(t_symbol *selector, int ac, t_atom* av){
   t_break *x = (t_break *)pd_new(break_class);
-  if(argc && ((argv)->a_type == A_SYMBOL)){
-        x->x_separator = atom_getsymbol(argv)->s_name[0];
+  if(ac && ((av)->a_type == A_SYMBOL)){
+        x->x_separator = atom_getsymbol(av)->s_name[0];
         x->x_break = 1;
         }
   outlet_new(&x->x_obj, &s_anything);
