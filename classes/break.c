09e4c100dd052e746389d557c1de7a88cd1b9d25
porres <porres@gmail.com>
2017-10-27 13:34:10 -0200

    all bugs broken - need to delete posts only

--- classes/break.c
+++ classes/break.c
@@ -10,35 +10,47 @@ typedef struct _break{
   char			x_separator;
 }t_break;
 
+static void break_float(t_break *x, t_float f){
+    post("FLOAT");
+    outlet_float(x->x_obj.ob_outlet, f);
+}
+
+static void break_symbol(t_break *x, t_symbol *s){
+    post("Symbol");
+    outlet_symbol(x->x_obj.ob_outlet, s);
+}
+
 static void break_anything(t_break *x, t_symbol *s, int ac, t_atom *av){
+    post("ac = %d", ac);
     if (x->x_break){
         if(!ac)
             outlet_anything(x->x_obj.ob_outlet, s, ac, av);
         else{
-            int i = -1, first = 1, ac_break;
+            int i = -1, first = 1, n;
             while(i < ac){
+// i = starting point and j is broken item
                 int j = i + 1;
-//                post("i = %d / j = %d (i + 1)", i, j);
+// j starts as next item from previous iteration (and as 0 in the first iteration)
+                post("i = %d / j = %d (i + 1)", i, j);
                 for (j; j < ac; j++){
-//                    post("j = %d", j);
+                    post("for j = %d", j);
                     if ((av+j)->a_type == A_SYMBOL && x->x_separator == (atom_getsymbol(av+j))->s_name[0]){
-                      //  post("break");
+                        post("break");
                         break;
                     }
                 }
-                ac_break = j - i - 1;
-               // post("i = %d / j = %d, ac_break = %d (j - i - 1)", i, j, ac_break);
-                if(first){ // start here and counting from 0;
-                    if(j == 0)
-                        outlet_anything(x->x_obj.ob_outlet, s, ac_break, av - 1);
-                    else
-                        outlet_anything(x->x_obj.ob_outlet, s, ac_break, av);
+// n is number of extra elements in the broken message (that's why we have - 1)
+                n = j - i - 1;
+                post("i = %d / j = %d, n = %d (j - i - 1)", i, j, n);
+                if(first){
+                    if(n == 0) // it's a selector
+                        outlet_anything(x->x_obj.ob_outlet, s, n, av - 1); // output selector
                     first = 0;
-                }
+                    }
                 else
-                    outlet_anything(x->x_obj.ob_outlet, atom_getsymbol(av + i), ac_break, av + i + 1);
+                    outlet_anything(x->x_obj.ob_outlet, atom_getsymbol(av + i), n, av + i + 1);
                 i = j;
-             //   post("i = j = %d", i);
+                post("i = j = %d", i);
             }
         }
     }
@@ -58,5 +70,7 @@ static void *break_new(t_symbol *selector, int ac, t_atom* av){
 
 void break_setup(void){
   break_class = class_new(gensym("break"), (t_newmethod)break_new, 0, sizeof(t_break), 0, A_GIMME, 0);
-  class_addanything(break_class, break_anything);
+    class_addanything(break_class, break_anything);
+    class_addsymbol(break_class, break_symbol);
+    class_addfloat(break_class, break_float);
 }
