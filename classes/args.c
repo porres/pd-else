497352bfcbd6cdf87034bf2ff9696f290de6d39c
porres <porres@gmail.com>
2017-10-27 21:28:48 -0200

    whole new args

--- classes/args.c
+++ classes/args.c
@@ -1,47 +1,55 @@
 #include "m_pd.h"
-#include "g_canvas.h"
+
+#define IS_A_SYMBOL(atom,index) ((atom+index)->a_type == A_SYMBOL)
+#define IS_A_FLOAT(atom,index) ((atom+index)->a_type == A_FLOAT)
 
 static t_class *args_class;
 
 typedef struct _args{
-  t_object  x_obj;
-  t_canvas  *x_canvas;
+  t_object                  	x_obj;
+  t_outlet*			       	 	outlet;
+  t_canvas*						canvas;
+  int                           size;
+  int                           ac;
+  t_atom*                       av;
 } t_args;
 
+// Prepends the proper selector
+static void args_output(t_outlet* o, int argc, t_atom* argv) {
+		if (!argc) outlet_bang(o);
+		else if (IS_A_SYMBOL(argv,0)) outlet_anything(o,atom_getsymbol(argv),argc-1,argv+1);
+		else if (IS_A_FLOAT(argv,0) && argc==1) outlet_float(o,atom_getfloat(argv));
+		else outlet_anything(o,&s_list,argc,argv);
+}
+
+// Dump args
 static void args_bang(t_args *x){
-  int argc = 0;
-  t_atom*argv = 0;
-  t_binbuf*b = 0;
-  if(!x->x_canvas)
-      return;
-  b = x->x_canvas->gl_obj.te_binbuf;
-  if(b){
-    argc = binbuf_getnatom(b) - 1;
-    argv = binbuf_getvec(b) + 1;
-  }
-  else{
-    canvas_setcurrent(x->x_canvas);
-    canvas_getargs(&argc, &argv);
-    canvas_unsetcurrent(x->x_canvas);
-  }
-  if(argv)
-    outlet_list(x->x_obj.ob_outlet, &s_list, argc, argv);
+    args_output(x->outlet, x->ac, x->av);
 }
 
 static void args_free(t_args *x){
-  x->x_canvas = 0;
+    freebytes(x->av,x->ac*sizeof(*(x->av)));
 }
 
-static void *args_new(void){
+static void copy_atoms(t_atom *src, t_atom *dst, int n){
+  while(n--)
+    *dst++ = *src++;
+}
+
+static void *args_new(t_symbol *selector, int argc, t_atom* argv) {
   t_args *x = (t_args *)pd_new(args_class);
-  t_glist *glist=(t_glist *)canvas_getcurrent();
-  x->x_canvas = (t_canvas*)glist_getcanvas(glist);
-  outlet_new(&x->x_obj, 0);
+  int ac;
+  t_atom* av;
+  canvas_getargs(&ac, &av);
+   x->ac = ac;
+   x->av = getbytes(x->ac * sizeof(*(x->av)));
+   copy_atoms(av, x->av, x->ac);
+   x->outlet = outlet_new(&x->x_obj, &s_anything);
   return (x);
 }
 
-void args_setup(void){
-  args_class = class_new(gensym("args"), (t_newmethod)args_new,
-        (t_method)args_free, sizeof(t_args), 0, 0);
-  class_addbang(args_class, (t_method)args_bang);
+void args_setup(void) {
+  args_class = class_new(gensym("args"), (t_newmethod)args_new, (t_method)args_free,
+    sizeof(t_args), 0, A_GIMME, 0);
+  class_addbang(args_class, args_bang);
 }
