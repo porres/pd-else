3dea88965c561b4750b4f584b5e8f75e6c014121
porres <porres@gmail.com>
2017-10-30 22:44:48 -0200

    welcome past

--- classes/past~.c
+++ classes/past~.c
@@ -1,4 +1,4 @@
-// Porres 2016
+// Porres 2017
 
 #include "m_pd.h"
 
@@ -16,25 +16,22 @@ static t_int *past_perform(t_int *w){
     int nblock = (t_int)(w[2]);
     t_float *in1 = (t_float *)(w[3]);
     t_float *in2 = (t_float *)(w[4]);
-    t_float *out = (t_float *)(w[5]);
+    t_float *out1 = (t_float *)(w[5]);
+    t_float *out2 = (t_float *)(w[6]);
     t_float lastin = x->x_lastin;
     while (nblock--){
         float input = *in1++;
         float threshold = *in2++;
-        float output;
-        if(input > threshold && lastin <= threshold)
-            output = 1;
-        else
-            output = 0;
-        *out++ = output;
+        *out1++ = input > threshold && lastin <= threshold;
+        *out2++ = input <= threshold && lastin > threshold;
         lastin = input;
     }
     x->x_lastin = lastin;
-    return (w + 6);
+    return (w + 7);
 }
 
 static void past_dsp(t_past *x, t_signal **sp){
-    dsp_add(past_perform, 5, x, sp[0]->s_n, sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec);
+    dsp_add(past_perform, 6, x, sp[0]->s_n, sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec);
 }
 
 static void *past_free(t_past *x){
@@ -47,6 +44,7 @@ static void *past_new(t_floatarg f){
     x->x_lastin = 0;
     x->x_thresh_inlet = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
         pd_float((t_pd *)x->x_thresh_inlet, f);
+    outlet_new((t_object *)x, &s_signal);
     outlet_new((t_object *)x, &s_signal);
     return (x);
 }
