510b2db5beae9f1dd70f6ddb9b8c4427cadeef5c
porres <porres@gmail.com>
2017-12-02 15:09:03 -0200

    dir seek, n

--- classes/dir.c
+++ classes/dir.c
@@ -5,6 +5,7 @@
 #include "m_pd.h"
 #include "g_canvas.h"
 #include <string.h>
+#include <math.h>
 
 static t_class *dir_class;
 
@@ -13,32 +14,83 @@ typedef struct dir{
     DIR      *x_dir;
     char      x_directory[MAXPDSTRING];
     t_symbol *x_getdir;
+    t_int     x_nfiles;
+    t_int     x_ignored;
+    t_outlet *x_out1;
+    t_outlet *x_out2;
 }t_dir;
 
-static void dir_open(t_dir *x, t_symbol *dirname){
-    if(!opendir(dirname->s_name))
-        pd_error(x, "filedir: cannot open directory: %s", dirname->s_name);
-    else{
-        strncpy(x->x_directory, dirname->s_name, MAXPDSTRING);
-        x->x_dir = opendir(dirname->s_name);
+static void dir_seek(t_dir *x, t_float f){
+    t_int seek = (int)f;
+    if(seek <= 0){
+        pd_error(x, "dir: seek value cannot be <= 0");
+        return;
     }
+    seek = ((seek - 1) % x->x_nfiles) + 1;
+    seek += x->x_ignored;
+    rewinddir(x->x_dir);
+    struct dirent *result = NULL;
+    t_int i;
+    for(i = 0; i < seek; i++)
+            result = readdir(x->x_dir);
+    outlet_symbol(x->x_out1, gensym(result->d_name));
+    rewinddir(x->x_dir);
+}
+
+static void dir_open(t_dir *x, t_symbol *dirname){
+    if(!opendir(dirname->s_name)){
+        pd_error(x, "filedir: cannot open directory: %s", dirname->s_name);
+        return;
+    }
+    strncpy(x->x_directory, dirname->s_name, MAXPDSTRING);
+    x->x_dir = opendir(dirname->s_name);
+    rewinddir(x->x_dir);
+    x->x_nfiles = x->x_ignored = 0;
+    struct dirent *result = NULL;
+    while(result = readdir(x->x_dir)){
+        if(strncmp(result->d_name, ".", 1 ))
+            x->x_nfiles++;
+        else
+            x->x_ignored++;
+    }
+    rewinddir(x->x_dir);
+}
+
+static void dir_n(t_dir *x){
+    outlet_float(x->x_out2, x->x_nfiles);
+}
+
+static void dir_reset(t_dir *x){
+    strncpy(x->x_directory, x->x_getdir->s_name, MAXPDSTRING);
+    x->x_dir = opendir(x->x_getdir->s_name);
+    rewinddir(x->x_dir);
+    x->x_nfiles = x->x_ignored = 0;
+    struct dirent *result = NULL;
+    while(result = readdir(x->x_dir)){
+        if(strncmp(result->d_name, ".", 1 ))
+            x->x_nfiles++;
+        else
+            x->x_ignored++;
+        }
+    rewinddir(x->x_dir);
 }
 
 static void dir_dump(t_dir *x){
     rewinddir(x->x_dir);
     struct dirent *result = NULL;
     while(result = readdir(x->x_dir))
-        outlet_symbol(x->x_obj.ob_outlet, gensym(result->d_name));
+        if(strncmp(result->d_name, ".", 1 ))
+            outlet_symbol(x->x_out1, gensym(result->d_name));
     rewinddir(x->x_dir);
 }
 
-static void dir_reset(t_dir *x){
-    x->x_dir = opendir(x->x_getdir->s_name);
-    strncpy(x->x_directory, x->x_getdir->s_name, MAXPDSTRING);
+static void dir_dir(t_dir *x){
+    outlet_symbol(x->x_out2, gensym(x->x_directory));
 }
 
 static void dir_bang(t_dir *x){
-    outlet_symbol(x->x_obj.ob_outlet, gensym(x->x_directory));
+    dir_dir(x);
+    dir_dump(x);
 }
 
 static void *dir_new(void){
@@ -46,14 +98,18 @@ static void *dir_new(void){
     t_canvas *canvas = canvas_getcurrent();
     x->x_getdir = canvas_getdir(canvas);
     dir_reset(x);
-    outlet_new(&x->x_obj, &s_symbol);
+    x->x_out1 = outlet_new(&x->x_obj, &s_anything);
+    x->x_out2 = outlet_new(&x->x_obj, &s_symbol);
     return (x);
 }
 
 void dir_setup(void){
     dir_class = class_new(gensym("dir"), (t_newmethod)dir_new, 0, sizeof(t_dir), 0, 0);
     class_addbang(dir_class, dir_bang);
+    class_addmethod(dir_class, (t_method)dir_n, gensym("n"), 0);
+    class_addmethod(dir_class, (t_method)dir_dir, gensym("dir"), 0);
     class_addmethod(dir_class, (t_method)dir_dump, gensym("dump"), 0);
     class_addmethod(dir_class, (t_method)dir_reset, gensym("reset"), 0);
     class_addmethod(dir_class, (t_method)dir_open, gensym("open"), A_DEFSYMBOL, 0);
+    class_addmethod(dir_class, (t_method)dir_seek, gensym("seek"), A_DEFFLOAT, 0);
 }
