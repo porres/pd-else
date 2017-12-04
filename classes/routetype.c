9c3aa6a4c48a3608aa36debb69d1621d37d0f393
porres <porres@gmail.com>
2017-12-04 01:27:11 -0200

    finishing routetype

--- classes/routetype.c
+++ classes/routetype.c
@@ -4,46 +4,122 @@ static t_class *routetype_class;
 
 typedef struct _routetype{
     t_object    x_obj;
+    t_int       x_b;
+    t_int       x_f;
+    t_int       x_s;
+    t_int       x_l;
+    t_int       x_a;
+    t_int       x_r;
     t_outlet    *x_out_bang;
     t_outlet    *x_out_float;
     t_outlet    *x_out_symbol;
     t_outlet    *x_out_list;
     t_outlet    *x_out_anything;
+    t_outlet    *x_out_reject;
 } t_routetype;
 
 static void routetype_bang(t_routetype *x){
-    outlet_bang(x->x_out_bang);
+    if(x->x_b)
+        outlet_bang(x->x_out_bang);
+    else if(x->x_r)
+        outlet_bang(x->x_out_reject);
 }
 
 static void routetype_float(t_routetype *x, t_floatarg f){
-    outlet_float(x->x_out_float, f);
+    if(x->x_f)
+        outlet_float(x->x_out_float, f);
+    else if(x->x_r)
+        outlet_float(x->x_out_reject, f);
 }
 
 static void routetype_symbol(t_routetype *x, t_symbol *s){
-    outlet_symbol(x->x_out_symbol, s);
+    if(x->x_s)
+        outlet_symbol(x->x_out_symbol, s);
+    else if(x->x_r)
+        outlet_symbol(x->x_out_reject, s);
 }
 
 static void routetype_list(t_routetype *x, t_symbol *sel, int argc, t_atom *argv){
-  outlet_list(x->x_out_list, gensym("list"), argc, argv);
+    if(argc == 0){
+        routetype_bang(x);
+        return;
+    }
+    if(argc == 1 && argv[0].a_type == A_SYMBOL){
+        routetype_symbol(x, atom_getsymbol(argv));
+        return;
+    }
+    if(argc == 1 && argv[0].a_type == A_FLOAT){
+        routetype_float(x, atom_getfloat(argv));
+        return;
+    }
+    if(x->x_l)
+        outlet_list(x->x_out_list, gensym("list"), argc, argv);
+    else if(x->x_r)
+        outlet_list(x->x_out_reject, gensym("list"), argc, argv);
 }
 
 static void routetype_anything(t_routetype *x, t_symbol *sel, int argc, t_atom *argv){
-    outlet_anything(x->x_out_anything, sel, argc, argv);
+    if(x->x_a)
+        outlet_anything(x->x_out_anything, sel, argc, argv);
+    else if(x->x_r)
+        outlet_anything(x->x_out_reject, sel, argc, argv);
 }
 
 static void *routetype_new(t_symbol *s, int argc, t_atom *argv){
     t_routetype *x = (t_routetype *)pd_new(routetype_class);
-    x->x_out_bang = outlet_new(&x->x_obj, &s_bang);
-    x->x_out_float = outlet_new(&x->x_obj, &s_float);
-    x->x_out_symbol = outlet_new(&x->x_obj, &s_symbol);
-    x->x_out_list = outlet_new(&x->x_obj, &s_list);
-    x->x_out_anything = outlet_new(&x->x_obj, &s_anything);
-    return (x);
+    x->x_b = x->x_f = x->x_s = x->x_l = x->x_a = x->x_r = 0;
+    t_int c = argc;
+    if(!argc){
+        x->x_out_reject = outlet_new(&x->x_obj, &s_anything);
+        x->x_r = 1;
+    }
+    else{
+        t_int n = 0;
+        while(argc > 0){
+            if(argv->a_type == A_SYMBOL){
+                t_symbol *curarg = atom_getsymbolarg(0, argc, argv);
+                if(strcmp(curarg->s_name, "f") == 0 || strcmp(curarg->s_name, "float") == 0){
+                    x->x_out_float = outlet_new(&x->x_obj, &s_float);
+                    x->x_f = 1;
+                }
+                else if(strcmp(curarg->s_name, "b") == 0 || strcmp(curarg->s_name, "bang") == 0){
+                    x->x_out_bang = outlet_new(&x->x_obj, &s_bang);
+                    x->x_b = 1;
+                }
+                else if(strcmp(curarg->s_name, "s") == 0 || strcmp(curarg->s_name, "symbol") == 0){
+                    x->x_out_symbol = outlet_new(&x->x_obj, &s_symbol);
+                    x->x_s = 1;
+                }
+                else if(strcmp(curarg->s_name, "l") == 0 || strcmp(curarg->s_name, "list") == 0){
+                    x->x_out_list = outlet_new(&x->x_obj, &s_list);
+                    x->x_l = 1;
+                }
+                else if(strcmp(curarg->s_name, "a") == 0 || strcmp(curarg->s_name, "anything") == 0){
+                    x->x_out_anything = outlet_new(&x->x_obj, &s_anything);
+                    x->x_a = 1;
+                }
+                else
+                    goto errstate;
+            }
+            else
+                goto errstate;
+            argv++;
+            argc--;
+        }
+        if(c < 5){
+            x->x_out_reject = outlet_new(&x->x_obj, &s_anything);
+            x->x_r = 1;
+        }
+    }
+    return(x);
+errstate:
+    pd_error(x, "routetype: improper args");
+    return NULL;
 }
 
 void routetype_setup(void){
     routetype_class = class_new(gensym("routetype"), (t_newmethod)routetype_new,
-                                0, sizeof(t_routetype), 0, 0);
+                                0, sizeof(t_routetype), 0, A_GIMME, 0);
     class_addbang(routetype_class, routetype_bang);
     class_addfloat(routetype_class, routetype_float);
     class_addsymbol(routetype_class, routetype_symbol);
