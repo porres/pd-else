d27abf45fec5ee6c689011856f36fe37fb5d4ef0
Alexandre Torres Porres <porres@gmail.com>
2018-02-25 05:29:23 -0300

    Update multigate~.c

--- classes/multigate~.c
+++ classes/multigate~.c
@@ -4,9 +4,9 @@
 
 typedef struct _multigate{
     t_object   x_obj;
-	t_float   *x_mainsig;  // the main inlet (leftmost) signal input
-    int 	   x_n_ch;     // number of channels
-	t_float    x_gate;     // 0 = off, nonzero = on
+    t_float   *x_mainsig;  // the main inlet (leftmost) signal input
+    int        x_n_ch;     // number of channels
+    t_float    x_gate;     // 0 = off, nonzero = on
     t_float  **x_ivecs;    // input vectors
     t_float  **x_ovecs;    // output vectors
 }t_multigate;
@@ -16,12 +16,12 @@ static t_class *multigate_class;
 static t_int *multigate_perform(t_int *w){
     t_multigate *x = (t_multigate *)(w[1]);
     int nblock = (int)(w[2]);
-	t_float *gate = x->x_mainsig;
+    t_float *gate = x->x_mainsig;
     t_float **ivecs = x->x_ivecs;  // input vectors
     t_float **ovecs = x->x_ovecs;  // output vectors
     int i, j;
-	int n_ch = x->x_n_ch;
-	for(i = 0; i < nblock; i++){
+    int n_ch = x->x_n_ch;
+    for(i = 0; i < nblock; i++){
         int state = gate[i] != 0;
         for(j = 0; j < n_ch; j++){ // signals counted unintuitively backwards/clockwise
             int ch_in = j;
@@ -33,20 +33,20 @@ static t_int *multigate_perform(t_int *w){
 }
 
 static void multigate_dsp(t_multigate *x, t_signal **sp){
-	int i, nblock = sp[0]->s_n;
+    int i, nblock = sp[0]->s_n;
     t_signal **sigp = sp;
-	x->x_mainsig = (*sigp++)->s_vec;            // gate
+	x->x_mainsig = (*sigp++)->s_vec;        // gate
     for(i = 0; i < x->x_n_ch; i++)              // inputs
         *(x->x_ivecs+i) = (*sigp++)->s_vec;
     for(i = 0; i < x->x_n_ch; i++)              // outputs
-		*(x->x_ovecs+i) = (*sigp++)->s_vec;
-	dsp_add(multigate_perform, 2, x, nblock);
+	*(x->x_ovecs+i) = (*sigp++)->s_vec;
+    dsp_add(multigate_perform, 2, x, nblock);
 }
 
 static void *multigate_new(t_symbol *s, int argc, t_atom *argv){
     t_multigate *x = (t_multigate *)pd_new(multigate_class);
     t_float n_ch = 1; // default is 1 channel
-	t_float gate = 0; // initially closed
+    t_float gate = 0; // initially closed
     int i;
     int argnum = 0;
     while(argc > 0){
@@ -54,11 +54,11 @@ static void *multigate_new(t_symbol *s, int argc, t_atom *argv){
             t_float argval = atom_getfloatarg(0, argc, argv);
             switch(argnum){
                 case 0:
-					n_ch = argval;
+		    n_ch = argval;
                     break;
-				case 1:
-					gate = argval;
-					break;
+		case 1:
+		    gate = argval;
+		    break;
                 default:
                     break;
             };
@@ -67,18 +67,18 @@ static void *multigate_new(t_symbol *s, int argc, t_atom *argv){
             argnum++;
         };
     };
-	if(n_ch < 1)
-		n_ch = 1;
+    if(n_ch < 1)
+	n_ch = 1;
     else if(n_ch > (t_float)MAX_N_CH)
         n_ch = MAX_N_CH;
-	x->x_n_ch = (int)n_ch;
+    x->x_n_ch = (int)n_ch;
     x->x_gate = gate != 0;
     x->x_ivecs = getbytes(n_ch * sizeof(*x->x_ivecs));
     for(i = 0; i < n_ch; i++)
         inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
-	x->x_ovecs = getbytes(n_ch * sizeof(*x->x_ovecs));
-	for(i = 0; i < n_ch; i++)
-	 	outlet_new(&x->x_obj, gensym("signal"));
+    x->x_ovecs = getbytes(n_ch * sizeof(*x->x_ovecs));
+    for(i = 0; i < n_ch; i++)
+	outlet_new(&x->x_obj, gensym("signal"));
     return(x);
 }
 
