15526599b8617dffdc2dd18a20c37cda28d35169
porres <porres@gmail.com>
2018-03-11 17:28:45 -0300

    brat: environment and free

--- classes/brat.c
+++ classes/brat.c
@@ -51,6 +51,42 @@ static t_cnv_canvaslist *addCanvas(const t_pd *parent){
     return list;
 }
 
+static void removeObjectFromCanvas(const t_pd*parent, const t_pd*obj) {
+    t_cnv_canvaslist*p=findCanvas(parent);
+    t_cnv_objlist*list=0, *last=0, *next=0;
+    if(!p || !obj)return;
+    list=p->obj;
+    if(!list)
+        return;
+    
+    while(list && obj!=list->obj) {
+        last=list;
+        list=list->next;
+    }
+    
+    if(!list) /* couldn't find this object */
+        return;
+    
+    next=list->next;
+    
+    if(last)
+        last->next=next;
+    else
+        p->obj=next;
+    
+    freebytes(list, sizeof(*list));
+    list=0;
+}
+
+static void removeObjectFromCanvases(const t_pd*obj) {
+    t_cnv_canvaslist*parents=s_canvaslist;
+    
+    while(parents) {
+        removeObjectFromCanvas(parents->parent, obj);
+        parents=parents->next;
+    }
+}
+
 static void addObjectToCanvas(const t_pd *parent, const t_pd *obj){
     t_cnv_canvaslist *p = addCanvas(parent);
     t_cnv_objlist *list = 0;
@@ -86,7 +122,11 @@ typedef struct _brat{
   t_outlet *x_click_bangout;
 }t_brat;
 
-t_propertiesfn s_orgfun = NULL;
+t_propertiesfn x_properties_fn = NULL;
+
+static void brat_free(t_brat *x){
+  removeObjectFromCanvases((t_pd*)x);
+}
 
 static void properties_bang(t_brat *x){
     outlet_bang(x->x_properties_bangout);
@@ -111,7 +151,7 @@ static void brat_click(t_gobj *z, t_canvas *x){
 static void brat_properties(t_gobj *z, t_glist *owner){
   t_cnv_objlist *objs = objectsInCanvas((t_pd*)z);
   if(objs == NULL)
-    s_orgfun(z, owner);
+    x_properties_fn(z, owner);
   while(objs){
       t_brat* x = (t_brat*)objs->obj;
       properties_bang(x);
@@ -120,24 +160,32 @@ static void brat_properties(t_gobj *z, t_glist *owner){
 }
 
 static void *brat_new(void){
-  t_brat *x = (t_brat *)pd_new(brat_class);
-  t_glist *glist = (t_glist *)canvas_getcurrent();
-  t_canvas *canvas = (t_canvas*)glist_getcanvas(glist);
-  t_class *class = ((t_gobj*)canvas)->g_pd;
-  t_propertiesfn orgfun = NULL;
-  x->x_properties_bangout = outlet_new((t_object *)x, &s_bang);
-  x->x_click_bangout = outlet_new((t_object *)x, &s_bang);
-  orgfun = class_getpropertiesfn(class);
-  if(orgfun != brat_properties)
-    s_orgfun = orgfun;
-  class_setpropertiesfn(class, brat_properties);
-  class_addmethod(class, (t_method)brat_click, gensym("click"), 0);
-  addObjectToCanvas((t_pd*)canvas, (t_pd*)x);
-  return(x);
+    t_brat *x = (t_brat *)pd_new(brat_class);
+    t_glist *glist = (t_glist *)canvas_getcurrent();
+    t_canvas *canvas = (t_canvas*)glist_getcanvas(glist);
+    t_class *class = ((t_gobj*)canvas)->g_pd;
+    while(!canvas->gl_env)
+        canvas = canvas->gl_owner; // yeah
+    x->x_properties_bangout = outlet_new((t_object *)x, &s_bang);
+    x->x_click_bangout = outlet_new((t_object *)x, &s_bang);
+    
+// properties
+    t_propertiesfn class_properties_fn = NULL;
+// get properties from the class
+    class_properties_fn = class_getpropertiesfn(class);
+    if(class_properties_fn != brat_properties)
+        x_properties_fn = class_properties_fn;
+    // else is NULL
+// set properties
+    class_setpropertiesfn(class, brat_properties);
+    
+    class_addmethod(class, (t_method)brat_click, gensym("click"), 0);
+    addObjectToCanvas((t_pd*)canvas, (t_pd*)x);
+    return(x);
 }
 
 void brat_setup(void){
   brat_class = class_new(gensym("brat"), (t_newmethod)brat_new,
-            0, sizeof(t_brat), CLASS_NOINLET, 0);
-    s_orgfun = NULL;
+            (t_method)brat_free, sizeof(t_brat), CLASS_NOINLET, 0);
+    x_properties_fn = NULL;
 }
--- shared/magic.h
+++ shared/magic.h
@@ -1,7 +1,6 @@
 #define MAGIC_NAN 0x7FFFFFFFul
 
-/* undeclared function returning float fields.
- Arguments: object/inlet number (0 indexed) */
+// returns float fields. Arguments: object/inlet number (0 indexed)
 EXTERN t_float *obj_findsignalscalar(t_object *x, int m);
 
 union magic_ui32_fl{
