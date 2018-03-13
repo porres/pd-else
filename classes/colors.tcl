03b39f5ae5a4f7cf7fe331b609e0ca5dcee4e9dc
porres <porres@gmail.com>
2018-03-12 21:19:28 -0300

    some bullshit

--- classes/colors.tcl
+++ classes/colors.tcl
@@ -1,3 +1,7 @@
+
+namespace eval else_colors {
+}
+
 proc else_colors_open {objectid initialcolor} {
     set color [tk_chooseColor -initialcolor $initialcolor]
     pdsend "$objectid callback $color"
