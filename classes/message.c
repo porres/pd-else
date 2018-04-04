f26e97cb9bd1abbbd6a6fd7a2306a9e055911779
porres <porres@gmail.com>
2018-04-04 14:00:45 -0300

    "i guess this is it" - message: send & comma

--- classes/message.c
+++ classes/message.c
@@ -10,142 +10,110 @@ typedef struct _message{
   t_symbol     *x_s;
 }t_message;
 
-static void message_output(t_message *x, t_symbol *s, int ac, t_atom *av){
-    if(!ac && strcmp(s->s_name, ",")) // only a selector, but not a comma
-        outlet_anything(x->x_obj.ob_outlet, s, 0, 0); // output selector
-    else{
+static void message_send(t_message *x, t_symbol *s, int ac, t_atom *av){
+    if(!ac){ // only a selector (but not ";" or ",")
+        if(!strcmp(s->s_name, ";") || !strcmp(s->s_name, ",")){
+        }  // ignore
+        else // output selector
+            outlet_anything(x->x_obj.ob_outlet, s, 0, 0);
+    }
+    else{ // Not only a Selector
         char separator = gensym(",")->s_name[0]; // split character
-        int i = 0, first = 1, n;
-// while
+        char send = gensym(";")->s_name[0]; // send character
+        t_int send_flag = 0;
+        t_int last_send_flag = 0, send_this = 0;
+        t_int last_comma_flag = 0, split_this = 0;
+        t_int i = 0, j = 0, n = 0;
+        t_symbol *send_sym = NULL;
         while(i < ac){
-            int j = i; // i = start point
-            for(j; j < ac; j++){
-                if((av+j)->a_type == A_SYMBOL && atom_getsymbol(av+j)->s_name[0] == separator)
-                    break; // j = comma
-            }
-            n = j - i; // n = number of elements in a message (counting the comma element)
-    // output messages
-            if(first){
-                if(n == 0){ // it's only the selector
+            if(last_send_flag == 1)
+                send_flag = 1; // from now on, send all the shit
+            send_this = last_send_flag;
+            split_this = last_comma_flag;
+            for(j = i; j < ac; j++){ // <=====    FOR
+                if((av+j)->a_type == A_SYMBOL && atom_getsymbol(av+j)->s_name[0] == send){
+                    last_comma_flag = 0; // next break point is a send, so comma = 0
+                    last_send_flag = 1; // update for next round cause next break point is a send
+                    break;
+                }
+                else if((av+j)->a_type == A_SYMBOL && atom_getsymbol(av+j)->s_name[0] == separator){
+                    last_send_flag = 0;  // next break is a comma, so fuck this
+                    last_comma_flag = 1; // update for next round cause next break point is a comma
+                    break;
+                }
+            } // <=====    END OF FOR
+            n = j - i; // j = breakpoint / n = elements in a message - counting the comma/semicolon 1st element
+            if(i == 0){ // <=====    FIRST MESSAGE!!!!
+                if(n == 0){ // the message only contains a selector
                     if(!strcmp(s->s_name, "list")) // if selector is list, turn to bang
                         outlet_bang(x->x_obj.ob_outlet);
-                    else if(!strcmp(s->s_name, ",")){
-                        // do nothing
-                    }
-                    else
-                        outlet_anything(x->x_obj.ob_outlet, s, n, av-1); // output selector
+                    else if(!strcmp(s->s_name, ";") || !strcmp(s->s_name, ",")){
+                    }    // ignore ";" or ","
+                    else // output selector
+                        outlet_anything(x->x_obj.ob_outlet, s, n, av-1);
                 }
-                else{ // it's not only the selector (this can be the whole message if there's no comma)
-                    if(!strcmp(s->s_name, ",")){
+                else{ // it's not only the selector
+                    if(!strcmp(s->s_name, ";")){ // IT'S A SEND!!!!!!
+                        if((av)->a_type == A_FLOAT)
+                            send_sym = gensym("float");
+                        else if((av)->a_type == A_SYMBOL)
+                            send_sym = atom_getsymbol(av);
+                        if(send_sym->s_thing && n > 1){ // there's a receive and a message, send it!
+                            if((av+1)->a_type == A_FLOAT)
+                                typedmess(send_sym->s_thing, &s_list, n-1, av+1);
+                            else if((av+1)->a_type == A_SYMBOL)
+                                typedmess(send_sym->s_thing, atom_getsymbol(av+1), n-2, av+2);
+                        }
+                        last_send_flag = 1; // mark it as a flag
+                    }
+                    else if(!strcmp(s->s_name, ",")){ // Selector is a comma, so ignore it and output the rest
                         if((av)->a_type == A_FLOAT)
                             outlet_anything(x->x_obj.ob_outlet, &s_list, n, av);
-                        else if((av)->a_type == A_SYMBOL){
-                            t_symbol *sym = atom_getsymbol(av);
-                            outlet_anything(x->x_obj.ob_outlet, sym, n-1, av+1);
-                        }
+                        else if((av)->a_type == A_SYMBOL)
+                            outlet_anything(x->x_obj.ob_outlet, atom_getsymbol(av), n-1, av+1);
                     }
-                    else
+                    else // output the message (can be the whole message if there's no breakage)
                         outlet_anything(x->x_obj.ob_outlet, s, n, av);
                 }
-                first = 0;
             }
-            else{ // messages after a comma
-                if((av+i)->a_type == A_SYMBOL && strcmp(atom_getsymbol(av+i)->s_name, ",")) // ignore commas
-                    outlet_anything(x->x_obj.ob_outlet, atom_getsymbol(av+i), n-1, av + i+1);
-                else if((av+i)->a_type == A_FLOAT)
-                    outlet_anything(x->x_obj.ob_outlet, &s_list, n, av+i);
-            }
-    // next start point
-            i = j + 1;
-        }
-// end of while
-    }
-}
-
-static void message_send(t_message *x, t_symbol *s, int ac, t_atom *av){
-    if(!ac && strcmp(s->s_name, ";")) // only a selector (but not a semicolon
-        outlet_anything(x->x_obj.ob_outlet, s, 0, 0); // output selector
-    else{
-        char send = gensym(";")->s_name[0]; // send character
-        int i = 0, first = 1, n;
-        while(i < ac){
-            int j = i; // i = start point
-            for(j; j < ac; j++){
-                if((av+j)->a_type == A_SYMBOL && atom_getsymbol(av+j)->s_name[0] == send)
-                    break; // j = semicolon
-            }
-            n = j - i; // n = number of elements in a message (counting the semicolon element)
-            if(first){
-                if(n == 0){ // it's only the selector
-                    if(!strcmp(s->s_name, "list")) // if selector is list, turn to bang
-                        outlet_bang(x->x_obj.ob_outlet);
-                    else if(!strcmp(s->s_name, ";")){
-                        // do nothing
-                    }
-                    else
-                        outlet_anything(x->x_obj.ob_outlet, s, n, av-1); // output selector
-                }
-                else{ // it's not only the selector (this can be the whole message if there's no comma)
-                    if(!strcmp(s->s_name, ";")){
-                        if(n > 1){ // needs to have more than the send symbol
-                            t_symbol *send_sym;
-                            if((av)->a_type == A_FLOAT){
-                                send_sym = gensym("float");
-                                if(send_sym->s_thing){ // there's a receive!
-                                    if((av+1)->a_type == A_FLOAT)
-                                        typedmess(send_sym->s_thing, &s_list, n-1, av+1);
-                                    else if((av+1)->a_type == A_SYMBOL)
-                                        typedmess(send_sym->s_thing, atom_getsymbol(av+1), n-2, av+2);
-                                }
-                            }
-                            else if((av)->a_type == A_SYMBOL){
-                                send_sym = atom_getsymbol(av);
-                                if(send_sym->s_thing){ // there's a receive!
-                                    if((av+1)->a_type == A_FLOAT)
-                                        typedmess(send_sym->s_thing, &s_list, n-1, av+1);
-                                    else if((av+1)->a_type == A_SYMBOL)
-                                        typedmess(send_sym->s_thing, atom_getsymbol(av+1), n-2, av+2);
-                                }
-                            }
-                        }
-                    }
-                    else
-                        outlet_anything(x->x_obj.ob_outlet, s, n, av);
-                }
-                first = 0;
-            }
-            else{ // following messages after a semicolon
-                if(n > 1){ // needs to have more than the send symbol
-                    t_symbol *send_sym;
-                    t_symbol *sel;
-                    if((av+i)->a_type == A_FLOAT){
-                        send_sym = gensym("float");
-                        if(send_sym->s_thing){ // there's a receive!
+            else{ // <=====    NEXT MESSAGES!!!!
+                if(send_flag){ // we're sending stuff
+                    if(send_this){ // it's a new send!
+                        if((av+i)->a_type == A_FLOAT)
+                            send_sym = gensym("float");
+                        else if((av+i)->a_type == A_SYMBOL)
+                            send_sym = atom_getsymbol(av+i);
+                        if(send_sym->s_thing && n > 1){ // there's a receive and a message, send it!
                             if((av+i+1)->a_type == A_FLOAT)
-                                typedmess(send_sym->s_thing, &s_list, n-1, av + i+1);
+                                typedmess(send_sym->s_thing, &s_list, n-1, av+i+1);
                             else if((av+i+1)->a_type == A_SYMBOL)
-                                typedmess(send_sym->s_thing, atom_getsymbol(av+i+1), n-2, av + i+2);
+                                typedmess(send_sym->s_thing, atom_getsymbol(av+i+1), n-2, av+i+2);
                         }
                     }
-                    else if((av+i)->a_type == A_SYMBOL){
-                        send_sym = atom_getsymbol(av+i);
-                         if(send_sym->s_thing){ // there's a receive
-                             if((av+i+1)->a_type == A_FLOAT)
-                                 typedmess(send_sym->s_thing, &s_list, n-1, av + i+1);
-                             else if((av+i+1)->a_type == A_SYMBOL)
-                                 typedmess(send_sym->s_thing, atom_getsymbol(av+i+1), n-2, av + i+2);
-                         }
+                    else if(split_this){ // it's an old send
+                        if(send_sym->s_thing){ // there's a receive and a message, send it!
+                            if((av+i)->a_type == A_FLOAT)
+                                typedmess(send_sym->s_thing, &s_list, n, av+i);
+                            else if((av+i)->a_type == A_SYMBOL)
+                                typedmess(send_sym->s_thing, atom_getsymbol(av+i), n-1, av+i+1);
+                        }
                     }
+                }
+                else{ // we're *NOT* sending stuff
+                    if((av+i)->a_type == A_SYMBOL && strcmp(s->s_name, ",")) // ignore commas
+                        outlet_anything(x->x_obj.ob_outlet, atom_getsymbol(av+i), n-1, av + i+1);
+                    else if((av+i)->a_type == A_FLOAT)
+                        outlet_anything(x->x_obj.ob_outlet, &s_list, n, av+i);
                 }
             }
             i = j + 1; // next start point
-        } // end of while
+        } // <=====    END OF WHILE
     }
 }
 
 static void message_anything(t_message *x, t_symbol *s, int ac, t_atom *av){
     if(x->x_av)
-        message_output(x, x->x_s, x->x_ac, x->x_av);
+        message_send(x, x->x_s, x->x_ac, x->x_av);
 }
 
 static void message_click(t_message *x){
