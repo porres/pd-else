f94d36b1e0e367d9ce7d1921f07a589b34a5aadc
porres <porres@gmail.com>
2019-01-07 14:54:43 -0200

    update rec~

--- Classes/Source/rec~.c
+++ Classes/Source/rec~.c
@@ -1,3 +1,4 @@
+// based on cyclone's record~
 
 #include "m_pd.h"
 #include "m_imp.h"
@@ -10,29 +11,29 @@
 typedef struct _rec{
     t_object    x_obj;
     t_buffer   *x_buffer;
-    t_float    *x_gate_vec; // gate signal
-    t_float     x_last_gate;
-    int         x_continuemode;
-    int         x_loopmode;
-    int         x_phase;       /* writing head */
     t_float     x_f; // dummy input float
+    t_float    *x_gate_vec; // gate signal vector
+    t_float     x_last_gate;
+    t_int         x_continue_flag;
+    t_int         x_loop_flag;
+    t_int         x_phase;       // write head
     t_float     x_sync; // output sync value
     t_clock    *x_clock;
     double      x_clocklasttick;
-    int         x_isrunning;
-    int         x_newrun; // if running turned from off and on for this current block
+    t_int         x_isrunning;
+    t_int         x_newrun;
+    t_float     x_start; // start position (in ms) vector
+    t_float     x_end; // endposition (in ms) vec
     t_outlet    *x_outlet;
-    t_float     x_ksr; //sample rate in ms
-    int 	    x_numchans;
+    t_float     x_ksr;
+    t_int         x_numchans;
     t_float   **x_ivecs; // input vectors
-    t_float     x_start; //start position (in ms) vector
-    t_float     x_end; //endposition (in ms) vec
-    t_float    *x_ovec; //output vector
+    t_float    *x_ovec; // signal output
 }t_rec;
 
 static t_class *rec_class;
 
-static void rec_tick(t_rec *x){
+static void rec_tick(t_rec *x){ // Redraw!
     double timesince = clock_gettimesince(x->x_clocklasttick);
     if(timesince >= REC_DRAW_PERIOD){
         buffer_redraw(x->x_buffer);
@@ -48,112 +49,90 @@ static void rec_set(t_rec *x, t_symbol *s){
 
 static void rec_reset(t_rec *x){
     if(x->x_sync > 0)
-        x->x_isrunning = 1;
+        x->x_isrunning = 1; // ???
     x->x_start = 0;
     x->x_end = (t_float)x->x_buffer->c_npts/x->x_ksr; //array size in samples
 }
 
 static int rec_startpoint(t_rec *x, t_floatarg f){
-    int npts = x->x_buffer->c_npts;
-    //some bounds checking to prevent overflow
-    int startindex;
-    if(f < REC_MAXBD)
-        startindex = (int)(f * x->x_ksr);
-    else
-        startindex = SHARED_INT_MAX;
+    t_int npts = x->x_buffer->c_npts;
+    t_int startindex = f < REC_MAXBD ? (int)(f * x->x_ksr) : SHARED_INT_MAX;
     if(startindex < 0)
-		startindex = 0;  /* CHECKED */
+		startindex = 0;
     else if(startindex >= npts) // make it the last index addressable
-		startindex = npts - 1;  /* CHECKED (both ways) */
+		startindex = npts - 1;
     return startindex;
 }
 
 static int rec_endpoint(t_rec *x, t_floatarg f){
-    // noninclusive
-    int endindex;
-    int npts = x->x_buffer -> c_npts;
-    //some bounds checking to prevent overflow
-    if(f < REC_MAXBD)
-        endindex = (int)(f * x->x_ksr);
-    else
-        endindex = SHARED_INT_MAX;
+    t_int endindex = f < REC_MAXBD ? (int)(f * x->x_ksr) : SHARED_INT_MAX;
+    t_int npts = x->x_buffer->c_npts;
     if(endindex < 0)
         endindex = 0;
-    if (endindex >= npts) // if bigger than the end, set it to the end
-		endindex = npts;  // CHECKED (both ways)
+    if(endindex >= npts) // if bigger than the end, set it to the end
+		endindex = npts;
     return endindex;
 }
 
 static void rec_continue(t_rec *x, t_floatarg f){
-	x->x_continuemode = (f != 0);
+	x->x_continue_flag = (f != 0);
 }
 
 static void rec_loop(t_rec *x, t_floatarg f){
-    x->x_loopmode = (f != 0);
+    x->x_loop_flag = (f != 0);
 }
 
 static void rec_rec(t_rec *x){
-    x->x_isrunning = 1;
-    x->x_newrun = 1;
+    x->x_isrunning = x->x_newrun = 1;
 }
 
 static void rec_stop(t_rec *x){
-    x->x_isrunning = 0;
-    clock_delay(x->x_clock, 0); // trigger a redraw
-    x->x_sync = 0.;
-    if(x->x_continuemode == 0)
+    if(!x->x_continue_flag)
         x->x_phase = 0.;
+    x->x_isrunning = x->x_sync = 0;
+    buffer_redraw(x->x_buffer);
 }
 
 static t_int *rec_perform(t_int *w){
     t_rec *x = (t_rec *)(w[1]);
     t_buffer * c = x->x_buffer;
-    int nch = c->c_numchans;
-    int nblock = (int)(w[2]);
+    t_int nch = c->c_numchans;
+    t_int nblock = (int)(w[2]);
     t_float *gatein = x->x_gate_vec;
     t_float *out = x->x_ovec;
-    t_float gate, startms, endms, sync;
-    int startsamp, endsamp, phase, range, i, j;
+    t_int phase, range, i, j;
     buffer_validate(c, 0);
     t_float last_gate = x->x_last_gate;
+    clock_delay(x->x_clock, 0);
     for(i = 0; i < nblock; i++){
-        gate = gatein[i];
+        t_float gate = gatein[i];
         if(gate != 0 && last_gate == 0)
             rec_rec(x);
         else if(gate == 0 && last_gate != 0)
             rec_stop(x);
         last_gate = gate;
-        startms = x->x_start;
-        endms = x->x_end;
+        t_float startms = x->x_start;
+        t_float endms = x->x_end;
         if((startms < endms) && c->c_playable && x->x_isrunning){
-            startsamp = rec_startpoint(x, startms);
-            endsamp = rec_endpoint(x, endms);
+            t_int startsamp = rec_startpoint(x, startms);
+            t_int endsamp = rec_endpoint(x, endms);
             range = endsamp - startsamp;
-            //continue mode shouldn't reset phase
-            if(x->x_newrun == 1 && x->x_continuemode == 0){
-                //isrunning 0->1 from last block, means reset phase appropriately
+            if(x->x_newrun == 1 && x->x_continue_flag == 0){ // continue shouldn't reset phase
                 x->x_newrun = 0;
                 x->x_phase = startsamp;
                 x->x_sync = 0.;
             };
             phase = x->x_phase;
-            //boundschecking, do it here because points might changed when paused
-            //easier case, when we're "done"
-            if(phase >= endsamp){
-                if(x->x_loopmode == 1)
+            if(phase >= endsamp){ // boundscheck (might've changed when paused)
+                if(x->x_loop_flag == 1)
                     phase = startsamp;
-                else{
-                    //not looping, just stop it
+                else{ // stop
                     x->x_isrunning = 0;
-                    //mb a bit redundant, but just make sure x->x_sync is 1
-                    x->x_sync = 1;
-                    //trigger redraw
+                    x->x_sync = 1; // maybe redundant???
                 };
-                //in either case calculate a redraw
-	        clock_delay(x->x_clock, 0);
+            buffer_redraw(x->x_buffer);
             };
-            // harder case up to interpretation
-            if(phase < startsamp) // if before startsamp, just jump to startsamp?
+            if(phase < startsamp)
                 phase = startsamp;
             if(x->x_isrunning == 1){ // if we're still running after boundschecking
                 for(j = 0; j < nch; j++){
@@ -162,15 +141,11 @@ static t_int *rec_perform(t_int *w){
                     if(vp)
                         vp[phase].w_float = insig[i];
                 };
-                //sync output
-                sync = (t_float)(phase - startsamp)/(t_float)range;
-                //increment stage
+                x->x_sync = (t_float)(phase - startsamp)/(t_float)range;
                 phase++;
                 x->x_phase = phase;
-                x->x_sync = sync;
              };
         };
-        //in any case, output sync value
         out[i] = x->x_sync;
     };
     x->x_last_gate = last_gate;
@@ -182,10 +157,10 @@ static void rec_dsp(t_rec *x, t_signal **sp){
     x->x_ksr= sp[0]->s_sr * 0.001;
     int i, nblock = sp[0]->s_n;
     t_signal **sigp = sp;
-    x->x_gate_vec = (*sigp++)->s_vec; // the first sig in is the gate
-    for(i = 0; i < x->x_numchans; i++) //input vectors first
+    x->x_gate_vec = (*sigp++)->s_vec; // first sig is the gate input
+    for(i = 0; i < x->x_numchans; i++) // input vectors for each channel
         *(x->x_ivecs+i) = (*sigp++)->s_vec;
-    x->x_ovec = (*sigp++)->s_vec;
+    x->x_ovec = (*sigp++)->s_vec; // output
     dsp_add(rec_perform, 2, x, nblock);
 }
 
@@ -198,9 +173,7 @@ static void rec_free(t_rec *x){
 }
 
 static void rec_start(t_rec *x, t_float start){
-    if(start < 0)
-        start = 0;
-    x->x_start = start;
+    x->x_start = start < 0 ? 0 : start;
 }
 
 static void rec_end(t_rec *x, t_float end){
@@ -214,60 +187,62 @@ static void rec_end(t_rec *x, t_float end){
     x->x_end = end;
 }
 
-static void *rec_new(t_symbol *s, int argc, t_atom *argv){
+static void *rec_new(t_symbol *s, int ac, t_atom *av){
     t_symbol *dummy = s;
     dummy = NULL;
+    t_rec *x = (t_rec *)pd_new(rec_class);
+    x->x_ksr = (float)sys_getsr() * 0.001;
+    x->x_last_gate = x->x_newrun = x->x_isrunning = x->x_sync = x->x_phase = 0;
     t_float numchan = 1;
-    t_float continuemode = 0;
-    t_float loopstatus = 0;
+    t_float continue_flag = 0;
+    t_float loop_flag = 0;
     t_float start = 0;
     t_float end = -1;
-    int nameset = 0; //flag if name is set
-    t_symbol * arrname = NULL;
-    if(argc > 0 && argv ->a_type == A_SYMBOL){
-        if(!nameset){ //if name not passed so far, count arg as array name
-            arrname = atom_getsymbolarg(0, argc, argv);
-            argc--;
-            argv++;
+    t_int nameset = 0; // flag if name is set
+    t_symbol *name = NULL;
+    if(ac > 0 && av->a_type == A_SYMBOL){
+        if(!nameset){ // if name not passed so far, count arg as array name
+            name = atom_getsymbolarg(0, ac, av);
+            ac--;
+            av++;
             nameset = 1;
         }
     };
-    int argnum = 0;
-    while(argc > 0){
-        if(argv->a_type == A_SYMBOL){
-            t_symbol * curarg = atom_getsymbolarg(0, argc, argv);
+    while(ac > 0){
+        if(av->a_type == A_SYMBOL){
+            t_symbol * curarg = atom_getsymbolarg(0, ac, av);
             if(!strcmp(curarg->s_name, "-continue")){
-                if(argc >= 2){
-                    continuemode = atom_getfloatarg(1, argc, argv);
-                    argc-=2;
-                    argv+=2;
+                if(ac >= 2){
+                    continue_flag = atom_getfloatarg(1, ac, av);
+                    ac-=2;
+                    av+=2;
                 }
                 else
                     goto errstate;
             }
             else if(!strcmp(curarg->s_name, "-loop")){
-                if(argc >= 2){
-                    loopstatus = atom_getfloatarg(1, argc, argv);
-                    argc-=2;
-                    argv+=2;
+                if(ac >= 2){
+                    loop_flag = atom_getfloatarg(1, ac, av);
+                    ac-=2;
+                    av+=2;
                 }
                 else
                     goto errstate;
             }
             else if(!strcmp(curarg->s_name, "-start")){
-                if(argc >= 2){
-                    start = atom_getfloatarg(1, argc, argv);
-                    argc-=2;
-                    argv+=2;
+                if(ac >= 2){
+                    start = atom_getfloatarg(1, ac, av);
+                    ac-=2;
+                    av+=2;
                 }
                 else
                     goto errstate;
             }
             else if(!strcmp(curarg->s_name, "-end")){
-                if(argc >= 2){
-                    end = atom_getfloatarg(1, argc, argv);
-                    argc-=2;
-                    argv+=2;
+                if(ac >= 2){
+                    end = atom_getfloatarg(1, ac, av);
+                    ac-=2;
+                    av+=2;
                 }
                 else
                     goto errstate;
@@ -275,68 +250,38 @@ static void *rec_new(t_symbol *s, int argc, t_atom *argv){
             else
                 goto errstate;
         }
-        else if(argv->a_type == A_FLOAT){
-            if(nameset){
-                t_float argval = atom_getfloatarg(0, argc, argv);
-                switch(argnum){
-                    case 0:
-                        numchan = argval;
-                        break;
-                    default:
-                        break;
-                };
-                argnum++;
-                argc--;
-                argv++;
-            }
-            else
-                goto errstate;
+        else if(av->a_type == A_FLOAT && nameset){
+            numchan = atom_getfloatarg(0, ac, av);
+            ac--;
+            av++;
         }
         else
             goto errstate;
     };
     int chn_n = (int)numchan > 64 ? 64 : (int)numchan;
-    //old arsic notes - chn_n number of channels, 0 nsigs 1 nauxsigs
-    t_rec *x = (t_rec *)pd_new(rec_class);
-    x->x_ksr = (float)sys_getsr() * 0.001;
-    x->x_buffer = buffer_init((t_class *)x, arrname, chn_n, 0);
+    x->x_buffer = buffer_init((t_class *)x, name, chn_n, 0);
     t_buffer * c = x->x_buffer;
-    // init
-    x->x_last_gate = 0;
-    x->x_newrun = 0;
-    x->x_isrunning = 0;
-    x->x_sync = 0;
-    x->x_phase = 0;
-    if(c){ //setting channels and array sizes
+    if(c){ // set channels and array sizes
         x->x_numchans = c->c_numchans;
         t_float arraysmp = (t_float)c->c_npts;
-        //allocate input vectors
-        x->x_ivecs = getbytes(x->x_numchans * sizeof(*x->x_ivecs));
-        if(end < 0){ // bounds checking
-            //if end not set
-            //if less than 0 and if nonzero array found, set it to arraylen in ms
-            if(arraysmp > 0)
-                end = (arraysmp/x->x_ksr);
-            else //else default to max size of float (defaults to size of array with boundschecking
-                end = REC_MAXBD;
-        };
-        if(start < 0)
-            start = 0;
-        x->x_start = start;
+        x->x_ivecs = getbytes(x->x_numchans * sizeof(*x->x_ivecs)); // allocate in vectors
+        x->x_start = start < 0 ? 0 : start;
+        if(end < 0) // if end not set, set to array size in ms or max float size
+            end = arraysmp > 0 ? arraysmp/x->x_ksr : REC_MAXBD;
         x->x_end = end;
         buffer_setminsize(x->x_buffer, 2);
-        rec_continue(x, continuemode);
-        rec_loop(x, loopstatus);
+        x->x_continue_flag = (continue_flag != 0);
+        x->x_loop_flag = (loop_flag != 0);
         x->x_clock = clock_new(x, (t_method)rec_tick);
         x->x_clocklasttick = clock_getlogicaltime();
         inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
-        for(int i = 1; i < x->x_numchans; i++)
+        for(t_int i = 1; i < x->x_numchans; i++)
             inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
-        x->x_outlet = outlet_new(&x->x_obj, gensym("signal")); // sync output
+        x->x_outlet = outlet_new(&x->x_obj, gensym("signal"));
     };
     return(x);
 errstate:
-    post("rec~: improper args");
+    post("[rec~]: improper args");
     return NULL;
 }
 
