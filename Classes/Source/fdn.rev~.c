584ed7bdc8af394f18aae385b38ab1c943ad1bdb
porres <porres@gmail.com>
2019-02-09 00:40:15 -0200

    Update fdn.rev~.c

--- Classes/Source/fdn.rev~.c
+++ Classes/Source/fdn.rev~.c
@@ -1,13 +1,7 @@
-// 25
+// 12
 
-// This is a modified version of the code of creb/fdn~ by Alexandre Porres
-
-// [fdn.rev~] is a Feedback Delay Network with a householder reflection feedback matrix
-//    (In - 2/n 11T).
-
-// each delay line is filtered with a 1st order filter:
-//        H(z) = 2 gl gh / (gl + gh - z^-1 (gl - gh)) - where gl: dc gain & gh: ny gain
-//        Difference equation: yk = (2 gl gh ) / (gl + gh) x + (gl - gh) / (gl + gh) yk-1
+// This is Feedback Delay Network with a householder reflection feedback matrix (In - 2/n 11T)
+// a modification of [creb/fdn~] by Alexandre Porres
 
 // TODO (from original code) - Add: delay time generation code / prime calculation for delay lengths
 //       / more diffuse feedback matrix (hadamard) & check filtering code
@@ -40,6 +34,7 @@ typedef union{
 
 #define IS_DENORMAL(f) (((((t_flint)(f)).i) & 0x7f800000) == 0)
 
+// por mim essa merda toda vem pra baixo
 typedef struct fdnctl{
     t_int    c_order;
     t_int    c_maxorder;
@@ -49,8 +44,6 @@ typedef struct fdnctl{
     t_float *c_buf;
     t_float *c_gain_in;
     t_float *c_gain_state;
-    t_float  c_t60_lo;
-    t_float  c_t60_hi;
     t_int   *c_tap;         // cirular feed: N+1 pointers: 1 read, (N-1)r/w, 1 write
     t_float *c_time_ms;
     t_int    c_bufsize;
@@ -63,33 +56,40 @@ typedef struct fdn{
     t_object x_obj;
     t_fdnctl x_ctl;
     t_int    x_exp;
+    t_float  x_damping;
+    t_float  x_t60_lo;
+    t_float  x_t60_hi;
 }t_fdn;
 
 t_class *fdn_class;
 
-// Delay lines are filtered by a 1 pole filter (gl: DC gain, gh: Nyquist gain):
-// The equation is: yn = (2*gl*gh ) / (gl+gh) x + (gl-gh) / (gl+gh) y[n-1]
+// Decay filter equation: yn = (2*gl*gh ) / (gl+gh) x + (gl-gh) / (gl+gh) y[n-1]
+// were gl is the DC gain and gh is the Nyquist gain (both calculated from t_60):
 // Source: https://ccrma.stanford.edu/~jos/pasp/First_Order_Delay_Filter_Design.html
 static void fdn_setgain(t_fdn *x){
     t_float gl, gh;
-    t_float t60_l = x->x_ctl.c_t60_lo * 1000;
-    t_float t60_h = x->x_ctl.c_t60_hi * 1000;
     for(t_int i = 0; i < x->x_ctl.c_order; i++){
-        gl = pow(10, -3.f * x->x_ctl.c_time_ms[i] / t60_l);
-        gh = pow(10, -3.f * x->x_ctl.c_time_ms[i] / t60_h);
+        gl = pow(10, -3.f * x->x_ctl.c_time_ms[i] / x->x_t60_lo);
+        gh = pow(10, -3.f * x->x_ctl.c_time_ms[i] / x->x_t60_hi);
         x->x_ctl.c_gain_in[i] = 2.0f * gl * gh / (gl + gh);
         x->x_ctl.c_gain_state[i] = (gl - gh) / (gl + gh);
     }
 }
 
-static void fdn_decaylow(t_fdn *x, t_float f){
-    x->x_ctl.c_t60_lo = f < .01f ? .01f : f;
+static void fdn_set_t60_hi(t_fdn *x){
+    t_float t60 = x->x_t60_lo;
+    x->x_t60_hi = t60 + (10 - t60) * x->x_damping; // rescale from 10ms to t60]
     fdn_setgain(x);
 }
 
-static void fdn_decayhigh(t_fdn *x, t_float f){
-    x->x_ctl.c_t60_hi = f < .01f ? .01f : f;
-    fdn_setgain(x);
+static void fdn_damping(t_fdn *x, t_float f){
+    x->x_damping = (f < 0 ? 0 : f > 100 ? 100 : f) / 100;
+    fdn_set_t60_hi(x);
+}
+
+static void fdn_time(t_fdn *x, t_float f){
+    x->x_t60_lo = (f < .01f ? .01f : f) * 1000;
+    fdn_set_t60_hi(x);
 }
 
 static void fdn_delsizes(t_fdn *x){
@@ -118,20 +118,21 @@ static void fdn_order(t_fdn *x, t_int order){
 static void fdn_set(t_fdn *x, t_float size, t_float min, t_float max){
     t_int order = ((int)size) & 0xfffffffc; // clip to powers of 2
     if(order < 4){
-        post("[fdn.rev~]: order needs to be at least 4");
-        return;
+        post("[fdn.rev~]: number of delay lines clipped to minimum of 4");
+        order = 4;
     }
     if(order > x->x_ctl.c_maxorder){
-        post("[fdn.rev~]: order %d can't be larger than maxorder %d:", order, x->x_ctl.c_maxorder);
-        return;
+        post("[fdn.rev~]: number of delay lines clipped to maximum of %d:",
+             x->x_ctl.c_maxorder);
+        order = x->x_ctl.c_maxorder;
     }
     if(min <= 0){
-        post("[fdn.rev~]: min can't be equal or less than 0");
-        return;
+        post("[fdn.rev~]: min can't be equal or less than 0, clipped to 1");
+        min = 1;
     }
     if(max <= 0){
-        post("[fdn.rev~]: max can't be equal or less than 0");
-        return;
+        post("[fdn.rev~]: max can't be equal or less than 0, clipped to 1");
+        max = 1;
     }
     t_float inc;
     if(x->x_exp)
@@ -159,15 +160,20 @@ static void fdn_list (t_fdn *x,  t_symbol *s, int argc, t_atom *argv){
     dummy = NULL;
     int order = argc & 0xfffffffc; // clip to powers of two
     if(order < 4){
-        post("[fdn.rev~]: order needs to be at least 4");
+        post("[fdn.rev~]: needs at least 4 delay taps (list ignored)");
         return;
     }
-    if(order > x->x_ctl.c_maxorder)
-        post("[fdn.rev~]: order clipped to %d:", order = x->x_ctl.c_maxorder);
+    if(order > x->x_ctl.c_maxorder){
+        post("[fdn.rev~]: list can't be bigger than %d (list ignored)",
+             order = x->x_ctl.c_maxorder);
+        return;
+    }
     fdn_order(x, order);
     for(t_int i = 0; i < order; i++){
         if(argv[i].a_type == A_FLOAT)
             x->x_ctl.c_time_ms[i] = argv[i].a_w.w_float;
+        else
+            post("[fdn.rev~]: non float element in the list ignored");
     }
     fdn_delsizes(x);
 }
@@ -176,6 +182,8 @@ static void fdn_print(t_fdn *x){
     post("[fdn.rev~]: max delay lines %d, buffer size %d samples (%.2f ms)",
          x->x_ctl.c_maxorder, x->x_ctl.c_bufsize ,
          ((float)x->x_ctl.c_bufsize) * 1000 / sys_getsr());
+    post("damping = %.2f%%, t60_lo = %.2f sec, t60_hi = %.2f sec",
+         x->x_damping * 100, x->x_t60_lo / 1000, x->x_t60_hi / 1000);
     post("delay times:");
     for(t_int i = 0; i < x->x_ctl.c_order; i++)
         post("line %d: %.2f ms", i + 1, x->x_ctl.c_time_ms[i]);
@@ -280,30 +288,103 @@ static void fdn_free(t_fdn *x){
         free (x->x_ctl.c_vectorbuffer);
 }
 
-static void *fdn_new(t_floatarg size){
+static void *fdn_new(t_symbol *s, int ac, t_atom *av){
     t_fdn *x = (t_fdn *)pd_new(fdn_class);
+    t_symbol *dummy = s;
+    dummy = NULL;
+////////
     t_int order = 1024; // maximum order is 1024
-/*    if(order < 4)
-        order = 4; */
+    t_int size = 19;
+    t_float t60 = 4;
+    t_float damping = 0;
+    x->x_exp = 0;
+////////////////////////////////////////////////////////////////////////////////////
+    int argnum = 0;
+    int flag = 0;
+    while(ac > 0){
+        if(av->a_type == A_FLOAT && !flag){
+            // t_float aval = atom_getfloatarg(0, ac, av);
+            switch(argnum){
+                    /* case 0:
+                     freq = aval;
+                     break;
+                     case 1:
+                     reson = aval;
+                     break;*/
+                default:
+                    break;
+            };
+            argnum++;
+            ac--;
+            av++;
+        }
+        else if(av->a_type == A_SYMBOL){
+            if(!flag)
+                flag = 1;
+            t_symbol * cursym = atom_getsymbolarg(0, ac, av);
+            if(!strcmp(cursym->s_name, "-size")){
+                if(ac >= 2 && (av+1)->a_type == A_FLOAT){
+                    size = (int)atom_getfloatarg(1, ac, av);
+                    ac -= 2;
+                    av += 2;
+                }
+                else
+                    goto errstate;
+            }
+            else if(!strcmp(cursym->s_name, "-time")){
+                if(ac >= 2 && (av+1)->a_type == A_FLOAT){
+                    t60 = atom_getfloatarg(1, ac, av);
+                    ac -= 2;
+                    av += 2;
+                }
+                else
+                    goto errstate;
+            }
+            else if(!strcmp(cursym->s_name, "-damping")){
+                if(ac >= 2 && (av+1)->a_type == A_FLOAT){
+                    damping = atom_getfloatarg(1, ac, av);
+                    ac -= 2;
+                    av += 2;
+                }
+                else
+                    goto errstate;
+            }
+            else if(!strcmp(cursym->s_name, "-max")){
+                if(ac >= 2 && (av+1)->a_type == A_FLOAT){
+                    order = (int)atom_getfloatarg(1, ac, av);
+                    ac -= 2;
+                    av += 2;
+                }
+                else
+                    goto errstate;
+            }
+            else if(!strcmp(cursym->s_name, "-exp"))
+                x->x_exp = 1;
+            else
+                goto errstate;
+        }
+        else
+            goto errstate;
+    };
+////////////////////////////////////////////////////////////////////////////////////
+     if(order < 4)
+         order = 4;
+    order = ((int)order) & 0xfffffffc; // clip to powers of 2
+    
     if(size < 16) // size in samples
         size = 16;
     if(size > 30) // size in samples
         size = 30;
-    t_int bufsize = pow(2, (t_int)size);
+    size = pow(2, size);
     
-    t_int bufround = 1;
-    while(bufround < bufsize)
-        bufround *= 2;
-    bufsize = bufround;
-
-    t_float lo = 10;
-    t_float hi = 10;
+    t60 = (t60 < .01f ? .01f : t60) * 1000; // minimum 10 ms
+    x->x_t60_lo = t60;
+    x->x_damping = (damping < 0.f ? 0.f : damping > 100 ? 100 : damping) / 100;
+    x->x_t60_hi = t60 + (10 - t60) * x->x_damping;
     
-    x->x_exp = 0;
-
     x->x_ctl.c_maxorder = order;
-    x->x_ctl.c_buf = (float *)malloc(sizeof(float) * bufsize);
-    x->x_ctl.c_bufsize = bufsize;
+    x->x_ctl.c_bufsize = size;
+    x->x_ctl.c_buf = (float *)malloc(sizeof(float) * size);
     x->x_ctl.c_tap = (t_int *)malloc((order + 1) * sizeof(t_int));
     x->x_ctl.c_time_ms = (t_float *)malloc(order * sizeof(t_int));
     x->x_ctl.c_gain_in = (t_float *)malloc(order * sizeof(t_float));
@@ -313,34 +394,34 @@ static void *fdn_new(t_floatarg size){
     x->x_ctl.c_curvector = 0;
     x->x_ctl.c_vector[0] = &x->x_ctl.c_vectorbuffer[0];
     x->x_ctl.c_vector[1] = &x->x_ctl.c_vectorbuffer[order];
-    x->x_ctl.c_t60_lo = lo < .01f ? .01f : lo;
-    x->x_ctl.c_t60_hi = hi < .01f ? .01f : hi;
-// default preset
+
+// default input list
     t_atom at[4];
     SETFLOAT(at, 0.f);
     SETFLOAT(at+1, 0.f);
     SETFLOAT(at+2, 0.f);
     SETFLOAT(at+3, 0.f);
     fdn_list(x, &s_list, 4, at);
-// end of preset
-    
-    fdn_clear(x); // clear delay memory to zero
-    
-    inlet_new(&x->x_obj, &x->x_obj.ob_pd, gensym("float"), gensym("lo"));
-    inlet_new(&x->x_obj, &x->x_obj.ob_pd, gensym("float"), gensym("hi"));
+// clear delay memory to zero
+    fdn_clear(x);
+    inlet_new(&x->x_obj, &x->x_obj.ob_pd, gensym("float"), gensym("time"));
+    inlet_new(&x->x_obj, &x->x_obj.ob_pd, gensym("float"), gensym("damping"));
     outlet_new(&x->x_obj, gensym("signal"));
     outlet_new(&x->x_obj, gensym("signal"));
     return(void *)x;
+errstate:
+    pd_error(x, "[fdn.rev~]: improper args");
+    return NULL;
 }
 
 void setup_fdn0x2erev_tilde(void){
     fdn_class = class_new(gensym("fdn.rev~"), (t_newmethod)fdn_new,
-    	(t_method)fdn_free, sizeof(t_fdn), 0, A_DEFFLOAT, 0);
+    	(t_method)fdn_free, sizeof(t_fdn), 0, A_GIMME, 0);
     class_addmethod(fdn_class, nullfn, gensym("signal"), 0);
     class_addmethod(fdn_class, (t_method)fdn_dsp, gensym("dsp"), 0);
     class_addlist(fdn_class, (t_method)fdn_list);
-    class_addmethod(fdn_class, (t_method)fdn_decaylow, gensym("lo"), A_DEFFLOAT, 0);
-    class_addmethod(fdn_class, (t_method)fdn_decayhigh, gensym("hi"), A_DEFFLOAT, 0);
+    class_addmethod(fdn_class, (t_method)fdn_time, gensym("time"), A_DEFFLOAT, 0);
+    class_addmethod(fdn_class, (t_method)fdn_damping, gensym("damping"), A_DEFFLOAT, 0);
     class_addmethod(fdn_class, (t_method)fdn_set, gensym("set"),
                     A_DEFFLOAT, A_DEFFLOAT, A_DEFFLOAT, 0);
     class_addmethod(fdn_class, (t_method)fdn_exponential, gensym("exp"), A_DEFFLOAT, 0);
