09ce122bec5bf9440c79361330853fd217f8a748
porres <porres@gmail.com>
2019-04-30 15:30:59 -0300

    update change
    remove 'set' method and include a new inlet to set internal state

--- Classes/Source/changed.c
+++ Classes/Source/changed.c
@@ -3,34 +3,53 @@
 #include "m_pd.h"
 
 static t_class *changed_class;
+static t_class *changed_proxy_class;
+
+typedef struct _changed_proxy{
+    t_pd              p_pd;
+    struct _changed  *p_owner;
+}t_changed_proxy;
 
 typedef struct _changed{
-    t_object   x_obj;
-    t_atom     x_a[4096];
-    int        x_c;
-    int        x_change;
-    t_symbol  *x_sym;
-//    t_inlet   *x_set_inlet;
-    t_outlet  *x_bang_out;
-} t_changed;
+    t_object        x_obj;
+    t_changed_proxy x_proxy;
+    t_atom          x_a[4096];
+    int             x_c;
+    int             x_change;
+    t_symbol       *x_sym;
+    t_outlet       *x_bang_out;
+}t_changed;
+
+static void changed_proxy_init(t_changed_proxy * p, t_changed *x){
+    p->p_pd = changed_proxy_class;
+    p->p_owner = x;
+}
+
+static void changed_proxy_anything(t_changed_proxy *p, t_symbol *s, int ac, t_atom *av){
+    t_changed *x = p->p_owner;
+    x->x_sym = s;
+    x->x_c = ac;
+    for(int i = 0; i < ac; i++)
+        x->x_a[i] = av[i];
+}
 
 static void changed_bang(t_changed *x){
     outlet_anything(x->x_obj.ob_outlet, x->x_sym, x->x_c, x->x_a);
 }
 
-static void changed_anything(t_changed *x, t_symbol *s, int argc, t_atom *argv){
+static void changed_anything(t_changed *x, t_symbol *s, int ac, t_atom *av){
     int i;
     x->x_sym = s;
-    if(x->x_c == argc){
-        for(i = 0; i < argc; i++){
+    if(x->x_c == ac){
+        for(i = 0; i < ac; i++){
             if(x->x_a[i].a_type == A_FLOAT){
-                if(x->x_a[i].a_w.w_float != argv[i].a_w.w_float){
+                if(x->x_a[i].a_w.w_float != av[i].a_w.w_float){
                     x->x_change = 1;
                     break;
                 }
             }
             else if(x->x_a[i].a_type == A_SYMBOL){
-                if(x->x_a[i].a_w.w_symbol != argv[i].a_w.w_symbol){
+                if(x->x_a[i].a_w.w_symbol != av[i].a_w.w_symbol){
                     x->x_change = 1;
                     break;
                 }
@@ -38,70 +57,48 @@ static void changed_anything(t_changed *x, t_symbol *s, int argc, t_atom *argv){
         }
     }
     else{
-        x->x_c = argc;
+        x->x_c = ac;
         x->x_change = 1;
     }
     if(x->x_change){
-        for (i = 0; i < argc; i++)
-            x->x_a[i] = argv[i];
-        outlet_anything(x->x_obj.ob_outlet, s, argc, argv);
+        for (i = 0; i < ac; i++)
+            x->x_a[i] = av[i];
+        outlet_anything(x->x_obj.ob_outlet, s, ac, av);
         x->x_change = 0;
     }
     else
         outlet_bang(x->x_bang_out);
 }
 
-static void changed_set(t_changed *x, t_symbol *s, int argc, t_atom *argv){
-    int i;
-    if(argc == 0)
-        x->x_sym = &s_;
-    else if(argc == 1){
-        if ((argv)->a_type == A_SYMBOL){
-            x->x_sym = atom_getsymbol(argv);
-            argc--;
-        }
-        else if ((argv)->a_type == A_FLOAT)
-            x->x_sym = &s_float;
-    }
-    else{
-        if((argv)->a_type == A_SYMBOL){
-            x->x_sym = atom_getsymbol(argv++);
-            argc--;
-        }
-        else
-            x->x_sym = &s_list;
-    }
-    x->x_c = argc;
-    for (i = 0; i < argc; i++)
-        x->x_a[i] = argv[i];
-}
-
-static void *changed_new(t_symbol *s, int argc, t_atom *argv){
+static void *changed_new(t_symbol *s, int ac, t_atom *av){
     t_changed *x = (t_changed *)pd_new(changed_class);
+    t_symbol *dummy = s;
+    dummy = NULL;
     int i;
-    if(argc == 0)
+    if(ac == 0)
         x->x_sym = &s_;
-    else if(argc == 1){
-        if ((argv)->a_type == A_SYMBOL){
-            x->x_sym = atom_getsymbol(argv);
-            argc--; 
+    else if(ac == 1){
+        if ((av)->a_type == A_SYMBOL){
+            x->x_sym = atom_getsymbol(av);
+            ac--;
         }
-        else if ((argv)->a_type == A_FLOAT)
+        else if ((av)->a_type == A_FLOAT)
                 x->x_sym = &s_float;
     }
     else{
-        if((argv)->a_type == A_SYMBOL){
-            x->x_sym = atom_getsymbol(argv++);
-            argc--;
+        if((av)->a_type == A_SYMBOL){
+            x->x_sym = atom_getsymbol(av++);
+            ac--;
         }
         else
             x->x_sym = &s_list;
     }
-    x->x_c = argc;
-    for(i = 0; i < argc; i++)
-        x->x_a[i] = argv[i];
+    x->x_c = ac;
+    for (i = 0; i < ac; i++)
+        x->x_a[i] = av[i];
     x->x_change = 0;
-//    inlet_new((t_object *)x, (t_pd *)x, &s_anything, gensym("set"));
+    changed_proxy_init(&x->x_proxy, x);
+    inlet_new(&x->x_obj, &x->x_proxy.p_pd, 0, 0);
     outlet_new(&x->x_obj, &s_anything);
     x->x_bang_out = outlet_new(&x->x_obj, &s_bang);
     return (x);
@@ -112,5 +109,7 @@ void changed_setup(void){
     	sizeof(t_changed), 0, A_GIMME, 0);
     class_addbang(changed_class, changed_bang);
     class_addanything(changed_class, changed_anything);
-    class_addmethod(changed_class, (t_method)changed_set, gensym("set"), A_GIMME, 0);
+    changed_proxy_class = (t_class *)class_new(gensym("changed proxy"),
+                            0, 0, sizeof(t_changed_proxy), 0, 0);
+    class_addanything(changed_proxy_class, changed_proxy_anything);
 }
