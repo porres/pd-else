87ded98947a8969866a08b6dec237027a666e4ad
porres <porres@gmail.com>
2019-07-14 03:28:38 -0300

    exponential glides

--- Classes/Source/glide2~.c
+++ Classes/Source/glide2~.c
@@ -1,136 +1,196 @@
-// porres 2019
+// porres 2017
 
 #include "m_pd.h"
 #include "math.h"
 
 typedef struct _glide2{
     t_object x_obj;
-    t_inlet  *glide2;
+    t_inlet  *x_inlet_ms_up;
+    t_inlet  *x_inlet_ms_down;
     t_float  x_in;
-    int      x_nup;
-    int      x_ndown;
-    double   x_upcoef;
-    double   x_downcoef;
-    t_float  x_last;
-    t_float  x_target;
-    t_float  x_sr_khz;
-    double   x_incr;
-    int      x_nleft;
-    int      x_change; // reset coeffs
+    int      x_n_up;
+    int      x_nleft_up;
+    int      x_n_down;
+    int      x_nleft_down;
+    int      x_reset;
+    float    x_last_in;
+    float    x_last_out;
+    float    x_sr_khz;
+    float    x_delta;
+    float    x_start;
+    float    x_exp;
 }t_glide2;
 
 static t_class *glide2_class;
 
+static void glide2_exp(t_glide2 *x, t_floatarg f){
+    x->x_exp = f;
+}
+
+static void glide2_reset(t_glide2 *x){
+    x->x_reset = 1;
+}
+
+static float glide2_get_step(t_glide2 *x, int n, int nleft){
+    float step = (float)(n-nleft)/(float)n;
+    if(fabs(x->x_exp) != 1){ // EXPONENTIAL
+        if(x->x_exp >= 0){ // positive exponential
+            if(x->x_delta > 0)
+                step = pow(step, x->x_exp);
+            else
+                step = 1-pow(1-step, x->x_exp);
+        }
+        else{ // negative exponential
+            if(x->x_delta > 0)
+                step = 1-pow(1-step, -x->x_exp);
+            else
+                step = pow(step, -x->x_exp);
+        }
+    }
+    return(step);
+}
+
 static t_int *glide2_perform(t_int *w){
     t_glide2 *x = (t_glide2 *)(w[1]);
     int n = (int)(w[2]);
-    t_float *in = (t_float *)(w[3]);
-    t_float *out = (t_float *)(w[4]);
-    t_float last = x->x_last;
-    t_float target = x->x_target;
-    int change = x->x_change;
-    double incr = x->x_incr;
-    int nleft = x->x_nleft;
+    t_float *in1 = (t_float *)(w[3]);
+    t_float *in2 = (t_float *)(w[4]);
+    t_float *in3 = (t_float *)(w[5]);
+    t_float *out = (t_float *)(w[6]);
+    float last_in = x->x_last_in;
+    float last_out = x->x_last_out;
+    float start = x->x_start;
     while(n--){
-        t_float f = *in++;
-        if(f != target || change){
-            target = f;
-            change = 0;
-            if(f > last){
-                if(x->x_nup > 1){
-                    incr = (f - last) * x->x_upcoef;
-                    nleft = x->x_nup;
-                    *out++ = (last += incr);
-                    continue;
-                }
-            }
-            else if(f < last){
-                if(x->x_ndown > 1){
-                    incr = (f - last) * x->x_downcoef;
-                    nleft = x->x_ndown;
-                    *out++ = (last += incr);
-                    continue;
-                }
-            }
-            incr = 0.;
-            nleft = 0;
-            *out++ = last = f;
+        t_float in = *in1++;
+        t_float ms_up = *in2++;
+        t_float ms_down = *in3++;
+        if(ms_up <= 0)
+            ms_up  = 0;
+        if(ms_down <= 0)
+            ms_down  = 0;
+        x->x_n_up = (int)roundf(ms_up * x->x_sr_khz) + 1; // n samples
+        x->x_n_down = (int)roundf(ms_down * x->x_sr_khz) + 1; // n samples
+        if(x->x_reset){ // reset
+            x->x_nleft_up = x->x_nleft_down = 0;
+            x->x_reset = 0;
+            *out++ = last_out = last_in = in;
         }
-        else if(nleft > 0){
-            *out++ = (last += incr);
-            if(--nleft == 1){
-                incr = 0.;
-                last = target;
+        else if(in != last_in){ // input change, update
+            start = last_out;
+            x->x_delta = (in - last_out);
+            if(x->x_delta > 0){
+                x->x_nleft_up = x->x_n_up - 1;
+                float step = glide2_get_step(x, x->x_n_up, x->x_nleft_up);
+                float inc = step * x->x_delta;
+                *out++ = last_out = (last_in + inc);
+                last_in = in;
+            }
+            else{
+                x->x_nleft_down = x->x_n_down - 1;
+                float step = glide2_get_step(x, x->x_n_down, x->x_nleft_down);
+                float inc = step * x->x_delta;
+                *out++ = last_out = (last_in + inc);
+                last_in = in;
             }
         }
-        else
-            *out++ = target;
+        else{
+            if(x->x_delta > 0){
+                if(x->x_nleft_up > 0){
+                    x->x_nleft_up--;
+                    float step = glide2_get_step(x, x->x_n_up, x->x_nleft_up);
+                    float inc = step * x->x_delta;
+                    *out++ = last_out = (start + inc);
+                }
+                else
+                    *out++ = last_out = last_in = in;
+            }
+            else{
+                if(x->x_nleft_down > 0){
+                    x->x_nleft_down--;
+                    float step = glide2_get_step(x, x->x_n_down, x->x_nleft_down);
+                    float inc = step * x->x_delta;
+                    *out++ = last_out = (start + inc);
+                }
+                else
+                    *out++ = last_out = last_in = in;
+            }
+        }
+        
     };
-    x->x_change = change;
-    x->x_last = (PD_BIGORSMALL(last) ? 0. : last);
-    x->x_target = (PD_BIGORSMALL(target) ? 0. : target);
-    x->x_incr = incr;
-    x->x_nleft = nleft;
-    return(w+5);
+    x->x_start = start;
+    x->x_last_in = last_in;
+    x->x_last_out = last_out;
+    return(w+7);
 }
 
 static void glide2_dsp(t_glide2 *x, t_signal **sp){
     x->x_sr_khz = sp[0]->s_sr * 0.001;
-    dsp_add(glide2_perform, 4, x, sp[0]->s_n, sp[0]->s_vec, sp[1]->s_vec);
+    dsp_add(glide2_perform, 6, x, sp[0]->s_n, sp[0]->s_vec,
+            sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec);
 }
 
-static void glide2_up(t_glide2 *x, t_floatarg f){
-    double upcoef;
-    int i = (int)roundf(f * x->x_sr_khz);
-    if(i > 1){
-        x->x_nup = i;
-        upcoef = 1. / (float)i;
-    }
-    else{
-        x->x_nup = 0;
-        upcoef = 0.;
-    };
-    if(upcoef != x->x_upcoef){
-        x->x_upcoef = upcoef;
-        x->x_change = 1;
-    };
-}
-
-static void glide2_down(t_glide2 *x, t_floatarg f){
-    double downcoef;
-    int i = (int)roundf(f * x->x_sr_khz);
-    if(i > 1){
-        x->x_ndown = i;
-        downcoef = 1./(float)i;
-    }
-    else{
-        x->x_ndown = 0;
-        downcoef = 0.;
-    };
-    if(downcoef != x->x_downcoef){
-        x->x_downcoef = downcoef;
-        x->x_change = 1;
-    };
-}
-
-static void *glide2_new(t_floatarg up, t_floatarg down){
+static void *glide2_new(t_symbol *s, int ac, t_atom *av){
     t_glide2 *x = (t_glide2 *)pd_new(glide2_class);
+    t_symbol *cursym = s;
+    float ms_up = 0;
+    float ms_down = 0;
     x->x_sr_khz = sys_getsr() * 0.001;
-    glide2_up(x, up);
-    glide2_down(x, down);
-    x->x_change = 1;
-    x->x_last = x->x_target = x->x_incr = x->x_nleft = 0;
-    inlet_new((t_object *)x, (t_pd *)x, &s_float, gensym("up"));
-    inlet_new((t_object *)x, (t_pd *)x, &s_float, gensym("down"));
+    x->x_last_in = 0.;
+    x->x_reset = x->x_nleft_up = 0;
+    x->x_exp = 1.;
+    int symarg = 0;
+    int argnum = 0;
+    while(ac > 0){
+        if(av->a_type == A_FLOAT && !symarg){
+            float argval = atom_getfloatarg(0, ac, av);
+            switch(argnum){
+                case 0:
+                    ms_up = argval;
+                    break;
+                case 1:
+                    ms_down = argval;
+                    break;
+                default:
+                    break;
+            };
+            argnum++;
+            ac--;
+            av++;
+        }
+        else if(av->a_type == A_SYMBOL){
+            if(!symarg)
+                symarg = 1;
+            cursym = atom_getsymbolarg(0, ac, av);
+            if(cursym == gensym("-exp")){
+                if(ac == 2){
+                    ac--, av++;
+                    x->x_exp = atom_getfloatarg(0, ac, av);
+                    ac--, av++;
+                }
+                else goto errstate;
+            }
+            else goto errstate;
+        }
+        else goto errstate;
+    }
+    x->x_inlet_ms_up = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
+    pd_float((t_pd *)x->x_inlet_ms_up, ms_up);
+    x->x_inlet_ms_down = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
+    pd_float((t_pd *)x->x_inlet_ms_down, ms_down);
     outlet_new((t_object *)x, &s_signal);
     return(x);
+errstate:
+    pd_error(x, "[glide2~]: improper args");
+    return NULL;
 }
 
 void glide2_tilde_setup(void){
     glide2_class = class_new(gensym("glide2~"), (t_newmethod)glide2_new, 0,
-            sizeof(t_glide2), 0, A_DEFFLOAT, A_DEFFLOAT, 0);
+                             sizeof(t_glide2), 0, A_GIMME, 0);
     CLASS_MAINSIGNALIN(glide2_class, t_glide2, x_in);
     class_addmethod(glide2_class, (t_method) glide2_dsp, gensym("dsp"), A_CANT, 0);
-    class_addmethod(glide2_class, (t_method)glide2_up, gensym("up"), A_FLOAT, 0);
-    class_addmethod(glide2_class, (t_method)glide2_down, gensym("down"), A_FLOAT, 0);
+    class_addmethod(glide2_class, (t_method)glide2_reset, gensym("reset"), 0);
+    class_addmethod(glide2_class, (t_method)glide2_exp, gensym("exp"), A_FLOAT, 0);
 }
+
+
--- Classes/Source/glide~.c
+++ Classes/Source/glide~.c
@@ -5,125 +5,147 @@
 
 typedef struct _glide{
     t_object x_obj;
-    t_float  x_in;
     t_inlet  *x_inlet_ms;
+    t_float  x_in;
     int      x_n;
-    double   x_coef;
-    t_float  x_last;
-    t_float  x_target;
-    t_float  x_sr_khz;
-    double   x_incr;
     int      x_nleft;
     int      x_reset;
+    float    x_last_in;
+    float    x_last_out;
+    float    x_sr_khz;
+    float    x_delta;
+    float    x_start;
+    float    x_exp;
 }t_glide;
 
+static t_class *glide_class;
+
+static void glide_exp(t_glide *x, t_floatarg f){
+    x->x_exp = f;
+}
 
 static void glide_reset(t_glide *x){
     x->x_reset = 1;
 }
 
-static t_class *glide_class;
+static float glide_get_step(t_glide *x){
+    float step = (float)(x->x_n-x->x_nleft)/(float)x->x_n;
+    if(fabs(x->x_exp) != 1){ // EXPONENTIAL
+        if(x->x_exp >= 0){ // positive exponential
+            if(x->x_delta > 0)
+                step = pow(step, x->x_exp);
+            else
+                step = 1-pow(1-step, x->x_exp);
+        }
+        else{ // negative exponential
+            if(x->x_delta > 0)
+                step = 1-pow(1-step, -x->x_exp);
+            else
+                step = pow(step, -x->x_exp);
+        }
+    }
+    return(step);
+}
 
 static t_int *glide_perform(t_int *w){
     t_glide *x = (t_glide *)(w[1]);
-    int nblock = (int)(w[2]);
+    int n = (int)(w[2]);
     t_float *in1 = (t_float *)(w[3]);
     t_float *in2 = (t_float *)(w[4]);
     t_float *out = (t_float *)(w[5]);
-    t_float last = x->x_last;
-    t_float target = x->x_target;
-    double incr = x->x_incr;
-    int nleft = x->x_nleft;
-    while (nblock--){
-        t_float f = *in1++;
+    float last_in = x->x_last_in;
+    float last_out = x->x_last_out;
+    float start = x->x_start;
+    while(n--){
+        t_float in = *in1++;
         t_float ms = *in2++;
-        if (ms < 0)
-            ms = 0;
-        x->x_n = roundf(ms * x->x_sr_khz);
-        double coef;
-        if (x->x_n == 0)
-            coef = 0.;
-        else
-            coef = 1. / (float)x->x_n;
-        
-        if(x->x_reset){
-            incr = 0.;
-            nleft = 0;
-            *out++ = last = target = f;
-            x->x_reset = 0;
+        if(ms <= 0)
+            ms  = 0;
+        x->x_n = (int)roundf(ms * x->x_sr_khz) + 1; // n samples
+        if(x->x_n == 1)
+            *out++ = last_out = last_in = in;
+        else{
+            if(x->x_reset){ // reset
+                x->x_nleft = 0;
+                x->x_reset = 0;
+                *out++ = last_out = last_in = in;
+            }
+            else if(in != last_in){ // input change, update
+                start = last_out;
+                x->x_delta = (in - last_out);
+                x->x_nleft = x->x_n - 1;
+                float inc = glide_get_step(x) * x->x_delta;
+                *out++ = last_out = (last_in + inc);
+                last_in = in;
+            }
+            else{
+                if(x->x_nleft > 0){
+                    x->x_nleft--;
+                    float inc = glide_get_step(x) * x->x_delta;
+                    *out++ = last_out = (start + inc);
+                }
+                else
+                    *out++ = last_out = last_in = in;
+            }
         }
-        
-        else if(coef != x->x_coef){
-            x->x_coef = coef;
-            if (f != last){
-                if (x->x_n > 1){
-                    incr = (f - last) * x->x_coef;
-                    nleft = x->x_n;
-                    *out++ = (last += incr);
-                    continue;
-                    }
-                }
-            incr = 0.;
-            nleft = 0;
-            *out++ = last = f;
-            }
-        
-        else if (f != target){
-            target = f;
-            if (f != last){
-                if (x->x_n > 1){
-                    incr = (f - last) * x->x_coef;
-                    nleft = x->x_n;
-                    *out++ = (last += incr);
-                    continue;
-                }
-            }
-	    incr = 0.;
-	    nleft = 0;
-	    *out++ = last = f;
-        }
-        
-        else if (nleft > 0){
-            *out++ = (last += incr);
-            if (--nleft == 1){
-                incr = 0.;
-                last = target;
-                }
-            }
-        else *out++ = target;
-        };
-    x->x_last = (PD_BIGORSMALL(last) ? 0. : last);
-    x->x_target = (PD_BIGORSMALL(target) ? 0. : target);
-    x->x_incr = incr;
-    x->x_nleft = nleft;
-    return (w + 6);
+    };
+    x->x_start = start;
+    x->x_last_in = last_in;
+    x->x_last_out = last_out;
+    return(w+6);
 }
 
 static void glide_dsp(t_glide *x, t_signal **sp){
     x->x_sr_khz = sp[0]->s_sr * 0.001;
-    dsp_add(glide_perform, 5, x, sp[0]->s_n, sp[0]->s_vec,
-        sp[1]->s_vec, sp[2]->s_vec);
+    dsp_add(glide_perform, 5, x, sp[0]->s_n, sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec);
 }
 
-static void *glide_new(t_floatarg ms){
+static void *glide_new(t_symbol *s, int ac, t_atom *av){
     t_glide *x = (t_glide *)pd_new(glide_class);
+    t_symbol *cursym = s;
+    float ms = 0;
     x->x_sr_khz = sys_getsr() * 0.001;
-    x->x_last = 0.;
-    x->x_target = 0.;
-    x->x_incr = 0.;
-    x->x_nleft = 0;
-    x->x_reset = 0;
-    x->x_coef = 0.;
+    x->x_last_in = 0.;
+    x->x_reset = x->x_nleft = 0;
+    x->x_exp = 1.;
+    int symarg = 0;
+    while(ac > 0){
+        if(av->a_type == A_FLOAT && !symarg){
+            ms = atom_getfloatarg(0, ac, av);
+            ac--;
+            av++;
+        }
+        else if(av->a_type == A_SYMBOL){
+            if(!symarg)
+                symarg = 1;
+            cursym = atom_getsymbolarg(0, ac, av);
+            if(cursym == gensym("-exp")){
+                if(ac == 2){
+                    ac--, av++;
+                    x->x_exp = atom_getfloatarg(0, ac, av);
+                    ac--, av++;
+                }
+                else goto errstate;
+            }
+            else goto errstate;
+        }
+        else goto errstate;
+    }
     x->x_inlet_ms = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
-        pd_float((t_pd *)x->x_inlet_ms, ms);
+    pd_float((t_pd *)x->x_inlet_ms, ms);
     outlet_new((t_object *)x, &s_signal);
-    return (x);
+    return(x);
+errstate:
+    pd_error(x, "[glide~]: improper args");
+    return NULL;
 }
 
 void glide_tilde_setup(void){
     glide_class = class_new(gensym("glide~"), (t_newmethod)glide_new, 0,
-				 sizeof(t_glide), 0, A_DEFFLOAT, 0);
+                            sizeof(t_glide), 0, A_GIMME, 0);
     CLASS_MAINSIGNALIN(glide_class, t_glide, x_in);
     class_addmethod(glide_class, (t_method) glide_dsp, gensym("dsp"), A_CANT, 0);
     class_addmethod(glide_class, (t_method)glide_reset, gensym("reset"), 0);
+    class_addmethod(glide_class, (t_method)glide_exp, gensym("exp"), A_FLOAT, 0);
 }
+
