21f0da89407f34e3718a60116b5fd5f33bc4be58
porres <porres@gmail.com>
2019-11-29 01:44:25 -0300

    receiver update

--- Classes/Source/receiver.c
+++ Classes/Source/receiver.c
@@ -1,7 +1,8 @@
 // porres
 
-#include <string.h>
 #include "m_pd.h"
+#include "g_canvas.h"
+#include <string.h>
 
 static t_class *receiver_class;
 static t_class *receiver_proxy_class;
@@ -14,6 +15,7 @@ typedef struct _receiver_proxy{
 typedef struct _receiver{
     t_object          x_obj;
     t_receiver_proxy  x_proxy;
+    t_glist          *x_glist;
     t_symbol         *x_sym;
     int               x_bound;
 }t_receiver;
@@ -23,11 +25,20 @@ static void receiver_proxy_init(t_receiver_proxy * p, t_receiver *x){
     p->p_owner = x;
 }
 
+static void receiver_proxy_clear(t_receiver_proxy *p){
+    t_receiver *x = p->p_owner;
+    if(x->x_bound){
+        pd_unbind(&x->x_obj.ob_pd, x->x_sym);
+        x->x_bound = 0;
+    }
+}
+
 static void receiver_proxy_symbol(t_receiver_proxy *p, t_symbol* s){
     t_receiver *x = p->p_owner;
     if(x->x_bound)
         pd_unbind(&x->x_obj.ob_pd, x->x_sym);
-    pd_bind(&x->x_obj.ob_pd, x->x_sym = s);
+    x->x_sym = canvas_realizedollar(x->x_glist, s);
+    pd_bind(&x->x_obj.ob_pd, x->x_sym);
     x->x_bound = 1;
 }
 
@@ -57,18 +68,18 @@ static void receiver_anything(t_receiver *x, t_symbol *s, int argc, t_atom *argv
 
 static void *receiver_new(t_symbol *s){
     t_receiver *x = (t_receiver *)pd_new(receiver_class);
+    x->x_glist = canvas_getcurrent();
+    receiver_proxy_init(&x->x_proxy, x);
+    inlet_new(&x->x_obj, &x->x_proxy.p_pd, 0, 0);
     x->x_sym = s;
-    if(!*x->x_sym->s_name){
-        receiver_proxy_init(&x->x_proxy, x);
-        inlet_new(&x->x_obj, &x->x_proxy.p_pd, 0, 0);
+    if(!*x->x_sym->s_name)
         x->x_bound = 0;
-    }
     else{
         pd_bind(&x->x_obj.ob_pd, s);
         x->x_bound = 1;
     }
     outlet_new(&x->x_obj, 0);
-    return (x);
+    return(x);
 }
 
 static void receiver_free(t_receiver *x){
@@ -88,4 +99,5 @@ void receiver_setup(void){
     receiver_proxy_class = (t_class *)class_new(gensym("receiver proxy"),
             0, 0, sizeof(t_receiver_proxy), 0, 0);
     class_addsymbol(receiver_proxy_class, receiver_proxy_symbol);
+    class_addmethod(receiver_proxy_class, (t_method)receiver_proxy_clear, gensym("clear"), 0);
 }
