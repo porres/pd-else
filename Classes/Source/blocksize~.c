74934b1a0404a62d7115b0a4be8f5a9d7dee36b9
porres <porres@gmail.com>
2020-01-17 00:15:30 -0300

    Update blocksize~.c

--- Classes/Source/blocksize~.c
+++ Classes/Source/blocksize~.c
@@ -10,6 +10,7 @@ typedef struct _blocksize{
     float       x_size;
     float       x_n;
     float       x_sr;
+    float       x_new_sr;
     int         x_mode;
 }t_blocksize;
 
@@ -56,6 +57,15 @@ static void blocksize_tick(t_blocksize *x){
         }
         outlet_float(x->x_obj.ob_outlet, size);
     }
+    else if(x->x_new_sr != x->x_sr && x->x_mode){
+        x->x_sr = x->x_new_sr;
+        float size = x->x_size;
+        if(x->x_mode == 1)
+            size *= (1000./x->x_sr);
+        else if(x->x_mode == 2)
+            size = x->x_sr/size;
+        outlet_float(x->x_obj.ob_outlet, size);
+    }
 }
 
 static t_int *blocksize_perform(t_int *w){
@@ -66,7 +76,7 @@ static t_int *blocksize_perform(t_int *w){
 
 static void blocksize_dsp(t_blocksize *x, t_signal **sp){
     x->x_n = (float)sp[0]->s_n;
-    x->x_sr = (float)sp[0]->s_sr;
+    x->x_new_sr = (float)sp[0]->s_sr;
     dsp_add(blocksize_perform, 1, x);
 }
 
@@ -78,7 +88,7 @@ static void *blocksize_new(t_symbol *s, int ac, t_atom *av){
     t_blocksize *x = (t_blocksize *)pd_new(blocksize_class);
     s = NULL;
     x->x_size = x->x_mode = 0;
-    x->x_sr = (float)sys_getsr();
+    x->x_sr = x->x_new_sr = (float)sys_getsr();
     if(ac){
         if(av->a_type == A_SYMBOL){
             t_symbol *sym = atom_getsymbolarg(0, ac, av);
