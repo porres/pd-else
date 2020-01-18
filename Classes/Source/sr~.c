2b7fbad39683ddc796cbfe3e0b9a58403b16f61a
porres <porres@gmail.com>
2020-01-17 21:33:16 -0300

    Update sr~.c

--- Classes/Source/sr~.c
+++ Classes/Source/sr~.c
@@ -1,4 +1,4 @@
-// Porres 2017-2018
+// Porres 2017-2020
 
 #include "m_pd.h"
 #include "s_stuff.h"
@@ -10,12 +10,14 @@ static t_class *sr_class;
 typedef struct _settings{
     int naudioindev, audioindev[MAXAUDIOINDEV], chindev[MAXAUDIOINDEV];
     int naudiooutdev, audiooutdev[MAXAUDIOOUTDEV], choutdev[MAXAUDIOOUTDEV];
-    int rate, advance, callback, blocksize;
+    int rate, advance, callback, sr;
 }t_settings;
 
 typedef struct _sr{
     t_object    x_obj;
-    t_float     x_sr;
+    t_clock    *x_clock;
+    float       x_sr;
+    float       x_new_sr;
     int         x_khz;
     int         x_period;
     t_settings  x_settings;
@@ -24,9 +26,9 @@ typedef struct _sr{
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 static void audio_settings(int *pnaudioindev, int *paudioindev, int *pchindev, int *pnaudiooutdev,
-    int *paudiooutdev, int *pchoutdev, int *prate, int *padvance, int *pcallback, int *pblocksize){
+    int *paudiooutdev, int *pchoutdev, int *prate, int *padvance, int *pcallback, int *psr){
         sys_get_audio_params(pnaudioindev , paudioindev , pchindev, pnaudiooutdev,
-            paudiooutdev, pchoutdev, prate, padvance, pcallback, pblocksize);
+            paudiooutdev, pchoutdev, prate, padvance, pcallback, psr);
 }
 
 static void sr_apply(t_sr *x){
@@ -34,19 +36,18 @@ static void sr_apply(t_sr *x){
     int ac = 2*MAXAUDIOINDEV + 2*MAXAUDIOOUTDEV + 3;
     int i = 0;
     for(i = 0; i < MAXAUDIOINDEV; i++){
-        SETFLOAT(av+i + 0*MAXAUDIOINDEV, (t_float)(x->x_settings.audioindev[i]));
-        SETFLOAT(av+i + 1*MAXAUDIOINDEV, (t_float)(x->x_settings.chindev[i]));
+        SETFLOAT(av+i + 0*MAXAUDIOINDEV, (float)(x->x_settings.audioindev[i]));
+        SETFLOAT(av+i + 1*MAXAUDIOINDEV, (float)(x->x_settings.chindev[i]));
     }
     for(i = 0; i < MAXAUDIOOUTDEV; i++){
-        SETFLOAT(av+i + 2*MAXAUDIOINDEV + 0*MAXAUDIOOUTDEV, (t_float)(x->x_settings.audiooutdev[i]));
-        SETFLOAT(av+i + 2*MAXAUDIOINDEV + 1*MAXAUDIOOUTDEV, (t_float)(x->x_settings.choutdev[i]));
+        SETFLOAT(av+i + 2*MAXAUDIOINDEV + 0*MAXAUDIOOUTDEV, (float)(x->x_settings.audiooutdev[i]));
+        SETFLOAT(av+i + 2*MAXAUDIOINDEV + 1*MAXAUDIOOUTDEV, (float)(x->x_settings.choutdev[i]));
     }
-    SETFLOAT(av+2 * MAXAUDIOINDEV + 2*MAXAUDIOOUTDEV + 0, (t_float)(x->x_settings.rate));
-    SETFLOAT(av+2 * MAXAUDIOINDEV + 2*MAXAUDIOOUTDEV + 1, (t_float)(x->x_settings.advance));
-    SETFLOAT(av+2 * MAXAUDIOINDEV + 2*MAXAUDIOOUTDEV + 2, (t_float)(x->x_settings.callback));
+    SETFLOAT(av+2 * MAXAUDIOINDEV + 2*MAXAUDIOOUTDEV + 0, (float)(x->x_settings.rate));
+    SETFLOAT(av+2 * MAXAUDIOINDEV + 2*MAXAUDIOOUTDEV + 1, (float)(x->x_settings.advance));
+    SETFLOAT(av+2 * MAXAUDIOINDEV + 2*MAXAUDIOOUTDEV + 2, (float)(x->x_settings.callback));
     if(gensym("pd")->s_thing)
         typedmess(gensym("pd")->s_thing, gensym("audio-dialog"), ac, av);
-    //  "; pd audio-dialog indev[4]/inch[4]/outdev[4]/outch[4]/rate/advance/callback"
 }
 
 static void get_settings(t_settings *setts){
@@ -55,7 +56,7 @@ static void get_settings(t_settings *setts){
     setts->callback = -1;
     audio_settings(&setts->naudioindev,  setts->audioindev,  setts->chindev,
                &setts->naudiooutdev, setts->audiooutdev, setts->choutdev, &setts->rate,
-               &setts->advance, &setts->callback,    &setts->blocksize);
+               &setts->advance, &setts->callback,    &setts->sr);
     for(i = setts->naudioindev; i < MAXAUDIOINDEV; i++){
         setts->audioindev[i] = 0;
         setts->chindev[i] = 0;
@@ -69,7 +70,7 @@ static void get_settings(t_settings *setts){
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 static void sr_set(t_sr *x, t_floatarg f){
-    t_int rate = (int)f;
+    int rate = (int)f;
     if(rate > 0){
         x->x_settings.rate = rate;
         sr_apply(x);
@@ -77,8 +78,8 @@ static void sr_set(t_sr *x, t_floatarg f){
 }
 
 static void sr_bang(t_sr *x){
-    t_float sr = sys_getsr();
-    x->x_sr = sr;
+    float sr = sys_getsr();
+    x->x_sr = x->x_new_sr = sr;
     if(x->x_khz)
         sr *= 0.001;
     if(x->x_period)
@@ -109,20 +110,24 @@ static void sr_sec(t_sr *x){
 }
 
 static void sr_loadbang(t_sr *x, t_floatarg action){
-    if (action == LB_LOAD)
+    if(action == LB_LOAD)
         sr_bang(x);
 }
 
+static void sr_tick(t_sr *x){
+    if(x->x_new_sr != x->x_sr)
+        sr_bang(x);
+}
+
+static t_int *sr_perform(t_int *w){
+    t_sr *x = (t_sr *)(w[1]);
+    clock_delay(x->x_clock, 0);
+    return(w+2);
+}
+
 static void sr_dsp(t_sr *x, t_signal **sp){
-    t_float sr = sys_getsr();
-    if(sr != x->x_sr){
-        x->x_sr = sr;
-        if(x->x_khz)
-            sr *= 0.001;
-        if(x->x_period)
-            sr = 1./sr;
-        outlet_float(x->x_obj.ob_outlet, sr);
-    }
+    x->x_new_sr = (float)sp[0]->s_sr;
+    dsp_add(sr_perform, 1, x);
 }
 
 static void *sr_new(t_symbol *s, int ac, t_atom *av){
@@ -132,38 +137,35 @@ static void *sr_new(t_symbol *s, int ac, t_atom *av){
     if(ac <= 2){
         while(ac){
             if(av->a_type == A_SYMBOL){
-                t_symbol *curarg = s; // get rid of warning
-                curarg = atom_getsymbolarg(0, ac, av);
-                if(!strcmp(curarg->s_name, "-khz"))
+                t_symbol *sym = s; // get rid of warning
+                sym = atom_getsymbolarg(0, ac, av);
+                if(sym == gensym("-khz"))
                     x->x_khz = 1;
-                else if(!strcmp(curarg->s_name, "-ms"))
+                else if(sym == gensym("-ms"))
                     x->x_khz = x->x_period = 1;
-                else if(!strcmp(curarg->s_name, "-sec"))
+                else if(sym == gensym("-sec"))
                     x->x_period = 1;
-                else
-                    goto errstate;
-                ac--;
-                av++;
+                else goto errstate;
+                ac--, av++;
             }
             else{
                 sr_set(x, atom_getfloatarg(0, ac, av));
-                ac--;
-                av++;
+                ac--, av++;
             }
         }
     }
-    else
-        goto errstate;
+    else goto errstate;
+    x->x_clock = clock_new(x, (t_method)sr_tick);
     outlet_new(&x->x_obj, &s_float);
-    return (x);
+    return(x);
     errstate:
-        pd_error(x, "sr~: improper args");
-        return NULL;
+        pd_error(x, "[sr~]: improper args");
+        return(NULL);
 }
 
 void sr_tilde_setup(void){
-    sr_class = class_new(gensym("sr~"),
-            (t_newmethod)sr_new, 0, sizeof(t_sr), 0, A_GIMME, 0);
+    sr_class = class_new(gensym("sr~"), (t_newmethod)sr_new,
+        0, sizeof(t_sr), 0, A_GIMME, 0);
     class_addmethod(sr_class, nullfn, gensym("signal"), 0);
     class_addmethod(sr_class, (t_method)sr_dsp, gensym("dsp"), A_CANT, 0);
     class_addmethod(sr_class, (t_method)sr_loadbang, gensym("loadbang"), A_DEFFLOAT, 0);
