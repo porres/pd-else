6828607b41efd039979fd66ff9b12919cda948fb
porres <porres@gmail.com>
2020-11-29 01:39:04 -0300

    routeall

--- Classes/Source/routeall.c
+++ Classes/Source/routeall.c
@@ -7,36 +7,51 @@ typedef struct _routeall{
     t_atom      *x_av;
     t_int       x_ac;
     t_int       x_bytes;
+    t_float     x_i;
     t_outlet    **x_outs;
     t_outlet    *x_out_unmatch;
 } t_routeall;
 
 static void routeall_bang(t_routeall *x){
-    t_int n;
-    for(n = 0; n < x->x_ac; n++){
-        if(x->x_av[n].a_type == A_SYMBOL){
-            if(x->x_av[n].a_w.w_symbol == gensym("bang")){
-                outlet_bang(x->x_outs[n]);
-                return;
+    int i = (int)x->x_i;
+    if(i < 0)
+        i = 0;
+    if(!i){
+        t_int n;
+        for(n = 0; n < x->x_ac; n++){
+            if(x->x_av[n].a_type == A_SYMBOL){
+                if(x->x_av[n].a_w.w_symbol == gensym("bang")){
+                    outlet_bang(x->x_outs[n]);
+                    return;
+                }
             }
         }
+        outlet_bang(x->x_out_unmatch);
     }
-    outlet_bang(x->x_out_unmatch);
+    else
+        outlet_bang(x->x_out_unmatch);
 }
 
 static void routeall_symbol(t_routeall *x, t_symbol *s){
-    t_int n;
-    t_symbol *arg;
-    for(n = 0; n < x->x_ac; n++){
-        if(x->x_av[n].a_type == A_SYMBOL){
-            arg = x->x_av[n].a_w.w_symbol;
-            if(arg == gensym("symbol") || arg == s){
-                outlet_symbol(x->x_outs[n], s);
-                return;
+    int i = (int)x->x_i;
+    if(i < 0)
+        i = 0;
+    if(!i){
+        t_int n;
+        t_symbol *arg;
+        for(n = 0; n < x->x_ac; n++){
+            if(x->x_av[n].a_type == A_SYMBOL){
+                arg = x->x_av[n].a_w.w_symbol;
+                if(arg == gensym("symbol") || arg == s){
+                    outlet_symbol(x->x_outs[n], s);
+                    return;
+                }
             }
         }
+        outlet_symbol(x->x_out_unmatch, s);
     }
-    outlet_symbol(x->x_out_unmatch, s);
+    else
+        outlet_symbol(x->x_out_unmatch, s);
 }
 
 static void routeall_list(t_routeall *x, t_symbol *s, int ac, t_atom *av){
@@ -51,40 +66,79 @@ static void routeall_list(t_routeall *x, t_symbol *s, int ac, t_atom *av){
         routeall_symbol(x, atom_getsymbol(av));
         return;
     }
-    for(n = 0; n < x->x_ac; n++){
-        if(x->x_av[n].a_type == A_SYMBOL){
-            arg = x->x_av[n].a_w.w_symbol;
-            if(arg == gensym("list") || arg == atom_getsymbol(av)){
-                outlet_list(x->x_outs[n], gensym("list"), ac, av);
-                return;
+    int i = (int)x->x_i;
+    if(i < 0)
+        i = 0;
+    if(i < ac){
+        for(n = 0; n < x->x_ac; n++){
+            if(x->x_av[n].a_type == A_SYMBOL){
+                arg = x->x_av[n].a_w.w_symbol;
+                if(!i){
+                    if(arg == gensym("list") || arg == atom_getsymbol(av)){
+                        outlet_list(x->x_outs[n], gensym("list"), ac, av);
+                        return;
+                    }
+                }
+                else{
+                    if(arg == atom_getsymbol(av+i)){
+                        outlet_list(x->x_outs[n], gensym("list"), ac, av);
+                        return;
+                    }
+                }
+            }
+            else{
+                if(x->x_av[n].a_w.w_float == atom_getfloat(av+i)){
+                    outlet_list(x->x_outs[n], gensym("list"), ac, av);
+                    return;
+                }
             }
         }
-        else{
-            if(x->x_av[n].a_w.w_float == atom_getfloat(av)){
-                outlet_list(x->x_outs[n], gensym("list"), ac, av);
-                return;
-            }
-        }
+        outlet_list(x->x_out_unmatch, gensym("list"), ac, av);
     }
-    outlet_list(x->x_out_unmatch, gensym("list"), ac, av);
+    else
+        outlet_list(x->x_out_unmatch, gensym("list"), ac, av);
 }
 
 static void routeall_anything(t_routeall *x, t_symbol *s, int ac, t_atom *av){
     t_int n;
-    for(n = 0; n < x->x_ac; n++){
-        if(x->x_av[n].a_type == A_SYMBOL){
-            if(x->x_av[n].a_w.w_symbol == s){
-                outlet_anything(x->x_outs[n], s, ac, av);
-                return;
+    int i = (int)x->x_i;
+    if(i < 0)
+        i = 0;
+    if(i < ac){
+        for(n = 0; n < x->x_ac; n++){
+            if(!i){
+                if(x->x_av[n].a_type == A_SYMBOL){
+                    if(x->x_av[n].a_w.w_symbol == s){
+                        outlet_anything(x->x_outs[n], s, ac, av);
+                        return;
+                    }
+                }
+            }
+            else{
+                if(x->x_av[n].a_type == A_SYMBOL){
+                    if(x->x_av[n].a_w.w_symbol == atom_getsymbol(av+i-1)){
+                        outlet_anything(x->x_outs[n], s, ac, av);
+                        return;
+                    }
+                }
+                else{
+                    if(x->x_av[n].a_w.w_float == atom_getfloat(av+i-1)){
+                        outlet_anything(x->x_outs[n], s, ac, av);
+                        return;
+                    }
+                }
             }
         }
+        outlet_anything(x->x_out_unmatch, s, ac, av);
     }
-    outlet_anything(x->x_out_unmatch, s, ac, av);
+    else
+        outlet_anything(x->x_out_unmatch, s, ac, av);
 }
 
 static void *routeall_new(t_symbol *s, int argc, t_atom *argv){
     s = NULL;
     t_routeall *x = (t_routeall *)pd_new(routeall_class);
+    x->x_i = 0;
     t_outlet **outs;
     if(!argc){
         x->x_ac = 1;
@@ -111,6 +165,7 @@ static void *routeall_new(t_symbol *s, int argc, t_atom *argv){
             argc--;
         }
     }
+    floatinlet_new(&x->x_obj, &x->x_i);
     x->x_outs = (t_outlet **)getbytes(x->x_ac * sizeof(*outs));
     int i;
     for (i = 0; i < x->x_ac; i++)
@@ -121,7 +176,7 @@ static void *routeall_new(t_symbol *s, int argc, t_atom *argv){
 
 void routeall_setup(void){
     routeall_class = class_new(gensym("routeall"), (t_newmethod)routeall_new,
-                                0, sizeof(t_routeall), 0, A_GIMME, 0);
+        0, sizeof(t_routeall), 0, A_GIMME, 0);
     class_addbang(routeall_class, routeall_bang);
     class_addsymbol(routeall_class, routeall_symbol);
     class_addlist(routeall_class, routeall_list);
