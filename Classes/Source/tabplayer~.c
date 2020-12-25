b8290025d1e02c88e42cea87101cc2f2c66c9676
porres <porres@gmail.com>
2020-12-25 01:51:12 -0300

    fade/xfade intabplayer~

--- Classes/Source/tabplayer~.c
+++ Classes/Source/tabplayer~.c
@@ -2,7 +2,7 @@
 
 #include <math.h>
 #include "m_pd.h"
-#include "magic.h" // needed for toggle start
+#include "magic.h"
 #include "buffer.h"
 #include <stdlib.h>
 
@@ -15,28 +15,33 @@ typedef struct _tabplayer{
     t_object    x_obj;
     t_buffer   *x_buffer;
     t_glist    *x_glist;
-    int         x_hasfeeders; //if there's a signal coming in the main inlet
-    float       x_sr_khz; //pd's sample rate
-    float       x_array_sr_khz; //array's sample rate in ms
-    double      x_sr_ratio; //sample rate ratio (array/pd)
-    float       x_stms; //start position in ms
-    float       x_endms; //end pos in ms
-    unsigned long long x_npts; //array size in samples
-    unsigned long long x_start; //start position in samp
-    unsigned long long x_end; //end pos in samp
-    unsigned long long x_rangesamp; //length of loop in samples
-    unsigned long long x_fadesamp; //length of fade in samples
-    float       x_fadems; // fade time (fade-in starts at stms, fade-out at endms in xfade mode)
-    double      x_rate; // rate of playback
+    int         x_hasfeeders;       // if there's a signal coming in the main inlet
+    float       x_sr_khz;           // pd's sample rate
+    float       x_array_sr_khz;     // array's sample rate
+    double      x_sr_ratio;         // sample rate ratio (array/pd)
+    float       x_stms;             // start position in ms
+    float       x_endms;            // end pos in ms
+    unsigned long long x_npts;      // array size in samples
+    unsigned long long x_start;     // start position in samp
+    unsigned long long x_end;       // end pos in samp
+    unsigned long long x_rangesamp; // length of loop in samples
+    unsigned long long x_fadesamp;  // length of fade in samples
+    int         x_isneg;
+    int         x_first;
+    float       x_fadems;           // fade time (fade-in starts at stms, fade-out at endms in xfade mode)
+    double      x_rate;             // rate of playback
     double      x_phase;
-    int         x_xfade; // flag to set to xfade mode
-    int         x_pos; // phase/play position
-    int         x_loop; //if loop or not
-    int         x_playing; //if playing
-    int         x_playnew; //if started playing this particular block
+    double      x_fade_point;
+    int         x_xfade;            // flag to set to xfade mode
+    int         x_fading_in;        // flag to see if we're fading
+    int         x_fading_out;       // flag to see if we're fading
+    int         x_position;         // phase/play position
+    int         x_loop;             // if loop or not
+    int         x_playing;          // if playing
+    int         x_playnew;          // if started playing this particular block
     int         x_n_ch;
-    t_float    *x_ivec; // input vector
-    t_float   **x_ovecs; //out vectors
+    t_float    *x_ivec;             // input vector
+    t_float   **x_ovecs;            // output vectors
     t_outlet   *x_donelet;
 }t_play;
 
@@ -100,6 +105,7 @@ static void tabplayer_reset(t_play *x){
 
 static void tabplayer_speed(t_play *x, t_floatarg f){
     x->x_rate = f / 100;
+    x->x_isneg = x->x_rate < 0;
 }
 
 /*static void tabplayer_arraysr(t_play *x, t_floatarg f){
@@ -120,14 +126,14 @@ static void tabplayer_set(t_play *x, t_symbol *s){
 }
 
 static void tabplayer_pos(t_play *x, t_floatarg f){
-    x->x_pos = 1;
-    double pos = f < 0 ? 0 : f > 1 ? 1 : (double)f;
-    x->x_phase = pos * x->x_rangesamp + x->x_start;
+    x->x_position = 1;
+    double position = f < 0 ? 0 : f > 1 ? 1 : (double)f;
+    x->x_phase = position * x->x_rangesamp + x->x_start;
     x->x_playing = x->x_playnew = 1; // start playing
 }
 
 static void tabplayer_bang(t_play *x){
-    x->x_pos = 0;
+    x->x_position = 0;
     x->x_playing = x->x_playnew = 1; // start playing
 }
 
@@ -187,27 +193,48 @@ static void tabplayer_loop(t_play *x, t_floatarg f){
     x->x_loop = f > 0 ? 1 : 0;
 }
 
-static double tabplayer_fadeInOut(t_play *x, double phase, double out){
-    
-    /* if(x->x_loop && x->x_xfade){  // check loop conditions
-        x->x_stxsamp = x->x_start + x->x_fadesamp;
-        if(phase < x->x_stxsamp){ // in xfade period, get fade gain }*/
-    
-    if(phase < (x->x_start + x->x_fadesamp)){
-        double fade = (phase - x->x_start) / x->x_fadesamp;
-        out *= sin(fade*HALF_PI);
+static double tabplayer_fade_gain(t_play *x, double phase){
+    double fadegain = 1;
+    x->x_fading_in = x->x_fading_out = 0;
+    if(x->x_isneg){
+        if(phase < (x->x_start + x->x_fadesamp)){ // fade in
+            x->x_fade_point = (phase - x->x_start) / x->x_fadesamp;
+            fadegain = sin(x->x_fade_point * HALF_PI);
+            x->x_fading_in = 1;
+        }
+        else if(phase > (x->x_end - x->x_fadesamp)){ // fade out
+            if(x->x_xfade && !x->x_first)
+                return(1);
+            else{
+                x->x_fade_point = (phase - (x->x_end - x->x_fadesamp)) / x->x_fadesamp;
+                fadegain = cos(x->x_fade_point * HALF_PI);
+                x->x_fading_out = 1;
+            }
+        }
     }
-    else if(phase > (x->x_end - x->x_fadesamp)){
-        double fade = (phase - (x->x_end - x->x_fadesamp)) / x->x_fadesamp;
-        out *= cos(fade*HALF_PI);
+    else{
+        if(phase < (x->x_start + x->x_fadesamp)){ // fade in
+            if(x->x_xfade && !x->x_first)
+                return(1);
+            else{
+                x->x_fade_point = (phase - x->x_start) / x->x_fadesamp;
+                fadegain = sin(x->x_fade_point * HALF_PI);
+                x->x_fading_in = 1;
+            }
+        }
+        else if(phase > (x->x_end - x->x_fadesamp)){ // fade out
+            x->x_fade_point = (phase - (x->x_end - x->x_fadesamp)) / x->x_fadesamp;
+            fadegain = cos(x->x_fade_point * HALF_PI);
+            x->x_fading_out = 1;
+        }
     }
-    return(out);
+    return(fadegain);
 }
 
-static double tabplayer_interp(t_play *x, int chidx, double phase){
+static double tabplayer_interp(t_play *x, int ch, double phase){
     double out = 0.;
     t_word **vectable = x->x_buffer->c_vectors; // ??
-    t_word *vp = vectable[chidx]; // ??
+    t_word *vp = vectable[ch]; // ??
     if(vp){
         float f,  a,  b,  c,  d, cmb;
         int maxindex = x->x_npts - 3;
@@ -230,9 +257,7 @@ static double tabplayer_interp(t_play *x, int chidx, double phase){
         c = vp[1].w_float;
         d = vp[2].w_float;
         cmb = c-b;
-        out = b + f*(cmb - ONE_SIXTH*(1. - f)*((d - a - 3.0f*cmb)*f + (d + 2.0f*a - 3.0f*b)));
-        if(x->x_fadesamp > 0)
-            out = tabplayer_fadeInOut(x, phase, out);
+        out = b + f*(cmb - ONE_SIXTH*(1.-f)*((d - a - 3.0f*cmb)*f + (d + 2.0f*a - 3.0f*b)));
     }
     return(out);
 }
@@ -241,15 +266,15 @@ static t_int *tabplayer_perform(t_int *w){
     t_play *x = (t_play *)(w[1]);
     t_buffer *buffer = x->x_buffer;
     int n = (int)(w[2]);
-    int chidx, i;
+    int ch, i;
     if(buffer->c_playable){
         if(x->x_hasfeeders){ // signal input present, indexing into array
             t_float *xin = x->x_ivec;
             for(i = 0; i < n; i++){
                 float phase = *xin++;
-                for(chidx = 0; chidx < x->x_n_ch; chidx++){
-                    t_float *out = *(x->x_ovecs+chidx);
-                    out[i] = tabplayer_interp(x, chidx, phase);
+                for(ch = 0; ch < x->x_n_ch; ch++){
+                    t_float *output = *(x->x_ovecs+ch);
+                    output[i] = tabplayer_interp(x, ch, phase);
                 };
             };
         }
@@ -257,21 +282,22 @@ static t_int *tabplayer_perform(t_int *w){
             if(!x->x_playing) // not playing, out zeros
                 goto nullstate;
             else{ // PLAYING
-                int isneg = x->x_rate < 0;
                 if(x->x_playnew){
-                    if(!x->x_pos)
-                        x->x_phase = isneg ? (double)x->x_end : (double)x->x_start;
+                    if(!x->x_position)
+                        x->x_phase = x->x_isneg ? (double)x->x_end : (double)x->x_start;
                     else
-                        x->x_pos = 0;
+                        x->x_position = 0;
                     x->x_playnew = 0;
+                    x->x_first = 1;
                 };
                 for(i = 0; i < n; i++){
                     double phase = x->x_phase;
-                    if(isneg){ // bounds checking backwards
+                    if(x->x_isneg){ // bounds checking backwards
                         if(phase < x->x_start){
                             if(x->x_loop){
                                 double dif = (double)x->x_start - phase;
                                 phase = (double)(x->x_end) - dif;
+                                x->x_first = 0;
                                 outlet_bang(x->x_donelet);
                             }
                             else{ // done playing
@@ -284,6 +310,7 @@ static t_int *tabplayer_perform(t_int *w){
                         if(phase > x->x_end){
                             if(x->x_loop){
                                 phase = (double)x->x_start + phase - (double)x->x_end;
+                                x->x_first = 0;
                                 outlet_bang(x->x_donelet);
                             }
                             else{ //we're done
@@ -292,19 +319,36 @@ static t_int *tabplayer_perform(t_int *w){
                             };
                         }
                     };
-                    for(chidx = 0; chidx < x->x_n_ch; chidx++){ // get output
-                        t_float *output = *(x->x_ovecs+chidx);
-                        output[i] = tabplayer_interp(x, chidx, phase);
+                    for(ch = 0; ch < x->x_n_ch; ch++){ // get output
+                        t_float *output = *(x->x_ovecs+ch);
+                        output[i] = tabplayer_interp(x, ch, phase);
+                        if(x->x_fadesamp > 0){
+                            output[i] *= tabplayer_fade_gain(x, phase);
+                            if(x->x_xfade && x->x_loop){
+                                if(x->x_isneg){
+                                    if(x->x_fading_in){
+                                        double xphase = phase - (double)(x->x_start) + (double)(x->x_end);
+                                        output[i] += (tabplayer_interp(x, ch, xphase) * cos(x->x_fade_point * HALF_PI));
+                                    }
+                                }
+                                else{
+                                    if(x->x_fading_out){
+                                        double xphase = phase - (double)(x->x_end - x->x_fadesamp) + (double)(x->x_start - x->x_fadesamp);
+                                        output[i] += (tabplayer_interp(x, ch, xphase) * sin(x->x_fade_point * HALF_PI));
+                                    }
+                                }
+                            }
+                        }
                     };
-                    x->x_phase = phase + x->x_sr_ratio*x->x_rate; // increment for both forwards/backwards
+                    x->x_phase = phase + x->x_sr_ratio*x->x_rate; // increment phase
                 };
             }
         };
     }
     else{
         nullstate:
-            for(chidx = 0; chidx < x->x_n_ch; chidx++){
-                t_float *output = *(x->x_ovecs+chidx);
+            for(ch = 0; ch < x->x_n_ch; ch++){
+                t_float *output = *(x->x_ovecs+ch);
                 int nblock = n;
                 while(nblock--)
                     *output++ = 0;
@@ -391,6 +435,7 @@ static void *tabplayer_new(t_symbol * s, int ac, t_atom *av){
         x->x_donelet = outlet_new(&x->x_obj, &s_bang);
         x->x_playing = 0;
         x->x_playnew = 0;
+        x->x_isneg = 0;
         x->x_rate = 1;
         tabplayer_reset(x);
         x->x_loop = loop;
