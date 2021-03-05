298f2defd5f34ab74cdcc46c0a759a2d73254cea
porres <porres@gmail.com>
2021-03-05 01:07:46 -0300

    del~ is good now

--- Classes/Source/del~.c
+++ Classes/Source/del~.c
@@ -8,34 +8,42 @@ extern int ugen_getsortno(void);
 static t_class *del_in_class;
 
 typedef struct delwritectl{
-    int         c_n;
-    t_sample   *c_vec;
-    int         c_phase;
+    int             c_n;        // number of samples
+    t_sample       *c_vec;      // vector
+    int             c_phase;    // phase
 }t_delwritectl;
 
 typedef struct _del_in{
     t_object        x_obj;
     t_symbol       *x_sym;
-    t_float         x_deltime;  // delay in msec (added by Mathieu Bouchard)
+    t_float         x_deltime;  // delay size
     t_delwritectl   x_cspace;
     int             x_sortno;   // DSP sort number at which this was last put on chain
     int             x_rsortno;  // DSP sort # for first delread or write in chain
     int             x_vecsize;  // vector size for del~ out to use
     int             x_freeze;
+    unsigned int    x_ms;       // ms flag
+    unsigned int    x_maxsize;  // buffer size in samples
+    unsigned int    x_maxsofar; // largest maxsize so far
     t_float         x_sr;
     t_float         x_f;
 //    t_outlet       *x_symout;
 }t_del_in;
 
-#define XTRASAMPS 4
-#define SAMPBLK 4
+#define XTRASAMPS 4 // extra number of samples (for guard points)
+#define SAMPBLK   4 // ???
 
 static void del_in_update(t_del_in *x){ // added by Mathieu Bouchard
-    int nsamps = x->x_deltime * x->x_sr * (t_float)(0.001f);
+    int nsamps = x->x_deltime;
+    if(x->x_ms)
+        nsamps *= (x->x_sr * (t_float)(0.001f));
+//    post("nsamps = %d", nsamps);
     if(nsamps < 1)
         nsamps = 1;
-    nsamps += ((- nsamps) & (SAMPBLK - 1));
+    nsamps += ((-nsamps) & (SAMPBLK - 1));
+//    post("nsamps += ((- nsamps) & (SAMPBLK - 1)) = %d", nsamps);
     nsamps += x->x_vecsize;
+//    post("nsamps final ====> %d", nsamps);
     if(x->x_cspace.c_n != nsamps){
         x->x_cspace.c_vec = (t_sample *)resizebytes(x->x_cspace.c_vec,
             (x->x_cspace.c_n + XTRASAMPS) * sizeof(t_sample), (nsamps + XTRASAMPS) * sizeof(t_sample));
@@ -49,13 +57,23 @@ static void del_in_update(t_del_in *x){ // added by Mathieu Bouchard
         outlet_symbol(x->x_symout, x->x_sym);
 }*/
 
+static void del_in_freeze(t_del_in *x, t_floatarg f){
+    x->x_freeze = (int)(f != 0);
+}
+
 static void del_in_clear(t_del_in *x){
     if(x->x_cspace.c_n > 0)
         memset(x->x_cspace.c_vec, 0, sizeof(t_sample)*(x->x_cspace.c_n + XTRASAMPS));
 }
 
-static void del_in_freeze(t_del_in *x, t_floatarg f){
-    x->x_freeze = (int)(f != 0);
+static void del_in_size(t_del_in *x, t_floatarg f){
+    if(f < 0)
+        f = 0;
+    if(f != x->x_deltime){
+        x->x_deltime = f;
+        del_in_clear(x);
+        del_in_update(x);
+    }
 }
 
 // check that all ins/outs have the same vecsize
@@ -78,38 +96,26 @@ static t_int *del_in_perform(t_int *w){
     t_delwritectl *c = (t_delwritectl *)(w[2]);
     int n = (int)(w[3]);
     t_del_in *x = (t_del_in *)(w[4]);
-    int phase = c->c_phase, nsamps = c->c_n;
-    t_sample *vp = c->c_vec; // vector point
-    t_sample *bp = vp + phase; // vector point + phase = input
-    t_sample *ep = vp + (nsamps + XTRASAMPS); // end point = vector point + nsamples
-    phase += n; // phase + block size
-    if(x->x_freeze){
-        while(n--){
+    int phase = c->c_phase;                     // phase
+    int nsamps = c->c_n;                        // number of samples
+    t_sample *vp = c->c_vec;                    // vector
+    t_sample *bp = vp + phase;                  // buffer point = vector + phase
+    t_sample *ep = vp + (nsamps + XTRASAMPS);   // end point = vector point + nsamples
+    phase += n;                                 // phase += block size
+    while(n--){
+        if(x->x_freeze)
             bp++;
-            if(bp == ep){
-                vp[0] = ep[-4];
-                vp[1] = ep[-3];
-                vp[2] = ep[-2];
-                vp[3] = ep[-1];
-                bp = vp + XTRASAMPS;
-                phase -= nsamps;
-            }
-        }
-    }
-    else{
-        while(n--){
+        else{
             t_sample f = *in++;
-            if(PD_BIGORSMALL(f))
-                f = 0;
-            *bp++ = f;
-            if(bp == ep){
-                vp[0] = ep[-4];
-                vp[1] = ep[-3];
-                vp[2] = ep[-2];
-                vp[3] = ep[-1];
-                bp = vp + XTRASAMPS;
-                phase -= nsamps;
-            }
+            *bp++ = PD_BIGORSMALL(f) ? 0.f : f; // write input into buffer point
+        }
+        if(bp == ep){
+            vp[0] = ep[-4]; // copy guard points
+            vp[1] = ep[-3];
+            vp[2] = ep[-2];
+            vp[3] = ep[-1];
+            bp = vp + XTRASAMPS; // go back to the beggining
+            phase -= nsamps;     // go back to the beggining
         }
     }
     c->c_phase = phase; // update phase
@@ -130,10 +136,11 @@ static void del_in_free(t_del_in *x){
 }
 
 static void *del_in_new(t_symbol *s, int ac, t_atom *av){
-    post("del_in_new ac = %d", ac);
+//    post("del_in_new MOTHERFUCKER");
     s = NULL;
     t_del_in *x = (t_del_in *)pd_new(del_in_class);
-    x->x_deltime = 1000;
+    x->x_deltime = 0;
+    x->x_ms = 1;
     char buf[MAXPDSTRING];
     sprintf(buf, "#%lx", (long)x);
     x->x_sym = gensym(buf);
@@ -142,14 +149,26 @@ static void *del_in_new(t_symbol *s, int ac, t_atom *av){
         post("[del~] please define a delay line name");
     }
     else if(av->a_type == A_SYMBOL){
-        x->x_sym = av->a_w.w_symbol;
-        ac--, av++;
-        if((av)->a_type == A_FLOAT)
+//        post("del_in_new av->a_type == A_SYMBOL");
+        t_symbol *cursym = atom_getsymbolarg(0, ac, av);
+//        post("cursym = %s", cursym->s_name);
+        if(cursym == gensym("-samps")){
+//            post("[del~ in] samps");
+            x->x_ms = 0;
+            ac--;
+            av++;
+        }
+        if(ac && av->a_type == A_SYMBOL){
+             x->x_sym = atom_getsymbolarg(0, ac, av);
+             ac--, av++;
+//             post("x->x_sym = %s", x->x_sym->s_name);
+         }
+        if(ac && (av)->a_type == A_FLOAT){
             x->x_deltime = (av)->a_w.w_float;
+//            post("x_deltime = %d", (int)x->x_deltime);
+        }
     }
-//    post("s = %s / ms = %f", x->x_sym->s_name, x->x_deltime);
     pd_bind(&x->x_obj.ob_pd, x->x_sym);
-//    x->x_symout = outlet_new(&x->x_obj, &s_symbol);
     x->x_cspace.c_n = 0;
     x->x_cspace.c_vec = getbytes(XTRASAMPS * sizeof(t_sample));
     x->x_sortno = 0;
@@ -168,6 +187,8 @@ typedef struct _del_out{
     t_symbol       *x_sym;
     t_float         x_sr;       // samples per msec
     int             x_zerodel;  // 0 or vecsize depending on read/write order
+    unsigned int    x_ms;       // ms flag
+    int             x_lin;      // linear interpolation
     t_float         x_f;
 }t_del_out;
 
@@ -175,6 +196,17 @@ typedef struct _del_out{
     if(s != &s_)
         x->x_sym = s;
 }*/
+
+float interpolate(t_del_out *x, t_sample *bp, t_sample frac){
+    t_sample b = bp[-1], c = bp[-2];
+    if(x->x_lin)
+        return(b + (c-b)*frac);
+    else{
+        t_sample a = bp[0], d = bp[-3], cmb = c-b;
+        return(b + frac * (cmb - 0.1666667f * (1.-frac) *
+            ((d - a - 3.0f*cmb) * frac + (d + 2.0f*a - 3.0f*b))));
+    }
+}
 
 static t_int *del_out_perform(t_int *w){
     t_sample *in = (t_sample *)(w[1]);
@@ -184,42 +216,38 @@ static t_int *del_out_perform(t_int *w){
     int n = (int)(w[5]);
     int nsamps = ctl->c_n;              // number of samples
     t_sample limit = nsamps - n;        // limit = number of samples - block size
-    t_sample fn = n-1;                  // fn = block size - 1
-    t_sample *vp = ctl->c_vec;          // vp (vector point?) = delay vector
+    t_sample nm1 = n-1;                  // nm1 = block size - 1
+    t_sample *vp = ctl->c_vec;          // vector (buffer memory of delay lne)
     t_sample *bp;
     t_sample *wp = vp + ctl->c_phase;   // wp (write point?) = vp + phase
     t_sample zerodel = x->x_zerodel;
-    if(limit < 0){ // blocksize is larger than out buffer size
+    if(limit < 0){      // blocksize is larger than out buffer size
         while(n--)
-            *out++ = 0;
-        return(w+5);
+            *out++ = 0; // output zeros
+        return(w+6);
     }
     while(n--){
-        t_sample delsamps = x->x_sr * *in++ - zerodel; // delay in samples
-        t_sample frac; // delay point fractional part
-        int idelsamps; // delay point integer part
-        t_sample a, b, c, d, cminusb; // for interpolation
-        if(delsamps < 1.0f)    // too small or NAN
-            delsamps = 0.0f;
-        if(delsamps > limit)           // too big
+        t_sample delsamps = *in++;      // delay in samples = input
+        if(x->x_ms)                     // if input is in ms
+            delsamps *= x->x_sr;        // convert to samples
+        delsamps -= zerodel;            // compensate for order of execution
+        if(delsamps < 1.0f){            // too small or NAN
+            x->x_lin = 1;
+            if(delsamps < 0.0f)
+                delsamps = 0.0f;
+        }
+        else
+            x->x_lin = 0;
+        if(delsamps > limit)                            // if delay point is too big
             delsamps = limit;
-        delsamps += fn; // delay in samples + block size - 1
-        fn = fn - 1.0f;
-        idelsamps = delsamps;
-        frac = delsamps - (t_sample)idelsamps;
-        bp = wp - idelsamps; // write point - delay point
-        if(bp < vp + XTRASAMPS)
+        delsamps += nm1;                                // delay in samples + block size - 1
+        nm1 = nm1 - 1;                                  // ????
+        int idelsamps = (int)delsamps;                  // delay point integer part
+        t_sample frac = delsamps - (t_sample)idelsamps; // delay point fractional part
+        bp = wp - idelsamps;                            // buffer point = write point - integer delay point
+        if(bp < vp + XTRASAMPS)                         // if less than beegining, wrap to upper point
             bp += nsamps;
-        d = bp[-3];
-        c = bp[-2];
-        b = bp[-1];
-        a = bp[0];
-        cminusb = c-b;
-        *out++ = b + frac * (
-            cminusb - 0.1666667f * (1.-frac) * (
-                (d - a - 3.0f * cminusb) * frac + (d + 2.0f*a - 3.0f*b)
-            )
-        );
+        *out++ = interpolate(x, bp, frac);
     }
     return(w+6);
 }
@@ -242,63 +270,73 @@ static void del_out_dsp(t_del_out *x, t_signal **sp){
 
 static void *del_out_new(t_symbol *s, int ac, t_atom *av){
     t_del_out *x = (t_del_out *)pd_new(del_out_class);
-    post("del_out_new / ac = %d", ac);
     s = NULL;
     x->x_sym = &s_;
-    
-    if(ac && av->a_type == A_SYMBOL){
-        x->x_sym = av->a_w.w_symbol;
-        post("x->x_sym = %s", x->x_sym->s_name);
-        ac--;
-        av++;
-    }
-    
-    x->x_sr = 1;
+    x->x_ms = 1;
+    x->x_sr = 0;
+    x->x_lin = 0;
     x->x_zerodel = 0;
+//    post("del_out_new / ac = %d", ac);
+    if(ac && av->a_type == A_SYMBOL){
+//        post("ac && av->a_type == A_SYMBOL");
+        t_symbol * cursym = atom_getsymbolarg(0, ac, av);
+//        post("cursym = %s", cursym->s_name);
+        if(cursym == gensym("-samps")){
+            x->x_ms = 0;
+//            post("-samps bingo del out / ms = %d", x->x_ms);
+            ac--, av++;
+        }
+        if(ac && av->a_type == A_SYMBOL){
+            x->x_sym = atom_getsymbolarg(0, ac, av);
+            ac--, av++;
+//            post("x->x_sym = %s", x->x_sym->s_name);
+        }
+        else
+            post("[del~ out] please define a delay line name");
+        if(ac && av->a_type == A_FLOAT){
+            x->x_f = atom_getfloatarg(0, ac, av);
+            ac--, av++;
+//            post("x->x_sym = %s", x->x_sym->s_name);
+        }
+    }
     outlet_new(&x->x_obj, &s_signal);
-    x->x_f = 0;
+    if(ac){
+    }
     return(x);
 }
 
 // ----------------------- global setup ----------------
-static void *del_new(t_pd *dummy, t_symbol *s, int ac, t_atom *av){
-    dummy = NULL;
-    post("del~_new / ac = %d", ac);
+static void *del_new(t_symbol *s, int ac, t_atom *av){
+//    post("del~_new");
     if(!ac){
         post("[del~] please define a delay line name");
         pd_this->pd_newest = del_in_new(s, ac, av);
     }
     else{
-        post("del~_new / else (ac)");
+//        post("del~_new");
         t_symbol *s2 = av[0].a_w.w_symbol;
-        if(s2 == gensym("in")){
-            post("in");
+        if(s2 == gensym("in"))
             pd_this->pd_newest = del_in_new(s, ac-1, av+1);
-        }
-        else if(s2 == gensym("out")){
-            post("out");
+        else if(s2 == gensym("out"))
             pd_this->pd_newest = del_out_new(s, ac-1, av+1);
-        }
-        else{
-            post("else / neither in or out");
+        else
             pd_this->pd_newest = del_in_new(s, ac, av);
-        }
     }
     return(pd_this->pd_newest);
 }
 
 void del_tilde_setup(void){
-    del_in_class = class_new(gensym("del~ in"), (t_newmethod)del_in_new,
-        (t_method)del_in_free, sizeof(t_del_in), 0, A_GIMME, 0);
+    del_in_class = class_new(gensym("del~ in"), 0,
+        (t_method)del_in_free, sizeof(t_del_in), 0, 0);
     class_addcreator((t_newmethod)del_new, gensym("del~"), A_GIMME, 0);
     CLASS_MAINSIGNALIN(del_in_class, t_del_in, x_f);
 //    class_addbang(del_in_class, del_in_outsym);
     class_addmethod(del_in_class, (t_method)del_in_dsp, gensym("dsp"), A_CANT, 0);
     class_addmethod(del_in_class, (t_method)del_in_clear, gensym("clear"), 0);
-    class_addmethod(del_in_class, (t_method)del_in_freeze, gensym("freeze"), A_FLOAT, 0);
+    class_addmethod(del_in_class, (t_method)del_in_freeze, gensym("freeze"), A_DEFFLOAT, 0);
+    class_addmethod(del_in_class, (t_method)del_in_size, gensym("size"), A_DEFFLOAT, 0);
 
-    del_out_class = class_new(gensym("del~ out"), (t_newmethod)del_out_new, 0,
-        sizeof(t_del_out), 0, A_GIMME, 0);
+    del_out_class = class_new(gensym("del~ out"), (t_newmethod)del_out_new, 0, sizeof(t_del_out), 0, A_GIMME, 0);
     CLASS_MAINSIGNALIN(del_out_class, t_del_out, x_f);
     class_addmethod(del_out_class, (t_method)del_out_dsp, gensym("dsp"), A_CANT, 0);
 //    class_addsymbol(del_out_class, del_out_set);
