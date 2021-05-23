d80747dec69ab8d9ecaf9841ef451d78ce90735b
porres <porres@gmail.com>
2021-05-23 16:29:09 -0300

    Update retrieve.c

--- Classes/Source/retrieve.c
+++ Classes/Source/retrieve.c
@@ -37,12 +37,12 @@ typedef struct _retrieve{
     int             x_noutlets;   // not counting right one
     t_object	   *x_receiver;   // object containing the receiver
     int				x_maxobs;	  // maximum number of connections from receiver
-    t_object      **x_retrievebed;    // array of retrievebed objects
-    t_outconnect  **x_retrievecons;   // array of retrievebed connections
-    int            *x_nretrieveout;   // array; number of retrievebed object's outlets
+    t_object      **x_retrieved;    // array of retrieved objects
+    t_outconnect  **x_retrievecons;   // array of retrieved connections
+    int            *x_nretrieveout;   // array; number of retrieved object's outlets
     t_outlet       *x_rightout;   // right outlet
 // traversal helpers:
-    t_outconnect   *x_toretrievebed;  // a connection to retrievebed object
+    t_outconnect   *x_toretrieved;  // a connection to retrieved object
     t_bindelem     *x_bindelem;
     int             x_nonreceive; //* flag to signal whether processed non-[receive] object
 }t_retrieve;
@@ -64,13 +64,13 @@ static int retrieve_prep(t_retrieve *x, t_object *ob){
     && x->x_receiver->te_g.g_pd->c_name != gensym("receiver")))
 		ncons = 1;
 	else {
-        if(!(x->x_toretrievebed = ocp = outlet_connections(op)))
+        if(!(x->x_toretrieved = ocp = outlet_connections(op)))
             return(0);
         for(ncons = 0; ocp ; ++ncons)
             ocp = outlet_nextconnection(ocp, &dummy, &inno);
 	}
-	if (!x->x_retrievebed){
-		if (!((x->x_retrievebed = getbytes(ncons * sizeof(*x->x_retrievebed))) &&
+	if (!x->x_retrieved){
+		if (!((x->x_retrieved = getbytes(ncons * sizeof(*x->x_retrieved))) &&
 			 (x->x_nretrieveout = getbytes(ncons * sizeof(*x->x_nretrieveout))) &&
 			(x->x_retrievecons = getbytes(ncons * x->x_noutlets * sizeof(*x->x_retrievecons)))))
 		{
@@ -80,9 +80,9 @@ static int retrieve_prep(t_retrieve *x, t_object *ob){
 		x->x_maxobs = ncons;
 	}
 	else if(ncons > x->x_maxobs){
-		if (!((x->x_retrievebed = resizebytes(x->x_retrievebed,
-			x->x_maxobs * sizeof(*x->x_retrievebed),
-			ncons * sizeof(*x->x_retrievebed))) &&
+		if (!((x->x_retrieved = resizebytes(x->x_retrieved,
+			x->x_maxobs * sizeof(*x->x_retrieved),
+			ncons * sizeof(*x->x_retrieved))) &&
 			
 			(x->x_nretrieveout = resizebytes(x->x_nretrieveout,
 			x->x_maxobs * sizeof(*x->x_nretrieveout),
@@ -101,7 +101,7 @@ static int retrieve_prep(t_retrieve *x, t_object *ob){
 }
 
 static void retrieve_start(t_retrieve *x){
-    x->x_toretrievebed = 0;
+    x->x_toretrieved = 0;
     x->x_bindelem = 0;
     x->x_receiver = 0;
     x->x_nonreceive = 0;
@@ -127,9 +127,9 @@ static void retrieve_start(t_retrieve *x){
 }
 
 static int retrieve_next(t_retrieve *x){
-	if (!(x->x_retrievebed && x->x_retrievecons && x->x_nretrieveout))
+	if (!(x->x_retrieved && x->x_retrievecons && x->x_nretrieveout))
 		return(0);
-	t_object **retrievebedp = x->x_retrievebed;
+	t_object **retrievedp = x->x_retrieved;
 	t_outconnect **retrieveconsp = x->x_retrievecons;
 	int *nretrieveoutp = x->x_nretrieveout;
 	t_object *gr;
@@ -142,17 +142,17 @@ nextremote:
     (x->x_receiver->te_g.g_pd->c_name != gensym("receive") &&
     x->x_receiver->te_g.g_pd->c_name != gensym("receiver"))){
 		// post("nonreceive");
-		*retrievebedp = x->x_receiver;
+		*retrievedp = x->x_receiver;
 		*nretrieveoutp = 1;
 		*retrieveconsp = obj_starttraverseoutlet(x->x_receiver, &goutp, 0);
 		goutp->o_connections = obj_starttraverseoutlet((t_object *)x, &op, 0);
 		x->x_nonreceive = 1;
 		return(1);
 	}
-    else if (x->x_toretrievebed) {
-		while(x->x_toretrievebed){
+    else if (x->x_toretrieved) {
+		while(x->x_toretrieved){
 			//post("entering retrieve_next while loop");
-			x->x_toretrievebed = outlet_nextconnection(x->x_toretrievebed, &gr, &inno);
+			x->x_toretrieved = outlet_nextconnection(x->x_toretrieved, &gr, &inno);
 			if(gr){
 				if(inno){
 					if(x->x_rightout)
@@ -161,7 +161,7 @@ nextremote:
 						pd_error(x, "retrieve: remote proxy must feed leftmost inlet");
 				}
 				else{
-					*retrievebedp++ = gr;
+					*retrievedp++ = gr;
 					int goutno = obj_noutlets(gr);
 					if (goutno > x->x_noutlets) goutno = x->x_noutlets;
 					// post ("retrieve_next goutno: %d", goutno);
@@ -175,14 +175,14 @@ nextremote:
 				}
 			}
 		}
-		return(retrievebedp-x->x_retrievebed); // return number of objects stored
+		return(retrievedp-x->x_retrieved); // return number of objects stored
     }
     if(x->x_bindelem) while ((x->x_bindelem = x->x_bindelem->e_next)){
         t_object *ob;
         if((ob = pd_checkobject(x->x_bindelem->e_who))){
         	x->x_nonreceive = 0;
             retrieve_prep(x,ob);
-            retrievebedp = x->x_retrievebed;
+            retrievedp = x->x_retrieved;
             retrieveconsp = x->x_retrievecons;
             nretrieveoutp = x->x_nretrieveout;
             goto
@@ -193,18 +193,18 @@ nextremote:
 }
 
 static void retrieve_restore(t_retrieve *x, int nobs){
-	t_object **retrievebedp = x->x_retrievebed;
-	t_object **retrieveconsp = (t_object **)x->x_retrievecons;
+	t_object **retrievedp = x->x_retrieved;
+	t_outconnect **retrieveconsp = x->x_retrievecons;
 	int *nretrieveoutp = x->x_nretrieveout;
 	int goutno;
 	t_object *gr;
 	t_outlet *goutp;
 	while(nobs--){
-		gr = *retrievebedp++;
+		gr = *retrievedp++;
 		goutno = *nretrieveoutp++;
 		for(int i = 0; i < goutno ; i++){
 			obj_starttraverseoutlet(gr, &goutp, i);
-			goutp->o_connections = (t_outconnect *)*retrieveconsp++;
+			goutp->o_connections = *retrieveconsp++;
 		}
 	}
 }
@@ -214,72 +214,12 @@ static void retrieve_bang(t_retrieve *x){
     retrieve_start(x);
     while((nobs = retrieve_next(x))){
         if(x->x_receiver)
-        	pd_bang((t_pd *)x->x_receiver);
+        	pd_bang(&x->x_receiver->ob_pd);
         else
         	outlet_bang(x->x_rightout);
         retrieve_restore(x, nobs);
     }
 }
-
-/*static void retrieve_float(t_retrieve *x, t_float f){
-	int nobs;
-    retrieve_start(x);
-    while((nobs = retrieve_next(x))){
-    	if(x->x_receiver)
-        	pd_float((t_pd *)x->x_receiver, f);
-        else
-        	outlet_float(x->x_rightout, f);
-        retrieve_restore(x, nobs);
-    }
-}
-
-static void retrieve_symbol(t_retrieve *x, t_symbol *s){
-    int nobs;
-    retrieve_start(x);
-    while((nobs = retrieve_next(x))){
-    	if(x->x_receiver)
-        	pd_symbol((t_pd *)x->x_receiver, s);
-        else
-        	outlet_symbol(x->x_rightout, s);
-        retrieve_restore(x, nobs);
-    }
-}
-
-static void retrieve_pointer(t_retrieve *x, t_gpointer *gp){
-    int nobs;
-    retrieve_start(x);
-    while((nobs = retrieve_next(x))){
-    	if(x->x_receiver)
-        	pd_pointer((t_pd *)x->x_receiver, gp);
-        else
-        	outlet_pointer(x->x_rightout, gp);
-        retrieve_restore(x, nobs);
-    }
-}
-
-static void retrieve_list(t_retrieve *x, t_symbol *s, int ac, t_atom *av){
-    int nobs;
-    retrieve_start(x);
-    while((nobs = retrieve_next(x))){
-    	if(x->x_receiver)
-        	pd_list((t_pd *)x->x_receiver, s, ac, av);
-        else
-        	outlet_list(x->x_rightout, s, ac, av);
-       retrieve_restore(x, nobs);
-    }
-}
-
-static void retrieve_anything(t_retrieve *x, t_symbol *s, int ac, t_atom *av){
-    int nobs;
-    retrieve_start(x);
-    while((nobs = retrieve_next(x))){
-    	if(x->x_receiver)
-        	pd_anything((t_pd *)x->x_receiver, s, ac, av);
-        else
-        	outlet_anything(x->x_rightout, s, ac, av);
-       retrieve_restore(x, nobs);
-    }
-}*/
 
 static void retrieve_set(t_retrieve *x, t_symbol *s){
     if(x->x_target && s && s != &s_)
@@ -297,8 +237,8 @@ static void *retrieve_new(t_symbol *s){
 }
 
 static void retrieve_free(t_retrieve *x){
-    if(x->x_retrievebed)
-        freebytes(x->x_retrievebed, x->x_maxobs * sizeof(*x->x_retrievebed));
+    if(x->x_retrieved)
+        freebytes(x->x_retrieved, x->x_maxobs * sizeof(*x->x_retrieved));
     if(x->x_nretrieveout)
     	freebytes(x->x_nretrieveout, x->x_maxobs * sizeof(*x->x_nretrieveout));
     if(x->x_retrievecons)
