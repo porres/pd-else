38efefe62ae86e3010066420a506b2fbc4dd91cd
porres <porres@gmail.com>
2021-10-25 20:48:03 -0300

    Update else.c

--- Classes/Source/else.c
+++ Classes/Source/else.c
@@ -11,15 +11,15 @@ t_class *else_obj_class;
 static int printed;
 
 static int min_major = 0;
-static int min_minor = 51;
-static int min_bugfix = 3;
+static int min_minor = 52;
+static int min_bugfix = 0;
 
 static int else_major = 1;
 static int else_minor = 0;
 static int else_bugfix = 0;
 
 #define STATUS "beta"
-static int status_number = 44;
+static int status_number = 45;
 
 static void else_obj_version(t_else_obj *x){
     int ac = 5;
@@ -39,11 +39,13 @@ void print_else_obj(t_else_obj *x){
     post("-------------------------------------------------------------------------------");
     post("  ~~~~~~~~~~|| ELSE - EL Locus Solus' Externals for Pure Data ||~~~~~~~~~~");
     post("-------------------------------------------------------------------------------");
-    post("- Version: 1.0-0 %s-%d; Released september 27th 2021", STATUS, status_number);
+    post("- Version: 1.0-0 %s-%d; Unreleased", STATUS, status_number);
     post("- Author: Alexandre Torres Porres");
     post("- Repository: https://github.com/porres/pd-else");
     post("- License: Do What The Fuck You Want To Public License, unless otherwise noted");
-    if(major >= min_major && minor >= min_minor && bugfix >= min_bugfix)
+    if(major > min_major
+       || major == min_major && minor > min_minor
+       || major == min_major && minor == min_minor && bugfix >= min_bugfix)
         post("- ELSE 1.0.-0 %s-%d needs at least Pd %d.%d-%d (you have %d.%d-%d, you're good!)",
              STATUS, status_number, min_major, min_minor, min_bugfix, major, minor, bugfix);
     else
