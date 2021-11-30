4b79a2309ec39aaa692a3e51e492b8f748a883a1
porres <porres@gmail.com>
2021-11-30 01:02:37 -0300

    version bump

--- Classes/Source/else.c
+++ Classes/Source/else.c
@@ -19,7 +19,7 @@ static int else_minor = 0;
 static int else_bugfix = 0;
 
 #define STATUS "beta"
-static int status_number = 45;
+static int status_number = 46;
 
 static void else_obj_version(t_else_obj *x){
     int ac = 5;
@@ -39,7 +39,7 @@ void print_else_obj(t_else_obj *x){
     post("-------------------------------------------------------------------------------");
     post("  ~~~~~~~~~~|| ELSE - EL Locus Solus' Externals for Pure Data ||~~~~~~~~~~");
     post("-------------------------------------------------------------------------------");
-    post("- Version: %d.%d-%d %s-%d; Released november 28th 2021", else_major, else_minor, else_bugfix, STATUS, status_number);
+    post("- Version: %d.%d-%d %s-%d; Unreleased", else_major, else_minor, else_bugfix, STATUS, status_number);
     post("- Author: Alexandre Torres Porres");
     post("- Repository: https://github.com/porres/pd-else");
     post("- License: Do What The Fuck You Want To Public License, unless otherwise noted");
--- README.md
+++ README.md
@@ -4,9 +4,9 @@
 
 ### for the Pure Data programming language
 
-### Version: 1.0-0 beta-45 With Live Electronics Tutorial 
+### Version: 1.0-0 beta-46 With Live Electronics Tutorial 
 
-###   Released novrmber 28th 2021
+###   Unreleased
 
 
 
