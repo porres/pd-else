410cab3b29c01e5a21ccb56e02de26c831462323
porres <porres@gmail.com>
2022-04-15 18:57:04 -0300

    tabplayer~ signal triggering

--- Classes/Source/tabplayer~.c
+++ Classes/Source/tabplayer~.c
@@ -16,6 +16,8 @@ typedef struct _tabplayer{
     t_buffer   *x_buffer;
     t_glist    *x_glist;
     int         x_hasfeeders;       // if there's a signal coming in the main inlet
+    int         x_trig_mode;
+    float       x_lastin;
     float       x_sr_khz;           // pd's sample rate
     float       x_array_sr_khz;     // array's sample rate
     float       x_range_start;
@@ -197,6 +199,10 @@ static void tabplayer_loop(t_play *x, t_floatarg f){
     x->x_loop = f > 0 ? 1 : 0;
 }
 
+static void tabplayer_trigger(t_play *x, t_floatarg f){
+    x->x_trig_mode = f > 0 ? 1 : 0;
+}
+
 static double tabplayer_fade_gain(t_play *x, double phase){
     double fadegain = 1;
     x->x_fading_in = x->x_fading_out = 0;
@@ -271,18 +277,90 @@ static t_int *tabplayer_perform(t_int *w){
     t_buffer *buffer = x->x_buffer;
     int n = (int)(w[2]);
     int ch, i;
+    t_float *xin = x->x_ivec;
+    float last_sig_input = x->x_lastin;
     if(buffer->c_playable){
-        if(x->x_hasfeeders){ // signal input present, indexing into array
-            t_float *xin = x->x_ivec;
+        if(x->x_hasfeeders){ // signal input present
             for(i = 0; i < n; i++){
-                float phase = *xin++;
+                float sig_input = *xin++;
                 for(ch = 0; ch < x->x_n_ch; ch++){
                     t_float *output = *(x->x_ovecs+ch);
-                    output[i] = tabplayer_interp(x, ch, phase);
+                    if(sig_input != 0 && last_sig_input == 0){
+                        // bang
+                        x->x_position = 0;
+                        x->x_playing = x->x_playnew = 1; // start playing
+                    }
+                    else if(!x->x_trig_mode && sig_input == 0 && last_sig_input != 0){
+                        if(x->x_playing){
+                            x->x_playing = x->x_playnew = 0;
+                            outlet_bang(x->x_donelet);
+                        };
+                    }
+                    if(!x->x_playing) // not playing, out zeros
+                        output[i] = 0;
+                    else{ // PLAYING
+                        if(x->x_playnew){
+                            if(!x->x_position)
+                                x->x_phase = x->x_isneg ? (double)x->x_end : (double)x->x_start;
+                            else
+                                x->x_position = 0;
+                            x->x_playnew = 0;
+                                x->x_first = 1;
+                        };
+                        double phase = x->x_phase;
+                        if(x->x_isneg){ // bounds checking backwards
+                            if(phase < x->x_start){
+                                if(x->x_loop){
+                                    double dif = (double)x->x_start - phase;
+                                    phase = (double)(x->x_end) - dif;
+                                    x->x_first = 0;
+                                    outlet_bang(x->x_donelet);
+                                }
+                                else{ // done playing
+                                    outlet_bang(x->x_donelet);
+                                    x->x_playing = 0;
+                                };
+                            }
+                        }
+                        else{ // bounds checking forwards
+                            if(phase > x->x_end){
+                                if(x->x_loop){
+                                    phase = (double)x->x_start + phase - (double)x->x_end;
+                                    x->x_first = 0;
+                                    outlet_bang(x->x_donelet);
+                                }
+                                else{ //we're done
+                                    outlet_bang(x->x_donelet);
+                                    x->x_playing = 0;
+                                };
+                            }
+                        };
+                        output[i] = tabplayer_interp(x, ch, phase);
+                        if(x->x_fadesamp > 0){
+                            output[i] *= tabplayer_fade_gain(x, phase);
+                            if(x->x_xfade && x->x_loop){
+                                if(x->x_isneg){
+                                    if(x->x_fading_in){
+                                        double xphase = phase - (double)(x->x_start) + (double)(x->x_end);
+                                        output[i] += (tabplayer_interp(x, ch, xphase) * cos(x->x_fade_point * HALF_PI));
+                                    }
+                                }
+                                else{
+                                    if(x->x_fading_out){
+                                        double xphase = phase - (double)(x->x_end - x->x_fadesamp) + (double)(x->x_start - x->x_fadesamp);
+                                        output[i] += (tabplayer_interp(x, ch, xphase) * sin(x->x_fade_point * HALF_PI));
+                                    }
+                                }
+                            }
+                        }
+                        x->x_phase = phase + x->x_sr_ratio*x->x_rate; // increment phase
+                    }
                 };
+                last_sig_input = sig_input;
             };
         }
         else{ // no signal input present, auto playback mode
+            last_sig_input = 0;
             if(!x->x_playing) // not playing, out zeros
                 goto nullstate;
             else{ // PLAYING
@@ -358,6 +436,7 @@ static t_int *tabplayer_perform(t_int *w){
                     *output++ = 0;
             };
     };
+    x->x_lastin = last_sig_input;
     return(w+3);
 }
 
@@ -394,10 +473,12 @@ static void *tabplayer_new(t_symbol * s, int ac, t_atom *av){
     t_float range_start = 0;
     t_float range_end = 1;
     x->x_xfade = 0;
+    x->x_lastin = 0;
     x->x_sr_khz = (float)sys_getsr() * 0.001;
     x->x_array_sr_khz = x->x_sr_khz; // pd's sample rate for now
     x->x_loop = 0;
     x->x_rate = 1.f;
+    x->x_trig_mode = 0;
     int nameset = 0;
     int argn = 0;
     while(ac){
@@ -409,6 +490,10 @@ static void *tabplayer_new(t_symbol * s, int ac, t_atom *av){
             }
             else if(s == gensym("-xfade") && !argn){
                 x->x_xfade = 1;
+                ac--, av++;
+            }
+            else if(s == gensym("-tr") && !argn){
+                x->x_trig_mode = 1;
                 ac--, av++;
             }
             else if(s == gensym("-fade") && ac >= 2  && !argn){
@@ -490,6 +575,7 @@ void tabplayer_tilde_setup(void){
     class_addmethod(tabplayer_class, (t_method)tabplayer_range, gensym("range"), A_FLOAT, A_FLOAT, 0);
     class_addmethod(tabplayer_class, (t_method)tabplayer_speed, gensym("speed"), A_FLOAT, 0);
     class_addmethod(tabplayer_class, (t_method)tabplayer_loop, gensym("loop"), A_FLOAT, 0);
+    class_addmethod(tabplayer_class, (t_method)tabplayer_trigger, gensym("tr"), A_FLOAT, 0);
     class_addmethod(tabplayer_class, (t_method)tabplayer_fade, gensym("fade"), A_FLOAT, 0);
     class_addmethod(tabplayer_class, (t_method)tabplayer_xfade, gensym("xfade"), A_FLOAT, 0);
     class_addmethod(tabplayer_class, (t_method)tabplayer_arraysr, gensym("sr"), A_FLOAT, 0);
