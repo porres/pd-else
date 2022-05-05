66bb535c298989af2be75301f8490c1eed6ef373
porres <porres@gmail.com>
2022-05-05 14:14:24 -0300

    Update keyboard.c
    close shttps://github.com/porres/pd-else/issues/1245

--- Classes/Source/keyboard.c
+++ Classes/Source/keyboard.c
@@ -186,8 +186,10 @@ static int find_note(t_keyboard* x, float xpos, float ypos){
         }
     }
     x->x_velocity = x->x_norm > 0 ? x->x_norm : (int)((ypos / x->x_height) * 127);
-    if(x->x_velocity <= 0)
+    if(x->x_velocity < 1)
         x->x_velocity = 1;
+    if(x->x_velocity > 127)
+        x->x_velocity = 127;
     return(x->x_first_c+i);
 }
 
