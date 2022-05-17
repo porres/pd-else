d1f6a21e0c6f6a7db0aa6b9eded7339498551051
porres <porres@gmail.com>
2022-05-17 03:45:09 -0300

    Update rec.c

--- Classes/Source/rec.c
+++ Classes/Source/rec.c
@@ -40,14 +40,13 @@ typedef struct _rec_track{
     double         tr_prevtime;
     t_clock       *tr_clock;
     t_outlet      *tr_trackout;
-    t_elsefile    *tr_elsefilehandle;
 }t_rec_track;
 
 typedef void (*t_rec_trackfn)(t_rec_track *tp);
 
 typedef struct _rec{
     t_object       x_obj;
-    t_glist       *x_glist;
+    t_canvas      *x_canvas;
     int            x_ntracks;  
     t_rec_track  **x_tracks;
     t_elsefile    *x_elsefilehandle;
@@ -272,23 +271,6 @@ static void rec_track_clear(t_rec_track *tp){
     binbuf_clear(tp->tr_binbuf);
 }
 
-static void rec_doread(t_rec *x, t_rec_track *target, t_symbol *fname);
-static void rec_dowrite(t_rec *x, t_rec_track *source, t_symbol *fname);
-
-static void rec_track_readhook(t_pd *z, t_symbol *fname, int ac, t_atom *av){
-    ac = 0;
-    av = NULL;
-    t_rec_track *tp = (t_rec_track *)z;
-    rec_doread(tp->tr_owner, tp, fname);
-}
-
-static void rec_track_writehook(t_pd *z, t_symbol *fname, int ac, t_atom *av){
-    ac = 0;
-    av = NULL;
-    t_rec_track *tp = (t_rec_track *)z;
-    rec_dowrite(tp->tr_owner, tp, fname);
-}
-
 static void rec_track_speed(t_rec_track *tp, t_floatarg f){
     float newtempo;
     if(f < 1e-20)
@@ -365,11 +347,10 @@ static void rec_clear(t_rec *x, t_symbol *s, int ac, t_atom *av){
     rec_calltracks(x, rec_track_clear, s, ac, av);
 }
 
-static void rec_doread(t_rec *x, t_rec_track *target, t_symbol *fname){
+static void rec_doread(t_rec *x, t_symbol *fname){
     char path[MAXPDSTRING];
-    
     char *bufptr;
-    int fd = open_via_path(canvas_getdir(x->x_glist)->s_name,
+    int fd = open_via_path(canvas_getdir(x->x_canvas)->s_name,
         fname->s_name, "", path, &bufptr, MAXPDSTRING, 1);
     if(fd > 0){
         path[strlen(path)]='/';
@@ -390,10 +371,8 @@ static void rec_doread(t_rec *x, t_rec_track *target, t_symbol *fname){
             while(*line && (*line == ' ' || *line == '\t')) line++;
             if((linelen = strlen(line))){
                 if(tp){
-                    if(!strncmp(line, "end;", 4)){
-                        post("ok");
+                    if(!strncmp(line, "end;", 4))
                         tp = 0;
-                    }
                     else{
                         int ac;
                         binbuf_text(bb, line, linelen);
@@ -414,19 +393,12 @@ static void rec_doread(t_rec *x, t_rec_track *target, t_symbol *fname){
                 }
                 else if(!strncmp(line, "track ", 6)){
                     int id = strtol(line + 6, 0, 10);
-                    startpost("Track %d... ", id);
                     if(id < 1 || id > x->x_ntracks)
-                        post("no such track");  /* LATER rethink */
-                    else if(target){
-                        if(id == target->tr_id)
-                            tp = target;
-                        post("skipped");  /* LATER rethink */
-                    }
+                        post("[rec] cann't load track %d... no such track", id);  // LATER rethink
                     else
                         tp = x->x_tracks[id - 1];
-                    if(tp){
+                    if(tp)
                         binbuf_clear(tp->tr_binbuf);
-                    }
                 }
             }
         }
@@ -434,7 +406,7 @@ static void rec_doread(t_rec *x, t_rec_track *target, t_symbol *fname){
         binbuf_free(bb);
     }
     else
-        panel_click_open(target ? target->tr_elsefilehandle : x->x_elsefilehandle);
+        panel_click_open(x->x_elsefilehandle);
 }
 
 static int rec_writetrack(t_rec *x, t_rec_track *tp, FILE *fp){
@@ -447,9 +419,9 @@ static int rec_writetrack(t_rec *x, t_rec_track *tp, FILE *fp){
         fprintf(fp, "track %d;\n", tp->tr_id);
         for(; natoms--; ap++){
             int length;
-            /* from binbuf_write():
-	       ``estimate how many characters will be needed.  Printing out
-	       symbols may need extra characters for inserting backslashes.'' */
+            // from binbuf_write():
+	       // ``estimate how many characters will be needed.  Printing out
+	       // symbols may need extra characters for inserting backslashes.''
             if(ap->a_type == A_SYMBOL || ap->a_type == A_DOLLSYM)
                 length = 80 + strlen(ap->a_w.w_symbol->s_name);
             else
@@ -487,39 +459,32 @@ static int rec_writetrack(t_rec *x, t_rec_track *tp, FILE *fp){
         if(bp > sbuf && fwrite(sbuf, bp - sbuf, 1, fp) < 1)
             return(1);
         fputs("end;\n", fp);
-        post("Track %d done", tp->tr_id);  /* CHECKED (0-based: not emulated) */
+//        post("Track %d done", tp->tr_id);
     }
     return(0);
 }
 
 /* CHECKED empty sequence stored as an empty elsefile */
-static void rec_dowrite(t_rec *x, t_rec_track *source, t_symbol *fname){
+static void rec_dowrite(t_rec *x, t_symbol *fname){
     int failed = 0;
     char path[MAXPDSTRING];
     FILE *fp;
-    if(x->x_glist)
-        canvas_makefilename(x->x_glist, fname->s_name, path, MAXPDSTRING);
+    if(x->x_canvas)
+        canvas_makefilename(x->x_canvas, fname->s_name, path, MAXPDSTRING);
     else{
     	strncpy(path, fname->s_name, MAXPDSTRING);
     	path[MAXPDSTRING-1] = 0;
     }
     /* CHECKED no global message */
     if((fp = sys_fopen(path, "w"))){
-        /* CHECKED single-track writing does not seem to work (a bug?) */
-        if(source)
-            failed = rec_writetrack(x, source, fp);
-        else{
-            int id;
-            t_rec_track **tpp;
-            for(id = 0, tpp = x->x_tracks; id < x->x_ntracks; id++, tpp++)
-                if((failed = rec_writetrack(x, *tpp, fp)))
-                    break;
-        }
-//	if(failed) sys_unixerror(path);  /* LATER rethink */
+        int id;  // single-track writing does not seem to work (a bug?)
+        t_rec_track **tpp;
+        for(id = 0, tpp = x->x_tracks; id < x->x_ntracks; id++, tpp++)
+            if((failed = rec_writetrack(x, *tpp, fp)))
+                break;
         fclose(fp);
     }
     else{
-//	sys_unixerror(path);  /* LATER rethink */
         failed = 1;
     }
     if(failed)
@@ -529,27 +494,27 @@ static void rec_dowrite(t_rec *x, t_rec_track *source, t_symbol *fname){
 static void rec_readhook(t_pd *z, t_symbol *fname, int ac, t_atom *av){
     ac = 0;
     av = NULL;
-    rec_doread((t_rec *)z, 0, fname);
+    rec_doread((t_rec *)z, fname);
 }
 
 static void rec_writehook(t_pd *z, t_symbol *fname, int ac, t_atom *av){
     ac = 0;
     av = NULL;
-    rec_dowrite((t_rec *)z, 0, fname);
+    rec_dowrite((t_rec *)z, fname);
 }
 
 static void rec_read(t_rec *x, t_symbol *s){
     if(s && s != &s_)
-        rec_doread(x, 0, s);
-    else  /* CHECKED no default */
+        rec_doread(x, s);
+    else
         panel_click_open(x->x_elsefilehandle);
 }
 
 static void rec_write(t_rec *x, t_symbol *s){
     if(s && s != &s_)
-        rec_dowrite(x, 0, s);
-    else  /* CHECKED no default */
-        panel_save(x->x_elsefilehandle, canvas_getdir(x->x_glist), 0);
+        rec_dowrite(x, s);
+    else
+        panel_save(x->x_elsefilehandle, canvas_getdir(x->x_canvas), 0);
 }
 
 static void rec_click(t_rec *x){
@@ -572,60 +537,42 @@ static void rec_free(t_rec *x){
     }
 }
 
-static void *rec_new(t_floatarg f){
-    t_rec *x = 0;
-    int ntracks = (int)f;
-    t_rec_track **tracks;
-    if(ntracks < 1)
-        ntracks = 1;
-    if((tracks = getbytes(ntracks * sizeof(*tracks)))){
-        int i;
-        t_rec_track **tpp;
-        for(i = 0, tpp = tracks; i < ntracks; i++, tpp++){
-            if(!(*tpp = (t_rec_track *)pd_new(rec_track_class)) ||
-            !((*tpp)->tr_binbuf = binbuf_new()) ||
-            !((*tpp)->tr_clock = clock_new(*tpp, (t_method)rec_track_tick))){
-                if(*tpp)
-                    pd_free((t_pd *)*tpp);
-                if((*tpp)->tr_binbuf)
-                    binbuf_free((*tpp)->tr_binbuf);
-                while(i--){
-                    tpp--;
-                    binbuf_free((*tpp)->tr_binbuf);
-                    clock_free((*tpp)->tr_clock);
-                    pd_free((t_pd *)*tpp);
-                }
-                return(0);
-            }
-        }
-        if((x = (t_rec *)pd_new(rec_class))){
-            int id;
-            x->x_glist = canvas_getcurrent();
-            x->x_elsefilehandle = elsefile_new((t_pd *)x, rec_readhook, rec_writehook);
-            if(ntracks > REC_MAXTRACKS)
-                ntracks = REC_MAXTRACKS;
-            x->x_ntracks = ntracks;
-            x->x_tracks = tracks;
-            for(id = 1; id <= ntracks; id++, tracks++){ // 1-based
-                t_rec_track *tp = *tracks;
-                inlet_new((t_object *)x, (t_pd *)tp, 0, 0);
-                tp->tr_trackout = outlet_new((t_object *)x, &s_);
-                tp->tr_owner = x;
-                tp->tr_id = id;
-                tp->tr_listed = 0;
-                tp->tr_elsefilehandle = elsefile_new((t_pd *)tp, rec_track_readhook, rec_track_writehook);
-                tp->tr_mode = REC_STOPMODE;
-                tp->tr_muted = 0;
-                tp->tr_restarted = 0;
-                tp->tr_atdelta = 0;
-                tp->tr_ixnext = 0;
-                tp->tr_tempo = 1.;
-                tp->tr_clockdelay = 0.;
-                tp->tr_prevtime = 0.;
-            }
-            x->x_bangout = outlet_new((t_object *)x, &s_bang);
-        }
+static void *rec_new(t_floatarg f, t_symbol *name){
+    t_rec *x = (t_rec *)pd_new(rec_class);;
+    int ntracks = (int)f < 1 ? 1 : (int)f;
+    t_rec_track **tracks = getbytes(ntracks * sizeof(*tracks));
+    t_rec_track **tpp;
+    int i;
+    for(i = 0, tpp = tracks; i < ntracks; i++, tpp++){
+        *tpp = (t_rec_track *)pd_new(rec_track_class);
+        (*tpp)->tr_binbuf = binbuf_new();
+        (*tpp)->tr_clock = clock_new(*tpp, (t_method)rec_track_tick);
     }
+    x->x_canvas = canvas_getcurrent();
+    x->x_elsefilehandle = elsefile_new((t_pd *)x, rec_readhook, rec_writehook);
+    if(ntracks > REC_MAXTRACKS)
+        ntracks = REC_MAXTRACKS;
+    x->x_ntracks = ntracks;
+    x->x_tracks = tracks;
+    for(int id = 1; id <= ntracks; id++, tracks++){ // 1-based
+        t_rec_track *tp = *tracks;
+        inlet_new((t_object *)x, (t_pd *)tp, 0, 0);
+        tp->tr_trackout = outlet_new((t_object *)x, &s_);
+        tp->tr_owner = x;
+        tp->tr_id = id;
+        tp->tr_listed = 0;
+        tp->tr_mode = REC_STOPMODE;
+        tp->tr_muted = 0;
+        tp->tr_restarted = 0;
+        tp->tr_atdelta = 0;
+        tp->tr_ixnext = 0;
+        tp->tr_tempo = 1.;
+        tp->tr_clockdelay = 0.;
+        tp->tr_prevtime = 0.;
+    }
+    x->x_bangout = outlet_new((t_object *)x, &s_bang);
+    if(name != &s_)
+        rec_doread(x, name);
     return(x);
 }
 
@@ -637,7 +584,8 @@ void rec_setup(void){
     class_addanything(rec_track_class, rec_track_anything);
     class_addlist(rec_track_class, rec_track_list);
     
-    rec_class = class_new(gensym("rec"), (t_newmethod)rec_new, (t_method)rec_free, sizeof(t_rec), 0, A_DEFFLOAT, 0);
+    rec_class = class_new(gensym("rec"), (t_newmethod)rec_new,
+        (t_method)rec_free, sizeof(t_rec), 0, A_DEFFLOAT, A_DEFSYMBOL, 0);
     class_addmethod(rec_class, (t_method)rec_record, gensym("record"), A_GIMME, 0);
     class_addmethod(rec_class, (t_method)rec_play, gensym("play"), A_GIMME, 0);
     class_addmethod(rec_class, (t_method)rec_stop, gensym("stop"), A_GIMME, 0);
