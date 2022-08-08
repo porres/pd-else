522b523b35f250107d416888fba1e4401df6686b
porres <porres@gmail.com>
2022-08-08 19:50:08 -0300

    rand.i / rand.seq
    closes https://github.com/porres/pd-else/issues/1310

--- Classes/Source/rand.i.c
+++ Classes/Source/rand.i.c
@@ -1,47 +1,54 @@
 // Porres 2017
  
 #include "m_pd.h"
-
-static t_class *randi_class;
+#include <time.h>
 
 typedef struct _randi{
-    t_object        x_obj;
-    unsigned int    x_val;
-    t_float         x_min;
-    t_float         x_max;
-} t_randi;
+    t_object     x_obj;
+    unsigned int x_state;
+    t_float      x_min;
+    t_float      x_max;
+}t_randi;
 
-static void randi_seed(t_randi *x, t_float f){
-    x->x_val = f;
-}
+static t_class *randi_class;
+static int initflag = 0;
 
 static void randi_bang(t_randi *x){
-    unsigned int val = x->x_val;
-    t_int random;
-    t_int out_low = (int)x->x_min; // Output LOW
-    t_int out_high = (int)x->x_max + 1; // Output HIGH
-    if(out_low > out_high){
-        int temp = out_low;
-        out_low = out_high;
-        out_high = temp;
+    int min = (int)x->x_min; // Output LOW
+    int max = (int)x->x_max + 1; // Output HIGH
+    if(min > max){
+        int temp = min;
+        min = max;
+        max = temp;
     }
-    t_int range = out_high - out_low; // range
-    if(range == 0)
-        random = out_low;
-    else{
-        random = ((double)range) * ((double)val) * (1./4294967296.);
-        if(random > range)
-            random = range;
-        random += out_low;
-        x->x_val = val * 472940017 + 832416023;
+    int range = max - min; // range
+    int random = min;
+    if(range){
+        x->x_state = x->x_state * 472940017 + 832416023;
+        random = ((double)range) * ((double)x->x_state) * (1./4294967296.);
+        if(random >= range)
+            random = range-1;
+        random += min;
     }
     outlet_float(x->x_obj.ob_outlet, random);
+}
+
+static void randi_seed(t_randi *x, t_symbol *s, int ac, t_atom *av){
+    s = NULL;
+    if(!ac){
+        unsigned int seed = time(NULL) + (initflag * 151);
+        seed = seed * 435898247 + 938284287;
+        x->x_state = seed & 0x7fffffff;
+    }
+    else
+        x->x_state = (unsigned int)(atom_getfloat(av));
 }
 
 static void *randi_new(t_symbol *s, int argc, t_atom *argv){
     s = NULL;
     t_randi *x = (t_randi *) pd_new(randi_class);
-    static unsigned int random_seed = 1489853723;
+    unsigned int initseed = time(NULL);
+    int seed_flag = 0;
 //////////////////////////////////////////////////////////
     x->x_min = 0.;
     x->x_max = 1.;
@@ -54,7 +61,7 @@ static void *randi_new(t_symbol *s, int argc, t_atom *argv){
     else if(argc > 1 && argc <= 3){
         int numargs = 0;
         while(argc > 0){
-            if(argv -> a_type == A_FLOAT){
+            if(argv->a_type == A_FLOAT){
                 switch(numargs){
                     case 0: x->x_min = atom_getfloatarg(0, argc, argv);
                         numargs++;
@@ -66,7 +73,8 @@ static void *randi_new(t_symbol *s, int argc, t_atom *argv){
                         argc--;
                         argv++;
                         break;
-                    case 2: random_seed = atom_getfloatarg(0, argc, argv);
+                    case 2: initseed = atom_getfloatarg(0, argc, argv);
+                        seed_flag = 1;
                         numargs++;
                         argc--;
                         argv++;
@@ -84,19 +92,25 @@ static void *randi_new(t_symbol *s, int argc, t_atom *argv){
     else if(argc > 3)
         goto errstate;
 ////////////////////////////////////////////////////////////
-    random_seed = random_seed * 435898247 + 938284287;
-    x->x_val = random_seed & 0x7fffffff; // load seed value
+    if(seed_flag)
+        x->x_state = initseed;
+    else{
+        unsigned int seed = initseed + (initflag * 151);
+        seed = seed * 435898247 + 938284287;
+        x->x_state = seed & 0x7fffffff;
+    }
+    initflag++;
     floatinlet_new((t_object *)x, &x->x_min);
     floatinlet_new((t_object *)x, &x->x_max);
     outlet_new((t_object *)x, &s_float);
-    return (x);
+    return(x);
 errstate:
-    pd_error(x, "randi: improper args");
+    pd_error(x, "[rand.i]: improper args");
     return NULL;
 }
 
 void setup_rand0x2ei(void){
   randi_class = class_new(gensym("rand.i"), (t_newmethod)randi_new, 0, sizeof(t_randi), 0, A_GIMME, 0);
-  class_addmethod(randi_class, (t_method)randi_seed, gensym("seed"), A_DEFFLOAT, 0);
+  class_addmethod(randi_class, (t_method)randi_seed, gensym("seed"), A_GIMME, 0);
   class_addbang(randi_class, (t_method)randi_bang);
 }
