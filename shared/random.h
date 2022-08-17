a674968c571ed13c67a1f20b79f0bbbd226220d5
porres <porres@gmail.com>
2022-08-17 16:57:59 -0300

    Update random.h

--- shared/random.h
+++ shared/random.h
@@ -12,6 +12,7 @@ typedef struct _random_state{
 
 void random_init(t_random_state* rstate, float f);
 unsigned int get_seed(t_symbol *s, int ac, t_atom *av, int n);
+uint32_t random_trand(uint32_t* s1, uint32_t* s2, uint32_t* s3);
 float random_frand(uint32_t* s1, uint32_t* s2, uint32_t* s3);
 
 // These are for [pink~]
