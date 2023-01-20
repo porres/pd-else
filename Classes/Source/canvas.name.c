f605e7255f9bdc2189a31e5f48a90fda2e005706
porres <porres@gmail.com>
2023-01-20 02:16:41 -0300

    canvas.name

--- Classes/Source/canvas.name.c
+++ Classes/Source/canvas.name.c
@@ -7,7 +7,6 @@ static t_class *canvas_name_class;
 typedef struct canvas_name{
     t_object x_obj;
     t_symbol *x_name;
-    t_symbol *x_wname;
 }t_canvas_name;
 
 static void canvas_name_bang(t_canvas_name *x){
@@ -17,16 +16,13 @@ static void canvas_name_bang(t_canvas_name *x){
 static void *canvas_name_new(t_symbol *s, int ac, t_atom *av){
     s = NULL;
     t_canvas_name *x = (t_canvas_name *)pd_new(canvas_name_class);
-    int depth = 0, env = 0, w = 0;
+    int depth = 0, env = 0;
     t_canvas *canvas;
     while(ac && av->a_type == A_SYMBOL){
         t_symbol *cursym = atom_getsymbolarg(0, ac, av);
-        if(cursym == gensym("-env")){
+        if(cursym == gensym("-env"))
             env = 1;
-        }
-        else if(cursym == gensym("-w"))
-            w = 1;
-        ac--, av++;
+        ac--;
     }
     if(ac && av->a_type == A_FLOAT){
         float f = av->a_w.w_float;
@@ -43,10 +39,7 @@ static void *canvas_name_new(t_symbol *s, int ac, t_atom *av){
             canvas = canvas->gl_owner;
     }
     char buf[MAXPDSTRING];
-    if(w)
-        snprintf(buf, MAXPDSTRING, ".x%lx", (long unsigned int)canvas);
-    else
-        snprintf(buf, MAXPDSTRING, ".x%lx.c", (long unsigned int)canvas);
+    snprintf(buf, MAXPDSTRING, ".x%lx", (long unsigned int)canvas);
     x->x_name = gensym(buf);
     outlet_new(&x->x_obj, &s_);
     return(void *)x;
