68a50caa47eeeb1a0b9dc1485fd5bfda7b762cfc
Albert Graef <aggraef@gmail.com>
2023-02-07 03:31:25 +0100

    Reword the note about availability of the Live-Electronics-Tutorial.

--- Classes/Source/else.c
+++ Classes/Source/else.c
@@ -61,10 +61,10 @@ void print_else_obj(t_else_obj *x){
 #endif
     }
     post("-------------------------------------------------------------------");
-    post("- NOTE: This library also includes a tutorial that depends");
-    post("on this library by Alexandre Torres Porres!!!");
-    post("Find the \"Live-Electronics-Tutorial\" folder inside the \"else\"");
-    post("folder. Please check its README on how to install it!");
+    post("- NOTE: There's an accompanying tutorial by Alexandre Torres");
+    post("Porres which we recommend. You can find the tutorial under");
+    post("\"Live-Electronics-Tutorial\" at: https://github.com/porres/pd-else");
+    post("Please check its README on how to install it!");
     post("-------------------------------------------------------------------");
     post("- ALSO NOTE: Loading this binary did not install the ELSE library");
     post("you still need to add it to the \"preferences=>path\"");
