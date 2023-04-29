5e3eeb667ee30c3e9d4bb7d9ae2d5af84b41428e
porres <porres@gmail.com>
2023-04-29 11:23:28 -0300

    rescale

--- Classes/Source/rescale.c
+++ Classes/Source/rescale.c
@@ -10,6 +10,7 @@ typedef struct _rescale{
     t_object    x_obj;
     t_outlet   *x_outlet;
     int         x_clip;
+    int         x_log;
     float       x_f;
     float       x_minin;
     float       x_maxin;
@@ -28,7 +29,6 @@ static float convert(t_rescale *x, float f){
     float maxout = x->x_maxout;
     float rangein = x->x_maxin - minin;
     float rangeout = maxout - minout;
-    float e = x->x_exp;
     if(f == minin)
         return(minout);
     if(f == x->x_maxin)
@@ -39,20 +39,20 @@ static float convert(t_rescale *x, float f){
         else if(f > x->x_maxin)
             return(maxout);
     }
-    if(e == 0) // linear
-        return(minout + rangeout * (f-minin)/rangein);
     float p = (f-minin)/rangein; // position
-    if(e == 1){ // 'log'
+    if(x->x_log){ // 'log'
         if((minout <= 0 && maxout >= 0) || (minout >= 0 && maxout <= 0)){
             pd_error(x, "[rescale]: output range cannot contain '0' in log mode");
             return(0);
         }
         return(exp(p * log(maxout / minout)) * minout);
     }
-    if(e > 0) // exponential
-        return(pow(p, e) * rangeout + minout);
+    if(fabs(x->x_exp) == 1 || x->x_exp == 0) // linear
+        return(minout + rangeout * (f-minin)/rangein);
+    if(x->x_exp > 0) // exponential
+        return(pow(p, x->x_exp) * rangeout + minout);
     else // negative exponential
-        return((1 - pow(1-p, -e)) * rangeout + minout);
+        return((1 - pow(1-p, -x->x_exp)) * rangeout + minout);
 }
 
 static void rescale_list(t_rescale *x, t_symbol *s, int ac, t_atom *av){
@@ -70,8 +70,17 @@ static void rescale_list(t_rescale *x, t_symbol *s, int ac, t_atom *av){
     }
 }
 
+static void rescale_in(t_rescale *x, t_floatarg f1, t_floatarg f2){
+    x->x_minin = f1;
+    x->x_maxin = f2;
+}
+
 static void rescale_exp(t_rescale *x, t_floatarg f){
     x->x_exp = f;
+}
+
+static void rescale_log(t_rescale *x, t_floatarg f){
+    x->x_log = (f != 0);
 }
 
 static void *rescale_new(t_symbol *s, int ac, t_atom *av){
@@ -82,83 +91,50 @@ static void *rescale_new(t_symbol *s, int ac, t_atom *av){
     x->x_minout = 0;
     x->x_maxout = 1;
     x->x_exp = 0.f;
+    x->x_log = 0;
     t_int numargs = 0;
-    if(ac > 0){
+    while(ac){
         if(av->a_type == A_SYMBOL){
-            if(atom_getsymbolarg(0, ac, av) == gensym("-clip") && !numargs)
+            t_symbol *sym = atom_getsymbol(av);
+            if(sym == gensym("-clip") && !numargs)
                 x->x_clip = 1;
+            else if(sym == gensym("-log") && !numargs)
+                x->x_log = 1;
+            else if(ac >= 2 && sym == gensym("-exp") && !numargs){
+                ac--, av++;
+                x->x_exp = atom_getfloat(av);
+            }
+            else if(ac >= 3 && sym == gensym("-in") && !numargs){
+                ac--, av++;
+                x->x_minin = atom_getfloat(av);
+                ac--, av++;
+                x->x_maxin = atom_getfloat(av);
+            }
             else
                 goto errstate;
             ac--, av++;
         }
-        if(ac <= 3){
-            while(ac){
-                if(av->a_type == A_FLOAT){
-                    float argval = atom_getfloatarg(0, ac, av);
-                    switch(numargs){
-                        case 0:
-                            x->x_minout = argval;
-                            break;
-                        case 1:
-                            x->x_maxout = argval;
-                            break;
-                        case 2:
-                            x->x_exp = argval;
-                            break;
-                        default:
-                            break;
-                    };
-                }
-                else
-                    goto errstate;
-                numargs++;
-                ac--, av++;
-            }
+        else{
+            switch(numargs){
+                case 0:
+                    x->x_minout = atom_getfloat(av);
+                    break;
+                case 1:
+                    x->x_maxout = atom_getfloat(av);
+                    break;
+                case 2:
+                    x->x_exp = atom_getfloat(av);
+                    break;
+                default:
+                    break;
+            };
+            numargs++;
+            ac--, av++;
         }
-        else if(ac <= 5){
-            while(ac){
-                if(av->a_type == A_FLOAT){
-                    float argval = atom_getfloatarg(0, ac, av);
-                    switch(numargs){
-                        case 0:
-                            x->x_minin = argval;
-                            break;
-                        case 1:
-                            x->x_maxin = argval;
-                            break;
-                        case 2:
-                            x->x_minout = argval;
-                            break;
-                        case 3:
-                            x->x_maxout = argval;
-                            break;
-                        case 4:
-                            x->x_exp = argval;
-                            break;
-                        default:
-                            break;
-                    };
-                    numargs++;
-                    ac--, av++;
-                }
-                else
-                    goto errstate;
-            }
-        }
-        else
-            goto errstate;
     }
+    floatinlet_new(&x->x_obj, &x->x_minout);
+    floatinlet_new(&x->x_obj, &x->x_maxout);
     x->x_outlet = outlet_new(&x->x_obj, 0);
-    if(numargs <= 3){
-        floatinlet_new(&x->x_obj, &x->x_minout);
-        floatinlet_new(&x->x_obj, &x->x_maxout);
-    }
-    else{
-        floatinlet_new(&x->x_obj, &x->x_minin);
-        floatinlet_new(&x->x_obj, &x->x_maxin);
-        floatinlet_new(&x->x_obj, &x->x_minout);
-        floatinlet_new(&x->x_obj, &x->x_maxout);
-    }
     return(x);
 errstate:
     post("[rescale]: improper args");
@@ -169,6 +145,8 @@ void rescale_setup(void){
     rescale_class = class_new(gensym("rescale"), (t_newmethod)rescale_new, 0,
         sizeof(t_rescale), 0, A_GIMME, 0);
     class_addlist(rescale_class, (t_method)rescale_list);
-    class_addmethod(rescale_class, (t_method)rescale_exp, gensym("exp"), A_DEFFLOAT, 0);
+    class_addmethod(rescale_class, (t_method)rescale_in, gensym("in"), A_FLOAT, A_FLOAT, 0);
+    class_addmethod(rescale_class, (t_method)rescale_exp, gensym("exp"), A_FLOAT, 0);
     class_addmethod(rescale_class, (t_method)rescale_clip, gensym("clip"), A_FLOAT, 0);
+    class_addmethod(rescale_class, (t_method)rescale_log, gensym("log"), A_FLOAT, 0);
 }
