621810a370e4ccff2ee15be5b440e6b4afa25080
porres <porres@gmail.com>
2023-06-16 13:01:10 -0300

    Update nchs~.c

--- Classes/Source/nchs~.c
+++ Classes/Source/nchs~.c
@@ -15,19 +15,23 @@ static void nchs_bang(t_nchs *x){
 
 static void nchs_tilde_dsp(t_nchs *x, t_signal **sp){
     x->x_nchans = sp[0]->s_nchans;
-    nchs_bang(x);
 }
 
 static void *nchs_tilde_new(void){
     t_nchs *x = (t_nchs *)pd_new(nchs_tilde_class);
     x->x_nchans = 0;
+    pd_bind(&x->x_obj.ob_pd, gensym("pd-dsp-started"));
     outlet_new(&x->x_obj, &s_float);
     return(x);
 }
 
+void nchs_free(t_nchs *x){
+    pd_unbind(&x->x_obj.ob_pd, gensym("pd-dsp-started"));
+}
+
 void nchs_tilde_setup(void){
-    nchs_tilde_class = class_new(gensym("nchs~"),
-        (t_newmethod)nchs_tilde_new, 0, sizeof(t_nchs), 0, 0);
+    nchs_tilde_class = class_new(gensym("nchs~"), (t_newmethod)nchs_tilde_new,
+        (t_method)nchs_free, sizeof(t_nchs), 0, 0);
     class_setdspflags(nchs_tilde_class, CLASS_MULTICHANNEL);
     class_addmethod(nchs_tilde_class, nullfn, gensym("signal"), 0);
     class_addmethod(nchs_tilde_class, (t_method)nchs_tilde_dsp, gensym("dsp"), A_CANT, 0);
