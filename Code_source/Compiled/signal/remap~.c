55ac3466b8e2042e6e146b71131db3f30e9a6e12
porres <porres@gmail.com>
2023-07-23 02:12:13 -0300

    Update remap~.c

--- Code_source/Compiled/signal/remap~.c
+++ Code_source/Compiled/signal/remap~.c
@@ -1,15 +1,15 @@
 // porres
 
 #include "m_pd.h"
+#include <stdlib.h>
 
 static t_class *remap_class;
-
-#define INPUTLIMIT 32768 // 512 * 64
 
 typedef struct _remap{
     t_object x_obj;
     int      x_n;
     t_atom  *x_vec;
+    t_float *x_input;
 }t_remap;
 
 static void remap_list(t_remap *x, t_symbol *s, int ac, t_atom *av){
@@ -24,12 +24,16 @@ static void remap_list(t_remap *x, t_symbol *s, int ac, t_atom *av){
 }
     
 static t_int *remap_perform(t_int *w){
+    int i;
     t_remap *x = (t_remap *)(w[1]);
     t_int n = (t_int)(w[2]);
     t_int nchans = (t_int)(w[3]);
-    t_sample *in = (t_sample *)(w[4]);
+    t_sample *input = (t_sample *)(w[4]);
     t_sample *out = (t_sample *)(w[5]);
-    for(int i = 0; i < x->x_n; i++){ // channels to copy
+    t_float *in = x->x_input;
+    for(i = 0; i < n*nchans; i++)
+        in[i] = input[i];
+    for(i = 0; i < x->x_n; i++){ // channels to copy
         int ch = x->x_vec[i].a_w.w_float - 1; // get channel number
         if(ch >= nchans)
             ch = nchans - 1;
@@ -45,11 +49,13 @@ static t_int *remap_perform(t_int *w){
 
 static void remap_dsp(t_remap *x, t_signal **sp){
     signal_setmultiout(&sp[1], x->x_n);
-/*    post("x->x_n = %d", x->x_n);
-    post("length = %d", sp[0]->s_length);
-    post("nchans = %d", sp[0]->s_nchans);*/
+    x->x_input = (t_float *)malloc(sp[0]->s_length*sp[0]->s_nchans * sizeof(t_float *));
     dsp_add(remap_perform, 5, x, sp[0]->s_length, sp[0]->s_nchans,
         sp[0]->s_vec, sp[1]->s_vec);
+}
+
+void remap_free(t_remap *x){
+    free(x->x_input);
 }
 
 static void *remap_new(t_symbol *s, int ac, t_atom *av){
@@ -65,7 +71,7 @@ static void *remap_new(t_symbol *s, int ac, t_atom *av){
 
 void remap_tilde_setup(void){
     remap_class = class_new(gensym("remap~"), (t_newmethod)remap_new,
-        0, sizeof(t_remap), CLASS_MULTICHANNEL, A_GIMME, 0);
+        (t_method)remap_free, sizeof(t_remap), CLASS_MULTICHANNEL, A_GIMME, 0);
     class_addlist(remap_class, remap_list);
     class_addmethod(remap_class, nullfn, gensym("signal"), 0);
     class_addmethod(remap_class, (t_method)remap_dsp, gensym("dsp"), 0);
