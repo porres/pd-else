61f689063f227408d2eb493a2d3b56a1af0c0e3c
porres <porres@gmail.com>
2023-08-05 18:23:17 -0300

    rescale~

--- Code_source/Compiled/signal/rescale~.c
+++ Code_source/Compiled/signal/rescale~.c
@@ -5,13 +5,16 @@
 
 typedef struct _rescale{
     t_object  x_obj;
+    t_inlet  *x_inlet_minout;
+    t_inlet  *x_inlet_maxout;
     t_float   x_exp;
     int       x_log;
     t_int     x_clip;
     t_float   x_minin;
     t_float   x_maxin;
-    t_float   x_minout;
-    t_float   x_maxout;
+    float    *x_minout;
+    float    *x_maxout;
+    int       x_nchans;
 }t_rescale;
 
 static t_class *rescale_class;
@@ -33,10 +36,8 @@ static void rescale_clip(t_rescale *x, t_floatarg f){
     x->x_clip = f != 0;
 }
 
-static float convert(t_rescale *x, float f){
+static float rescale_convert(t_rescale *x, float f, float minout, float maxout){
     float minin = x->x_minin;
-    float minout = x->x_minout;
-    float maxout = x->x_maxout;
     float rangein = x->x_maxin - minin;
     float rangeout = maxout - minout;
     if(f == minin)
@@ -75,24 +76,59 @@ static float convert(t_rescale *x, float f){
 
 static t_int *rescale_perform(t_int *w){
     t_rescale *x = (t_rescale *)(w[1]);
-    t_sample *in = (t_sample *)(w[2]);
-    t_sample *out = (t_sample *)(w[3]);
-    int n = (int)(w[4]);
-    while(n--)
-        *out++ = convert(x, *in++);
-    return(w+5);
+    int n = (int)(w[2]);
+    int ch2 = (int)(w[3]);
+    int ch3 = (int)(w[4]);
+    t_float *in1 = (t_float *)(w[5]);
+    t_float *in2 = (t_float *)(w[6]);
+    t_float *in3 = (t_float *)(w[7]);
+    t_float *out = (t_float *)(w[8]);
+    for(int j = 0; j < x->x_nchans; j++){
+        for(int i = 0; i < n; i++){
+            t_float in = in1[j*n + i];
+            t_float minout = ch2 == 1 ? in2[i] : in2[j*n + i];
+            t_float maxout = ch3 == 1 ? in3[i] : in3[j*n + i];
+            out[j*n + i] = rescale_convert(x, in, minout, maxout);
+        }
+    }
+    return(w+9);
 }
 
 static void rescale_dsp(t_rescale *x, t_signal **sp){
-    signal_setmultiout(&sp[1], sp[0]->s_nchans);
-    dsp_add(rescale_perform, 4, x, sp[0]->s_vec, sp[1]->s_vec,
-        ((t_int)((sp[0])->s_length * (sp[0])->s_nchans)));
+    int chs = sp[0]->s_nchans, ch2 = sp[1]->s_nchans, ch3 = sp[2]->s_nchans, n = sp[0]->s_n;
+    signal_setmultiout(&sp[3], chs);
+    if(x->x_nchans != chs){
+        x->x_minout = (float *)resizebytes(x->x_minout,
+            x->x_nchans * sizeof(float), chs * sizeof(float));
+        x->x_maxout = (float *)resizebytes(x->x_maxout,
+            x->x_nchans * sizeof(float), chs * sizeof(float));
+        x->x_nchans = chs;
+    }
+    if((ch2 > 1 && ch2 != x->x_nchans) || (ch3 > 1 && ch3 != x->x_nchans)){
+        dsp_add_zero(sp[3]->s_vec, chs*n);
+        pd_error(x, "[rescale~]: channel sizes mismatch");
+    }
+    else
+        dsp_add(rescale_perform, 8, x, n, ch2, ch3, sp[0]->s_vec,
+            sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec);
+}
+
+static void *rescale_free(t_rescale *x){
+    inlet_free(x->x_inlet_minout);
+    inlet_free(x->x_inlet_maxout);
+    freebytes(x->x_minout, x->x_nchans * sizeof(*x->x_minout));
+    freebytes(x->x_maxout, x->x_nchans * sizeof(*x->x_maxout));
+    return(void *)x;
 }
 
 static void *rescale_new(t_symbol *s, int ac, t_atom *av){
     s = NULL;
     t_rescale *x = (t_rescale *)pd_new(rescale_class);
-    x->x_minin = -1, x->x_maxin = 1, x->x_minout = 0, x->x_maxout = 1;
+    x->x_nchans = 1;
+    x->x_minout = (float *)getbytes(sizeof(*x->x_minout));
+    x->x_maxout = (float *)getbytes(sizeof(*x->x_maxout));
+    x->x_minin = -1, x->x_maxin = 1;
+    float minout = 0, maxout = 1;
     x->x_exp = 0, x->x_clip = 1;
     t_int numargs = 0;
     while(ac){
@@ -108,9 +144,9 @@ static void *rescale_new(t_symbol *s, int ac, t_atom *av){
             }
             else if(ac >= 3 && sym == gensym("-in") && !numargs){
                 ac--, av++;
-                x->x_minin = atom_getfloat(av);
+                minout = atom_getfloat(av);
                 ac--, av++;
-                x->x_maxin = atom_getfloat(av);
+                maxout = atom_getfloat(av);
             }
             else
                 goto errstate;
@@ -119,10 +155,10 @@ static void *rescale_new(t_symbol *s, int ac, t_atom *av){
         else{
             switch(numargs){
                 case 0:
-                    x->x_minout = atom_getfloat(av);
+                    minout = atom_getfloat(av);
                     break;
                 case 1:
-                    x->x_maxout = atom_getfloat(av);
+                    maxout = atom_getfloat(av);
                     break;
                 case 2:
                     x->x_exp = atom_getfloat(av);
@@ -134,8 +170,10 @@ static void *rescale_new(t_symbol *s, int ac, t_atom *av){
             ac--, av++;
         }
     }
-    floatinlet_new(&x->x_obj, &x->x_minout);
-    floatinlet_new(&x->x_obj, &x->x_maxout);
+    x->x_inlet_minout = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
+        pd_float((t_pd *)x->x_inlet_minout, minout);
+    x->x_inlet_maxout = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
+        pd_float((t_pd *)x->x_inlet_maxout, maxout);
     outlet_new((t_object *)x, &s_signal);
     return(x);
 errstate:
@@ -145,7 +183,7 @@ errstate:
 
 void rescale_tilde_setup(void){
     rescale_class = class_new(gensym("rescale~"),(t_newmethod)rescale_new,
-        0, sizeof(t_rescale), CLASS_MULTICHANNEL, A_GIMME, 0);
+        (t_method)rescale_free, sizeof(t_rescale), CLASS_MULTICHANNEL, A_GIMME, 0);
     class_addmethod(rescale_class, nullfn, gensym("signal"), 0);
     class_addmethod(rescale_class, (t_method)rescale_dsp, gensym("dsp"), A_CANT, 0);
     class_addmethod(rescale_class, (t_method)rescale_in, gensym("in"), A_FLOAT, A_FLOAT, 0);
