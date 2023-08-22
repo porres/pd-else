fd771fa64fe01b59e4c1f6b0c455bb2f8849f837
tomara_x <86204514+tomara-x@users.noreply.github.com>
2023-08-22 11:30:41 +0300

    update resources

--- Code_source/Compiled/signal/plaits~/plaits/resources.h
+++ Code_source/Compiled/signal/plaits~/plaits/resources.h
@@ -1,6 +1,6 @@
-// Copyright 2016 Olivier Gillet.
+// Copyright 2016 Emilie Gillet.
 //
-// Author: Olivier Gillet (ol.gillet@gmail.com)
+// Author: Emilie Gillet (emilie.o.gillet@gmail.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -42,20 +42,28 @@ namespace plaits {
 
 typedef uint8_t ResourceId;
 
-extern const float* lookup_table_table[];
+extern const uint8_t* const fm_patches_table[];
 
-extern const int16_t* lookup_table_i16_table[];
+extern const float* const lookup_table_table[];
 
-extern const int8_t* lookup_table_i8_table[];
+extern const int16_t* const lookup_table_i16_table[];
 
-extern const int16_t* wavetables_table[];
+extern const int32_t* const lookup_table_i32_table[];
 
+extern const int8_t* const lookup_table_i8_table[];
+
+extern const int16_t* const wavetables_table[];
+
+extern const uint8_t syx_bank_0[];
+extern const uint8_t syx_bank_1[];
+extern const uint8_t syx_bank_2[];
 extern const float lut_sine[];
 extern const float lut_fm_frequency_quantizer[];
 extern const float lut_fold[];
 extern const float lut_fold_2[];
 extern const float lut_stiffness[];
 extern const float lut_svf_shift[];
+extern const float lut_4x_downsampler_fir[];
 extern const int16_t lut_ws_inverse_tan[];
 extern const int16_t lut_ws_inverse_sin[];
 extern const int16_t lut_ws_linear[];
@@ -63,10 +71,16 @@ extern const int16_t lut_ws_bump[];
 extern const int16_t lut_ws_double_bump[];
 extern const int8_t lut_lpc_excitation_pulse[];
 extern const int16_t wav_integrated_waves[];
+#define SYX_BANK_0 0
+#define SYX_BANK_0_SIZE 4096
+#define SYX_BANK_1 1
+#define SYX_BANK_1_SIZE 4096
+#define SYX_BANK_2 2
+#define SYX_BANK_2_SIZE 4096
 #define LUT_SINE 0
 #define LUT_SINE_SIZE 1281
 #define LUT_FM_FREQUENCY_QUANTIZER 1
-#define LUT_FM_FREQUENCY_QUANTIZER_SIZE 129
+#define LUT_FM_FREQUENCY_QUANTIZER_SIZE 130
 #define LUT_FOLD 2
 #define LUT_FOLD_SIZE 516
 #define LUT_FOLD_2 3
@@ -75,6 +89,8 @@ extern const int16_t wav_integrated_waves[];
 #define LUT_STIFFNESS_SIZE 65
 #define LUT_SVF_SHIFT 5
 #define LUT_SVF_SHIFT_SIZE 257
+#define LUT_4X_DOWNSAMPLER_FIR 6
+#define LUT_4X_DOWNSAMPLER_FIR_SIZE 4
 #define LUT_WS_INVERSE_TAN 0
 #define LUT_WS_INVERSE_TAN_SIZE 257
 #define LUT_WS_INVERSE_SIN 1
