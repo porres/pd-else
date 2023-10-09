bb1ec25d7e23b7fcd178f61d3b16ef75941b8fba
porres <porres@gmail.com>
2023-10-09 02:01:03 -0300

    Update var.c

--- Code_source/Compiled/control/var.c
+++ Code_source/Compiled/control/var.c
@@ -17,11 +17,10 @@ typedef struct _var{
     t_float   **x_floatstar;
 }t_var;
 
-// get a pointer to a named floating-point variable.  The variable
-// belongs to a "vcommon" object, which is created if necessary.
+// get pointer to a named variable, which belongs to a "vcommon" object
 t_float *var_get(t_symbol *s){
     t_vcommon *c = (t_vcommon *)pd_findbyclass(s, vcommon_class);
-    if(!c){
+    if(!c){ // create named variable because it doesn't exist yet
         c = (t_vcommon *)pd_new(vcommon_class);
         c->c_f = 0;
         c->c_refcount = 0;
@@ -31,8 +30,7 @@ t_float *var_get(t_symbol *s){
     return(&c->c_f);
 }
 
-// release a variable.  This only frees the "vcommon" resource when the
-// last interested party releases it.
+// release a variable. Only frees "vcommon" resource when last one releases it.
 void var_release(t_symbol *s){
     t_vcommon *c = (t_vcommon *)pd_findbyclass(s, vcommon_class);
     if(c){
@@ -42,7 +40,7 @@ void var_release(t_symbol *s){
         }
     }
     else
-        post("var_release");
+        post("var release bug, releaing");
 }
 
 static void vcommon_float(t_vcommon *x, t_float f){
@@ -63,7 +61,6 @@ static void var_list(t_var *x, t_symbol *s, int ac, t_atom *av){
         var_bang(x);
     else{
         int n = ac > x->x_n_vars ? x->x_n_vars : ac;
-//        post("list: n = %d", n);
         for(int i = 0; i < n; i++){
             t_float f = *x->x_floatstar[i] = atom_getfloat(av+i);
             if((x->x_sym[i])->s_thing)
@@ -81,7 +78,6 @@ static void *var_new(t_symbol *s, int ac, t_atom *av){
     s = NULL;
     t_var *x = (t_var *)pd_new(var_class);
     x->x_n_vars = (ac ? ac : 1);
-//    post("x->x_n_vars = %d", x->x_n_vars);
     x->x_sym = getbytes(sizeof(t_symbol) * x->x_n_vars);
     x->x_floatstar = getbytes(sizeof(float) * x->x_n_vars);
     if(ac){
@@ -99,5 +95,5 @@ void var_setup(void){
         (t_method)var_free, sizeof(t_var), 0, A_GIMME, 0);
     class_addlist(var_class, var_list);
     vcommon_class = class_new(gensym("var"), 0, 0, sizeof(t_vcommon), CLASS_PD, 0);
-    class_addfloat(vcommon_class, vcommon_float);
+    class_addfloat(vcommon_class, vcommon_float); // why???
 }
