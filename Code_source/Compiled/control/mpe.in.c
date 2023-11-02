0fb5f72649f69cc24d7c06bc705e24cbbac0856f
porres <porres@gmail.com>
2023-11-02 18:55:50 -0300

    mpe in

--- Code_source/Compiled/control/mpe.in.c
+++ Code_source/Compiled/control/mpe.in.c
@@ -8,6 +8,7 @@ typedef struct _mpein{
     t_object       x_obj;
     int            x_ready;
     int            x_type;
+    int            x_ext;
     unsigned char  x_channel;
     unsigned char  x_byte1;
 }t_mpein;
@@ -47,8 +48,10 @@ static void mpein_float(t_mpein *x, t_float f){ // raw MIDI
     }
     else{ // < 128, byte value
         if(!x->x_ready){
-            if(x->x_type == SLIDE && val == 74) // if CC#74, get ready
-                x->x_ready = 1;
+            if(x->x_type == SLIDE){
+                if(val == 74) // if CC#74, get ready
+                    x->x_ready = 1;
+            }
             else if(x->x_type >= 0){
                 x->x_byte1 = val;
                 x->x_ready = 1;
@@ -57,7 +60,13 @@ static void mpein_float(t_mpein *x, t_float f){ // raw MIDI
         else{ // it's ready
             if(x->x_type == NOTEON){
                 t_atom at[4];
+                
                 SETFLOAT(at, x->x_channel);
+                SETFLOAT(at + 1, x->x_byte1);
+                SETFLOAT(at + 2, val);
+                SETFLOAT(at + 3, 0);
+                outlet_list(((t_object *)x)->ob_outlet, &s_list, 4, at);
+                
                 SETSYMBOL(at + 1, gensym("noteon"));
                 SETFLOAT(at + 2, x->x_byte1);
                 SETFLOAT(at + 3, val);
@@ -65,7 +74,13 @@ static void mpein_float(t_mpein *x, t_float f){ // raw MIDI
             }
             else if(x->x_type == NOTEOFF){
                 t_atom at[4];
+                
                 SETFLOAT(at, x->x_channel);
+                SETFLOAT(at + 1, x->x_byte1);
+                SETFLOAT(at + 2, 0);
+                SETFLOAT(at + 3, val);
+                outlet_list(((t_object *)x)->ob_outlet, &s_list, 4, at);
+                
                 SETSYMBOL(at + 1, gensym("noteoff"));
                 SETFLOAT(at + 2, x->x_byte1);
                 SETFLOAT(at + 3, val);
@@ -98,16 +113,38 @@ static void mpein_float(t_mpein *x, t_float f){ // raw MIDI
     }
 }
 
-void *mpein_new(void){
+static void mpein_list(t_mpein *x, t_symbol *s, int ac, t_atom *av){
+    s = NULL;
+    if(!ac)
+        return;
+    if(!x->x_ext)
+        mpein_float(x, atom_getfloat(av));
+}
+
+static void mpein_ext(t_mpein *x, t_floatarg f){
+    x->x_ext = f != 0;
+}
+
+static void mpein_free(t_mpein *x){
+    pd_unbind(&x->x_obj.ob_pd, gensym("#midiin"));
+}
+
+static void *mpein_new(t_symbol *s, int ac, t_atom *av){
+    s = NULL;
     t_mpein *x = (t_mpein *)pd_new(mpein_class);
-    x->x_ready = 0;
+    x->x_ready = x->x_ext = 0;
     x->x_type = -1;
+    if(ac && atom_getsymbol(av) == gensym("-ext"))
+        x->x_ext = 1;
     outlet_new((t_object *)x, &s_list);
+    pd_bind (&x->x_obj.ob_pd, gensym("#midiin"));
     return(x);
 }
 
 void setup_mpe0x2ein(void){
-    mpein_class = class_new(gensym("mpe.in"), (t_newmethod)mpein_new, 0, sizeof(t_mpein), 0, 0);
+    mpein_class = class_new(gensym("mpe.in"), (t_newmethod)mpein_new,
+        (t_method)mpein_free, sizeof(t_mpein), 0, A_GIMME, 0);
     class_addfloat(mpein_class, mpein_float);
+    class_addlist(mpein_class, mpein_list);
+    class_addmethod(mpein_class, (t_method)mpein_ext, gensym("ext"), A_DEFFLOAT, 0);
 }
-
