17800907b049631de32ceadf3d9000e167100ac1
porres <porres@gmail.com>
2023-12-10 19:49:10 -0300

    findfile

--- Code_source/Compiled/control/findfile.c
+++ Code_source/Compiled/control/findfile.c
@@ -3,16 +3,16 @@
 #include "m_pd.h"
 #include "g_canvas.h"
 
-static t_class *canvas_file_class;
+static t_class *findfile_class;
 
-typedef struct _canvas_file{
+typedef struct _findfile{
     t_object    x_obj;
     t_canvas   *x_cv;
     t_int       x_i;
     t_outlet   *x_fail;
-}t_canvas_file;
+}t_findfile;
 
-static void canvas_file_symbol(t_canvas_file *x, t_symbol *file){
+static void findfile_symbol(t_findfile *x, t_symbol *file){
     char path[MAXPDSTRING], *fn;
     int fd = canvas_open(x->x_cv, file->s_name, "", path, &fn, MAXPDSTRING, 1);
     if(fd >= 0){
@@ -25,14 +25,14 @@ static void canvas_file_symbol(t_canvas_file *x, t_symbol *file){
         outlet_symbol(x->x_fail, file);
 }
 
-static void canvas_file_anything(t_canvas_file *x, t_symbol *s, int ac, t_atom *av){
+static void findfile_anything(t_findfile *x, t_symbol *s, int ac, t_atom *av){
     ac = 0;
     av = NULL;
-    canvas_file_symbol(x, s);
+    findfile_symbol(x, s);
 }
 
-static void *canvas_file_new(t_floatarg f){
-    t_canvas_file *x = (t_canvas_file *)pd_new(canvas_file_class);
+static void *findfile_new(t_floatarg f){
+    t_findfile *x = (t_findfile *)pd_new(findfile_class);
     x->x_cv = canvas_getrootfor(canvas_getcurrent());
     int i = f < 0 ? 0 : (int)f;
     while(i-- && x->x_cv->gl_owner)
@@ -42,9 +42,9 @@ static void *canvas_file_new(t_floatarg f){
     return(x);
 }
 
-void setup_canvas0x2efile(void){
-    canvas_file_class = class_new(gensym("canvas.file"), (t_newmethod)canvas_file_new,
-        0, sizeof(t_canvas_file), 0, A_DEFFLOAT, 0);
-    class_addsymbol(canvas_file_class, canvas_file_symbol);
-    class_addanything(canvas_file_class, canvas_file_anything);
+void setup_findfile(void){
+    findfile_class = class_new(gensym("findfile"), (t_newmethod)findfile_new,
+        0, sizeof(t_findfile), 0, A_DEFFLOAT, 0);
+    class_addsymbol(findfile_class, findfile_symbol);
+    class_addanything(findfile_class, findfile_anything);
 }
