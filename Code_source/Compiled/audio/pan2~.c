a97df1b3cf8dd1366d2c1f7cc434159bea731b32
porres <porres@gmail.com>
2023-12-12 22:20:38 -0300

    pan2

--- Code_source/Compiled/audio/pan2~.c
+++ Code_source/Compiled/audio/pan2~.c
@@ -7,61 +7,101 @@
 
 static t_class *pan2_class;
 
-typedef struct _pan2
-{
-    t_object x_obj;
-    t_inlet  *x_panlet;
-} t_pan2;
+typedef struct _pan2{
+    t_object    x_obj;
+    t_int       x_n;
+    t_int       x_mc;
+    t_inlet    *x_panlet;
+}t_pan2;
 
-static t_int *pan2_perform(t_int *w)
-{
-    int nblock = (t_int)(w[2]);
-    t_float *in1 = (t_float *)(w[3]); // in
-    t_float *in2 = (t_float *)(w[4]); // pan
-    t_float *out1 = (t_float *)(w[5]); // L
-    t_float *out2 = (t_float *)(w[6]); // R
-    while (nblock--)
-    {
+static t_int *pan2_perform(t_int *w){
+    t_pan2 *x = (t_pan2 *)(w[1]);
+    t_float *in1 = (t_float *)(w[2]); // in
+    t_float *in2 = (t_float *)(w[3]); // pan
+    t_float *out1 = (t_float *)(w[4]); // L
+    t_float *out2 = (t_float *)(w[5]); // R
+    int n = x->x_n;
+    while(n--){
         float in = *in1++;
-        float pan = *in2++;
-        if (pan < -1) pan = -1;
-        if (pan > 1) pan = 1;
-        pan = (pan + 1) * 0.5;
+        float pan = (*in2++ + 1) * 0.5;
+        if(pan < -1)
+            pan = -1;
+        if(pan > 1)
+            pan = 1;
         *out1++ = in * (pan == 1 ? 0 : cos(pan * HALF_PI));
         *out2++ = in * sin(pan * HALF_PI);
     }
-    return (w + 7);
+    return(w+6);
 }
 
-static void pan2_dsp(t_pan2 *x, t_signal **sp)
-{
-    dsp_add(pan2_perform, 6, x, sp[0]->s_n,
-            sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec);
+static t_int *pan2_perform_mc(t_int *w){
+    t_pan2 *x = (t_pan2 *)(w[1]);
+    t_float *in1 = (t_float *)(w[2]);   // in
+    t_float *in2 = (t_float *)(w[3]);   // pan
+    t_float *out = (t_float *)(w[4]);
+    for(int i = 0; i < x->x_n; i++){
+        float in = in1[i];
+        float pan = (in2[i] + 1) * 0.5;
+        if(pan < 0)
+            pan = 0;
+        if(pan > 1)
+            pan = 1;
+        for(int j = 0; j < 2; j++){
+            float output;
+            if(j == 0)
+                output = in * (pan == 1 ? 0 : cos(pan * HALF_PI));
+            else
+                output = in * sin(pan * HALF_PI);
+            out[j*x->x_n + i] = output;
+        }
+    }
+    return(w+5);
 }
 
-static void *pan2_free(t_pan2 *x)
-{
+static void pan2_dsp(t_pan2 *x, t_signal **sp){
+    x->x_n = sp[0]->s_n;
+    if(!x->x_mc){
+        signal_setmultiout(&sp[2], 1);
+        signal_setmultiout(&sp[3], 1);
+        dsp_add(pan2_perform, 5, x, sp[0]->s_vec,
+            sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec);
+    }
+    else{
+        signal_setmultiout(&sp[2], 2);
+        dsp_add(pan2_perform_mc, 4, x, sp[0]->s_vec,
+            sp[1]->s_vec, sp[2]->s_vec);
+    }
+}
+
+static void *pan2_free(t_pan2 *x){
     inlet_free(x->x_panlet);
-    return (void *)x;
+    return(void *)x;
 }
 
-static void *pan2_new(t_floatarg f)
-{
+static void *pan2_new(t_symbol *s, int ac, t_atom *av){
+    s = NULL;
     t_pan2 *x = (t_pan2 *)pd_new(pan2_class);
-    if(f < -1) f = -1;
-    if(f > 1) f = 1;
+    float f = 0;
+    x->x_mc = 0;
+    while(ac && av->a_type == A_SYMBOL){
+        if(atom_getsymbol(av) == gensym("-mc")){
+            x->x_mc = 1;
+            ac--, av++;
+        }
+    }
+    if(ac && av->a_type == A_FLOAT)
+        f = atom_getfloat(av);
     x->x_panlet = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
-    pd_float((t_pd *)x->x_panlet, f);
+        pd_float((t_pd *)x->x_panlet, f);
     outlet_new((t_object *)x, &s_signal);
-    outlet_new((t_object *)x, &s_signal);
-    return (x);
+    if(!x->x_mc)
+        outlet_new((t_object *)x, &s_signal);
+    return(x);
 }
 
-void pan2_tilde_setup(void)
-{
+void pan2_tilde_setup(void){
     pan2_class = class_new(gensym("pan2~"), (t_newmethod)pan2_new,
-        (t_method)pan2_free, sizeof(t_pan2), CLASS_DEFAULT, A_DEFFLOAT, 0);
-        class_addmethod(pan2_class, nullfn, gensym("signal"), 0);
-        class_addmethod(pan2_class, (t_method)pan2_dsp, gensym("dsp"), A_CANT, 0);
+        (t_method)pan2_free, sizeof(t_pan2), CLASS_MULTICHANNEL, A_GIMME, 0);
+    class_addmethod(pan2_class, nullfn, gensym("signal"), 0);
+    class_addmethod(pan2_class, (t_method)pan2_dsp, gensym("dsp"), A_CANT, 0);
 }
-
