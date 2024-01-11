7220f500585917965da3cf64da3e27231525cf3e
porres <porres@gmail.com>
2024-01-11 18:39:17 -0300

    maybe pgm.in miraculously works now by mere chance in mac arm?
    people have been reporting a crash in https://github.com/porres/pd-else/issues/1988

--- Code_source/Compiled/control/pgm.in.c
+++ Code_source/Compiled/control/pgm.in.c
@@ -1,41 +1,33 @@
-// porres 2018
+// porres 2018-2024
 
 #include "m_pd.h"
-#include <string.h>
 
 typedef struct _pgmin{
     t_object       x_obj;
-    t_int          x_omni;
-    t_float        x_ch;
-    t_float        x_ch_in;
-    t_int          x_ext;
-    unsigned char  x_pgm;
-    unsigned char  x_channel;
+    t_int          x_ext, x_pgm, x_channel;
+    t_float        x_ch_in; // float input channel
     t_outlet      *x_chanout;
 }t_pgmin;
 
 static t_class *pgmin_class;
 
 static void pgmin_float(t_pgmin *x, t_float f){
-    if(f < 0 || f > 256){
+    if(f < 0 || f > 256){ // out of range
         x->x_pgm = 0;
         return;
     }
-    else{
-        t_int ch = (t_int)x->x_ch_in;
-        if(ch != x->x_ch && ch >= 0 && ch <= 16)
-            x->x_omni = ((x->x_ch = (t_int)ch) == 0);
-        unsigned char val = (int)f;
+    else{ // in range
+        unsigned char val = (unsigned char)f;
         if(val & 0x80){ // message type > 128)
-            if((x->x_pgm = ((val & 0xF0) == 0xC0))) // if pgm change
+            if((x->x_pgm = ((val & 0xF0) == 0xC0))) // is pgm change
                 x->x_channel = (val & 0x0F) + 1; // get channel
         }
         else if(x->x_pgm && val < 128){ // output value
-            if(x->x_omni){
+            if(x->x_ch_in <= 0 || x->x_ch_in > 16){ // omni
                 outlet_float(x->x_chanout, x->x_channel);
                 outlet_float(((t_object *)x)->ob_outlet, val);
             }
-            else if(x->x_ch == x->x_channel)
+            else if(x->x_channel == (t_int)x->x_ch_in)
                 outlet_float(((t_object *)x)->ob_outlet, val);
             x->x_pgm = 0;
         }
@@ -56,28 +48,26 @@ static void pgmin_ext(t_pgmin *x, t_floatarg f){
     x->x_ext = f != 0;
 }
 
-static void pgmin_free(t_pgmin *x){
+void *pgmin_free(t_pgmin *x){
+    outlet_free(x->x_chanout);
     pd_unbind(&x->x_obj.ob_pd, gensym("#midiin"));
+    return(void *)x;
 }
 
 static void *pgmin_new(t_symbol *s, int ac, t_atom *av){
     s = NULL;
     t_pgmin *x = (t_pgmin *)pd_new(pgmin_class);
     x->x_pgm = 0;
-    int channel = 0;
-    if(ac){
-        if(atom_getsymbolarg(0, ac, av) == gensym("-ext")){
+    x->x_ch_in = 0;
+    x->x_ext = 0;
+    if(ac > 0){
+        if(av->a_type == A_SYMBOL && (atom_getsymbol(av) == gensym("-ext"))){
             x->x_ext = 1;
             ac--, av++;
         }
-        channel = (t_int)atom_getfloatarg(0, ac, av);
+        if(ac)
+            x->x_ch_in = atom_getint(av);
     }
-    if(channel < 0)
-        channel = 0;
-    if(channel > 16)
-        channel = 16;
-    x->x_omni = (channel == 0);
-    x->x_ch = x->x_ch_in = channel;
     floatinlet_new((t_object *)x, &x->x_ch_in);
     outlet_new((t_object *)x, &s_float);
     x->x_chanout = outlet_new((t_object *)x, &s_float);
