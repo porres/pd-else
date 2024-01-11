56b8f5281ddfd796dd4ad2269629fe70067c46a7
porres <porres@gmail.com>
2024-01-10 22:04:18 -0300

    spread mc

--- Code_source/Compiled/audio/spread.mc~.c
+++ Code_source/Compiled/audio/spread.mc~.c
@@ -1,112 +1,106 @@
-// porres 2023
+// porres 2024
 
 #include "m_pd.h"
 #include "buffer.h"
-#include <stdlib.h>
+
+#define MAX_INOUT 4096
 
 static t_class *spreadmc_class;
 
 typedef struct _spreadmc{
     t_object    x_obj;
-    int         x_nchs;
-    int         x_block;
-    int         x_outchs;
-    t_float    *x_input;
-    t_float    *x_amp1;
-    t_float    *x_amp2;
-    t_int      *x_idx;
+    int         x_n, x_n_ins, x_n_outs;
+    t_float    *x_input;                 // inputs copy
+    t_float     x_ratio;
+    t_inlet    *x_inlet_spread;
 }t_spreadmc;
 
 static void spreadmc_n(t_spreadmc *x, t_floatarg f){
-    int n = f < 2 ? 2 : f > 512 ? 512 : (int)f;
-    if(x->x_outchs != n){
-        x->x_outchs = n;
-        float chratio = (float)(x->x_outchs - 1)/(float)(x->x_nchs - 1);
-        for(int i = 1; i < x->x_nchs - 1; i++){
-            float frac = i * chratio;
-            x->x_idx[i] = floor(frac);
-            double pos = (double)(frac - x->x_idx[i]) * 0.25;
-            x->x_amp1[i] = read_sintab(pos + 0.25);
-            x->x_amp2[i] = read_sintab(pos);
-        }
+    int n = f < 2 ? 2 : f > MAX_INOUT ? MAX_INOUT : (int)f;
+    if(x->x_n_outs != n){
+        x->x_n_outs = n;
+        x->x_ratio = (float)(x->x_n_outs - 1)/(float)(x->x_n_ins - 1);
         canvas_update_dsp();
     }
 }
 
 t_int *spreadmc_perform(t_int *w){
-    t_spreadmc *x = (t_spreadmc*)w[1];
+    t_spreadmc *x = (t_spreadmc*) w[1];
     t_float *input = (t_float *)(w[2]);
-    t_float *out = (t_float *)(w[3]);
+    t_float *spreadin = (t_float *)(w[3]);
+    t_float *out = (t_float *)(w[4]);
     t_float *in = x->x_input;
-    int chs = x->x_nchs, n = x->x_block, outchs = x->x_outchs;
-    for(int i = 0; i < n*chs; i++) // copy input
-        in[i] = input[i];
-    for(int i = 0; i < n; i++){
-        for(int j = 1; j < outchs - 1; j++) // zero output channels
-            out[j*n + i] = 0;
-        out[i] = in[i]; // copy leftmost channel
-        out[(outchs-1)*n + i] = in[(chs-1)*n + i]; // copy rightmost channel
-        for(int j = 1; j < chs - 1; j++){ // spread the rest
-            int idx = x->x_idx[j], ch1 = idx * n, ch2 = (idx+1) * n;
-            out[ch1+i] += (x->x_amp1[j] * in[j*n + i]);
-            out[ch2+i] += (x->x_amp2[j] * in[j*n + i]);;
+	int n, ch, i, o, n_ins = x->x_n_ins, n_outs = x->x_n_outs;
+	for(n = 0; n < x->x_n; n++){ // block loop
+        t_float spread = spreadin[n];
+        if(spread < 0.1)
+            spread = 0.1;
+        float spread2 = spread*2;
+        float range = n_outs / spread2;
+        for(ch = 0; ch < n_ins; ch++) // copy input sample frames
+            in[ch] = input[ch*x->x_n+n];
+        for(ch = 0; ch < n_outs; ch++) // zero outs
+            out[ch*x->x_n+n] = 0.0;
+        for(i = 0; i < n_ins; i++){ // in loop
+            float pos = i * x->x_ratio + spread;
+            for(o = 0; o < n_outs; o++){ // out loop
+                float chanpos = (pos - o) / spread2;
+                chanpos = chanpos - range * floor(chanpos/range);
+                float g = chanpos >= 1 ? 0 : read_sintab(chanpos*0.5);
+                out[o*x->x_n+n] += in[i] * g;
+            }
         }
 	}
-    return(w+4);
+    return(w+5);
 }
 
 void spreadmc_dsp(t_spreadmc *x, t_signal **sp){
-    int n = sp[0]->s_n, chs = sp[0]->s_nchans;
-    signal_setmultiout(&sp[1], x->x_outchs);
-    if(x->x_block != n || x->x_nchs != chs){
-        if(x->x_nchs != chs){
-            x->x_idx = (t_int *)resizebytes(x->x_idx,
-                x->x_nchs * sizeof(t_int), chs * sizeof(t_int));
-            x->x_amp1 = (t_float *)resizebytes(x->x_amp1,
-                x->x_nchs * sizeof(t_float), chs * sizeof(t_float));
-            x->x_amp2 = (t_float *)resizebytes(x->x_amp2,
-                x->x_nchs * sizeof(t_float), chs * sizeof(t_float));
-            float chratio = (float)(x->x_outchs - 1)/(float)(chs - 1);
-            for(int i = 1; i < chs - 1; i++){
-                float frac = i * chratio;
-                x->x_idx[i] = floor(frac);
-                double pos = (double)(frac - x->x_idx[i]) * 0.25;
-                x->x_amp1[i] = read_sintab(pos + 0.25);
-                x->x_amp2[i] = read_sintab(pos);
-            }
-        }
+    x->x_n = sp[0]->s_n;
+    signal_setmultiout(&sp[2], x->x_n_outs);
+    int ins = sp[0]->s_nchans;
+    if(x->x_n_ins != ins){ // check if only one and add zero, check right inlet too
         x->x_input = (t_float *)resizebytes(x->x_input,
-            x->x_block*x->x_nchs * sizeof(t_float), n*chs * sizeof(t_float));
-        x->x_block = n, x->x_nchs = chs;
+            x->x_n_ins * sizeof(t_float), ins * sizeof(t_float));
+        x->x_n_ins = ins;
+        x->x_ratio = (float)(x->x_n_outs - 1)/(float)(x->x_n_ins - 1);
     }
-    dsp_add(spreadmc_perform, 3, x, sp[0]->s_vec, sp[1]->s_vec);
+    dsp_add(spreadmc_perform, 4, x, sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec);
 }
 
 void spreadmc_free(t_spreadmc *x){
-    freebytes(x->x_input, x->x_block*x->x_nchs * sizeof(*x->x_input));
-    freebytes(x->x_amp1, x->x_nchs * sizeof(*x->x_amp1));
-    freebytes(x->x_amp2, x->x_nchs * sizeof(*x->x_amp2));
-    freebytes(x->x_idx, x->x_nchs * sizeof(*x->x_idx));
+    freebytes(x->x_input, x->x_n_ins * sizeof(*x->x_input));
+    inlet_free(x->x_inlet_spread);
 }
 
-void *spreadmc_new(t_floatarg f){
+void *spreadmc_new(t_symbol *s, int ac, t_atom *av){
+    s = NULL;
     t_spreadmc *x = (t_spreadmc *)pd_new(spreadmc_class);
     init_sine_table();
-    x->x_outchs = f < 2 ? 2 : f > 512 ? 512 : (int)f;
-    x->x_nchs = 1;
-    x->x_block = sys_getblksize();
-    x->x_input = (t_float *)getbytes(x->x_block*sizeof(*x->x_input));
-    x->x_amp1 = (t_float *)getbytes(sizeof(*x->x_amp1));
-    x->x_amp2 = (t_float *)getbytes(sizeof(*x->x_amp2));
-    x->x_idx = (t_int *)getbytes(sizeof(*x->x_idx));
+    int outs = 2;
+    float spread = 1;
+    x->x_n = sys_getblksize();
+    if(ac){
+        outs = atom_getint(av);
+        ac--, av++;
+    }
+    if(ac){
+        spread = atom_getfloat(av);
+        ac--, av++;
+    }
+    x->x_n_ins = 2;
+    x->x_n_outs = outs < 2 ? 2 : outs > MAX_INOUT ? MAX_INOUT : outs;
+    x->x_ratio = (t_float)(x->x_n_outs - 1)/(t_float)(x->x_n_ins - 1);
+    x->x_input = getbytes(x->x_n_ins * sizeof(*x->x_input));
+    x->x_inlet_spread = inlet_new(&x->x_obj, &x->x_obj.ob_pd,  &s_signal, &s_signal);
+        pd_float((t_pd *)x->x_inlet_spread, spread);
     outlet_new(&x->x_obj, gensym("signal"));
     return(x);
 }
 
 void setup_spread0x2emc_tilde(void){
     spreadmc_class = class_new(gensym("spread.mc~"), (t_newmethod)spreadmc_new,
-        (t_method)spreadmc_free, sizeof(t_spreadmc), CLASS_MULTICHANNEL, A_DEFFLOAT, 0);
+        (t_method)spreadmc_free, sizeof(t_spreadmc), CLASS_MULTICHANNEL, A_GIMME, 0);
     class_addmethod(spreadmc_class, nullfn, gensym("signal"), 0);
-    class_addmethod(spreadmc_class, (t_method)spreadmc_dsp, gensym("dsp"), A_CANT, 0);
+    class_addmethod(spreadmc_class, (t_method)spreadmc_dsp, gensym("dsp"),0);
     class_addmethod(spreadmc_class, (t_method)spreadmc_n, gensym("n"), A_DEFFLOAT, 0);
 }
