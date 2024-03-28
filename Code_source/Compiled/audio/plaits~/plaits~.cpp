23ae0c503921fcd8a6b9573329168b9f83b58abe
porres <porres@gmail.com>
2024-03-28 14:33:20 -0300

    Update plaits~.cpp

--- Code_source/Compiled/audio/plaits~/plaits~.cpp
+++ Code_source/Compiled/audio/plaits~/plaits~.cpp
@@ -399,11 +399,11 @@ t_int *plaits_perform_midi(t_int *w){
 void plaits_dsp(t_plaits *x, t_signal **sp){
     x->x_pitch_correction = log2f(48000.f / sys_getsr());
     x->x_n = sp[0]->s_n;
-    post("midi mode = %d", x->x_midi_mode);
+/*    post("midi mode = %d", x->x_midi_mode);
     if(x->x_midi_mode)
         dsp_add(plaits_perform_midi, 7, x, sp[3]->s_vec, sp[4]->s_vec, sp[5]->s_vec,
             sp[6]->s_vec, sp[7]->s_vec, sp[8]->s_vec);
-    else
+    else*/
         dsp_add(plaits_perform, 10, x, sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec,
             sp[4]->s_vec, sp[5]->s_vec, sp[6]->s_vec, sp[7]->s_vec, sp[8]->s_vec);
 }
