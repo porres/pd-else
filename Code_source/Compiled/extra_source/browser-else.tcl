ced30d775c213d33db4b34018bab2fe359c79e24
porres <porres@gmail.com>
2024-07-30 17:22:46 -0300

    sfload help

--- Code_source/Compiled/extra_source/browser-else.tcl
+++ Code_source/Compiled/extra_source/browser-else.tcl
@@ -73,7 +73,7 @@ proc category_else_menu::load_menutree {} {
             {fx\ filters
                 {allpass.2nd~ allpass.filt~ bitnormal~ comb.filt~ lop.bw~ hip.bw~ biquads~ bandpass~ bandstop~ crossover~ bpbank~ bicoeff2 brickwall~ eq~ highpass~ highshelf~ lop2~ lowpass~ lowshelf~ mov.avg~ resonbank~ resonbank2~ vcf2~ resonant~ resonant2~ svfilter~}}
             {sampling\ playing\ granulation
-                {player~ gran.player~ stretch.shift~ pvoc.player~ pvoc.live~ batch.rec~ bach.write~ rec.file~ play.file~ tabplayer~ tabwriter~ sample~}}
+                {player~ gran.player~ stretch.shift~ pvoc.player~ pvoc.live~ batch.rec~ bach.write~ rec.file~ play.file~ tabplayer~ tabwriter~ sfload sample~}}
             {synthesis:\ synthesizers
                 {pm2~ pm4~ pm6~ sfont~ sfz~ plaits~ synth~}}
             {synthesis:\ granular
