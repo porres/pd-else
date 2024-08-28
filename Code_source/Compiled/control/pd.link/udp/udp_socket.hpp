04f97f7edb05ecc883eb8335cb621efc9afecafc
Timothy Schoen <timschoen123@gmail.com>
2024-08-28 16:23:18 +0200

    Remove unused headers

--- Code_source/Compiled/control/pd.link/udp/udp_socket.hpp
+++ Code_source/Compiled/control/pd.link/udp/udp_socket.hpp
@@ -1,10 +1,6 @@
-#include <locale.h>
 #include <wchar.h>
 #include <iostream>
-#include <thread>
-#include <chrono>
 #include <array>
-#include <signal.h>
 
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
