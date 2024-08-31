3262ec9b972ea96645e072f339cd66cbf36b182c
porres <porres@gmail.com>
2024-08-31 05:37:10 -0300

    readme/browser

--- Source/Audio/conv~.c
+++ Source/Audio/conv~.c
@@ -6,7 +6,7 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 #include "m_pd.h"
-#include "kiss_fft.h"
+#include <kiss_fft/kiss_fft.h>
 #include <math.h>
 
 #define MINSIZE 64
---
+++ Source/Shared/shared.cmake
@@ -0,0 +1,27 @@
+file(GLOB SOURCES
+    ${CMAKE_CURRENT_SOURCE_DIR}/Source/Shared/*.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/Source/Shared/libsamplerate/*.c
+)
+
+add_library(else_shared SHARED ${SOURCES})
+
+set_target_properties(else_shared PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${PD_OUTPUT_PATH})
+set_target_properties(else_shared PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${PD_OUTPUT_PATH})
+set_target_properties(else_shared PROPERTIES ARCHIVE_OUTPUT_DIRECTORY ${PD_OUTPUT_PATH})
+foreach(OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES})
+	    string(TOUPPER ${OUTPUTCONFIG} OUTPUTCONFIG)
+		set_target_properties(else_shared PROPERTIES LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${PD_OUTPUT_PATH})
+		set_target_properties(else_shared PROPERTIES RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${PD_OUTPUT_PATH})
+		set_target_properties(else_shared PROPERTIES ARCHIVE_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${PD_OUTPUT_PATH})
+endforeach(OUTPUTCONFIG CMAKE_CONFIGURATION_TYPES)
+
+if(APPLE)
+    target_link_options(else_shared PRIVATE -undefined dynamic_lookup)
+elseif(WIN32)
+    find_library(PD_LIBRARY NAMES pd HINTS ${PD_LIB_PATH})
+	target_link_libraries(else_shared PRIVATE ws2_32 ${PD_LIBRARY})
+endif()
+
+if(PD_FLOATSIZE64)
+    target_compile_definitions(else_shared PRIVATE PD_FLOATSIZE=64)
+endif()
