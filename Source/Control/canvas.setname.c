fa99e9de30fe1fc157a6bce83764cca5a3ddfcbf
porres <porres@gmail.com>
2024-12-18 10:27:50 -0300

    improve midi.learn with new canvas.setname features (add methods to set name, depth and mode)

--- Source/Control/canvas.setname.c
+++ Source/Control/canvas.setname.c
@@ -1,3 +1,5 @@
+// porres
+
 #include <m_pd.h>
 #include <g_canvas.h>
 
@@ -5,34 +7,75 @@ static t_class *canvas_setname_class;
 
 typedef struct _canvas_setname{
     t_object    x_obj;
-    t_symbol   *x_sym;
+    t_symbol   *x_bindsym;
+    t_canvas   *x_cv;
+    t_int       x_depth;
+    t_int       x_mode;
     t_pd       *x_owner;
-}t_canvas_setname;
+}t_setname;
 
-static void *canvas_setname_new(t_symbol *s, t_floatarg f1, t_floatarg f2){
-    t_canvas_setname *x = (t_canvas_setname *)pd_new(canvas_setname_class);
-    t_canvas *cv = canvas_getcurrent();
-    x->x_sym = s;
-    int depth = (int)f1 < 0 ? 0 : (int)f1;
+static void setname_name(t_setname *x, t_symbol *s){
+    if(x->x_bindsym != &s_)
+        pd_unbind(x->x_owner, x->x_bindsym);
+    x->x_bindsym = s;
+    if(x->x_bindsym != &s_)
+        pd_bind(x->x_owner, x->x_bindsym);
+}
+
+static void setname_setowner(t_setname *x){
+    t_canvas *cv = x->x_cv;
+    int depth = x->x_depth;
+    if(x->x_mode)
+        cv = canvas_getrootfor(cv->gl_owner);
     while(depth-- && cv->gl_owner){
-        if(f2 != 0)
+        if(x->x_mode)
             cv = canvas_getrootfor(cv->gl_owner);
         else
             cv = cv->gl_owner;
     }
     x->x_owner = (t_pd *)cv;
-    if(*s->s_name)
-        pd_bind(x->x_owner, s);
-    return(x);
 }
 
-static void canvas_setname_free(t_canvas_setname *x){
-    if(*x->x_sym->s_name)
-        pd_unbind(x->x_owner, x->x_sym);
+static void setname_reset(t_setname *x){
+    if(x->x_bindsym != &s_)
+        pd_unbind(x->x_owner, x->x_bindsym);
+    setname_setowner(x);
+    if(x->x_bindsym != &s_)
+        pd_bind(x->x_owner, x->x_bindsym);
+}
+
+static void setname_depth(t_setname *x, t_floatarg f){
+    x->x_depth = (int)f < 0 ? 0 : (int)f;
+    setname_reset(x);
+}
+
+static void setname_mode(t_setname *x, t_floatarg f){
+    x->x_mode = (int)(f != 0);
+    setname_reset(x);
+}
+
+static void canvas_setname_free(t_setname *x){
+    if(x->x_bindsym != &s_)
+        pd_unbind(x->x_owner, x->x_bindsym);
+}
+
+static void *canvas_setname_new(t_symbol *s, t_floatarg f1, t_floatarg f2){
+    t_setname *x = (t_setname *)pd_new(canvas_setname_class);
+    x->x_depth = (int)f1 < 0 ? 0 : (int)f1;
+    x->x_mode = (int)(f2 != 0);
+    x->x_cv = canvas_getcurrent();
+    setname_setowner(x);
+    x->x_bindsym = s;
+    if(x->x_bindsym != &s_)
+        pd_bind(x->x_owner, x->x_bindsym);
+    return(x);
 }
 
 void setup_canvas0x2esetname(void){
     canvas_setname_class = class_new(gensym("canvas.setname"),
         (t_newmethod)canvas_setname_new, (t_method)canvas_setname_free,
-            sizeof(t_canvas_setname), CLASS_NOINLET, A_DEFSYM, A_DEFFLOAT, A_DEFFLOAT, 0);
+            sizeof(t_setname), CLASS_DEFAULT, A_DEFSYM, A_DEFFLOAT, A_DEFFLOAT, 0);
+    class_addmethod(canvas_setname_class, (t_method)setname_depth, gensym("depth"), A_FLOAT, 0);
+    class_addmethod(canvas_setname_class, (t_method)setname_mode, gensym("mode"), A_FLOAT, 0);
+    class_addmethod(canvas_setname_class, (t_method)setname_name, gensym("name"), A_SYMBOL, 0);
 }
