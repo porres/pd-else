537df6ed7299e5f0dce6e9d2589a550fffcd3eb7
porres <porres@gmail.com>
2025-02-06 02:02:40 -0300

    dust~ add 'set'/'list' and '-mc'

--- Source/Audio/dust~.c
+++ Source/Audio/dust~.c
@@ -1,7 +1,11 @@
 // Porres 2017-2024
 
 #include <m_pd.h>
-#include <random.h>
+#include <stdlib.h>
+#include "random.h"
+#include "magic.h"
+
+#define MAXLEN 1024
 
 static t_class *dust_class;
 
@@ -9,11 +13,15 @@ typedef struct _dust{
     t_object       x_obj;
     t_float        x_sample_dur;
     t_random_state x_rstate;
-    t_float        x_density;
+    float         *x_density_list;
+    t_int          x_list_size;
+    t_int          x_sig1;
     int            x_id;
     int            x_nchans;
     int            x_ch;
     int            x_n;
+    t_glist    *x_glist; // object list
+    t_symbol      *x_ignore;
 }t_dust;
 
 static void dust_seed(t_dust *x, t_symbol *s, int ac, t_atom *av){
@@ -23,6 +31,32 @@ static void dust_seed(t_dust *x, t_symbol *s, int ac, t_atom *av){
 static void dust_ch(t_dust *x, t_floatarg f){
     x->x_ch = f < 1 ? 1 : (int)f;
     canvas_update_dsp();
+}
+
+static void dust_set(t_dust *x, t_symbol *s, int ac, t_atom *av){
+    x->x_ignore = s;
+    if(ac != 2)
+        return;
+    int i = atom_getint(av);
+    float f = atom_getint(av+1);
+    if(i >= x->x_list_size)
+        i = x->x_list_size;
+    if(i <= 0)
+        i = 1;
+    i--;
+    x->x_density_list[i] = f;
+}
+
+static void dust_list(t_dust *x, t_symbol *s, int ac, t_atom * av){
+    x->x_ignore = s;
+    if(ac == 0)
+        return;
+    if(x->x_list_size != ac){
+        x->x_list_size = ac;
+        canvas_update_dsp();
+    }
+    for(int i = 0; i < ac; i++)
+        x->x_density_list[i] = atom_getfloat(av+i);
 }
 
 static t_int *dust_perform(t_int *w){
@@ -35,7 +69,20 @@ static t_int *dust_perform(t_int *w){
     uint32_t *s3 = &x->x_rstate.s3;
     for(int i = 0; i < x->x_n; i++){
         for(int j = 0; j < x->x_nchans; j++){ // for 'n' out channels
-            t_float density = chs == 1 ? in[i] : in[j*x->x_n + i];
+//            t_float density = chs == 1 ? in[i] : in[j*x->x_n + i];
+            t_float density;
+            if(x->x_sig1){
+                if(chs == 1)
+                    density = in[i];
+                else
+                    density = in[j*x->x_n + i];
+            }
+            else{
+                if(x->x_list_size > 1)
+                    density = x->x_density_list[j];
+                else
+                    density = x->x_density_list[0];
+            }
             t_float thresh = density * x->x_sample_dur;
             t_float scale = thresh > 0 ? 1./thresh : 0;
             t_float random = (t_float)(random_frand(s1, s2, s3) * 0.5 + 0.5);
@@ -46,10 +93,16 @@ static t_int *dust_perform(t_int *w){
     return(w+5);
 }
 
+static void *dust_free(t_dust *x){
+    free(x->x_density_list);
+    return(void *)x;
+}
+
 static void dust_dsp(t_dust *x, t_signal **sp){
     x->x_sample_dur = 1./sp[0]->s_sr;
     x->x_n = sp[0]->s_n;
-    int chs = sp[0]->s_nchans;
+    x->x_sig1 = else_magic_inlet_connection((t_object *)x, x->x_glist, 0, &s_signal);
+    int chs = x->x_sig1 ? sp[0]->s_nchans : x->x_list_size;
     if(chs == 1)
         chs = x->x_ch;
     x->x_nchans = chs;
@@ -63,10 +116,13 @@ static void *dust_new(t_symbol *s, int ac, t_atom *av){
     x->x_nchans = 1;
     dust_seed(x, s, 0, NULL);
     x->x_ch = 1;
-    x->x_density = 0;
+    x->x_density_list = (float*)malloc(MAXLEN * sizeof(float));
+    x->x_density_list[0] = 0;
+    x->x_list_size = 1;
     if(ac){
         while(av->a_type == A_SYMBOL){
-            if(atom_getsymbol(av) == gensym("-seed")){
+            t_symbol *sym = atom_getsymbol(av);
+            if(sym == gensym("-seed")){
                 if(ac >= 2){
                     t_atom at[1];
                     SETFLOAT(at, atom_getfloat(av+1));
@@ -78,7 +134,7 @@ static void *dust_new(t_symbol *s, int ac, t_atom *av){
                     return(NULL);
                 }
             }
-            else if(atom_getsymbol(av) == gensym("-ch")){
+            else if(sym == gensym("-ch")){
                 if(ac >= 2){
                     int n = atom_getint(av+1);
                     dust_ch(x, n < 1 ? 1 : n);
@@ -89,14 +145,27 @@ static void *dust_new(t_symbol *s, int ac, t_atom *av){
                     return(NULL);
                 }
             }
+            else if(sym == gensym("-mc")){
+                ac--, av++;
+                if(!ac || av->a_type != A_FLOAT)
+                    goto errstate;
+                int n = 0;
+                while(ac && av->a_type == A_FLOAT){
+                    x->x_density_list[n] = atom_getfloat(av);
+                    ac--, av++, n++;
+                }
+                x->x_list_size = n;
+            }
             else{
-                pd_error(x, "[dust~]: improper flag (%s)", atom_getsymbol(av)->s_name);
+            errstate:
+                pd_error(x, "[dust~]: improper args");
                 return(NULL);
             }
         }
     }
     if(ac)
-        x->x_density = atom_getfloat(av);
+        x->x_density_list[0] = atom_getfloat(av);
+    x->x_glist = canvas_getcurrent();
     outlet_new(&x->x_obj, &s_signal);
     return(x);
 }
@@ -104,8 +173,10 @@ static void *dust_new(t_symbol *s, int ac, t_atom *av){
 void dust_tilde_setup(void){
     dust_class = class_new(gensym("dust~"), (t_newmethod)dust_new,
         0, sizeof(t_dust), CLASS_MULTICHANNEL, A_GIMME, 0);
-    CLASS_MAINSIGNALIN(dust_class, t_dust, x_density);
+    class_addmethod(dust_class, nullfn, gensym("signal"), 0);
     class_addmethod(dust_class, (t_method)dust_dsp, gensym("dsp"), A_CANT, 0);
+    class_addlist(dust_class, dust_list);
+    class_addmethod(dust_class, (t_method)dust_set, gensym("set"), A_GIMME, 0);
     class_addmethod(dust_class, (t_method)dust_seed, gensym("seed"), A_GIMME, 0);
     class_addmethod(dust_class, (t_method)dust_ch, gensym("ch"), A_DEFFLOAT, 0);
 }
