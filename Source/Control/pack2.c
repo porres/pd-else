1c273a106daa46a3e11249b2e8a92af8cbb83a33
porres <porres@gmail.com>
2025-02-13 17:27:22 -0300

    changes to pack2

--- Source/Control/pack2.c
+++ Source/Control/pack2.c
@@ -12,127 +12,104 @@ typedef struct _pack2{
     t_int                x_n;
     t_atom*              x_vec;
     t_atom*              x_out;
-    struct _pack2_inlet* x_ins;
+    struct _pack2_inlet *x_ins;
+    t_symbol            *x_ignore;
 }t_pack2;
 
 typedef struct _pack2_inlet{
-    t_class* x_pd;
-    t_atom*  x_atoms;
-    t_int    x_max;
-    t_pack2* x_owner;
-    int      x_idx; //index of inlet
+    t_class*  x_pd;
+    t_atom*   x_atoms;
+    t_int     x_max;
+    t_pack2*  x_owner;
+    int       x_idx; //index of inlet
+    t_symbol *x_ignore;
 }t_pack2_inlet;
 
-static void pack2_bang(t_pack2 *x){
+static void pack2_out(t_pack2 *x){
     for(int i = 0; i < x->x_n; ++i)
         x->x_out[i] = x->x_vec[i];
-    if(x->x_out[0].a_type == A_FLOAT)
-        outlet_anything(x->x_obj.ob_outlet, &s_list, x->x_n, x->x_out);
-    else{
-        int n = x->x_n - 1;
-        outlet_anything(x->x_obj.ob_outlet, atom_getsymbol(x->x_out), n, x->x_out + 1);
-    }
+    outlet_anything(x->x_obj.ob_outlet, &s_list, x->x_n, x->x_out);
 }
 
-static void pack2_copy(t_pack2 *x, int ndest, t_atom* dest, int nsrc, t_atom* src, int srcidx){
-    x = NULL;
-    srcidx = 0; // avoid warning
-    int i;
-    for(i = 0; i < ndest && i < nsrc; ++i){
+static void pack2_copy(int ndest, t_atom* dest, int nsrc, t_atom* src){
+    for(int i = 0; i < ndest && i < nsrc; ++i){
         if(src[i].a_type == A_FLOAT){
-            if(dest[i].a_type == A_FLOAT)
-                dest[i].a_w.w_float = src[i].a_w.w_float;
-            else if(dest[i].a_type == A_SYMBOL){
+            if(dest[i].a_type == A_SYMBOL)
                 dest[i].a_type = A_FLOAT;
-                dest[i].a_w.w_float = src[i].a_w.w_float; // symbol becomes float
-            }
+            dest[i].a_w.w_float = src[i].a_w.w_float;
         }
         else if(src[i].a_type == A_SYMBOL){
-            if(dest[i].a_type == A_SYMBOL)
-                dest[i].a_w.w_symbol = src[i].a_w.w_symbol;
-            else if(dest[i].a_type == A_FLOAT){
+            if(dest[i].a_type == A_FLOAT)
                 dest[i].a_type = A_SYMBOL;
-                dest[i].a_w.w_symbol = src[i].a_w.w_symbol; // float becomes symbol
-            }
+            dest[i].a_w.w_symbol = src[i].a_w.w_symbol; 
         }
     }
 }
 
 static void pack2_inlet_float(t_pack2_inlet *x, float f){
-    if(x->x_atoms->a_type == A_FLOAT){
-        x->x_atoms->a_w.w_float = f;
-        pack2_bang(x->x_owner);
-    }
-    else if(x->x_atoms->a_type == A_SYMBOL){
-        x->x_atoms->a_type      = A_FLOAT; // symbol becomes float
-        x->x_atoms->a_w.w_float = f;
-        pack2_bang(x->x_owner);
-    }
+    if(x->x_atoms->a_type == A_SYMBOL)
+        x->x_atoms->a_type = A_FLOAT;
+    x->x_atoms->a_w.w_float = f;
+    pack2_out(x->x_owner);
 }
 
 static void pack2_inlet_symbol(t_pack2_inlet *x, t_symbol* s){
-    if(x->x_atoms->a_type == A_SYMBOL){
-        x->x_atoms->a_w.w_symbol = s;
-        pack2_bang(x->x_owner);
-    }
-    else if(x->x_atoms->a_type == A_FLOAT){
-        x->x_atoms->a_type = A_SYMBOL; // float becomes symbol
-        x->x_atoms->a_w.w_symbol = s;
-        pack2_bang(x->x_owner);
-    }
+    if(x->x_atoms->a_type == A_FLOAT)
+        x->x_atoms->a_type = A_SYMBOL;
+    x->x_atoms->a_w.w_symbol = s;
+    pack2_out(x->x_owner);
 }
 
-static void pack2_inlet_list(t_pack2_inlet *x, t_symbol* s, int argc, t_atom* argv){
-    s = NULL;
-    if(!argc)
-        pack2_bang(x->x_owner);
-    else if(argc == 1){
-        if(argv->a_type == A_FLOAT)
-            pack2_inlet_float(x, atom_getfloat(argv));
-        else if(argv->a_type == A_SYMBOL)
-            pack2_inlet_symbol(x, atom_getsymbol(argv));
+static void pack2_inlet_list(t_pack2_inlet *x, t_symbol* s, int ac, t_atom* av){
+    x->x_ignore = s;
+    if(!ac && x->x_idx != 0){
+        pd_error(x, "pack2: secondary inlet doesn't expect bang");
+        return;
+    }
+    else if(x->x_idx != 0){
+        pd_error(x, "pack2: secondary inlet doesn't expect list");
+        return;
+    }
+    else if(!ac){
+        pack2_out(x->x_owner);
+        return;
+    }
+    if(ac == 1){
+        if(av->a_type == A_FLOAT)
+            pack2_inlet_float(x, atom_getfloat(av));
+        else if(av->a_type == A_SYMBOL)
+            pack2_inlet_symbol(x, atom_getsymbol(av));
     }
     else{
-        pack2_copy(x->x_owner, x->x_max, x->x_atoms, argc, argv, x->x_idx);
-        pack2_bang(x->x_owner);
+        pack2_copy(x->x_max, x->x_atoms, ac, av);
+        pack2_out(x->x_owner);
     }
 }
 
-static void pack2_inlet_anything(t_pack2_inlet *x, t_symbol* s, int argc, t_atom* argv){
-    if(x->x_atoms[0].a_type == A_SYMBOL)
-        x->x_atoms[0].a_w.w_symbol = s;
-    else if (x->x_atoms[0].a_type == A_FLOAT){
-        x->x_atoms[0].a_type = A_SYMBOL;
-        x->x_atoms[0].a_w.w_symbol = s; // float becomes symbol
-    }
-    pack2_copy(x->x_owner, x->x_max-1, x->x_atoms+1, argc, argv, x->x_idx);
-    pack2_bang(x->x_owner);
+static void pack2_inlet_set(t_pack2_inlet *x, t_symbol* s, int ac, t_atom* av){
+    x->x_ignore = s;
+    pack2_copy(x->x_max, x->x_atoms, ac = 1, av);
 }
 
-static void pack2_inlet_set(t_pack2_inlet *x, t_symbol* s, int argc, t_atom* argv){
-    s = NULL;
-    pack2_copy(x->x_owner, x->x_max, x->x_atoms, argc, argv, x->x_idx);
-}
-
-static void *pack2_new(t_symbol *s, int argc, t_atom *argv){
-    s = NULL;
-    int i;
+static void *pack2_new(t_symbol *s, int ac, t_atom *av){
     t_pack2 *x = (t_pack2 *)pd_new(pack2_class);
+    x->x_ignore = s;
+    int i;
     t_atom defarg[2];
-    if(!argc){
-        argv = defarg;
-        argc = 2;
+    if(!ac){
+        av = defarg;
+        ac = 2;
         SETFLOAT(&defarg[0], 0);
         SETFLOAT(&defarg[1], 0);
     }
-    x->x_n = argc;
-    x->x_vec = (t_atom *)getbytes(argc * sizeof(*x->x_vec));
-    x->x_out = (t_atom *)getbytes(argc * sizeof(*x->x_out));
-    x->x_ins = (t_pack2_inlet *)getbytes(argc * sizeof(*x->x_ins));
+    x->x_n = ac;
+    x->x_vec = (t_atom *)getbytes(ac * sizeof(*x->x_vec));
+    x->x_out = (t_atom *)getbytes(ac * sizeof(*x->x_out));
+    x->x_ins = (t_pack2_inlet *)getbytes(ac * sizeof(*x->x_ins));
     for(i = 0; i < x->x_n; ++i){
-        if(argv[i].a_type == A_FLOAT){
+        if(av[i].a_type == A_FLOAT){
             x->x_vec[i].a_type      = A_FLOAT;
-            x->x_vec[i].a_w.w_float = argv[i].a_w.w_float;
+            x->x_vec[i].a_w.w_float = av[i].a_w.w_float;
             x->x_ins[i].x_pd = pack2_inlet_class;
             x->x_ins[i].x_atoms = x->x_vec+i;
             x->x_ins[i].x_max = x->x_n-i;
@@ -140,9 +117,9 @@ static void *pack2_new(t_symbol *s, int argc, t_atom *argv){
             x->x_ins[i].x_idx = i;
             inlet_new((t_object *)x, &(x->x_ins[i].x_pd), 0, 0);
         }
-        else if(argv[i].a_type == A_SYMBOL){
-            if(argv[i].a_w.w_symbol == gensym("f") ||
-               argv[i].a_w.w_symbol == gensym("float")){ 
+        else if(av[i].a_type == A_SYMBOL){
+            if(av[i].a_w.w_symbol == gensym("f") ||
+               av[i].a_w.w_symbol == gensym("float")){ 
                 x->x_vec[i].a_type = A_FLOAT;
                 x->x_vec[i].a_w.w_float = 0.f;
                 x->x_ins[i].x_pd = pack2_inlet_class;
@@ -154,7 +131,7 @@ static void *pack2_new(t_symbol *s, int argc, t_atom *argv){
             }
             else{
                 x->x_vec[i].a_type = A_SYMBOL;
-                x->x_vec[i].a_w.w_symbol = argv[i].a_w.w_symbol; // loads symbol from arg
+                x->x_vec[i].a_w.w_symbol = av[i].a_w.w_symbol;
                 x->x_ins[i].x_pd = pack2_inlet_class;
                 x->x_ins[i].x_atoms = x->x_vec+i;
                 x->x_ins[i].x_max = x->x_n-i;
@@ -179,7 +156,8 @@ extern void pack2_setup(void){
     c = class_new(gensym("pack2-inlet"), 0, 0, sizeof(t_pack2_inlet), CLASS_PD, 0);
     if(c){
         class_addlist(c, (t_method)pack2_inlet_list);
-        class_addanything(c, (t_method)pack2_inlet_anything);
+        class_addfloat(c, (t_method)pack2_inlet_float);
+        class_addsymbol(c, (t_method)pack2_inlet_symbol);
         class_addmethod(c, (t_method)pack2_inlet_set, gensym("set"), A_GIMME, 0);
     }
     pack2_inlet_class = c;
