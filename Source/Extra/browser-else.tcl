0a707e4bb606c5cf97a1ea2ad42130b84f46e659
porres <porres@gmail.com>
2025-03-15 01:31:22 -0300

    doc updates with new objects and release date

--- Source/Extra/browser-else.tcl
+++ Source/Extra/browser-else.tcl
@@ -11,7 +11,7 @@ proc menu_send_else_obj {w x y item} {
         pdsend "$w obj $x $y $item"
     } else {
         pdsend "$w obj $x $y else/$item"
-        set abslist {abs.pd~ allpass.filt~  batch.rec~ batch.write~ bin.shift~ bl.osc~ bl.wavetable~ blip~ bpbank~ brickwall~ chorus~ compress~ crusher~ damp.osc~ dispatch drunkard~ duck~ echo.rev~ envelope~ expand~ flanger~ float2sig~ free.rev~ freeze~ gain~ gain2~ gatehold~ grain.live~ grain.sampler~ grain.synth~ gran.player~ graph~ hann~ hip.bw~ lop.bw~ mag~ maxpeak~ meter~ meter2~ meter4~ meter8~ metronome~ mix2~ mix4~ mono.rev~ morph~ ms2samps~ noisegate~ norm~ oscbank~ oscnoise~ out.mc~ out~ out4~ out8~ perlin~ phaser~ ping.pong~ pitch.shift~ plate.rev~ play.file~ player~ polymetro~ pvoc.freeze~ pvoc.live~ pvoc.player~ rec.file~ resonbank~ resonbank2~ revdelay~ rm~ sample~ samps2ms~ setdsp~ spectrograph~ stereo.rev~ stretch.shift~ synth~ tremolo~ trig2bang~ vibrato~ vocoder~ voices~ above add amean any2symbol autotune autotune2 avg bangdiv biplot bpm brown car2pol cents2frac cents2scale chrono circle clock coeff2pz combine count db2lin dec2frac dec2hex deg2rad delete display drum.seq drunkard e eqdiv equal euclid frac.add frac.mul frac2cents frac2dec freq2midi glide glide2 gmean group hex2dec histogram impulse insert interpolate iterate keymap keypress lastvalue lcm lfnoise lfo lin2db list.inc list.seq mag makenote2 markov median meter midi.clock midi.in midi.learn midi.out midi2freq mono mono~ morph mov.avg ms2samps mtx.ctl multi.vsl nmess notedur2ratio note2midi op osc.receive osc.send pattern phasor pi pick pimp midi2note pol2car polymetro presets pulse pz2coeff rad2deg rampnoise rand.dist rand.list randpulse randpulse2 range.hsl range rec2 remove replace retune reverse rotate samps2ms scala scale2cents scale2freq scales schmitt score score2 scramble sendmidi sequencer slew slew2 slider2d speed stack status stepnoise store stream sum swap2 sysrt.in sysrt.out tabgen tap tempo timed.gate trig2bang unite zbiplot}
+        set abslist {abs.pd~ allpass.filt~ bang2imp~ batch.rec~ batch.write~ bin.shift~ bl.osc~ bl.wavetable~ blip~ bpbank~ brickwall~ chorus~ compress~ crusher~ dir damp.osc~ dispatch drunkard~ duck~ echo.rev~ envelope~ expand~ flanger~ float2sig~ free.rev~ freeze~ gain~ gain2~ level~ gatehold gatehold~ gaterelease gaterelease~ grain.live~ grain.sampler~ grain.synth~ gran.player~ graph~ hann~ hip.bw~ lop.bw~ mag~ maxpeak~ meter~ meter2~ meter4~ meter8~ metronome~ mix2~ mix4~ mono.rev~ morph~ ms2samps~ noisegate~ norm~ oscbank~ oscnoise~ out.mc~ out~ out4~ out8~ perlin~ phaser~ ping.pong~ pitch.shift~ plate.rev~ play.file~ player~ polymetro~ pvoc.freeze~ pvoc.live~ pvoc.player~ rec.file~ resonbank~ resonbank2~ revdelay~ rm~ sample~ samps2ms~ setdsp~ spectrograph~ stereo.rev~ stretch.shift~ synth~ tremolo~ trig2bang~ vibrato~ vocoder~ voices~ above add amean any2symbol autotune autotune2 avg bangdiv biplot bpm brown car2pol cents2frac cents2scale chrono circle clock coeff2pz combine count db2lin dec2frac dec2hex deg2rad delete display drum.seq drunkard e eqdiv equal euclid frac.add frac.mul frac2cents frac2dec freq2midi glide glide2 gmean group hex2dec histogram impulse insert interpolate iterate keymap keypress lastvalue lcm lfnoise lfo lin2db list.inc list.seq mag makenote2 markov median meter midi.clock midi.in midi.learn midi.out midi2freq mono mono~ morph mov.avg ms2samps mtx.ctl multi.vsl nmess notedur2ratio note2midi op osc.receive osc.send pattern phasor pi pick pimp midi2note pol2car polymetro presets pulse pz2coeff rad2deg rampnoise rand.dist rand.list randpulse randpulse2 range.hsl range rec2 remove replace retune reverse rotate samps2ms scala scale2cents scale2freq scales schmitt score score2 scramble sendmidi sequencer slew slew2 slider2d speed stack status stepnoise store stream sum swap2 sysrt.in sysrt.out tabgen tap tempo timed.gate trig2bang unite zbiplot}
         foreach abstraction $abslist {
             if {$item eq $abstraction} {  
                 pdsend "pd-$item.pd loadbang"
@@ -29,7 +29,7 @@ proc category_else_menu::load_menutree {} {
             {assorted
                 {else}}
             {gui
-                {knob numbox~ drum.seq bicoeff pad messbox mtx.ctl biplot zbiplot pic colors function circle slider2d display out.mc~ out~ out4~ out8~ gain~ gain2~ button keyboard graph~ range.hsl multi.vsl spectrograph~ meter~ meter2~ meter4~ meter8~ note mix2~ mix4~ setdsp~ openfile scope~}}
+                {knob numbox~ drum.seq bicoeff pad messbox mtx.ctl biplot zbiplot pic colors function circle slider2d display out.mc~ out~ out4~ out8~ gain~ gain2~ level~ button popmenu keyboard graph~ range.hsl multi.vsl spectrograph~ meter~ meter2~ meter4~ meter8~ note mix2~ mix4~ setdsp~ openfile scope~ scope3d~}}
             {time
                 {chrono datetime}}
             {fft
@@ -43,7 +43,7 @@ proc category_else_menu::load_menutree {} {
             {message\ management
                 {format swap2 nmess unite separate symbol2any any2symbol changed hot initmess message default pack2 pick limit spread router route2 routeall routetype selector stack store morph interpolate sig2float~ float2sig~ pipe2}}
             {list\ management
-                {break order combine delete remove equal group iterate insert scramble sort reverse rotate replace sum slice stream merge unmerge amean gmean list.inc}}
+                {break order combine delete remove equal group iterate insert scramble sort reverse rotate replace sum slice stream merge unmerge amean gmean list.inc lace delace}}
             {file\ management
                 {dir}}
             {midi
@@ -51,7 +51,7 @@ proc category_else_menu::load_menutree {} {
             {osc/network
                 {osc.route osc.format osc.parse osc.send osc.receive pdlink pdlink~}}
             {math\ functions
-                {add add~ median avg mov.avg count gcd lcm frac.add frac.mul ceil ceil~ factor floor floor~ trun trunc~ rint rint~ quantizer quantizer~ fold fold~ lastvalue mag mag~ sin~ wrap2 wrap2~ op op~}}
+                {add add~ median avg mov.avg count gcd lcm frac.add frac.mul ceil ceil~ factor floor floor~ trun trunc~ rint rint~ quantizer quantizer~ fold fold~ lastvalue mag mag~ sin~ tanh~ wrap2 wrap2~ op op~}}
             {math\ conversion
                 {hex2dec dec2hex bpm car2pol car2pol~ cents2ratio cents2ratio~ ms2samps ms2samps~ db2lin db2lin~ float2bits hz2rad lin2db lin2db~ deg2rad rad2deg pz2coeff coeff2pz rad2hz ratio2cents ratio2cents~ samps2ms samps2ms~ pol2car pol2car~ rescale rescale~}}
             {math\ constant\ values
@@ -59,7 +59,7 @@ proc category_else_menu::load_menutree {} {
             {logic
                 {loop}}
             {audio\ multichannel\ tools
-                {nchs~ sigs~ repeat~ select~ pick~ get~ sum~ merge~ unmerge~ slice~}}       
+                {nchs~ sigs~ repeat~ select~ pick~ get~ sum~ merge~ unmerge~ slice~ lace~ delace~}}       
             {analog\ circuitry\ emulation
                 {circuit~}}
             {fx\ assorted
@@ -73,7 +73,7 @@ proc category_else_menu::load_menutree {} {
             {fx\ filters
                 {allpass.2nd~ allpass.filt~ bitnormal~ comb.filt~ lop.bw~ hip.bw~ biquads~ bandpass~ bandstop~ crossover~ bpbank~ bicoeff2 brickwall~ eq~ highpass~ highshelf~ lop2~ lowpass~ lowshelf~ mov.avg~ resonbank~ resonbank2~ resonant~ resonator~ resonator2~ svfilter~}}
             {sampling\ playing\ granulation
-                {player~ gran.player~ stretch.shift~ pvoc.player~ pvoc.live~ batch.rec~ bach.write~ rec.file~ play.file~ tabplayer~ tabwriter~ sfload sample~}}
+                {player~ gran.player~ stretch.shift~ pvoc.player~ pvoc.live~ batch.rec~ bach.write~ rec.file~ play.file~ tabplayer~ tabwriter~ sfload sfinfo sample~}}
             {synthesis:\ synthesizers
                 {pm2~ pm4~ pm6~ sfont~ sfz~ plaits~ synth~}}
             {synthesis:\ granular
@@ -99,7 +99,7 @@ proc category_else_menu::load_menutree {} {
             {control:\ control\ rate\ lfo
                 {lfo phasor pimp impulse pulse}}
             {control:\ triggers
-                {above above~ bangdiv chance chance~ dust~ dust2~ gatehold~ gate2imp~ pimp~ pimpmul~ pulsecount~ pulsediv~ sh~ schmitt schmitt~ status status~ trig.delay~ trig.delay2~ toggleff~ timed.gate timed.gate~ match~ trig2bang trig2bang~ trighold~}}
+                {above above~ bangdiv chance chance~ dust~ dust2~ gatehold gatehold~ gaterelease gaterelease~ gate2imp~ pimp~ pimpmul~ pulsecount~ pulsediv~ sh~ schmitt schmitt~ status status~ trig.delay~ trig.delay2~ toggleff~ timed.gate timed.gate~ match~ trig2bang trig2bang~ trighold~}}
             {control:\ triggers\ clock
                 {clock metronome metronome~ polymetro polymetro~ speed tempo tempo~}}
             {analysis
