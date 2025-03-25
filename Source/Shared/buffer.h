7e84f4c9e01c74e083bba2d5e6b481f64f92fc97
José Henrique Padovani <zepadovani@gmail.com>
2025-03-25 16:19:47 +0100

    buffer.* / buffer_init allows <array>-<ch> by setting 5th arg

--- Source/Shared/buffer.h
+++ Source/Shared/buffer.h
@@ -33,6 +33,8 @@ typedef struct _buffer{
     int         c_disabled;
     int         c_single;    // flag: 0-regular mode, 1-load this particular channel (1-idx)
                              // should be used with c_numchans == 1
+    // Now buffer will have a bufname mode: 0 - <ch>-<bufname> [default/legacy], 1 - <bufname>-<ch> 
+    int         c_bufnamemode; 
 }t_buffer;
 
 double interp_lin(double frac, double b, double c);
@@ -71,7 +73,8 @@ void buffer_setminsize(t_buffer *c, int i);
 void buffer_enable(t_buffer *c, t_floatarg f);
 
 // single channel mode used for poke~/peek~
-void *buffer_init(t_class *owner, t_symbol *bufname, int numchans, int singlemode);
+// Now buffer will have a bufname mode: 0 - <ch>-<bufname> [default/legacy], 1 - <bufname>-<ch>
+void *buffer_init(t_class *owner, t_symbol *bufname, int numchans, int singlemode, int bufnamemode); 
 void buffer_free(t_buffer *c);
 void buffer_checkdsp(t_buffer *c);
 void buffer_getchannel(t_buffer *c, int chan_num, int complain);
