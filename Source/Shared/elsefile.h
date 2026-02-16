5ee53dc23f65cd35fb2fc6a4a7842673f875012a
porres <porres@gmail.com>
2026-02-15 22:19:14 -0300

    1st attempt to expand elsefile functionality

--- Source/Shared/elsefile.h
+++ Source/Shared/elsefile.h
@@ -9,41 +9,49 @@
 EXTERN_STRUCT _osdir;
 #define t_osdir  struct _osdir
 
-#define OSDIR_elsefileMODE  1
+#define OSDIR_FILEMODE  1
 #define OSDIR_DIRMODE   2
 
-int elsefile_ospath_length(char *path, char *cwd);
-char *elsefile_ospath_absolute(char *path, char *cwd, char *result);
+int ospath_length(char *path, char *cwd);
+char *ospath_absolute(char *path, char *cwd, char *result);
 
-t_osdir *elsefile_osdir_open(char *dirname);
-void elsefile_osdir_setmode(t_osdir *dp, int flags);
-void elsefile_osdir_close(t_osdir *dp);
-void elsefile_osdir_rewind(t_osdir *dp);
-char *elsefile_osdir_next(t_osdir *dp);
-int elsefile_osdir_isfile(t_osdir *dp);
-int elsefile_osdir_isdir(t_osdir *dp);
+FILE *fileread_open(char *filename, t_canvas *cv, int textmode);
+FILE *filewrite_open(char *filename, t_canvas *cv, int textmode);
+
+t_osdir *osdir_open(char *dirname);
+void osdir_setmode(t_osdir *dp, int flags);
+void osdir_close(t_osdir *dp);
+void osdir_rewind(t_osdir *dp);
+char *osdir_next(t_osdir *dp);
+int osdir_isfile(t_osdir *dp);
+int osdir_isdir(t_osdir *dp);
 
 #endif
 
-#ifndef __ELSEFILE_H__
-#define __ELSEFILE_H__
+#ifndef __FILE_H__
+#define __FILE_H__
 
 EXTERN_STRUCT _elsefile;
 #define t_elsefile  struct _elsefile
 
 typedef void (*t_elsefilefn)(t_pd *, t_symbol *, int, t_atom *);
+typedef void (*t_embedfn)(t_pd *, t_binbuf *, t_symbol *);
 
+void editor_open(t_elsefile *f, char *title, char *owner);
+void editor_close(t_elsefile *f, int ask);
+void editor_append(t_elsefile *f, char *contents);
+void editor_setdirty(t_elsefile *f, int flag);
 void elsefile_panel_click_open(t_elsefile *f);
-void elsefile_panel_setopendir(t_elsefile *f, t_symbol *dir);
-t_symbol *elsefile_panel_getopendir(t_elsefile *f);
+void panel_setopendir(t_elsefile *f, t_symbol *dir);
+t_symbol *panel_getopendir(t_elsefile *f);
 void elsefile_panel_save(t_elsefile *f, t_symbol *inidir, t_symbol *inifile);
-void elsefile_panel_setsavedir(t_elsefile *f, t_symbol *dir);
-t_symbol *elsefile_panel_getsavedir(t_elsefile *f);
+void panel_setsavedir(t_elsefile *f, t_symbol *dir);
+t_symbol *panel_getsavedir(t_elsefile *f);
 int elsefile_ismapped(t_elsefile *f);
 int elsefile_isloading(t_elsefile *f);
 int elsefile_ispasting(t_elsefile *f);
 void elsefile_free(t_elsefile *f);
-t_elsefile *elsefile_new(t_pd *master, t_elsefilefn readfn, t_elsefilefn writefn);
-void elsefile_setup(void);
+t_elsefile *elsefile_new(t_pd *master, t_embedfn embedfn, t_elsefilefn readfn, t_elsefilefn writefn, t_elsefilefn updatefn);
+void elsefile_setup(t_class *c, int embeddable);
 
 #endif
