b351ff77c4ad90c1326c531f7cea7f830f226165
porres <porres@gmail.com>
2026-05-21 17:28:19 -0300

    ikeda: single oputput and builtin hip~

--- Source/Audio/ikeda~.c
+++ Source/Audio/ikeda~.c
@@ -10,10 +10,12 @@
 typedef struct _ikeda{
     t_object    x_obj;
     double     *x_phase;
+    double     *x_xnm1;
     double     *x_ynm1;
-    double     *x_ynm2;
-    double      x_init_ynm1;
-    double      x_init_ynm2;
+    double     *x_y1;
+    double     *x_y2;
+    double      x_init_y1;
+    double      x_init_y2;
     double      x_u;
     int         x_nchans;
     int         x_ch;
@@ -22,7 +24,6 @@ typedef struct _ikeda{
     float      *x_freq_list;
     t_int       x_list_size;
     t_symbol   *x_ignore;
-    t_outlet   *x_outlet;
     double      x_sr_rec;
     t_glist    *x_glist;
 }t_ikeda;
@@ -42,10 +43,10 @@ static void ikeda_coeffs(t_ikeda *x, t_symbol *s, int ac, t_atom *av){
                     x->x_u = curf;
                     break;
                 case 2:
-                    x->x_init_ynm1 = curf;
+                    x->x_init_y1 = curf;
                     break;
                 case 3:
-                    x->x_init_ynm2 = curf;
+                    x->x_init_y2 = curf;
                     break;
             };
             argnum++;
@@ -55,8 +56,9 @@ static void ikeda_coeffs(t_ikeda *x, t_symbol *s, int ac, t_atom *av){
     };
     for(int i = 0; i < x->x_nchans; i++){
         x->x_phase[i] = x->x_freq_list[i] >= 0;
-        x->x_ynm1[i] = x->x_init_ynm1;
-        x->x_ynm2[i] = x->x_init_ynm2;
+        x->x_xnm1[i] = x->x_ynm1[i] = 0;
+        x->x_y1[i] = x->x_init_y1;
+        x->x_y2[i] = x->x_init_y2;
     }
 }
 
@@ -64,12 +66,13 @@ static t_int *ikeda_perform(t_int *w){
     t_ikeda *x = (t_ikeda *)(w[1]);
     int chs = (t_int)(w[2]); // number of channels in main input signal (density)
     t_float *freq = (t_float *)(w[3]);
-    t_float *out1 = (t_float *)(w[4]);
-    t_float *out2 = (t_float *)(w[5]);
+    t_float *out = (t_float *)(w[4]);
     double u = x->x_u;
     double *phase = x->x_phase;
+    double *xnm1 = x->x_xnm1;
     double *ynm1 = x->x_ynm1;
-    double *ynm2 = x->x_ynm2;
+    double *y1 = x->x_y1;
+    double *y2 = x->x_y2;
     for(int j = 0; j < x->x_nchans; j++){
         for(int i = 0, n = x->x_n; i < n; i++){
             t_float hz;
@@ -95,19 +98,26 @@ static t_int *ikeda_perform(t_int *w){
                     phase[j] += 1.;
             }
             if(trig){ // update
-                float t = 0.4 - 6/(1 + pow(ynm1[j], 2) + pow(ynm2[j], 2));
-                ynm1[j] = 1 + u * (ynm1[j] * cos(t) - ynm2[j] * sin(t));
-                ynm2[j] = u * (ynm1[j] * sin(t) + ynm2[j] * cos(t));
+                float t = 0.4 - 6/(1 + pow(y1[j], 2) + pow(y2[j], 2));
+                y1[j] = 1 + u * (y1[j] * cos(t) - y2[j] * sin(t));
+                y2[j] = u * (y1[j] * sin(t) + y2[j] * cos(t));
             }
-            out1[j*x->x_n + i] = ynm1[j];
-            out2[j*x->x_n + i] = ynm2[j];
+            double output = (y1[j] - xnm1[j]) + 0.999345*ynm1[j];
+            
+            out[j*x->x_n + i] = output;
+            
+            ynm1[j] = output;
+            xnm1[j] = y1[j];
+            
             phase[j] += step;
         }
     }
     x->x_phase = phase;
+    x->x_xnm1 = xnm1;
     x->x_ynm1 = ynm1;
-    x->x_ynm2 = ynm2;
-    return(w+6);
+    x->x_y1 = y1;
+    x->x_y2 = y2;
+    return(w+5);
 }
 
 static void ikeda_dsp(t_ikeda *x, t_signal **sp){
@@ -120,21 +130,25 @@ static void ikeda_dsp(t_ikeda *x, t_signal **sp){
     if(x->x_nchans != chs){
         x->x_phase = (double *)resizebytes(x->x_phase,
             x->x_nchans * sizeof(double), chs * sizeof(double));
+        x->x_xnm1 = (double *)resizebytes(x->x_xnm1,
+            x->x_nchans * sizeof(double), chs * sizeof(double));
         x->x_ynm1 = (double *)resizebytes(x->x_ynm1,
             x->x_nchans * sizeof(double), chs * sizeof(double));
-        x->x_ynm2 = (double *)resizebytes(x->x_ynm2,
+        x->x_y1 = (double *)resizebytes(x->x_y1,
+            x->x_nchans * sizeof(double), chs * sizeof(double));
+        x->x_y2 = (double *)resizebytes(x->x_y2,
             x->x_nchans * sizeof(double), chs * sizeof(double));
         x->x_nchans = chs;
         for(int i = 0; i < x->x_nchans; i++){
             if(x->x_freq_list[i] >= 0)
                 x->x_phase[i] = 1;
-            x->x_ynm1[i] = x->x_init_ynm1;
-            x->x_ynm2[i] = x->x_init_ynm2;
+            x->x_y1[i] = 0.;
+            x->x_y1[i] = x->x_init_y1;
+            x->x_y2[i] = x->x_init_y2;
         }
     }
     signal_setmultiout(&sp[1], x->x_nchans);
-    signal_setmultiout(&sp[2], x->x_nchans);
-    dsp_add(ikeda_perform, 5, x, nchans, sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec);
+    dsp_add(ikeda_perform, 4, x, nchans, sp[0]->s_vec, sp[1]->s_vec);
 }
 
 static void ikeda_list(t_ikeda *x, t_symbol *s, int ac, t_atom * av){
@@ -164,10 +178,11 @@ static void ikeda_set(t_ikeda *x, t_symbol *s, int ac, t_atom *av){
 }
 
 static void *ikeda_free(t_ikeda *x){
-    outlet_free(x->x_outlet);
     freebytes(x->x_phase, x->x_nchans * sizeof(*x->x_phase));
+    freebytes(x->x_xnm1, x->x_nchans * sizeof(*x->x_xnm1));
     freebytes(x->x_ynm1, x->x_nchans * sizeof(*x->x_ynm1));
-    freebytes(x->x_ynm2, x->x_nchans * sizeof(*x->x_ynm2));
+    freebytes(x->x_y1, x->x_nchans * sizeof(*x->x_y1));
+    freebytes(x->x_y2, x->x_nchans * sizeof(*x->x_y2));
     return(void *)x;
 }
 
@@ -180,7 +195,7 @@ static void *ikeda_new(t_symbol *s, int ac, t_atom *av){
     x->x_freq_list = (float*)malloc(MAXLEN * sizeof(float));
     x->x_freq_list[0] = sys_getsr() * 0.5;
     double u = 0.75;
-    x->x_init_ynm1 = 0, x->x_init_ynm2 = 0; // default parameters
+    x->x_init_y1 = 0, x->x_init_y2 = 0; // default parameters
     while(ac && av->a_type == A_SYMBOL){
         if(atom_getsymbol(av) == gensym("-mc")){
             ac--, av++;
@@ -203,9 +218,9 @@ static void *ikeda_new(t_symbol *s, int ac, t_atom *av){
             u = av->a_w.w_float;
             ac--; av++;
             if(ac && av->a_type == A_FLOAT) {
-                x->x_init_ynm1 = av->a_w.w_float;
+                x->x_init_y1 = av->a_w.w_float;
                 if(ac && av->a_type == A_FLOAT) {
-                    x->x_init_ynm2 = av->a_w.w_float;
+                    x->x_init_y2 = av->a_w.w_float;
                 }
             }
         }
@@ -213,18 +228,19 @@ static void *ikeda_new(t_symbol *s, int ac, t_atom *av){
     x->x_u = u;
     
     x->x_phase = (double *)getbytes(sizeof(*x->x_phase) * x->x_list_size);
+    x->x_xnm1 = (double *)getbytes(sizeof(*x->x_xnm1) * x->x_list_size);
     x->x_ynm1 = (double *)getbytes(sizeof(*x->x_ynm1) * x->x_list_size);
-    x->x_ynm2 = (double *)getbytes(sizeof(*x->x_ynm2) * x->x_list_size);
-    x->x_phase[0] = x->x_ynm1[0] = 0;
+    x->x_y1 = (double *)getbytes(sizeof(*x->x_y1) * x->x_list_size);
+    x->x_y2 = (double *)getbytes(sizeof(*x->x_y2) * x->x_list_size);
+    x->x_phase[0] = x->x_y1[0]  = x->x_y2[0] = x->x_xnm1[0] = x->x_ynm1[0] = 0;
     
     for(int i = 0; i < x->x_list_size; i++){
         if(x->x_freq_list[i] >= 0)
             x->x_phase[i] = 1;
-        x->x_ynm1[i] = x->x_init_ynm1;
-        x->x_ynm2[i] = x->x_init_ynm2;
+        x->x_y1[i] = x->x_init_y1;
+        x->x_y2[i] = x->x_init_y2;
     }
-    x->x_outlet = outlet_new(&x->x_obj, &s_signal);
-    x->x_outlet = outlet_new(&x->x_obj, &s_signal);
+    outlet_new(&x->x_obj, &s_signal);
     x->x_glist = canvas_getcurrent();
     return(x);
 errstate:
