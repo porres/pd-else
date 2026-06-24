e8c7baff11ed02567ecd3a8f4465209d90f68a4d
porres <porres@gmail.com>
2026-06-24 15:50:25 -0300

    Update mov.avg/rms~ and add MC
    rms~

--- Source/Audio/mov.avg~.c
+++ Source/Audio/mov.avg~.c
@@ -1,118 +1,135 @@
+// porres
+
 #include <m_pd.h>
 #include <stdlib.h>
-#include <math.h>
 
 #define MAVG_MAXBUF         192000000   // max buffer size - undocumented
-#define MAVG_DEF_BUFSIZE          100   // default size
+#define MAVG_DEF_BUFSIZE    100         // default size
 
 typedef struct _mavg{
     t_object        x_obj;
-    t_inlet        *x_inlet_n;                  // inlet for n samples
-    unsigned int    x_last_n;                   // last # of samples for moving average
-    unsigned int    x_count;                    // for 1st round of accumulation
-    double          x_accum;                    // accumulation
+    unsigned int    x_size;
     double         *x_buf;                      // buffer pointer
-    double          x_stack[MAVG_DEF_BUFSIZE];  // buffer
-    int             x_alloc;                    // if x_buf is allocated or stack ?????
-    unsigned int    x_size;                     // allocated size for x_buf
-    unsigned int    x_bufrd;                    // buffer readhead
+    unsigned int   *x_count;                    // for 1st round of accumulation
+    double         *x_accum;                    // accumulation
+    unsigned int   *x_bufrd;                    // buffer readhead
+    unsigned int    x_n_samps;                  // number of samples to average
+    unsigned int    x_n;
+    unsigned int    x_nchans;
 }t_mavg;
 
 static t_class *mavg_class;
 
 static void mavg_clear(t_mavg * x){ // clear buffer and reset things to 0
-    x->x_count = x->x_accum = x->x_bufrd = 0;;
+    unsigned int i;
+    for(i = 0; i < x->x_nchans; i++){
+        x->x_count[i] = x->x_bufrd[i] = 0;
+        x->x_accum[i] = 0.0;
+    }
     for(unsigned int i = 0; i < x->x_size; i++)
         x->x_buf[i] = 0.;
 };
 
-static void mavg_size(t_mavg *x, t_float f){ // deals with allocation issues if needed
+// set size and deal with allocation if needed
+static void mavg_size(t_mavg *x, t_float f){
     unsigned int cursz = x->x_size;                     // current size
     unsigned int newsz = f < 1 ? 1 : (unsigned int)f;   // new requested size
     if(newsz > MAVG_MAXBUF)
         newsz = MAVG_MAXBUF;
-    if(!x->x_alloc && newsz > MAVG_DEF_BUFSIZE){  // allocate
-        x->x_buf = (double *)malloc(sizeof(double)*newsz);
-        x->x_alloc = 1;
-    }
-    else if(x->x_alloc && newsz > cursz) // reallocate
-        x->x_buf = (double *)realloc(x->x_buf, sizeof(double)*newsz);
-    else if(x->x_alloc && newsz < MAVG_DEF_BUFSIZE){ // reset
-        free(x->x_buf);
-        x->x_size = MAVG_DEF_BUFSIZE;
-        x->x_buf = x->x_stack;
-        x->x_alloc = 0;
-    };
+    x->x_buf = (double *)realloc(x->x_buf, sizeof(double) * newsz * x->x_nchans);
     x->x_size = newsz;
     mavg_clear(x);
 }
 
+static void mavg_n(t_mavg *x, t_float f){
+    unsigned int n = f < 1 ? 1 : f > x->x_size ? x->x_size : (unsigned int)f;
+    if(n != x->x_n_samps){
+        x->x_n_samps = n;
+        mavg_clear(x);
+    }
+}
+
 static t_int *mavg_perform(t_int *w){
     t_mavg *x = (t_mavg *)(w[1]);
-    int nblock = (int)(w[2]);
-    t_float *in1 = (t_float *)(w[3]);
-    t_float *in2 = (t_float *)(w[4]);
-    t_float *out = (t_float *)(w[5]);
-    float last_n = x->x_last_n;
-    for(int i = 0; i < nblock; i++){
-        double result, input = (double)in1[i];
-        float in_samples = in2[i];
-        if(in_samples < 1)
-            in_samples = 1;
-        unsigned int n = in_samples;
-        if(n > x->x_size)
-            n = x->x_size;
-        if(n != last_n)
-            mavg_clear(x);
-        if(n > 1){ // do average
-            x->x_accum += input;                // accumulate
-            if(x->x_count < n)       // update count
-                x->x_count++;
-            else // subtract first sample out of bounds from the moving sum
-                x->x_accum -= x->x_buf[x->x_bufrd];
-            x->x_buf[x->x_bufrd++] = input; // store input, increment bufrd
-            if(x->x_bufrd >= n) // loop bufrd
-                x->x_bufrd = 0;
-            result = x->x_accum/(double)n; // get average
+    t_float *in = (t_float *)(w[2]);
+    t_float *out = (t_float *)(w[3]);
+    unsigned int nsamps = x->x_n_samps;
+    unsigned int n = x->x_n;
+    for(int j = 0; j < x->x_nchans; j++){
+        for(int i = 0; i < n; i++){
+            double input = (double)in[i+j*n], result;
+            if(nsamps == 1)  // passes through
+                result = input;
+            else{
+                if(x->x_count[j] < nsamps)
+                    x->x_count[j]++;
+                else // subtract oldest sample when full
+                    x->x_accum[j] -= x->x_buf[j * x->x_size + x->x_bufrd[j]];
+                x->x_accum[j] += input; // add new input
+                x->x_buf[j * x->x_size + x->x_bufrd[j]++] = input;
+                if(x->x_bufrd[j] >= nsamps){  // wrap readhead and correct drift each cycle
+                    x->x_bufrd[j] = 0;
+                    double resum = 0.0;
+                    for(unsigned int idx = 0; idx < nsamps; idx++)
+                        resum += x->x_buf[j * x->x_size + idx];
+                    x->x_accum[j] = resum;
+                }
+                result = x->x_accum[j] / (double)x->x_count[j];
+            }
+            out[i+j*n] = (t_float)result;
         }
-        else // npoints = 1, just pass through
-            result = input;
-        out[i] = result;
-        last_n = n;
-    };
-    x->x_last_n = last_n;
-    return(w + 6);
+    }
+    return(w+4);
 }
 
 static void mavg_dsp(t_mavg *x, t_signal **sp){
-    dsp_add(mavg_perform, 5, x, sp[0]->s_n, sp[0]->s_vec,
-            sp[1]->s_vec, sp[2]->s_vec);
+    int n = sp[0]->s_n;
+    int chs = sp[0]->s_nchans;
+    if(n != x->x_n || chs != x->x_nchans){
+        x->x_count = (unsigned int *)resizebytes(x->x_count,
+            x->x_nchans * sizeof(unsigned int), chs * sizeof(unsigned int));
+        x->x_bufrd = (unsigned int *)resizebytes(x->x_bufrd,
+            x->x_nchans * sizeof(unsigned int), chs * sizeof(unsigned int));
+        x->x_accum = (double *)resizebytes(x->x_accum,
+            x->x_nchans * sizeof(double), chs * sizeof(double));
+        x->x_n = n;
+        x->x_nchans = chs;
+        mavg_size(x, (float)x->x_size);
+        mavg_clear(x);
+    }
+    signal_setmultiout(&sp[1], x->x_nchans);
+    dsp_add(mavg_perform, 3, x, sp[0]->s_vec, sp[1]->s_vec);
 }
 
 static void mavg_free(t_mavg *x){
-    if(x->x_alloc)
-        free(x->x_buf);
+    freebytes(x->x_count, x->x_nchans * sizeof(*x->x_count));
+    freebytes(x->x_bufrd, x->x_nchans * sizeof(*x->x_bufrd));
+    freebytes(x->x_accum, x->x_nchans * sizeof(*x->x_accum));
+    free(x->x_buf);
 }
 
-static void *mavg_new(t_symbol *s, int argc, t_atom * argv){
+static void *mavg_new(t_symbol *s, int ac, t_atom * av){
+    (void)s;
     t_mavg *x = (t_mavg *)pd_new(mavg_class);
-    t_symbol *dummy = s;
-    dummy = NULL;
 // default buf / size / n
-    x->x_buf = x->x_stack;
     x->x_size = MAVG_DEF_BUFSIZE;
-    float n_arg = 1;
-    x->x_alloc = 0.;
+    x->x_nchans = 1;
+    x->x_n = 64;
+    x->x_n_samps = 1;
+    x->x_count = (unsigned int*)getbytes(sizeof(*x->x_count));
+    x->x_bufrd = (unsigned int*)getbytes(sizeof(*x->x_bufrd));
+    x->x_accum = (double*)getbytes(sizeof(*x->x_accum));
 /////////////////////////////////////////////////////////////////////////////////
     int argn = 0;
-    while(argc > 0){
-        if(argv->a_type == A_SYMBOL){
-            t_symbol * cursym = atom_getsymbolarg(0, argc, argv);
+    int flag = 0;
+    while(ac > 0){
+        if(av->a_type == A_SYMBOL){
+            t_symbol * cursym = atom_getsymbol(av);
             if(cursym == gensym("-size") && !argn){
-                if(argc >= 2 && (argv+1)->a_type == A_FLOAT){
-                    t_float curfloat = atom_getfloatarg(1, argc, argv);
-                    x->x_size = (int)curfloat;
-                    argc-=2, argv+=2;
+                if(ac >= 2 && (av+1)->a_type == A_FLOAT){
+                    x->x_size = atom_getintarg(1, ac, av);
+                    ac-=2, av+=2;
+                    flag = 1;
                 }
                 else
                     goto errstate;
@@ -120,32 +137,35 @@ static void *mavg_new(t_symbol *s, int argc, t_atom * argv){
             else
                 goto errstate;
         }
-        else if(argv->a_type == A_FLOAT){
+        else if(av->a_type == A_FLOAT){
             argn = 1;
-            n_arg = (int)atom_getfloatarg(0, argc, argv);
-            n_arg = (n_arg < 1 ? 1 : n_arg);
-            x->x_size = (unsigned int)n_arg;
-            argc--, argv++;
+            unsigned int nsamps = (unsigned int)atom_getint(av);
+            if(nsamps < 1)
+                nsamps = 1;
+            x->x_n_samps = nsamps;
+            if(!flag)
+                x->x_size = nsamps;
+            ac--, av++;
         }
         else
             goto errstate;
     };
 /////////////////////////////////////////////////////////////////////////////////
-    mavg_size(x, (float)x->x_size); // set size and allocate x_buf if necessary
-    x->x_inlet_n = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
-    pd_float((t_pd *)x->x_inlet_n, n_arg);
+    x->x_buf = NULL;
+    mavg_size(x, (float)x->x_size);
     outlet_new((t_object *)x, &s_signal);
-    return (x);
+    return(x);
 errstate:
     pd_error(x, "[mov.avg~]: improper args");
-    return NULL;
+    return(NULL);
 }
 
 void setup_mov0x2eavg_tilde(void){
     mavg_class = class_new(gensym("mov.avg~"), (t_newmethod)mavg_new,
-            (t_method)mavg_free, sizeof(t_mavg), 0, A_GIMME, 0);
+            (t_method)mavg_free, sizeof(t_mavg), CLASS_MULTICHANNEL, A_GIMME, 0);
     class_addmethod(mavg_class, (t_method) mavg_dsp, gensym("dsp"), A_CANT, 0);
     class_addmethod(mavg_class, nullfn, gensym("signal"), 0);
     class_addmethod(mavg_class, (t_method)mavg_clear, gensym("clear"), 0);
     class_addmethod(mavg_class, (t_method)mavg_size, gensym("size"), A_DEFFLOAT, 0);
+    class_addmethod(mavg_class, (t_method)mavg_n, gensym("n"), A_DEFFLOAT, 0);
 }
--- Source/Audio/mov.rms~.c
+++ Source/Audio/mov.rms~.c
@@ -1,168 +1,194 @@
+// porres
+
 #include <m_pd.h>
 #include <stdlib.h>
 #include <math.h>
 
 #define MRMS_MAXBUF         192000000   // max buffer size - undocumented
-#define MRMS_DEF_BUFSIZE        1024    // default size
-#define LOGTEN 2.302585092994
+#define MRMS_DEF_BUFSIZE    1024        // default size
+#define LOGTEN              2.302585092994
 
 typedef struct _mrms{
     t_object        x_obj;
-    t_inlet        *x_inlet_n;                  // inlet for n samples
-    unsigned int    x_last_n;                   // last # of samples for moving average
-    unsigned int    x_count;                    // for 1st round of accumulation
-    double          x_accum;                    // accumulation
-    double         *x_buf;                      // buffer pointer
-    double          x_stack[MRMS_DEF_BUFSIZE];  // buffer
-    int             x_alloc;                    // if x_buf is allocated or stack ?????
-    unsigned int    x_size;                     // allocated size for x_buf
-    unsigned int    x_bufrd;                    // buffer readhead
+    unsigned int    x_size;
+    double         *x_buf;          // buffer pointer
+    unsigned int   *x_count;        // for 1st round of accumulation
+    double         *x_accum;        // accumulation
+    unsigned int   *x_bufrd;        // buffer readhead
+    unsigned int    x_n_samps;      // number of samples to average
+    unsigned int    x_n;
+    unsigned int    x_nchans;
     int             x_db;
 }t_mrms;
 
 static t_class *mrms_class;
 
 static void mrms_clear(t_mrms * x){ // clear buffer and reset things to 0
-    x->x_count = x->x_accum = x->x_bufrd = 0;;
+    unsigned int i;
+    for(i = 0; i < x->x_nchans; i++){
+        x->x_count[i] = x->x_bufrd[i] = 0;
+        x->x_accum[i] = 0.0;
+    }
     for(unsigned int i = 0; i < x->x_size; i++)
         x->x_buf[i] = 0.;
 };
 
-static void mrms_size(t_mrms *x, t_float f){ // deals with allocation issues if needed
+// set size and deal with allocation if needed
+static void mrms_size(t_mrms *x, t_float f){
     unsigned int cursz = x->x_size;                     // current size
     unsigned int newsz = f < 1 ? 1 : (unsigned int)f;   // new requested size
     if(newsz > MRMS_MAXBUF)
         newsz = MRMS_MAXBUF;
-    if(!x->x_alloc && newsz > MRMS_DEF_BUFSIZE){  // allocate
-        x->x_buf = (double *)malloc(sizeof(double)*newsz);
-        x->x_alloc = 1;
-    }
-    else if(x->x_alloc && newsz > cursz) // reallocate
-        x->x_buf = (double *)realloc(x->x_buf, sizeof(double)*newsz);
-    else if(x->x_alloc && newsz < MRMS_DEF_BUFSIZE){ // reset
-        free(x->x_buf);
-        x->x_size = MRMS_DEF_BUFSIZE;
-        x->x_buf = x->x_stack;
-        x->x_alloc = 0;
-    };
+    x->x_buf = (double *)realloc(x->x_buf, sizeof(double) * newsz * x->x_nchans);
     x->x_size = newsz;
     mrms_clear(x);
 }
 
-static void mrms_linear(t_mrms *x, t_floatarg f){
+static void mrms_lin(t_mrms *x, t_float f){
     x->x_db = f == 0;
+}
+
+static void mrms_n(t_mrms *x, t_float f){
+    unsigned int n = f < 1 ? 1 : f > x->x_size ? x->x_size : (unsigned int)f;
+    if(n != x->x_n_samps){
+        x->x_n_samps = n;
+        mrms_clear(x);
+    }
 }
 
 static t_int *mrms_perform(t_int *w){
     t_mrms *x = (t_mrms *)(w[1]);
-    int nblock = (int)(w[2]);
-    t_float *in1 = (t_float *)(w[3]);
-    t_float *in2 = (t_float *)(w[4]);
-    t_float *out = (t_float *)(w[5]);
-    float last_n = x->x_last_n;
-    for(int i = 0; i < nblock; i++){
-        double result, input = (double)in1[i];
-        double squared = input * input;
-        float in_samples = in2[i];
-        if(in_samples < 1)
-            in_samples = 1;
-        unsigned int n = in_samples;
-        if(n > x->x_size)
-            n = x->x_size;
-        if(n != last_n)
-            mrms_clear(x);
-        x->x_accum += squared;   // accumulate
-        if(x->x_count < n)       // update count
-            x->x_count++;
-        else // subtract first sample out of bounds from the moving sum
-            x->x_accum -= x->x_buf[x->x_bufrd];
-        x->x_buf[x->x_bufrd++] = squared; // store input, increment bufrd
-        if(x->x_bufrd >= n) // loop bufrd
-            x->x_bufrd = 0;
-        result = x->x_accum/(double)n; // get average
-        if(result <= 0)
-            result = 0;
-        else
-            result = sqrt(result);  // get rms
-        if(x->x_db){ // convert to db
-            result = 20. * log(result)/LOGTEN;
-            if(result < -999)
-                result = -999;
+    t_float *in = (t_float *)(w[2]);
+    t_float *out = (t_float *)(w[3]);
+    unsigned int nsamps = x->x_n_samps;
+    unsigned int n = x->x_n;
+    for(int j = 0; j < x->x_nchans; j++){
+        for(int i = 0; i < n; i++){
+            double input = (double)in[i+j*n], result;
+            double squared = input * input;
+            if(nsamps == 1)  // passes through
+                result = input;
+            else{
+                if(x->x_count[j] < nsamps)
+                    x->x_count[j]++;
+                else // subtract oldest sample when full
+                    x->x_accum[j] -= x->x_buf[j * x->x_size + x->x_bufrd[j]];
+                x->x_accum[j] += squared; // add squared value
+                x->x_buf[j * x->x_size + x->x_bufrd[j]++] = squared;
+                if(x->x_bufrd[j] >= nsamps){  // wrap readhead and correct drift each cycle
+                    x->x_bufrd[j] = 0;
+                    double resum = 0.0;
+                    for(unsigned int idx = 0; idx < nsamps; idx++)
+                        resum += x->x_buf[j * x->x_size + idx];
+                    x->x_accum[j] = resum;
+                }
+                result = x->x_accum[j] / (double)x->x_count[j];
+                if(result < 0)
+                    result = 0;
+                else
+                    result = sqrt(result);  // get rms
+                if(x->x_db){ // convert to db
+                    result = 20. * log(result)/LOGTEN;
+                    if(result < -999)
+                        result = -999;
+                }
+            }
+            out[i+j*n] = (t_float)result;
         }
-        out[i] = result;
-        last_n = n;
-    };
-    x->x_last_n = last_n;
-    return(w + 6);
+    }
+    return(w+4);
 }
 
 static void mrms_dsp(t_mrms *x, t_signal **sp){
-    dsp_add(mrms_perform, 5, x, sp[0]->s_n, sp[0]->s_vec,
-            sp[1]->s_vec, sp[2]->s_vec);
+    int n = sp[0]->s_n;
+    int chs = sp[0]->s_nchans;
+    if(n != x->x_n || chs != x->x_nchans){
+        x->x_count = (unsigned int *)resizebytes(x->x_count,
+            x->x_nchans * sizeof(unsigned int), chs * sizeof(unsigned int));
+        x->x_bufrd = (unsigned int *)resizebytes(x->x_bufrd,
+            x->x_nchans * sizeof(unsigned int), chs * sizeof(unsigned int));
+        x->x_accum = (double *)resizebytes(x->x_accum,
+            x->x_nchans * sizeof(double), chs * sizeof(double));
+        x->x_n = n;
+        x->x_nchans = chs;
+        mrms_size(x, (float)x->x_size);
+        mrms_clear(x);
+    }
+    signal_setmultiout(&sp[1], x->x_nchans);
+    dsp_add(mrms_perform, 3, x, sp[0]->s_vec, sp[1]->s_vec);
 }
 
 static void mrms_free(t_mrms *x){
-    if(x->x_alloc)
-        free(x->x_buf);
+    freebytes(x->x_count, x->x_nchans * sizeof(*x->x_count));
+    freebytes(x->x_bufrd, x->x_nchans * sizeof(*x->x_bufrd));
+    freebytes(x->x_accum, x->x_nchans * sizeof(*x->x_accum));
+    free(x->x_buf);
 }
 
-static void *mrms_new(t_symbol *s, int argc, t_atom * argv){
-    s = NULL;
+static void *mrms_new(t_symbol *s, int ac, t_atom * av){
+    (void)s;
     t_mrms *x = (t_mrms *)pd_new(mrms_class);
 // default buf / size / n
-    x->x_buf = x->x_stack;
     x->x_size = MRMS_DEF_BUFSIZE;
-    float n_arg = 1;
-    x->x_alloc = 0;
     x->x_db = 1;
+    x->x_nchans = 1;
+    x->x_n = 64;
+    x->x_n_samps = 1;
+    x->x_count = (unsigned int*)getbytes(sizeof(*x->x_count));
+    x->x_bufrd = (unsigned int*)getbytes(sizeof(*x->x_bufrd));
+    x->x_accum = (double*)getbytes(sizeof(*x->x_accum));
 /////////////////////////////////////////////////////////////////////////////////
     int argn = 0;
-    while(argc > 0){
-        if(argv->a_type == A_SYMBOL){
-            t_symbol * cursym = atom_getsymbolarg(0, argc, argv);
+    int flag = 0;
+    while(ac > 0){
+        if(av->a_type == A_SYMBOL){
+            t_symbol * cursym = atom_getsymbol(av);
             if(cursym == gensym("-size") && !argn){
-                if(argc >= 2 && (argv+1)->a_type == A_FLOAT){
-                    t_float curfloat = atom_getfloatarg(1, argc, argv);
-                    x->x_size = (int)curfloat;
-                    argc-=2, argv+=2;
+                if(ac >= 2 && (av+1)->a_type == A_FLOAT){
+                    x->x_size = atom_getintarg(1, ac, av);
+                    ac-=2, av+=2;
+                    flag = 1;
                 }
                 else
                     goto errstate;
             }
             else if(cursym == gensym("-lin") && !argn){
                 x->x_db = 0;
-                argc--, argv++;
+                ac--, av++;
             }
             else
                 goto errstate;
         }
-        else if(argv->a_type == A_FLOAT){
+        else if(av->a_type == A_FLOAT){
             argn = 1;
-            n_arg = (int)atom_getfloatarg(0, argc, argv);
-            n_arg = (n_arg < 1 ? 1 : n_arg);
-            x->x_size = (unsigned int)n_arg;
-            argc--, argv++;
+            unsigned int nsamps = (unsigned int)atom_getint(av);
+            if(nsamps < 1)
+                nsamps = 1;
+            x->x_n_samps = nsamps;
+            if(!flag)
+                x->x_size = nsamps;
+            ac--, av++;
         }
         else
             goto errstate;
     };
 /////////////////////////////////////////////////////////////////////////////////
-    mrms_size(x, (float)x->x_size); // set size and allocate x_buf if necessary
-    x->x_inlet_n = inlet_new((t_object *)x, (t_pd *)x, &s_signal, &s_signal);
-    pd_float((t_pd *)x->x_inlet_n, n_arg);
+    x->x_buf = NULL;
+    mrms_size(x, (float)x->x_size);
     outlet_new((t_object *)x, &s_signal);
-    return (x);
+    return(x);
 errstate:
     pd_error(x, "[mov.rms~]: improper args");
-    return NULL;
+    return(NULL);
 }
 
 void setup_mov0x2erms_tilde(void){
     mrms_class = class_new(gensym("mov.rms~"), (t_newmethod)mrms_new,
-            (t_method)mrms_free, sizeof(t_mrms), 0, A_GIMME, 0);
+            (t_method)mrms_free, sizeof(t_mrms), CLASS_MULTICHANNEL, A_GIMME, 0);
     class_addmethod(mrms_class, (t_method) mrms_dsp, gensym("dsp"), A_CANT, 0);
     class_addmethod(mrms_class, nullfn, gensym("signal"), 0);
     class_addmethod(mrms_class, (t_method)mrms_clear, gensym("clear"), 0);
     class_addmethod(mrms_class, (t_method)mrms_size, gensym("size"), A_DEFFLOAT, 0);
-    class_addmethod(mrms_class, (t_method)mrms_linear, gensym("lin"), A_DEFFLOAT, 0);
+    class_addmethod(mrms_class, (t_method)mrms_lin, gensym("lin"), A_DEFFLOAT, 0);
+    class_addmethod(mrms_class, (t_method)mrms_n, gensym("n"), A_DEFFLOAT, 0);
 }
