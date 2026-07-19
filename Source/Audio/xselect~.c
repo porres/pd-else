452261a9db0e565fb977d3d36fe1e2f66602aa67
porres <porres@gmail.com>
2026-07-19 20:19:00 -0300

    mc to xselect~

--- Source/Audio/xselect~.c
+++ Source/Audio/xselect~.c
@@ -19,7 +19,6 @@ typedef struct _xselect{
     int         x_active_channel[INPUTLIMIT];
     int         x_counter[INPUTLIMIT];
     double      x_fade[INPUTLIMIT];
-    double      x_sum[INPUTLIMIT];
     t_float   **x_ins;
     t_float    *x_out;
     t_outlet   *x_out_status;
@@ -37,60 +36,31 @@ void xselect_float(t_xselect *x, t_floatarg ch){
   }
 }
 
-/*static t_int *xselect_perform(t_int *w){
-    int i;
-    t_xselect *x = (t_xselect *)(w[1]);
-    int n = (int)(w[2]);
-    for(i = 0; i < x->x_ninlets; i++)
-        x->x_in[i] = (t_float *)(w[3 + i]); // all inputs
-    float *out = (t_float *)(w[3 + x->x_ninlets]);
-    while(n--){
-        float sum = 0;
-        for(i = 0; i < x->x_ninlets; i++){
-            if(x->x_active_channel[i] && x->x_counter[i] < x->x_fade_in_samps)
-                x->x_counter[i]++;
-            else if(!x->x_active_channel[i] && x->x_counter[i] > 0){
-                x->x_counter[i]--;
-                if(x->x_counter[i] == 0){
-                    t_atom at[2];
-                    SETFLOAT(at, i + 1);
-                    SETFLOAT(at+1, 0);
-                    outlet_list(x->x_out_status, gensym("list"), 2, at);
-                }
-            }
-            x->x_fade[i] = x->x_counter[i] / x->x_fade_in_samps;
-            x->x_fade[i] = read_sintab(x->x_fade[i] * 0.25); // equal power
-            sum += *x->x_in[i]++ * x->x_fade[i];
-        }
-        *out++ = sum;
-    }
-    return(w + 4 + x->x_ninlets);
-}*/
-
 static t_int *xselect_perform(t_int *w){
     t_xselect *x = (t_xselect *)(w[1]);
-    for(int n = 0; n < x->x_n; n++){
-        float sum = 0;
-        for(int i = 0; i < x->x_ninlets; i++){
-            if(x->x_active_channel[i] && x->x_counter[i] < x->x_fade_in_samps)
-                x->x_counter[i]++;
-            else if(!x->x_active_channel[i] && x->x_counter[i] > 0){
-                x->x_counter[i]--;
-                if(x->x_counter[i] == 0){
+    int n = x->x_n, chs = x->x_nchs;
+    for(int i = 0; i < n; i++){
+        for(int in = 0; in < x->x_ninlets; in++){
+            if(x->x_active_channel[in] && x->x_counter[in] < x->x_fade_in_samps)
+                x->x_counter[in]++;
+            else if(!x->x_active_channel[in] && x->x_counter[in] > 0){
+                x->x_counter[in]--;
+                if(x->x_counter[in] == 0){
                     t_atom at[2];
-                    SETFLOAT(at, i + 1);
-                    SETFLOAT(at+1, 0);
+                    SETFLOAT(at, in + 1);
+                    SETFLOAT(at + 1, 0);
                     outlet_list(x->x_out_status, gensym("list"), 2, at);
                 }
             }
-            x->x_fade[i] = x->x_counter[i] / x->x_fade_in_samps;
-            x->x_fade[i] = read_sintab(x->x_fade[i] * 0.25); // equal power
-//            for(int ch = 0; i < x->x_nchs; ch++){
-                sum += x->x_ins[i][n] * x->x_fade[i];
-//                x->x_out[n] = sum;
-//            }
+            x->x_fade[in] = x->x_fade_in_samps ? x->x_counter[in] / x->x_fade_in_samps : 1;
+            x->x_fade[in] = read_sintab(x->x_fade[in] * 0.25);
         }
-        x->x_out[n] = sum;
+        for(int j = 0; j < chs; j++){
+            double sum = 0;
+            for(int in = 0; in < x->x_ninlets; in++)
+                sum += x->x_ins[in][j*n + i] * x->x_fade[in];
+            x->x_out[j*n + i] = sum;
+        }
     }
     return(w+2);
 }
@@ -99,20 +69,17 @@ static void xselect_dsp(t_xselect *x, t_signal **sp){
     x->x_n = sp[0]->s_n;
     x->x_sr_khz = sp[0]->s_sr * 0.001;
     t_signal **sigp = sp;
-//    x->x_nchs = sp[0]->s_nchans;
-//    signal_setmultiout(&sp[x->x_ninlets], x->x_nchs);
-    *(x->x_ins) = (*sigp++)->s_vec;   // 1st in
-    for(int i = 1; i < x->x_ninlets; i++){
-/*        int chs = sp[i]->s_n;
-        if(chs > 1 && chs != x->x_nchs){
+    x->x_nchs = sp[0]->s_nchans;
+    signal_setmultiout(&sp[x->x_ninlets], x->x_nchs);
+    for(int i = 0; i < x->x_ninlets; i++){
+        if(sp[i]->s_nchans != x->x_nchs){
             post("[xselect~]: multichannel inputs don't match");
-            dsp_add_zero(sp[x->x_ninlets]->s_vec, x->x_nchs*x->x_n);
-            break;
+            dsp_add_zero(sp[x->x_ninlets]->s_vec, x->x_nchs * x->x_n);
             return;
-        }*/
-        *(x->x_ins+i) = (*sigp++)->s_vec;   // other inlets
+        }
+        x->x_ins[i] = (*sigp++)->s_vec;
     }
-    x->x_out = (*sigp++)->s_vec;             // output
+    x->x_out = (*sigp)->s_vec;
     dsp_add(xselect_perform, 1, x);
 }
 
@@ -120,9 +87,11 @@ static void xselect_time(t_xselect *x, t_floatarg ms){
     double last_fade_in_samps = x->x_fade_in_samps;
     ms = ms < 0 ? 0 : ms;
     x->x_fade_in_samps = x->x_sr_khz * ms;
-    for(int i = 0; i < x->x_ninlets; i++)
-        if(x->x_counter[i]) // adjust counters
-            x->x_counter[i] = x->x_counter[i] / last_fade_in_samps * x->x_fade_in_samps;
+    for(int i = 0; i < x->x_ninlets; i++){
+        if(x->x_counter[i] && last_fade_in_samps)
+            x->x_counter[i] =
+                x->x_counter[i] / last_fade_in_samps * x->x_fade_in_samps;
+    }
 }
 
 void *xselect_free(t_xselect *x){
@@ -163,7 +132,7 @@ static void *xselect_new(t_symbol *s, int ac, t_atom *av){
     x->x_ins = getbytes(x->x_ninlets * sizeof(*x->x_ins));
     for(i = 0; i < x->x_ninlets - 1; i++)
         inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
-    outlet_new(&x->x_obj, gensym("signal"));
+    outlet_new(&x->x_obj, &s_signal);
     x->x_out_status = outlet_new(&x->x_obj, &s_list);
     ms = ms > 0 ? ms : 0;
     x->x_fade_in_samps = x->x_sr_khz * ms + 1;
@@ -179,7 +148,7 @@ static void *xselect_new(t_symbol *s, int ac, t_atom *av){
 
 void xselect_tilde_setup(void){
     xselect_class = class_new(gensym("xselect~"), (t_newmethod)xselect_new,
-        (t_method)xselect_free, sizeof(t_xselect), CLASS_DEFAULT, A_GIMME, 0);
+        (t_method)xselect_free, sizeof(t_xselect), CLASS_MULTICHANNEL, A_GIMME, 0);
     class_addfloat(xselect_class, (t_method)xselect_float);
     class_addmethod(xselect_class, nullfn, gensym("signal"), 0);
     class_addmethod(xselect_class, (t_method)xselect_dsp, gensym("dsp"), A_CANT, 0);
